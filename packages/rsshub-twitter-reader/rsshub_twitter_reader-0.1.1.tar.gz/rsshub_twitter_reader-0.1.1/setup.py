0beba9a3aab20fea56f63f9f747cbf8c8ffd7d36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-26 15:04:30 +0000

    Add package rsshub-twitter-reader/0.1.1/rsshub_twitter_reader-0.1.1.tar.gz

---
+++ packages/rsshub-twitter-reader/rsshub_twitter_reader-0.1.1.tar.gz/rsshub_twitter_reader-0.1.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='rsshub_twitter_reader',
+    version='0.1.1',
+    description='RSSHub Twitter Reader: Captura de tweets via RSS com filtragem por palavras-chave',
+    long_description='RSSHub Twitter Reader: Captura de tweets via RSS com filtragem por palavras-chave',
+    long_description_content_type='text/markdown',
+    author='Flavio Lopes',
+    author_email='flavio.lopes@ideiasfactory.tech',
+    url='https://github.com/dadosnapratica/rsshub_twitter_reader',
+    license='MIT',
+    license_files=['LICENSE'],
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+        'pandas',
+        'lxml'
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
