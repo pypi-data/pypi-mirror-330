66c48182cad77d58e76e45bf564b2c9f40b1c6b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-26 16:04:03 +0000

    Add package deep-dummy-maths/0.1.0/deep_dummy_maths-0.1.0-cp39-abi3-win32.whl

---
+++ packages/deep-dummy-maths/deep_dummy_maths-0.1.0-cp39-abi3-win32.whl/deep_dummy_maths/__init__.py
@@ -0,0 +1,4 @@
+from deep_dummy_maths.my_maths import my_sum, my_diff, my_pro, my_div, my_factorial
+from deep_dummy_maths._core import hello_from_bin
+
+__all__ = ["my_sum", "my_diff", "my_pro", "my_div", "my_factorial", "hello_from_bin"]
