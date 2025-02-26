b08d44e5a05b315af6c1c52dbe847aa52c920be0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-26 15:03:59 +0000

    Add package whiskerrag/0.0.11/whiskerrag-0.0.11-py3-none-any.whl

---
+++ packages/whiskerrag/whiskerrag-0.0.11-py3-none-any.whl/whiskerrag_types/model/__init__.py
@@ -0,0 +1,38 @@
+from .chunk import Chunk
+from .knowledge import (
+    EmbeddingModelEnum,
+    Knowledge,
+    KnowledgeCreate,
+    KnowledgeSourceEnum,
+    KnowledgeSplitConfig,
+    KnowledgeTypeEnum,
+)
+from .page import PageParams, PageResponse
+from .retrieval import (
+    RetrievalByKnowledgeRequest,
+    RetrievalBySpaceRequest,
+    RetrievalChunk,
+    RetrievalEnum,
+)
+from .task import Task, TaskRestartRequest, TaskStatus
+from .tenant import Tenant
+
+__all__ = [
+    "Chunk",
+    "KnowledgeSourceEnum",
+    "KnowledgeTypeEnum",
+    "EmbeddingModelEnum",
+    "KnowledgeSplitConfig",
+    "KnowledgeCreate",
+    "Knowledge",
+    "PageParams",
+    "PageResponse",
+    "RetrievalEnum",
+    "RetrievalBySpaceRequest",
+    "RetrievalByKnowledgeRequest",
+    "RetrievalChunk",
+    "Task",
+    "TaskStatus",
+    "TaskRestartRequest",
+    "Tenant",
+]
