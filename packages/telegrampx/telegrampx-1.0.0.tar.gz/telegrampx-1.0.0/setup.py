1d05c4bf57d1080f32104eb278f930135c99ebd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-26 15:04:44 +0000

    Add package telegrampx/1.0.0/telegrampx-1.0.0.tar.gz

---
+++ packages/telegrampx/telegrampx-1.0.0.tar.gz/telegrampx-1.0.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="telegrampx",
+    version="1.0.0",
+    author="iliya kaviyani",
+    author_email="iliyakaviyani313@gmail.com",
+    description="یک کتابخانه فوق سریع برای ربات تلگرام",    long_description_content_type="text/markdown",
+    url="https://github.com/iliya8989/telegrampx",
+    packages=find_packages(),
+    install_requires=[
+        "requests",
+        "httpx"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
