456516c3f8f64abfcdb51efa26150c4eaac351ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-26 14:03:47 +0000

    Add package llmling/1.6.14/llmling-1.6.14.tar.gz

---
+++ packages/llmling/llmling-1.6.14.tar.gz/llmling-1.6.14/src/llmling/__init__.py
@@ -0,0 +1,67 @@
+from __future__ import annotations
+
+from fsspec_httpx import HTTPFileSystem
+import fsspec
+
+fsspec.register_implementation("http", HTTPFileSystem, clobber=True)
+
+from llmling.resources import (
+    ResourceLoader,
+    LoadedResource,
+    default_registry as resource_registry,
+)
+from llmling.config.runtime import RuntimeConfig
+from llmling.core.exceptions import (
+    LLMLingError,
+    ConfigError,
+    ResourceError,
+    LoaderError,
+    ProcessorError,
+    LLMError,
+)
+from llmling.processors.registry import ProcessorRegistry
+from llmling.tools import LLMCallableTool, ToolError
+from llmling.prompts import (
+    PromptMessage,
+    PromptParameter,
+    StaticPrompt,
+    DynamicPrompt,
+    BasePrompt,
+)
+from llmling.config.models import (
+    ConfigModel,
+    GlobalSettings,
+    LLMCapabilitiesConfig,
+    Config,
+)
+from llmling.config.store import ConfigStore
+from llmling.core.baseregistry import BaseRegistry
+
+__version__ = "1.6.14"
+
+__all__ = [
+    "BasePrompt",
+    "BaseRegistry",
+    "Config",
+    "ConfigError",
+    "ConfigModel",
+    "ConfigStore",
+    "DynamicPrompt",
+    "GlobalSettings",
+    "LLMCallableTool",
+    "LLMCapabilitiesConfig",
+    "LLMError",
+    "LLMLingError",
+    "LoadedResource",
+    "LoaderError",
+    "ProcessorError",
+    "ProcessorRegistry",
+    "PromptMessage",
+    "PromptParameter",
+    "ResourceError",
+    "ResourceLoader",
+    "RuntimeConfig",
+    "StaticPrompt",
+    "ToolError",
+    "resource_registry",
+]
