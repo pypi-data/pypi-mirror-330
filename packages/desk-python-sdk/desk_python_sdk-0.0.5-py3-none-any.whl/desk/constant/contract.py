438e9531a2856dd82eb834c007431376160891bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-26 14:03:36 +0000

    Add package desk-python-sdk/0.0.5/desk_python_sdk-0.0.5-py3-none-any.whl

---
+++ packages/desk-python-sdk/desk_python_sdk-0.0.5-py3-none-any.whl/desk/constant/contract.py
@@ -0,0 +1,12 @@
+CONTRACT_ADDRESS = {
+    # Base Mainnet
+    8453: {
+        "peripherals": {
+            "vault": "0x764ab030159466edf9663e3fe3198c5d5c26c122"
+        }
+    },
+
+}
+
+VAULT_CONTRACT_ABI = "abis/vault.json"
+ERC20_ABI_PATH = "abis/erc20.json"
