c8790fac496e9335703068fbda69167092944d7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-26 15:03:57 +0000

    Add package spaceone-dashboard/2.0.dev148/spaceone_dashboard-2.0.dev148-py3-none-any.whl

---
+++ packages/spaceone-dashboard/spaceone_dashboard-2.0.dev148-py3-none-any.whl/spaceone/dashboard/manager/identity_manager.py
@@ -0,0 +1,30 @@
+from spaceone.core import config
+from spaceone.core.manager import BaseManager
+from spaceone.core.connector.space_connector import SpaceConnector
+
+
+class IdentityManager(BaseManager):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.identity_conn: SpaceConnector = self.locator.get_connector(
+            "SpaceConnector", service="identity"
+        )
+
+    def check_workspace(self, workspace_id: str, domain_id: str) -> None:
+        system_token = config.get_global("TOKEN")
+
+        return self.identity_conn.dispatch(
+            "Workspace.check",
+            {"workspace_id": workspace_id, "domain_id": domain_id},
+            token=system_token,
+        )
+
+    def get_project(self, project_id: str) -> dict:
+        return self.identity_conn.dispatch("Project.get", {"project_id": project_id})
+
+    def list_service_accounts(self, workspace_id: str) -> dict:
+        if workspace_id:
+            query = {"workspace_id": workspace_id}
+        else:
+            query = {}
+        return self.identity_conn.dispatch("ServiceAccount.list", query)
