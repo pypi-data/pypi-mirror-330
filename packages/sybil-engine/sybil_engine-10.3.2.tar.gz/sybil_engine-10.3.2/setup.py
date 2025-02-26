8a65aa76e8db3cade489e65dc54a1580fa6e1e8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-26 16:03:54 +0000

    Add package sybil-engine/10.3.2/sybil_engine-10.3.2.tar.gz

---
+++ packages/sybil-engine/sybil_engine-10.3.2.tar.gz/sybil_engine-10.3.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+with open("requirements.txt", "r", encoding="utf-8") as file:
+    requirements = file.readlines()
+
+setup(
+    name='sybil_engine',
+    version='10.3.2',
+    py_modules=['sybil_engine'],
+    packages=find_packages(),
+    package_data={
+        'sybil_engine': ['resources/**/*'],
+    },
+    install_requires=requirements,
+    data_files=[('', ['requirements.txt'])],
+    author='Indeoo',
+    author_email='indeooars@gmail.com',
+    description='Engine for web3 smart contracts automatization.',
+    long_description=open('README.md', 'r', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/Indeoo/sybil-engine/',
+)
---
+++ packages/sybil-engine/sybil_engine-10.3.2.tar.gz/sybil_engine-10.3.2/test/module/test_module_executor.py
@@ -0,0 +1,60 @@
+import unittest
+
+from sybil_engine.module.module_executor import ModuleExecutor
+from sybil_engine.utils.accumulator import get_value
+from sybil_engine.utils.utils import AccountException
+from test import test_account
+from test.module.mock_fail_module import MockFailModule
+from test.module.mock_module import MockModule
+from test.module.mock_not_enoguth_native_module import MockNotEnoughNativeModule
+from test.module.repeatable_mock_module import RepeatableMockModule
+
+
+class TestModuleExecutor(unittest.TestCase):
+
+    def test_execute_module(self):
+        sleep_interval = {'from': 0, 'to': 0}
+        account = test_account
+        min_native_balance = {'from': 0.001, 'to': 0.001}
+        modules = [
+            (MockModule(min_native_balance, None), {}),
+            (MockModule(min_native_balance, None), {}),
+            (MockModule(min_native_balance, None), {})
+        ]
+
+        ModuleExecutor().execute_modules(modules, account, sleep_interval)
+
+        self.assertEqual(get_value("Finished accounts: "), [test_account])
+
+    def test_shouldThrowNotEnoughNativeBalance(self):
+        sleep_interval = {'from': 0, 'to': 0}
+        account = test_account
+        min_native_balance = {'from': 0.001, 'to': 0.001}
+        modules = [
+            (MockModule(min_native_balance, None), {}),
+            (MockModule(min_native_balance, None), {}),
+            (MockNotEnoughNativeModule(min_native_balance, None), {})
+        ]
+
+        ModuleExecutor().execute_modules(modules, account, sleep_interval)
+
+        self.assertEqual(len(get_value("Failed accounts: ")), 1)
+
+    def test_should_throwAccountExceptionOnModuleException(self):
+        sleep_interval = {'from': 0, 'to': 0}
+        account = test_account
+        min_native_balance = {'from': 0.001, 'to': 0.001}
+
+        module = MockFailModule(min_native_balance, None)
+
+        with self.assertRaises(AccountException):
+            ModuleExecutor().execute_module({}, account, module, sleep_interval)
+
+    def test_should_throwHandleExceptionOnRepeatableModuleException(self):
+        sleep_interval = {'from': 0, 'to': 0}
+        account = test_account
+        min_native_balance = {'from': 0.001, 'to': 0.001}
+
+        module = RepeatableMockModule(min_native_balance, None, 1)
+
+        ModuleExecutor().execute_module({}, account, module, sleep_interval)
