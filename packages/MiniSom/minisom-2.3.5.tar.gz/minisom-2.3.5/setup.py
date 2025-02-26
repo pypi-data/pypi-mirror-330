6f9277b3d5d3c8f503b50ef22c92a58564aacf01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-26 16:03:52 +0000

    Add package MiniSom/2.3.5/minisom-2.3.5.tar.gz

---
+++ packages/MiniSom/minisom-2.3.5.tar.gz/minisom-2.3.5/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+description = 'Minimalistic implementation of the Self Organizing Maps (SOM)'
+keywords = ['machine learning', 'neural networks',
+            'clustering', 'dimentionality reduction']
+
+long_description = 'See the github page https://github.com/JustGlowing/minisom'
+
+setup(name='MiniSom',
+      version='2.3.5',
+      description=description,
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      author='Giuseppe Vettigli',
+      package_data={'': ['Readme.md']},
+      include_package_data=True,
+      license="MIT",
+      py_modules=['minisom'],
+      requires=['numpy'],
+      url='https://github.com/JustGlowing/minisom',
+      download_url='https://github.com/JustGlowing/minisom/archive/master.zip',
+      keywords=keywords)
