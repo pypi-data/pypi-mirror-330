b06fcdbec09f278c71fbb3a50e0b54d23e8cc4c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-26 15:04:14 +0000

    Add package hyperpocket/0.4.5/hyperpocket-0.4.5.tar.gz

---
+++ packages/hyperpocket/hyperpocket-0.4.5.tar.gz/hyperpocket-0.4.5/hyperpocket/repository/__init__.py
@@ -0,0 +1,4 @@
+from hyperpocket.repository.tool_reference import ToolReference
+from hyperpocket.repository.repository import eject, pull
+
+__all__ = ["ToolReference", "pull", "eject"]
---
+++ packages/hyperpocket/hyperpocket-0.4.5.tar.gz/hyperpocket-0.4.5/hyperpocket/repository/repository.py
@@ -0,0 +1,8 @@
+def pull(urllike: str, git_ref: str):
+    # TODO
+    pass
+
+
+def eject(url: str, ref: str, remote_path: str):
+    # TODO
+    pass
