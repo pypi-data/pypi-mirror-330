9ac49a97cdcdcc474f09138b01ab1af45b88c7ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-26 16:03:47 +0000

    Add package machine-stats/2.7.1/machine_stats-2.7.1.tar.gz

---
+++ packages/machine-stats/machine_stats-2.7.1.tar.gz/machine_stats-2.7.1/setup.py
@@ -0,0 +1,28 @@
+from distutils.core import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="machine_stats",
+    version="v2.7.1",
+    author="Tidal SW",
+    author_email="support@tidalcloud.com",
+    description="A simple and effective way to gather machine statistics (RAM, Storage, CPU, etc.) from server environment",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=["machine_stats"],
+    package_dir={"": "src"},
+    package_data={"": ["modules/*", "plugins/*"]},
+    url="https://github.com/tidalmigrations/machine_stats",
+    install_requires=[
+        "ansible<2.10",
+        "pluginbase==1.0.1",
+    ],
+    entry_points={
+        "console_scripts": [
+            "machine_stats=machine_stats:main",
+            "machine-stats=machine_stats:main",
+        ]
+    },
+)
