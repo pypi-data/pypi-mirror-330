a4de70c2e2e2e20bc001eb2d4a9e7021e1551743
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-26 14:03:46 +0000

    Add package geoai-GDAL/3.4.3/geoai_GDAL-3.4.3-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/geoai-GDAL/geoai_GDAL-3.4.3-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/GDAL-3.4.3.data/scripts/gdal_retile.py
@@ -0,0 +1,11 @@
+#!python
+
+import sys
+# import osgeo_utils.gdal_retile as a convenience to use as a script
+from osgeo_utils.gdal_retile import *  # noqa
+from osgeo_utils.gdal_retile import main
+from osgeo.gdal import deprecation_warn
+
+
+deprecation_warn('gdal_retile')
+sys.exit(main(sys.argv))
