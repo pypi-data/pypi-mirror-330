b9f439b229e9b00682228019f1973f08623470a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-26 15:04:21 +0000

    Add package django-ahin/1.16.5/django_ahin-1.16.5.tar.gz

---
+++ packages/django-ahin/django_ahin-1.16.5.tar.gz/django_ahin-1.16.5/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django_ahin',
+    version='1.16.5',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'django-ahin = django_ahin.create_project:main',
+        ],
+    },
+)
