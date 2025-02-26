fec6c144a648786248bc8b732f6072c8dae6ae4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-26 15:04:22 +0000

    Add package pymysqlhelper/1.4.0/pymysqlhelper-1.4.0.tar.gz

---
+++ packages/pymysqlhelper/pymysqlhelper-1.4.0.tar.gz/pymysqlhelper-1.4.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+from setuptools.config.expand import entry_points
+
+setup(
+    name="pymysqlhelper",
+    version="1.4.0",
+    description="A simple MySQL database helper for easy interactions",
+    author="DEAMJAVA",
+    author_email="deamminecraft3@gmail.com",
+    packages=find_packages(),
+    install_requires=["pymysql", "sqlalchemy"],
+    python_requires=">=3.6",
+    entry_points={"console_scripts":["pymysqlhelper = pymysqlhelper:pymysqlhelper"]}
+)
