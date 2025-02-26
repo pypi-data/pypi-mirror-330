7c97e14c45ff371bcae2d8237dda5b16f37cb73b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-26 16:03:55 +0000

    Add package myrsa-encrypt/0.2.1/myrsa_encrypt-0.2.1.tar.gz

---
+++ packages/myrsa-encrypt/myrsa_encrypt-0.2.1.tar.gz/myrsa_encrypt-0.2.1/README.md
@@ -0,0 +1,8 @@
+# My RSA Library
+
+This is a simple implementation of RSA encryption and decryption in Python without using built-in RSA libraries.
+
+## Installation
+
+```bash
+pip install myrsa_encrypt
---
+++ packages/myrsa-encrypt/myrsa_encrypt-0.2.1.tar.gz/myrsa_encrypt-0.2.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="myrsa_encrypt",
+    version="0.2.1",
+    packages=find_packages(),
+    author="Deep Radadiya",
+    author_email="deepradadiya0987@gmail.com",
+    description="A simple RSA encryption library without using built-in RSA libraries.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/DeepJRadadiya/my_rsa",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+
+)
