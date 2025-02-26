e76a8522788559f020d61d07612fcb5da7c7bde5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-26 16:03:55 +0000

    Add package langtest/2.6.0rc1/langtest-2.6.0rc1.tar.gz

---
+++ packages/langtest/langtest-2.6.0rc1.tar.gz/langtest-2.6.0rc1/langtest/augmentation/__init__.py
@@ -0,0 +1,4 @@
+from .base import BaseAugmentaion, AugmentRobustness, TemplaticAugment
+from .augmenter import DataAugmenter
+
+__all__ = ["BaseAugmentaion", "AugmentRobustness", "TemplaticAugment", "DataAugmenter"]
