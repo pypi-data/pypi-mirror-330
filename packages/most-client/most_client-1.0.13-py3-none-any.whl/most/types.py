02e95278bc047a53032e4acee60625f137a31974
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-26 15:05:16 +0000

    Add package most-client/1.0.13/most_client-1.0.13-py3-none-any.whl

---
+++ packages/most-client/most_client-1.0.13-py3-none-any.whl/most/types.py
@@ -0,0 +1,91 @@
+from dataclasses import dataclass
+
+from bson import ObjectId
+from dataclasses_json import dataclass_json, DataClassJsonMixin
+from typing import Optional, List, Literal, Dict
+
+
+@dataclass_json
+@dataclass
+class StoredAudioData(DataClassJsonMixin):
+    id: str
+    data: Dict[str, str]
+
+
+@dataclass_json
+@dataclass
+class Audio(DataClassJsonMixin):
+    id: str
+    url: str
+
+
+@dataclass_json
+@dataclass
+class Text(DataClassJsonMixin):
+    id: str
+
+
+@dataclass_json
+@dataclass
+class SubcolumnResult(DataClassJsonMixin):
+    name: str
+    score: Optional[int]
+    description: str
+
+
+@dataclass_json
+@dataclass
+class ColumnResult(DataClassJsonMixin):
+    name: str
+    subcolumns: List[SubcolumnResult]
+
+
+@dataclass_json
+@dataclass
+class Column(DataClassJsonMixin):
+    name: str
+    subcolumns: List[str]
+
+
+@dataclass_json
+@dataclass
+class Script(DataClassJsonMixin):
+    columns: List[Column]
+
+
+@dataclass_json
+@dataclass
+class JobStatus(DataClassJsonMixin):
+    status: Literal["not_found", "pending", "completed", "error"]
+
+
+@dataclass_json
+@dataclass
+class Result(DataClassJsonMixin):
+    id: str
+    text: Optional[str]
+    url: Optional[str]
+    results: Optional[List[ColumnResult]]
+
+    def get_script(self) -> Optional[Script]:
+        if self.results is None:
+            return None
+
+        return Script(columns=[Column(name=column_result.name,
+                                      subcolumns=[subcolumn_result.name
+                                                  for subcolumn_result in column_result.subcolumns])
+                               for column_result in self.results])
+
+
+def is_valid_id(smth_id: Optional[str]) -> bool:
+    if smth_id is None:
+        return False
+
+    if smth_id.startswith("most-"):
+        smth_id = smth_id[5:]
+
+    try:
+        ObjectId(smth_id)
+        return True
+    except:
+        return False
