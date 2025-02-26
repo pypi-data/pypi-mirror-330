3c07b6a2db215490718893a33601655ce3cb1290
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-26 15:04:50 +0000

    Add package pybotvac/0.0.26/pybotvac-0.0.26.tar.gz

---
+++ packages/pybotvac/pybotvac-0.0.26.tar.gz/pybotvac-0.0.26/pybotvac/vorwerk.py
@@ -0,0 +1,14 @@
+import os
+
+from .neato import Vendor
+
+
+class Vorwerk(Vendor):
+    name = "vorwerk"
+    endpoint = "https://beehive.ksecosys.com/"
+    passwordless_endpoint = "https://mykobold.eu.auth0.com/passwordless/start"
+    token_endpoint = "https://mykobold.eu.auth0.com/oauth/token"  # nosec
+    scope = ["openid", "email", "profile", "read:current_user", "offline_access"]
+    audience = "https://mykobold.eu.auth0.com/userinfo"
+    source = "vorwerk_auth0"
+    cert_path = os.path.join(os.path.dirname(__file__), "cert", "ksecosys.com.crt")
