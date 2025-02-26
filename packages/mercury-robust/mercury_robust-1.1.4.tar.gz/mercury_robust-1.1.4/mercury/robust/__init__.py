66aa010b72fa0ea3343190b5f470aa01d2f6440e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-26 16:03:48 +0000

    Add package mercury-robust/1.1.4/mercury_robust-1.1.4.tar.gz

---
+++ packages/mercury-robust/mercury_robust-1.1.4.tar.gz/mercury_robust-1.1.4/mercury/robust/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+"""
+Top-level package for mercury robust.
+"""
+
+__version__ = '1.1.4'
+
+from .suite import TestSuite  # noqa: 402
+from .create_tutorials import create_tutorials
