768afb2bc3aa872531bed466da4e7e57db1fa585
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-26 14:04:13 +0000

    Add package extension-maker/1.0.2/extension_maker-1.0.2-py3-none-any.whl

---
+++ packages/extension-maker/extension_maker-1.0.2-py3-none-any.whl/extension_maker/maker.py
@@ -0,0 +1,17 @@
+import os
+def createFolder(folderName, callback):
+    download_path = os.path.join(os.path.expanduser("~"), "Downloads", folderName)
+    if os.path.exists(download_path):
+        i = 1
+        new_folder_name = os.path.join(os.path.expanduser("~"), "Downloads", f"{folderName}_{i}")
+        while os.path.exists(new_folder_name):
+            i += 1
+            new_folder_name = os.path.join(os.path.expanduser("~"), "Downloads", f"{folderName}_{i}")
+        os.makedirs(new_folder_name)
+        callback(f"Folder '{download_path}' already exists. Created '{new_folder_name}' instead.")
+        return new_folder_name
+    else:
+        os.makedirs(download_path)
+        callback(f"Folder '{download_path}' created.")
+        return download_path
+
