96544d8c1f1472757eb2c5ed715c1e72e58a62ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-26 15:04:56 +0000

    Add package pymysqlhelper/1.5.0/pymysqlhelper-1.5.0.tar.gz

---
+++ packages/pymysqlhelper/pymysqlhelper-1.5.0.tar.gz/pymysqlhelper-1.5.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+from setuptools.config.expand import entry_points
+
+with open("README.md", 'r') as f:
+    description = f.read()
+
+setup(
+    name="pymysqlhelper",
+    version="1.5.0",
+    description="A simple MySQL database helper for easy interactions",
+    author="DEAMJAVA",
+    author_email="deamminecraft3@gmail.com",
+    packages=find_packages(),
+    install_requires=["pymysql", "sqlalchemy"],
+    python_requires=">=3.6",
+    entry_points={"console_scripts":["pymysqlhelper = pymysqlhelper:pymysqlhelper_check",
+                                     "pymysqlhelper-license = pymysqlhelper:pymysqlhelper_license"]},
+    long_description=description,
+    long_description_content_type='text/markdown'
+)
