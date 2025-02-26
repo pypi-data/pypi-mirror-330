8810fbfaaa58e1255c90844111139169b603ce5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-26 16:03:52 +0000

    Add package multiview-stitcher/0.1.20/multiview_stitcher-0.1.20.tar.gz

---
+++ packages/multiview-stitcher/multiview_stitcher-0.1.20.tar.gz/multiview_stitcher-0.1.20/src/multiview_stitcher/_tests/test_spatial_image_utils.py
@@ -0,0 +1,47 @@
+import dask.array as da
+import numpy as np
+import pytest
+
+from multiview_stitcher import param_utils
+from multiview_stitcher import spatial_image_utils as si_utils
+
+
+@pytest.mark.parametrize(
+    "xp, ndim", [(xp, ndim) for xp in [np, da] for ndim in [2, 3]]
+)
+def test_sim_array_input_backends(xp, ndim):
+    sim = si_utils.get_sim_from_array(
+        xp.ones((5,) * ndim),
+        dims=si_utils.SPATIAL_DIMS[-ndim:],
+        scale={dim: 1.0 for dim in si_utils.SPATIAL_DIMS[-ndim:]},
+        translation={dim: -1.0 for dim in si_utils.SPATIAL_DIMS[-ndim:]},
+        affine=param_utils.identity_transform(ndim),
+    )
+
+    assert isinstance(sim.data, da.Array)
+
+
+def test_max_project():
+    ndim = 3
+    dim = "z"
+    transform_key = "test"
+
+    sim = si_utils.get_sim_from_array(
+        np.ones((5,) * ndim),
+        dims=si_utils.SPATIAL_DIMS[-ndim:],
+        scale={dim: 1.0 for dim in si_utils.SPATIAL_DIMS[-ndim:]},
+        translation={dim: -1.0 for dim in si_utils.SPATIAL_DIMS[-ndim:]},
+        affine=param_utils.identity_transform(ndim),
+        transform_key=transform_key,
+    )
+
+    sim_proj = si_utils.max_project_sim(sim, dim)
+
+    assert dim not in sim_proj.dims
+
+    affine = si_utils.get_affine_from_sim(sim, transform_key)
+
+    for pdim in ["x_in", "x_out"]:
+        assert dim not in affine.coords[pdim].values
+        assert "x" in affine.coords[pdim].values
+        assert "1" in affine.coords[pdim].values
