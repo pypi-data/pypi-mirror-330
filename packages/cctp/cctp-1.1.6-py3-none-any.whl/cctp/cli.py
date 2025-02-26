de1987887a888e0187ee0b7dcf2d471656bb1c96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-26 14:04:07 +0000

    Add package cctp/1.1.6/cctp-1.1.6-py3-none-any.whl

---
+++ packages/cctp/cctp-1.1.6-py3-none-any.whl/cctp/cli.py
@@ -0,0 +1,27 @@
+"""Console script for cctp."""
+
+import os
+
+import typer
+from rich.console import Console
+
+from cctp import __version__
+
+app = typer.Typer()
+console = Console()
+
+
+@app.command()
+def main(
+    version: bool = typer.Option(None, "--version", "-V", help="Show app version and exit."),
+):
+    """Console script for cctp."""
+    if version:
+        console.print(f"cctp v{__version__}")
+        return
+
+    os.system(" ".join(["uvx", "cookiecutter", "https://gitee.com/gooker_young/cctp.git"]))
+
+
+if __name__ == "__main__":
+    app()
