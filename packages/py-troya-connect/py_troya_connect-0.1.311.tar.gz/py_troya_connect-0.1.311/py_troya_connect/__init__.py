384563eaa59d4606000c51fd0a612a7b5105a012
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-26 15:04:54 +0000

    Add package py-troya-connect/0.1.311/py_troya_connect-0.1.311.tar.gz

---
+++ packages/py-troya-connect/py_troya_connect-0.1.311.tar.gz/py_troya_connect-0.1.311/py_troya_connect/__init__.py
@@ -0,0 +1,19 @@
+from .terminal import (
+    ExtraTerminal,
+    TerminalType,
+    ExtraTerminalError,
+    ConnectionError,
+    SessionError,
+    TerminalBusyError,
+    CommandError
+)
+
+__version__ = "0.1.311"
+__all__ = [
+    'ExtraTerminal',
+    'ExtraTerminalError',
+    'ConnectionError',
+    'SessionError',
+    'TerminalBusyError',
+    'CommandError'
+]
