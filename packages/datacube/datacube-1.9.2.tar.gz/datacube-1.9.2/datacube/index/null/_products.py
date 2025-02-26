b306fea4ac47614a9425e114d4f8471c8521d7f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-26 14:04:05 +0000

    Add package datacube/1.9.2/datacube-1.9.2.tar.gz

---
+++ packages/datacube/datacube-1.9.2.tar.gz/datacube-1.9.2/datacube/index/null/_products.py
@@ -0,0 +1,51 @@
+# This file is part of the Open Data Cube, see https://opendatacube.org for more information
+#
+# Copyright (c) 2015-2025 ODC Contributors
+# SPDX-License-Identifier: Apache-2.0
+import logging
+import datetime
+
+from datacube.index.abstract import AbstractProductResource
+from datacube.model import Product
+
+from typing import Iterable
+
+_LOG = logging.getLogger(__name__)
+
+
+class ProductResource(AbstractProductResource):
+    def add(self, product, allow_table_lock=False):
+        raise NotImplementedError()
+
+    def can_update(self, product, allow_unsafe_updates=False, allow_table_lock=False):
+        raise NotImplementedError()
+
+    def update(self, product: Product, allow_unsafe_updates=False, allow_table_lock=False):
+        raise NotImplementedError()
+
+    def delete(self, products: Iterable[Product], allow_delete_active: bool = False):
+        raise NotImplementedError()
+
+    def get_unsafe(self, id_):
+        raise KeyError(id_)
+
+    def get_by_name_unsafe(self, name):
+        raise KeyError(name)
+
+    def search_robust(self, **query):
+        return []
+
+    def search_by_metadata(self, metadata):
+        return []
+
+    def get_all(self) -> Iterable[Product]:
+        return []
+
+    def temporal_extent(self, product: str | Product) -> tuple[datetime.datetime, datetime.datetime]:
+        raise KeyError(str(product))
+
+    def spatial_extent(self, product, crs=None):
+        raise KeyError(str(product))
+
+    def most_recent_change(self, product: str | Product) -> datetime.datetime | None:
+        raise KeyError(str(product))
---
+++ packages/datacube/datacube-1.9.2.tar.gz/datacube-1.9.2/datacube/index/postgres/_users.py
@@ -0,0 +1,52 @@
+# This file is part of the Open Data Cube, see https://opendatacube.org for more information
+#
+# Copyright (c) 2015-2025 ODC Contributors
+# SPDX-License-Identifier: Apache-2.0
+from typing import Iterable, cast
+from datacube.index.abstract import AbstractUserResource
+from datacube.index.postgres._transaction import IndexResourceAddIn
+from datacube.drivers.postgres import PostgresDb
+
+
+class UserResource(AbstractUserResource, IndexResourceAddIn):
+    def __init__(self,
+                 db: PostgresDb,
+                 index
+                ) -> None:
+        """
+        :type db: datacube.drivers.postgres._connections.PostgresDb
+        """
+        from datacube.index.postgres.index import Index
+        self._db = db
+        self._index: Index = cast(Index, index)
+
+    def grant_role(self, role: str, *usernames: str) -> None:
+        """
+        Grant a role to users
+        """
+        with self._db_connection() as connection:
+            connection.grant_role(role, usernames)
+
+    def create_user(self, username: str, password: str,
+                    role: str, description: str | None = None) -> None:
+        """
+        Create a new user.
+        """
+        with self._db_connection() as connection:
+            connection.create_user(username, password, role, description=description)
+
+    def delete_user(self, *usernames: str) -> None:
+        """
+        Delete a user
+        """
+        with self._db_connection() as connection:
+            connection.drop_users(usernames)
+
+    def list_users(self) -> Iterable[tuple[str, str, str | None]]:
+        """
+        :return: list of (role, user, description)
+        :rtype: list[(str, str, str)]
+        """
+        with self._db_connection() as connection:
+            for role, user, description in connection.list_users():
+                yield role, user, description
