db9193cea21149bae80ff51d6dcaccfe0a6eb5b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-27 16:08:34 +0000

    Add package python-aidot/0.3.2/python_aidot-0.3.2.tar.gz

---
+++ packages/python-aidot/python_aidot-0.3.2.tar.gz/python_aidot-0.3.2/aidot/exceptions.py
@@ -0,0 +1,24 @@
+"""aidot Exceptions."""
+class AidotError(Exception):
+    """Aidot api exception."""
+
+class InvalidURL(AidotError):
+    """Invalid url exception."""
+
+class HTTPError(AidotError):
+    """Invalid host exception."""
+
+class InvalidHost(AidotError):
+    """Invalid host exception."""
+
+class AidotAuthTokenExpired(AidotError):
+    """Authentication failed because token is invalid or expired."""
+
+class AidotAuthFailed(AidotError):
+    """Authentication failed"""
+
+class AidotUserOrPassIncorrect(AidotError):
+    """Authentication failed"""
+
+class AidotOSError(Exception):
+    """Aidot exception."""
