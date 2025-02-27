ad236937d3427014adffe3e9c2337026fd30b9af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-27 04:03:51 +0000

    Add package pywfn/1.0.1/pywfn-1.0.1.tar.gz

---
+++ packages/pywfn/pywfn-1.0.1.tar.gz/pywfn-1.0.1/pywfn/bondprop/__init__.py
@@ -0,0 +1,24 @@
+from pywfn.utils import printer
+from pywfn.shell import Shell
+class Caler:
+
+    def calculate(self,idx1:int,idx2:int)->float:
+        return 0.0
+
+    def print(self,resStr:str):
+        printer.res(resStr)
+
+    def resStr(self,idx1:int,idx2:int)->str:
+        res=self.calculate(idx1,idx2)
+        return f'{res}'
+
+def onShell(shell:Shell):
+    from pywfn.bondprop import order
+    printer.options('键の属性',{
+      '1':'各种键级'
+    })
+    opt=input('请输入要计算的键属性:')
+    if opt=='1':
+        mols=shell.input.Moles()
+        caler=order.Calculator(mols[0])
+        caler.onShell(shell)
