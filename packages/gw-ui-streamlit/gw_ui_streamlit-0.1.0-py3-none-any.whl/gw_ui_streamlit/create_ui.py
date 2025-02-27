2014919fd474552d47b7bf996697c1eca0a3b67b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-27 16:08:40 +0000

    Add package gw-ui-streamlit/0.1.0/gw_ui_streamlit-0.1.0-py3-none-any.whl

---
+++ packages/gw-ui-streamlit/gw_ui_streamlit-0.1.0-py3-none-any.whl/gw_ui_streamlit/create_ui.py
@@ -0,0 +1,10 @@
+import gw_ui_streamlit._create_ui as _create_ui
+
+"""Exposes some of the private _create_ui functions"""
+def build_columns(item):
+    columns = _create_ui.build_columns(item)
+    return columns
+
+def build_dataframe(item, columns, default_rows):
+    df = _create_ui.build_dataframe(item, columns, default_rows)
+    return df
