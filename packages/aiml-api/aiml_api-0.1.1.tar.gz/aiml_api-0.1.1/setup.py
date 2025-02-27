1cdf0e4742164415cc697cad986e5fd3a3c5044c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-27 06:04:11 +0000

    Add package aiml-api/0.1.1/aiml_api-0.1.1.tar.gz

---
+++ packages/aiml-api/aiml_api-0.1.1.tar.gz/aiml_api-0.1.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="aiml_api",
+    version="0.1.1",
+    packages=find_packages(),
+    install_requires=[
+        "openai>=1.55.1",
+        "pydantic-settings>=2.7.1",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
