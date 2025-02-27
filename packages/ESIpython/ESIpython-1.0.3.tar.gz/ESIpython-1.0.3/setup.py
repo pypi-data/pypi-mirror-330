6d96a9128c502356fb81e71051f8e839cdcc7494
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-27 04:04:20 +0000

    Add package ESIpython/1.0.3/ESIpython-1.0.3.tar.gz

---
+++ packages/ESIpython/ESIpython-1.0.3.tar.gz/ESIpython-1.0.3/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name="ESIpython",
+    version="1.0.3",
+    description="Calculation of electronic aromaticity indicators",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Joan Grebol, Eduard Matito, Pedro Salvador",
+    url="https://github.com/jgrebol/ESIpy",
+    packages=["esipy"],
+    install_requires=[
+        "numpy<=1.23.3", # Earliest version supported via pip
+        "pyscf<=2.4",
+        "sphinx"
+    ],
+)
