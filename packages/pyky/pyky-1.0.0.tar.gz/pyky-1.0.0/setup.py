1a89a977e57d942a2e03908b6aa7e4507969a180
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-27 04:03:43 +0000

    Add package pyky/1.0.0/pyky-1.0.0.tar.gz

---
+++ packages/pyky/pyky-1.0.0.tar.gz/pyky-1.0.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pyky",
+    version="1.0.0",
+    author="Erhan",
+    author_email="erhan@example.com",
+    description="A Python implementation of Kyber Post-Quantum KEM",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/asdfjkl/pyky",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "numpy",
+        "pycryptodome"
+    ],
+    python_requires=">=3.7",
+)
