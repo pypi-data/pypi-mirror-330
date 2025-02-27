e3de6a4fe0101b0b28074b21721c619f5ffbc51d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-27 05:04:19 +0000

    Add package dektools/0.2.86/dektools-0.2.86.tar.gz

---
+++ packages/dektools/dektools-0.2.86.tar.gz/dektools-0.2.86/dektools/serializer/jsonl.py
@@ -0,0 +1,18 @@
+import json
+from .base import SerializerBase
+from ..file import iter_file_obj_lines
+
+
+class Jsonl(SerializerBase):
+    def _load_file(self, file):
+        return [json.loads(line) for line in iter_file_obj_lines(file) if line]
+
+    def _dump_file(self, obj, file, kwargs):
+        kwargs.update({
+            'ensure_ascii': False,
+            'indent': None
+        })
+        file.write('\n'.join(json.dumps(item, **kwargs) for item in obj))
+
+
+jsonl = Jsonl()
