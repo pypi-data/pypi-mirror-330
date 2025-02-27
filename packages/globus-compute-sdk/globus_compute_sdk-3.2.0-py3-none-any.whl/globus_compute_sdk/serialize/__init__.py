9829538fbce965e47d6fbb7947c878d1d133e6c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-27 07:04:35 +0000

    Add package globus-compute-sdk/3.2.0/globus_compute_sdk-3.2.0-py3-none-any.whl

---
+++ packages/globus-compute-sdk/globus_compute_sdk-3.2.0-py3-none-any.whl/globus_compute_sdk/serialize/__init__.py
@@ -0,0 +1,27 @@
+from globus_compute_sdk.serialize.base import SerializationStrategy
+from globus_compute_sdk.serialize.concretes import (
+    DEFAULT_STRATEGY_CODE,
+    DEFAULT_STRATEGY_DATA,
+    CombinedCode,
+    DillCode,
+    DillCodeSource,
+    DillCodeTextInspect,
+    DillDataBase64,
+    JSONData,
+)
+from globus_compute_sdk.serialize.facade import AllowlistWildcard, ComputeSerializer
+
+__all__ = [
+    "ComputeSerializer",
+    "AllowlistWildcard",
+    "SerializationStrategy",
+    "DEFAULT_STRATEGY_CODE",
+    "DEFAULT_STRATEGY_DATA",
+    # Selectable strategies:
+    "CombinedCode",
+    "DillCode",
+    "DillCodeSource",
+    "DillCodeTextInspect",
+    "DillDataBase64",
+    "JSONData",
+]
