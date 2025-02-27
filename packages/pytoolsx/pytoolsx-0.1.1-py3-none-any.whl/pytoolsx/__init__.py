156e6fa8a867f31ec3d50dc0677f9b3548f988f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-27 14:04:58 +0000

    Add package pytoolsx/0.1.1/pytoolsx-0.1.1-py3-none-any.whl

---
+++ packages/pytoolsx/pytoolsx-0.1.1-py3-none-any.whl/pytoolsx/__init__.py
@@ -0,0 +1,15 @@
+# pytools/__init__.py
+
+# Import specific functions from pytools.py
+from .pytoolsx import *
+
+# Define the public API (optional, but recommended)
+##__all__ = [
+##    "send_mail", "moveClick", "findOnPage", "recordClicks", "recordMouse", "playMouse",
+##    "textOnly", "numOnly", "ocr", "screenshot", "set_volume"
+##]
+
+# Package metadata
+__version__ = "0.1.0"
+__author__ = "Alex Sagar"
+__license__ = "MIT"
