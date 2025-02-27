5300a93a6abe63c2604138cf822538b8dc1d149b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-27 15:06:36 +0000

    Add package rumus-matematika/0.1.0/rumus_matematika-0.1.0-py3-none-any.whl

---
+++ packages/rumus-matematika/rumus_matematika-0.1.0-py3-none-any.whl/rumus_matematika.py
@@ -0,0 +1,17 @@
+class RumusMatematika:
+    def tambah(a, b):
+        return a + b
+
+    def kurang(a, b):
+        return a - b
+
+    def kali(a, b):
+        return a * b
+    def bagi(a, b):
+        return a / b
+
+    def pangkat(a, b):
+        return a ** b
+
+    def akar(a, b):
+        return a ** (1 / b)
