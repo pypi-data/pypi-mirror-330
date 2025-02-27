54baa930689269abb1cfbd49bc7b7fd7e5ab2da8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-27 16:08:21 +0000

    Add package ipitin/0.0.3/ipitin-0.0.3-py3-none-any.whl

---
+++ packages/ipitin/ipitin-0.0.3-py3-none-any.whl/ipitin/ipitin.py
@@ -0,0 +1,9 @@
+'''\
+# How To Use It?
+
+`from ipitin.ipitin import *`\
+'''
+
+import pandas as pd
+import tensorflow as tf
+import numpy as np
---
+++ packages/ipitin/ipitin-0.0.3-py3-none-any.whl/ipitin/menual.py
@@ -0,0 +1,7 @@
+if '__log_option__' in globals(): p = __log_option__
+else: p = True
+if p: print('''\
+# How To Use It?
+
+`from ipitin.ipitin import *`\
+''')
