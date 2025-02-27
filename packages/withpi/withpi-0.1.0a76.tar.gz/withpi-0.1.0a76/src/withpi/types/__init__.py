7dc8cc1bdc0caa278ea8db081905f45d8f45fd39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-27 08:04:22 +0000

    Add package withpi/0.1.0a76/withpi-0.1.0a76.tar.gz

---
+++ packages/withpi/withpi-0.1.0a76.tar.gz/withpi-0.1.0a76/src/withpi/types/__init__.py
@@ -0,0 +1,15 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from .shared import Example as Example, Contract as Contract, Dimension as Dimension, SubDimension as SubDimension
+from .contract_score_params import ContractScoreParams as ContractScoreParams
+from .data_generation_status import DataGenerationStatus as DataGenerationStatus
+from .prompt_optimize_params import PromptOptimizeParams as PromptOptimizeParams
+from .contracts_score_metrics import ContractsScoreMetrics as ContractsScoreMetrics
+from .prompt_optimization_status import PromptOptimizationStatus as PromptOptimizationStatus
+from .contract_read_from_hf_params import ContractReadFromHfParams as ContractReadFromHfParams
+from .query_generate_fanouts_params import QueryGenerateFanoutsParams as QueryGenerateFanoutsParams
+from .prompt_stream_messages_response import PromptStreamMessagesResponse as PromptStreamMessagesResponse
+from .query_generate_fanouts_response import QueryGenerateFanoutsResponse as QueryGenerateFanoutsResponse
+from .contract_generate_dimensions_params import ContractGenerateDimensionsParams as ContractGenerateDimensionsParams
