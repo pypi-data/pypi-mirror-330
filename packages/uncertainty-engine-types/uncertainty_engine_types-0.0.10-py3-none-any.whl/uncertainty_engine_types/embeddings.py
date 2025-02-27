3f22ff49132fee855660911e2a8834a26c365050
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-27 05:05:34 +0000

    Add package uncertainty-engine-types/0.0.10/uncertainty_engine_types-0.0.10-py3-none-any.whl

---
+++ packages/uncertainty-engine-types/uncertainty_engine_types-0.0.10-py3-none-any.whl/uncertainty_engine_types/embeddings.py
@@ -0,0 +1,32 @@
+from enum import StrEnum
+from typing import Optional
+
+from pydantic import BaseModel, model_validator
+
+
+class TextEmbeddingsProvider(StrEnum):
+    OPENAI = "openai"
+    OLLAMA = "ollama"
+
+
+class TextEmbeddingsConfig(BaseModel):
+    """
+    Connection configuration for text embedding models.
+    """
+
+    provider: str
+    model: Optional[str] = None
+    ollama_url: Optional[str] = None
+    openai_api_key: Optional[str] = None
+
+    @model_validator(mode="before")
+    @classmethod
+    def check_provider(cls, values):
+        provider = values.get("provider")
+        if provider == TextEmbeddingsProvider.OLLAMA and not values.get("ollama_url"):
+            raise ValueError("ollama_url must be provided for 'ollama' provider.")
+        if provider == TextEmbeddingsProvider.OPENAI and not values.get(
+            "openai_api_key"
+        ):
+            raise ValueError("openai_api_key must be provided for 'openai' provider.")
+        return values
