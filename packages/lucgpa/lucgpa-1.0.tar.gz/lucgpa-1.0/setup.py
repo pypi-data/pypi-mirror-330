e74847d9eedd87b5f8e747748c01fd2a7d201de8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-27 05:04:11 +0000

    Add package lucgpa/1.0/lucgpa-1.0.tar.gz

---
+++ packages/lucgpa/lucgpa-1.0.tar.gz/lucgpa-1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="lucgpa",
+    version="1.0",
+    packages=find_packages(),
+    install_requires=[],
+    author="lu boss",
+    author_email="lu@boss.com",
+    description="A simple example Python package",
+    url="https://github.com/naimur-naiyimu/lucgpa.git",
+)
