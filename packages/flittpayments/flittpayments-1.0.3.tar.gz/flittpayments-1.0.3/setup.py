6bba1962a7639fae17a8ecfe440b5f0d473137dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-27 03:10:48 +0000

    Add package flittpayments/1.0.3/flittpayments-1.0.3.tar.gz

---
+++ packages/flittpayments/flittpayments-1.0.3.tar.gz/flittpayments-1.0.3/setup.py
@@ -0,0 +1,38 @@
+from setuptools import find_packages, setup
+
+from flittpayments.configuration import __version__
+
+desc = """
+    Flitt python sdk. 
+    Docs   - https://https://docs.flitt.com/
+    README - https://https://github.com/flittpayments/python/blob/master/README.md
+  """
+
+requires_list = [
+    'requests',
+    'six'
+]
+
+setup(
+    name='flittpayments',
+    version=__version__,
+    url='https://github.com/flittpayments/python/',
+    license='MIT',
+    description='Python SDK for Flitt clients.',
+    long_description=desc,
+    author='Dmitriy Miroshnikov',
+    packages=find_packages(where='.', exclude=('tests*',)),
+    install_requires=requires_list,
+    classifiers=[
+        'Environment :: Web Environment',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ])
