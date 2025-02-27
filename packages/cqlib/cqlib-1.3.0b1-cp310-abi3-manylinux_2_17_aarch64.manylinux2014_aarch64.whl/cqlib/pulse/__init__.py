31d2a5ca3e8de94087cf2e90b1af6e663e794edb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-27 16:08:15 +0000

    Add package cqlib/1.3.0b1/cqlib-1.3.0b1-cp310-abi3-manylinux_2_17_aarch64.manylinux2014_aarch64.whl

---
+++ packages/cqlib/cqlib-1.3.0b1-cp310-abi3-manylinux_2_17_aarch64.manylinux2014_aarch64.whl/cqlib/pulse/__init__.py
@@ -0,0 +1,34 @@
+# This code is part of cqlib.
+#
+# Copyright (C) 2025 China Telecom Quantum Group, QuantumCTek Co., Ltd.,
+# Center for Excellence in Quantum Information and Quantum Physics.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+"""
+Pulse module based on the pulse instruction in the QCIS instruction.
+"""
+
+from .coupler_qubit import CouplerQubit
+from .waveform import Waveform, WaveformType, CosineWaveform, \
+    FlattopWaveform, SlepianWaveform, NumericWaveform
+from .g import G
+from .pz import PZ
+from .pz0 import PZ0
+from .pxy import PXY
+from .pulse_circuit import PulseCircuit
+
+__all__ = [
+    'CouplerQubit',
+    'Waveform', 'WaveformType',
+    'CosineWaveform', 'FlattopWaveform',
+    'SlepianWaveform', 'NumericWaveform',
+    'G', 'PXY', 'PZ', 'PZ0',
+    'PulseCircuit',
+]
