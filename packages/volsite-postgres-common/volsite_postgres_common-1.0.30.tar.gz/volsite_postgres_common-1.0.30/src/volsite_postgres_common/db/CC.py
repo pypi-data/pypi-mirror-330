f01c7bacc40e602b4b2bbbebea942ba9943a4aa0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-27 16:09:10 +0000

    Add package volsite-postgres-common/1.0.30/volsite_postgres_common-1.0.30.tar.gz

---
+++ packages/volsite-postgres-common/volsite_postgres_common-1.0.30.tar.gz/volsite_postgres_common-1.0.30/src/volsite_postgres_common/db/CC.py
@@ -0,0 +1,55 @@
+from typing import Final
+
+
+# ====== CC: Common Table Column ======
+class CC:
+    book_chapter_version_combo_id: Final[str] = 'book_chapter_version_combo_id'
+    book_count: Final[str] = 'book_count'
+    built_time: Final[str] = 'built_time'
+
+    chapter: Final[str] = 'chapter'
+    chapter_version_id: Final[str] = 'chapter_version_id'
+    chapter_bone_id: Final[str] = 'chapter_bone_id'
+    chapter_bone_word_id: Final[str] = 'chapter_bone_word_id'
+    chapter_bone_words: Final[str] = 'chapter_bone_words'
+    chapter_count: Final[str] = 'chapter_count'
+    collected_user_count: Final[str] = 'collected_user_count'
+    count_update_time: Final[str] = 'count_update_time'
+    count_synchronize_version: Final[str] = 'count_synchronize_version'
+
+    description_id: Final[str] = 'description_id'
+
+    event: Final[str] = 'event'
+
+    finished_user_count: Final[str] = 'finished_user_count'
+    focused_user_count: Final[str] = 'focused_user_count'
+    focusing_user_count: Final[str] = 'focusing_user_count'
+    formatted_description: Final[str] = 'formatted_description'
+
+    head_version_id: Final[str] = 'head_version_id'
+
+    info: Final = 'info'
+    is_public: Final[str] = 'is_public'
+
+    last_used_time: Final[str] = 'last_used_time'
+    length: Final[str] = 'length'
+
+    name: Final[str] = 'name'
+    note: Final[str] = 'note'
+
+    plain_description: Final[str] = 'plain_description'
+
+    series: Final[str] = 'series'
+    state: Final[str] = 'state'
+    synchronize_version: Final[str] = 'synchronize_version'
+
+    text: Final[str] = 'text'
+    title_id: Final[str] = 'title_id'
+
+    update_time: Final[str] = 'update_time'
+    used_count: Final[str] = 'used_count'
+    user_id: Final[str] = 'user_id'
+    user_name: Final[str] = 'user_name'
+
+    word: Final[str] = 'word'
+    word_count: Final[str] = 'word_count'
