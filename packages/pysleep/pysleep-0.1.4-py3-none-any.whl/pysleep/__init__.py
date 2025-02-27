c9bc5bd414f42d0614e6539bf7339749edd628d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-27 16:08:24 +0000

    Add package pysleep/0.1.4/pysleep-0.1.4-py3-none-any.whl

---
+++ packages/pysleep/pysleep-0.1.4-py3-none-any.whl/pysleep/__init__.py
@@ -0,0 +1,16 @@
+"""
+pysleep
+=====
+
+Provides
+  1. Definition of the sleep in python ecosystem
+  2. Metrics used in medical field
+"""
+
+from .label import APNEA, DEEP, HYPOPNEA, LIGHT, NO_EVENT, REM, SNORE, WAKE
+from .stat import SleepStat, SleepStatDelta
+
+__version__ = "0.1.4"
+
+
+__all__ = ["APNEA", "DEEP", "HYPOPNEA", "LIGHT", "NO_EVENT", "REM", "SNORE", "WAKE", "SleepStat", "SleepStatDelta"]
