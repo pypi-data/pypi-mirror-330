2b81cabf20305f0cb41ccfce1b093907d14c261a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-27 07:04:57 +0000

    Add package ceos-ard-cli/0.1.2/ceos_ard_cli-0.1.2-py3-none-any.whl

---
+++ packages/ceos-ard-cli/ceos_ard_cli-0.1.2-py3-none-any.whl/ceos_ard_cli/generate.py
@@ -0,0 +1,65 @@
+import subprocess
+import weasyprint
+
+from pathlib import Path
+from .compile import compile
+
+def generate_all(out, self_contained = True, pdf = True):
+    # read all folders from the pfs folder
+    pfs_folder = Path("pfs")
+    errors = 0
+    for folder in pfs_folder.iterdir():
+        if folder.is_dir():
+            pfs = folder.stem
+            print(pfs)
+            try:
+                pfs_folder = Path(out) / pfs
+                generate(pfs, pfs_folder, self_contained, pdf)
+            except Exception as e:
+                print(f"Error generating {folder}: {e}")
+                errors += 1
+
+    return errors
+
+def generate(pfs, out, self_contained = True, pdf = True):
+    print("- Generating editable Markdown")
+    compile(pfs, out, True)
+
+    print("- Generating Word")
+    run_pandoc(out, "docx", self_contained)
+
+    print("- Generating read-only Markdown")
+    compile(pfs, out, False)
+
+    print("- Generating HTML")
+    run_pandoc(out, "html", self_contained)
+
+    if pdf:
+        print("- Generating PDF")
+        weasyprint.HTML(f"{out}.html").write_pdf(f"{out}.pdf")
+
+def run_pandoc(out, format, self_contained = True):
+    cmd = [
+        "pandoc",
+        f"{out}.md", # input file
+        "-s", # standalone
+        "-o", f"{out}.{format}", # output file
+        "-t", format, # output format
+        "-F", "pandoc-crossref", # enable cross-references, must be before -C: https://lierdakil.github.io/pandoc-crossref/#citeproc-and-pandoc-crossref
+        "-C", # enable citation processing
+        f"--bibliography={out}.bib", # bibliography file
+        "-L", "templates/no-sectionnumbers.lua", # remove section numbers from reference links
+        "-L", "templates/pagebreak.lua", # page breaks
+        f"--template=templates/template.{format}", # template
+    ]
+
+    if format == "html":
+        cmd.append("--mathml")
+        if self_contained:
+            cmd.append("--embed-resources=true")
+    elif format == "docx":
+        cmd.append("--reference-doc=templates/style.docx")
+    else:
+        raise ValueError(f"Unsupported format {format}")
+
+    subprocess.run(cmd)
