1f48d0858bcf588a9c0207e25c1258d214c1f6ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-27 06:04:31 +0000

    Add package teon/0.5.6/teon-0.5.6.tar.gz

---
+++ packages/teon/teon-0.5.6.tar.gz/teon-0.5.6/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='teon',
+    version='0.5.6',
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        'teon': [
+            'level_editor/*',
+            'level_editor/images/*',
+            'level_editor/fonts/*',
+            'extra/*',
+            'icon.png',
+        ],
+    },
+    install_requires=[
+        'pygame',
+        'pillow',
+    ],
+)
