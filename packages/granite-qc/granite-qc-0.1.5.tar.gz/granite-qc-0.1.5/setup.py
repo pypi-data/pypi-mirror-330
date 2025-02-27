0bbde72da6aab4c593996cf415710cb09a39d9cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-27 14:05:01 +0000

    Add package granite-qc/0.1.5/granite-qc-0.1.5.tar.gz

---
+++ packages/granite-qc/granite-qc-0.1.5.tar.gz/granite-qc-0.1.5/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="granite-qc",
+    version="0.1.5", 
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "torch",
+        "torch_geometric",
+        "networkx",
+        "numpy"
+    ],
+    package_data={"granite_qc": ["trained_model.pkl"]},  # Include the model file
+    description="A package for graph-based energy optimization using a trained model.",
+    author="Bao Tran",
+    author_email="tranq3@vcu.edu",
+    url="https://github.com/quocbao0603/granite",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
