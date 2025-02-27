db778e8e6550b67e7469446d3ca19178706ef289
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-27 14:05:53 +0000

    Add package nemoai/0.1.0/nemoai-0.1.0.tar.gz

---
+++ packages/nemoai/nemoai-0.1.0.tar.gz/nemoai-0.1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="nemoai",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        # 필요한 패키지들
+    ],
+)
