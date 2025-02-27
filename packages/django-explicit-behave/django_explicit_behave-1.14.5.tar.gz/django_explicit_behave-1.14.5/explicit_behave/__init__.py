d66657d5483c07b20c52e7c1f314efc16d2e5076
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-27 16:09:05 +0000

    Add package django-explicit-behave/1.14.5/django_explicit_behave-1.14.5.tar.gz

---
+++ packages/django-explicit-behave/django_explicit_behave-1.14.5.tar.gz/django_explicit_behave-1.14.5/explicit_behave/__init__.py
@@ -0,0 +1,1 @@
+__version__ = VERSION = (1, 14, 5)
---
+++ packages/django-explicit-behave/django_explicit_behave-1.14.5.tar.gz/django_explicit_behave-1.14.5/explicit_behave/_steps.py
@@ -0,0 +1,33 @@
+from behave import *
+from django.core import mail
+from freezegun import freeze_time
+
+
+@step('hoy es el "([^"]+)"')
+@step('today is "([^"]+)"')
+def freeeze(context, date):
+    freezer = freeze_time(date, ignore=['behave.runner'])
+    freezer.start()
+
+
+@step('veo que se han enviado emails con los siguientes parametros')
+@step('I see that emails have been sent with the following parameters')
+def step_mpl(context):
+    context.test.assertEqual(len(mail.outbox), len(context.table.rows))
+    for row in context.table.rows:
+        mail_send = False
+        for email in mail.outbox:
+            mail_found = True
+            for key, value in row.as_dict().items():
+                mail_found &= value == str(getattr(email, key))
+            mail_send |= mail_found
+        assert mail_send
+    # for row, email in zip(context.table.rows, mail.outbox):
+    #     for key, value in row.as_dict().items():
+    #         assert value == str(getattr(email, key)), f'{key} expected: {value} actual:{getattr(email, key)}'
+
+
+@step('veo que no se ha enviado email')
+@step('I see that no email has been sent')
+def step_mpl(context):
+    assert len(mail.outbox) == 0
