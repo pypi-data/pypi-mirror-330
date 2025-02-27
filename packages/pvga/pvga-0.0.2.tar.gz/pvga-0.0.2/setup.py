f164b6d659ca37d3cc6fc55c7e8bf904d745d288
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-27 15:05:42 +0000

    Add package pvga/0.0.2/pvga-0.0.2.tar.gz

---
+++ packages/pvga/pvga-0.0.2.tar.gz/pvga-0.0.2/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='pvga',
+    
+    version='0.0.2',
+    
+    description='PVGA is a powerful virus-focused assembler that does both assembly and polishing.',
+    author='zhisong',
+    author_email='songzhics@gmail.com',
+    url='https://github.com/SoSongzhi/PVGA',
+
+    package_dir={"":"src/pvga"},
+    packages={""},
+)
