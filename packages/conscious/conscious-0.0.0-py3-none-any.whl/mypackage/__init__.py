d4bcdd7b491672cb451371207e90923c2db7fba1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-27 17:04:33 +0000

    Add package conscious/0.0.0/conscious-0.0.0-py3-none-any.whl

---
+++ packages/conscious/conscious-0.0.0-py3-none-any.whl/mypackage/__init__.py
@@ -0,0 +1,5 @@
+def tbd():
+    return "This package name has been reserved for a new open-source library "+ \
+        "scheduled for public release in March by OpenConscious, Japan. "+ \
+        "If the source code is not disclosed by December 2025, "+ \
+        "we plan to relinquish this PyPI package name."
