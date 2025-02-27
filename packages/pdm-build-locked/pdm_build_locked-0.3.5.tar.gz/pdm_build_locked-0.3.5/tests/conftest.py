005bd0ece0640a795401eed8c8a01ffd5b8253b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-27 16:08:33 +0000

    Add package pdm-build-locked/0.3.5/pdm_build_locked-0.3.5.tar.gz

---
+++ packages/pdm-build-locked/pdm_build_locked-0.3.5.tar.gz/pdm_build_locked-0.3.5/tests/conftest.py
@@ -0,0 +1,80 @@
+"""conftest"""
+
+import shutil
+from pathlib import Path
+from typing import Generator
+
+import pytest
+
+from tests.utils import get_pyproject_hash
+
+pytest_plugins = ["pdm.pytest"]
+
+
+@pytest.fixture(name="repo_path")
+def fixture_repo_path(request: pytest.FixtureRequest) -> Path:
+    """Test fixture returning path to current git repository.
+
+    Args:
+        request: pytest FixtureRequest from the calling module
+
+    Returns:
+        Path of this git repository.
+    """
+    return request.config.rootpath
+
+
+@pytest.fixture(name="tests_base_path")
+def fixture_tests_base_path(repo_path: Path) -> Path:
+    """Get path to /tests
+
+    Args:
+        repo_path: path to git repo root
+
+    Returns:
+        Path to tests folder (calling module)
+    """
+    return repo_path.joinpath("tests")
+
+
+@pytest.fixture(name="temp_dir")
+def fixture_temp_dir(request: pytest.FixtureRequest, tests_base_path: Path) -> Path:
+    """Test fixture to access a temp directory for the current test method.
+
+    Args:
+        request: pytest-internal fixture to access the current test function.
+        tests_base_path: pytest fixture for path to tests directory
+
+    Returns:
+        Named directory for the test function where this fixture is used.
+    """
+    module_path = request.path.relative_to(tests_base_path)
+    test_name = request.function.__name__
+    temp_path = tests_base_path.joinpath("_temp", module_path, test_name)
+    shutil.rmtree(temp_path, ignore_errors=True)
+    temp_path.mkdir(exist_ok=True, parents=True)
+    return temp_path
+
+
+@pytest.fixture(name="data_base_path")
+def fixture_data_base_path(tests_base_path: Path) -> Path:
+    """Test fixture returning path to test/data directory.
+
+    Args:
+        tests_base_path: Path to tests directory
+
+    Returns:
+        Path to the 'tests/data' folder.
+    """
+    return tests_base_path.joinpath("data").resolve()
+
+
+@pytest.fixture
+def assert_pyproject_unmodified(data_base_path: Path, test_project: str) -> Generator[None, None, None]:
+    """
+    A pytest fixture to assert that pyproject.toml is reset properly to its original state
+    """
+    project = data_base_path / test_project
+    hash_before = get_pyproject_hash(project)
+    yield
+    assert get_pyproject_hash(project) == hash_before, "pyproject.toml hashes do not match, check for modifications"
---
+++ packages/pdm-build-locked/pdm_build_locked-0.3.5.tar.gz/pdm_build_locked-0.3.5/tests/unit/test_build_command.py
@@ -0,0 +1,189 @@
+"""test pdm build --locked"""
+
+from __future__ import annotations
+
+from pathlib import Path
+from typing import TYPE_CHECKING
+
+import pytest
+
+from tests.utils import count_group_dependencies, wheel_from_tempdir
+
+if TYPE_CHECKING:
+    from pdm.pytest import PDMCallable
+
+
+@pytest.mark.usefixtures("assert_pyproject_unmodified")
+@pytest.mark.parametrize("test_project", ["simple-optional"])
+def test_build_locked_optional(pdm: PDMCallable, data_base_path: Path, temp_dir: Path, test_project: str) -> None:
+    """this project has optional dependencies, is not yet locked and has no setting in pyproject.toml
+
+    Args:
+        pdm: PDM runner fixture
+        data_base_path: path to tests/data
+        temp_dir: path to tests/_temp/... temporary directory
+        test_project: path to test project
+    """
+    project_path = data_base_path.joinpath(test_project)
+    cmd = ["build", "--locked", "--project", project_path.as_posix(), "--dest", temp_dir.as_posix()]
+    result = pdm(cmd)
+    assert result.exit_code == 0
+    project_path.joinpath("pdm.lock").unlink()
+
+    wheel = wheel_from_tempdir(temp_dir)
+    assert count_group_dependencies(wheel, "locked") == 5
+    assert count_group_dependencies(wheel, "socks-locked") == 1
+    assert count_group_dependencies(wheel, "test-locked") == 0  # dev-dependency test group should be skipped
+
+
+@pytest.mark.usefixtures("assert_pyproject_unmodified")
+@pytest.mark.parametrize("test_project", ["simple"])
+def test_build_locked(pdm: PDMCallable, data_base_path: Path, temp_dir: Path, test_project: str) -> None:
+    """this project has no optional dependencies, is not yet locked and has no setting in pyproject.toml
+
+    Args:
+        pdm: PDM runner fixture
+        data_base_path: path to tests/data
+        temp_dir: path to tests/_temp/... temporary directory
+        test_project: path to test project
+    """
+    project_path = data_base_path.joinpath(test_project)
+    cmd = ["build", "--locked", "--project", project_path.as_posix(), "--dest", temp_dir.as_posix()]
+    result = pdm(cmd)
+    assert result.exit_code == 0
+    project_path.joinpath("pdm.lock").unlink()
+
+    wheel = wheel_from_tempdir(temp_dir)
+    assert sum(1 for dist in wheel.requires_dist if "locked" in dist) == 5
+
+
+@pytest.mark.usefixtures("assert_pyproject_unmodified")
+@pytest.mark.parametrize("test_project", ["simple"])
+def test_build_locked_env(
+    pdm: PDMCallable, data_base_path: Path, temp_dir: Path, test_project: str, monkeypatch: pytest.MonkeyPatch
+) -> None:
+    """Build a project with PDM_BUILD_LOCKED=1 in the environment
+
+    Args:
+        pdm: PDM runner fixture
+        data_base_path: path to tests/data
+        temp_dir: path to tests/_temp/... temporary directory
+        test_project: path to test project
+    """
+    monkeypatch.setenv("PDM_BUILD_LOCKED", "1")
+    project_path = data_base_path.joinpath(test_project)
+    cmd = ["build", "--project", project_path.as_posix(), "--dest", temp_dir.as_posix()]
+    result = pdm(cmd)
+    assert result.exit_code == 0
+    project_path.joinpath("pdm.lock").unlink()
+
+    wheel = wheel_from_tempdir(temp_dir)
+    assert sum(1 for dist in wheel.requires_dist if "locked" in dist) == 5
+
+
+@pytest.mark.usefixtures("assert_pyproject_unmodified")
+@pytest.mark.parametrize("test_project", ["simple-optional"])
+def test_build_normal(pdm: PDMCallable, data_base_path: Path, temp_dir: Path, test_project: str) -> None:
+    """test that not using --locked doesn't add locked dependencies
+
+    Args:
+        pdm: PDM runner fixture
+        data_base_path: path to tests/data
+        temp_dir: path to tests/_temp/... temporary directory
+        test_project: path to test project
+    """
+    project_path = data_base_path.joinpath(test_project)
+    cmd = ["build", "--project", project_path.as_posix(), "--dest", temp_dir.as_posix()]
+    result = pdm(cmd)
+    assert result
+
+    wheel = wheel_from_tempdir(temp_dir)
+    assert not any("locked" in dist for dist in wheel.requires_dist)
+
+
+@pytest.mark.usefixtures("assert_pyproject_unmodified")
+@pytest.mark.parametrize("test_project", ["large"])
+def test_build_locked_pyproject(pdm: PDMCallable, data_base_path: Path, temp_dir: Path, test_project: str) -> None:
+    """this project has a lockfile and the pyproject.toml setting tool.pdm.build.locked
+
+    Args:
+        pdm: PDM runner fixture
+        data_base_path: path to tests/data
+        temp_dir: path to tests/_temp/... temporary directory
+        test_project: path to test project
+    """
+    project_path = data_base_path.joinpath(test_project).as_posix()
+    cmd = ["build", "--project", project_path, "--dest", temp_dir.as_posix()]
+    result = pdm(cmd)
+    assert result.exit_code == 0
+
+    wheel = wheel_from_tempdir(temp_dir)
+    assert count_group_dependencies(wheel, "locked") == 24
+    assert count_group_dependencies(wheel, "extras-locked") == 1
+    assert count_group_dependencies(wheel, "cow-locked") == 1
+
+
+@pytest.mark.usefixtures("assert_pyproject_unmodified")
+@pytest.mark.parametrize("test_project", ["large-selected"])
+def test_build_locked_pyproject_with_selected_groups(
+    pdm: PDMCallable, data_base_path: Path, temp_dir: Path, test_project: str
+) -> None:
+    """this project has a lockfile and the pyproject.toml setting tool.pdm.build.locked"""
+    project_path = data_base_path.joinpath(test_project).as_posix()
+    cmd = ["build", "--project", project_path, "--dest", temp_dir.as_posix()]
+    result = pdm(cmd)
+    assert result.exit_code == 0
+
+    wheel = wheel_from_tempdir(temp_dir)
+    assert count_group_dependencies(wheel, "locked") == 26
+    assert count_group_dependencies(wheel, "extras-locked") == 0
+    assert count_group_dependencies(wheel, "cow-locked") == 0
+
+
+@pytest.mark.usefixtures("assert_pyproject_unmodified")
+@pytest.mark.parametrize("test_project", ["invalid"])
+def test_build_locked_invalid(pdm: PDMCallable, data_base_path: Path, temp_dir: Path, test_project: str) -> None:
+    """this project's lockfile has a group containing "-locked" and will thus error
+
+    Args:
+        pdm: PDM runner fixture
+        data_base_path: path to tests/data
+        temp_dir: path to tests/_temp/... temporary directory
+        test_project: path to test project
+    """
+    project_path_invalid = data_base_path.joinpath(test_project)
+    cmd = [
+        "build",
+        "--locked",
+        "--project",
+        project_path_invalid.as_posix(),
+        "--dest",
+        temp_dir.as_posix(),
+    ]
+    result = pdm(cmd)
+    assert "PdmException" in result.stderr
+
+
+@pytest.mark.usefixtures("assert_pyproject_unmodified")
+@pytest.mark.parametrize("test_project", ["empty"])
+def test_build_locked_empty(pdm: PDMCallable, data_base_path: Path, temp_dir: Path, test_project: str) -> None:
+    """this project's lockfile has empty dependencies and dynamic optional-dependencies
+    doesn't make sense to use the plugin in this case - but it should work nevertheless
+
+    Args:
+        pdm: PDM runner fixture
+        data_base_path: path to tests/data
+        temp_dir: path to tests/_temp/... temporary directory
+        test_project: path to test project
+    """
+    project_path = data_base_path.joinpath(test_project)
+    cmd = [
+        "build",
+        "--locked",
+        "--project",
+        project_path.as_posix(),
+        "--dest",
+        temp_dir.as_posix(),
+    ]
+    result = pdm(cmd)
+    assert result.exit_code == 0
