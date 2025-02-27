51a585834d5d142b949eeff0b0d3482453f37857
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-27 05:04:06 +0000

    Add package leaguepedia-parser-thomasbarrepitous/0.0.1/leaguepedia_parser_thomasbarrepitous-0.0.1-py3-none-any.whl

---
+++ packages/leaguepedia-parser-thomasbarrepitous/leaguepedia_parser_thomasbarrepitous-0.0.1-py3-none-any.whl/leaguepedia_parser_thomasbarrepitous/__init__.py
@@ -0,0 +1,16 @@
+from leaguepedia_parser.parsers.game_parser import (
+    get_regions,
+    get_tournaments,
+    get_games,
+    get_game_details,
+)
+from leaguepedia_parser.parsers.team_parser import (
+    get_active_players,
+    get_team_logo,
+    get_long_team_name_from_trigram,
+    get_team_thumbnail,
+    get_all_team_assets
+)
+from leaguepedia_parser.parsers.player_parser import (
+    get_player_by_name,
+)
