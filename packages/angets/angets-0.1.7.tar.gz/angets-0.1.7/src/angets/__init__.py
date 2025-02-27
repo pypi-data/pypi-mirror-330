2c9152c7af9707cf1317f6dde4494019c4af96a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-27 04:04:19 +0000

    Add package angets/0.1.7/angets-0.1.7.tar.gz

---
+++ packages/angets/angets-0.1.7.tar.gz/angets-0.1.7/src/angets/__init__.py
@@ -0,0 +1,13 @@
+"""Angets (Ankha's Gets): Functions for user input."""
+import importlib.metadata
+
+__version__: str = importlib.metadata.version('angets')
+
+from angets._core import (
+    get_non_empty_str, get_constrained_number, get_float, get_constrained_float, get_positive_float,
+    get_non_negative_float, get_int, get_constrained_int, get_positive_int, get_non_negative_int, get_confirmation,
+    get_date
+)
+import angets._decorators as decorators
+import angets._helpers as helpers
+import angets._exceptions as exceptions
