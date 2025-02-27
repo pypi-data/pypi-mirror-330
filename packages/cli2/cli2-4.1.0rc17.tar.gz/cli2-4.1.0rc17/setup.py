9ad9f32b018be6c99b08e28be080f78b0ee5cc4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-27 03:10:57 +0000

    Add package cli2/4.1.0rc17/cli2-4.1.0rc17.tar.gz

---
+++ packages/cli2/cli2-4.1.0rc17.tar.gz/cli2-4.1.0rc17/setup.py
@@ -0,0 +1,44 @@
+from setuptools import setup
+
+
+setup(
+    name='cli2',
+    version='4.1.0rc17',
+    setup_requires='setupmeta',
+    install_requires=[
+        'docstring_parser',
+        'pyyaml',
+        'pygments',
+        'structlog',
+    ],
+    extras_require=dict(
+        client=[
+            'httpx',
+            'truststore',
+        ],
+        test=[
+            'freezegun',
+            'pytest',
+            'pytest-cov',
+            'pytest-mock',
+            'pytest-asyncio',
+            'pytest-httpx',
+        ],
+    ),
+    author='James Pic',
+    author_email='jamespic@gmail.com',
+    url='https://yourlabs.io/oss/cli2',
+    include_package_data=True,
+    license='MIT',
+    keywords='cli',
+    python_requires='>=3.6',
+    entry_points={
+        'console_scripts': [
+            'cli2 = cli2.cli2:main.entry_point',
+            'cli2-example = cli2.examples.obj:cli.entry_point',
+            'cli2-example2 = cli2.examples.obj2:cli.entry_point',
+            'cli2-example-nesting = cli2.examples.nesting:cli.entry_point',
+            'cli2-example-client = cli2.examples.client:cli.entry_point',
+        ],
+    },
+)
