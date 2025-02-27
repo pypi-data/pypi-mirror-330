ebbcc8e41eae8aec6a3f522c26d8bfa1910a6067
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-27 07:05:00 +0000

    Add package libertai-unstoppable-denver-agents/0.0.5/libertai_unstoppable_denver_agents-0.0.5.tar.gz

---
+++ packages/libertai-unstoppable-denver-agents/libertai_unstoppable_denver_agents-0.0.5.tar.gz/libertai_unstoppable_denver_agents-0.0.5/libertai_unstoppable_denver_agents/interfaces.py
@@ -0,0 +1,34 @@
+from typing import Literal, TypedDict
+
+from coinbase_agentkit import ActionProvider
+from libertai_agents.interfaces.llamacpp import CustomizableLlamaCppParams
+from libertai_agents.interfaces.tools import Tool
+from libertai_agents.models import Model
+from pydantic import BaseModel
+
+
+class ChatAgentArgs(TypedDict, total=False):
+    model: Model
+    system_prompt: str | None
+    tools: list[Tool] | None
+    llamacpp_params: CustomizableLlamaCppParams | None
+    expose_api: bool | None
+
+
+class AutonomousAgentConfig(BaseModel):
+    agentkit_additional_action_providers: list[ActionProvider] = []
+    compute_think_interval: int = 1
+    compute_think_unit: Literal["seconds", "minutes", "hours"] = "hours"
+    debug_run_once: bool = False
+    computing_credits_system_prompt: str = """
+    You are an autonomous AI Agent running on the Aleph decentralized cloud.
+    You have a Base wallet on which you have ALEPH tokens that are consumed to pay for your computing.
+    When you are called every hour, your task is to use the AlephConvertionProvider_get_aleph_info tool to check how much ALEPH do you have currently, how much is consumed per hour and how much ETH you have.
+    Once you have this information, decide if you want to buy some ALEPH with your if or not. If you want to buy, call AlephConvertionProvider_get_aleph_cloud_tokens.
+
+    You must make a decision and not only check the information.
+    If you decide to buy, you have to call the AlephConvertionProvider_get_aleph_cloud_tokens tool, don't respond that you will buy, just do it.
+    In all the cases, after making your decision and potentially buying ALEPH, respond by explaining your reasoning process."""
+
+    class Config:
+        arbitrary_types_allowed = True
