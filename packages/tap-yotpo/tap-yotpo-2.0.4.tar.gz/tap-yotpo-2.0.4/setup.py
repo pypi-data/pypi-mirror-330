e299d1e18284240520b6e99ebb5bfbc0ab820cbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-27 06:04:16 +0000

    Add package tap-yotpo/2.0.4/tap-yotpo-2.0.4.tar.gz

---
+++ packages/tap-yotpo/tap-yotpo-2.0.4.tar.gz/tap-yotpo-2.0.4/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+from setuptools import find_packages, setup
+
+setup(
+    name="tap-yotpo",
+    version="2.0.4",
+    description="Singer.io tap for extracting data from the Yotpo API",
+    author="Stitch",
+    url="https://singer.io",
+    classifiers=["Programming Language :: Python :: 3 :: Only"],
+    py_modules=["tap_yotpo"],
+    install_requires=[
+        "singer-python==5.13.0",
+        "requests==2.32.3",
+    ],
+    extras_require={
+        "dev": [
+            "pylint",
+            "ipdb",
+        ]
+    },
+    entry_points="""
+    [console_scripts]
+    tap-yotpo=tap_yotpo:main
+    """,
+    packages=find_packages(exclude=["tests"]),
+    package_data={"schemas": ["tap_yotpo/schemas/*.json"]},
+    include_package_data=True,
+)
