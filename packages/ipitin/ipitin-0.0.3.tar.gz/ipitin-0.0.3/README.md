d4934ef407b7408fa982c155c95039217d20e931
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-27 16:08:21 +0000

    Add package ipitin/0.0.3/ipitin-0.0.3.tar.gz

---
+++ packages/ipitin/ipitin-0.0.3.tar.gz/ipitin-0.0.3/README.md
@@ -0,0 +1,23 @@
+# ipitin
+Import Pandas Import Tenserflow Import Numpy
+
+# menual
+
+## [how to use?](./IPITIN/README.md)
+
+1. `pip install ipitin`
+2. `from ipitin.ipitin import *`
+
+## ways to see menual
+
+import ipitin.menual
+
+# what is this?
+
+code that
+
+```python
+import pandas as pd
+import tensorflow as tf
+import numpy as np
+```
---
+++ packages/ipitin/ipitin-0.0.3.tar.gz/ipitin-0.0.3/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ipitin',
+    version='0.0.3',
+    description='Import Pandas Import Tensorflow Import Numpy',
+    author='du7ec',
+    author_email='du7ec6834@gmail.com',
+    url='https://github.com/FarAway6834/ipitin',
+    install_requires=['pandas', 'tensorflow', 'numpy'],
+    packages=find_packages(exclude=[]),
+    keywords=['import pandas', 'import fensorflow', 'import numpy', 'Import Pandas Import Tenserflow Import Numpy', 'ipitin', 'FarAway6834'],
+    python_requires='>=3.8',
+    package_data={},
+    zip_safe=False,
+    classifiers=[
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12'
+    ]
+)
