768e34b3f149c768d572420233cd1d93965cb0c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-27 16:09:02 +0000

    Add package captcha-solver-selenium/0.1.9/captcha_solver_selenium-0.1.9.tar.gz

---
+++ packages/captcha-solver-selenium/captcha_solver_selenium-0.1.9.tar.gz/captcha_solver_selenium-0.1.9/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="captcha_solver_selenium", 
+    version="0.1.9",
+    author="DENO",
+    author_email="Vinhytb3010@gmail.com",
+    description="A package for solving Google reCAPTCHA using Selenium and Speech Recognition",
+    long_description=open("README.md", "r", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    packages=["captcha_solver"],
+    install_requires=[
+        "selenium>=4.9.1",
+        "selenium-wire>=5.0.0",
+        "requests>=2.31.0",
+        "SpeechRecognition",
+        "pyaudio"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.10',
+)
