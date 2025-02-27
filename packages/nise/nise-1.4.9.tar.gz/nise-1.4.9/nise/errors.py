e7e657484eaba75f1d5dfabb014ad26d50060a23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-27 08:04:21 +0000

    Add package nise/1.4.9/nise-1.4.9.tar.gz

---
+++ packages/nise/nise-1.4.9.tar.gz/nise-1.4.9/nise/errors.py
@@ -0,0 +1,103 @@
+# -*- coding: utf-8 -*-
+# This file was generated
+
+
+import platform
+import warnings
+
+
+def _is_success(code):
+    return (code == 0)
+
+
+def _is_error(code):
+    return (code < 0)
+
+
+def _is_warning(code):
+    return (code > 0)
+
+
+class Error(Exception):
+    '''Base error class for NI Switch Executive'''
+
+    def __init__(self, message):
+        super(Error, self).__init__(message)
+
+
+class DriverError(Error):
+    '''An error originating from the NI Switch Executive driver'''
+
+    def __init__(self, code, description):
+        assert _is_error(code), "Should not raise Error if code is not fatal."
+        self.code = code
+        self.description = description
+        super(DriverError, self).__init__(str(self.code) + ": " + self.description)
+
+
+class DriverWarning(Warning):
+    '''A warning originating from the NI Switch Executive driver'''
+
+    def __init__(self, code, description):
+        assert _is_warning(code), "Should not create Warning if code is not positive."
+        super(DriverWarning, self).__init__('Warning {} occurred.\n\n{}'.format(code, description))
+
+
+class UnsupportedConfigurationError(Error):
+    '''An error due to using this module in an usupported platform.'''
+
+    def __init__(self):
+        super(UnsupportedConfigurationError, self).__init__('System configuration is unsupported: ' + platform.architecture()[0] + ' ' + platform.system())
+
+
+class DriverNotInstalledError(Error):
+    '''An error due to using this module without the driver runtime installed.'''
+
+    def __init__(self):
+        super(DriverNotInstalledError, self).__init__('The NI Switch Executive runtime could not be loaded. Make sure it is installed and its bitness matches that of your Python interpreter. Please visit http://www.ni.com/downloads/drivers/ to download and install it.')
+
+
+class DriverTooOldError(Error):
+    '''An error due to using this module with an older version of the NI Switch Executive driver runtime.'''
+
+    def __init__(self):
+        super(DriverTooOldError, self).__init__('A function was not found in the NI Switch Executive runtime. Please visit http://www.ni.com/downloads/drivers/ to download a newer version and install it.')
+
+
+class DriverTooNewError(Error):
+    '''An error due to the NI Switch Executive driver runtime being too new for this module.'''
+
+    def __init__(self):
+        super(DriverTooNewError, self).__init__('The NI Switch Executive runtime returned an unexpected value. This can occur if it is too new for the nise Python module. Upgrade the nise Python module.')
+
+
+class InvalidRepeatedCapabilityError(Error):
+    '''An error due to an invalid character in a repeated capability'''
+
+    def __init__(self, invalid_character, invalid_string):
+        super(InvalidRepeatedCapabilityError, self).__init__('An invalid character ({}) was found in repeated capability string ({})'.format(invalid_character, invalid_string))
+
+
+def handle_error(library_interpreter, code, ignore_warnings, is_error_handling):
+    '''handle_error
+
+    Helper function for handling errors returned by nise.Library.
+    It calls back into the LibraryInterpreter to get the corresponding error
+    description and raises if necessary.
+    '''
+
+    if _is_success(code) or (_is_warning(code) and ignore_warnings):
+        return
+
+    if is_error_handling:
+        # The caller is in the midst of error handling and an error occurred.
+        # Don't try to get the description or we'll start recursing until the stack overflows.
+        description = ''
+    else:
+        description = library_interpreter.get_error_description(code)
+
+    if _is_error(code):
+        raise DriverError(code, description)
+
+    assert _is_warning(code)
+    warnings.warn(DriverWarning(code, description))
---
+++ packages/nise/nise-1.4.9.tar.gz/nise-1.4.9/setup.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python
+# This file was generated
+
+
+from setuptools import setup
+
+
+pypi_name = 'nise'
+
+
+def read_contents(file_to_read):
+    with open(file_to_read, 'r') as f:
+        return f.read()
+
+
+setup(
+    name=pypi_name,
+    zip_safe=True,
+    version='1.4.9',
+    description='NI Switch Executive Python API',
+    long_description=read_contents('README.rst'),
+    long_description_content_type='text/x-rst',
+    author='NI',
+    author_email="opensource@ni.com",
+    url="https://github.com/ni/nimi-python",
+    maintainer="NI",
+    maintainer_email="opensource@ni.com",
+    keywords=['nise'],
+    license='MIT',
+    include_package_data=True,
+    packages=['nise'],
+    python_requires='>=3.9',
+    install_requires=[
+        'hightime>=0.2.0',
+    ],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Manufacturing",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: POSIX",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Topic :: System :: Hardware :: Hardware Drivers"
+    ],
+    package_data={pypi_name: ['VERSION']},
+)
