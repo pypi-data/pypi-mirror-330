60d44e3ef984d823a1f8e1523f022b5ddddc4f6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-27 16:08:15 +0000

    Add package rclone-api/1.0.89/rclone_api-1.0.89.tar.gz

---
+++ packages/rclone-api/rclone_api-1.0.89.tar.gz/rclone_api-1.0.89/src/rclone_api/types.py
@@ -0,0 +1,35 @@
+from dataclasses import dataclass
+from enum import Enum
+
+
+class ModTimeStrategy(Enum):
+    USE_SERVER_MODTIME = "use-server-modtime"
+    NO_MODTIME = "no-modtime"
+
+
+class ListingOption(Enum):
+    DIRS_ONLY = "dirs-only"
+    FILES_ONLY = "files-only"
+    ALL = "all"
+
+
+class Order(Enum):
+    NORMAL = "normal"
+    REVERSE = "reverse"
+    RANDOM = "random"
+
+
+@dataclass
+class S3PathInfo:
+    remote: str
+    bucket: str
+    key: str
+
+
+@dataclass
+class SizeResult:
+    """Size result dataclass."""
+
+    prefix: str
+    total_size: int
+    file_sizes: dict[str, int]
