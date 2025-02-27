60761fd62ad41d449328acd9fe15fef2b2aae8a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-27 17:04:32 +0000

    Add package EwanSonicConnector/0.1.24/ewansonicconnector-0.1.24.tar.gz

---
+++ packages/EwanSonicConnector/ewansonicconnector-0.1.24.tar.gz/ewansonicconnector-0.1.24/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='EwanSonicConnector',
+    version='0.1.24',
+    packages=find_packages(),
+    install_requires=[
+        'redis>=4.5.4',
+        'requests>=2.31.0',
+        'airtest>=1.3.5',
+        'cos-python-sdk-v5>=1.9.33'
+    ],
+    author='fulage',
+    author_email='fulage@ewan.cn',
+    description='给ewan内部使用的一个连接云真机sonic的连接器',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ],
+    python_requires='>=3.6',
+)
