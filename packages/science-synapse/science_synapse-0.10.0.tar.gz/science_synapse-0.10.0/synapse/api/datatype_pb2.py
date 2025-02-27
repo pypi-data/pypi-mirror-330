ba1acffb45e9dc4ef695252dc6b3848994a671e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-27 07:04:55 +0000

    Add package science-synapse/0.10.0/science_synapse-0.10.0.tar.gz

---
+++ packages/science-synapse/science_synapse-0.10.0.tar.gz/science_synapse-0.10.0/synapse/api/datatype_pb2.py
@@ -0,0 +1,14 @@
+"""Generated protocol buffer code."""
+from google.protobuf import descriptor as _descriptor
+from google.protobuf import descriptor_pool as _descriptor_pool
+from google.protobuf import symbol_database as _symbol_database
+from google.protobuf.internal import builder as _builder
+_sym_db = _symbol_database.Default()
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x12api/datatype.proto\x12\x07synapse*h\n\x08DataType\x12\x14\n\x10kDataTypeUnknown\x10\x00\x12\x08\n\x04kAny\x10\x01\x12\x0e\n\nkBroadband\x10\x02\x12\x0f\n\x0bkSpiketrain\x10\x03\x12\x0f\n\x0bkTimestamps\x10\x04\x12\n\n\x06kImage\x10\x05b\x06proto3')
+_globals = globals()
+_builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, _globals)
+_builder.BuildTopDescriptorsAndMessages(DESCRIPTOR, 'api.datatype_pb2', _globals)
+if _descriptor._USE_C_DESCRIPTORS == False:
+    DESCRIPTOR._options = None
+    _globals['_DATATYPE']._serialized_start = 31
+    _globals['_DATATYPE']._serialized_end = 135
