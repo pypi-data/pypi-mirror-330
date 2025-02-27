5a6529ce7f4ba1d92d8e54cb001495fe50bfb85c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-27 05:04:11 +0000

    Add package python-copilot-docs-hook/0.1.10/python_copilot_docs_hook-0.1.10.tar.gz

---
+++ packages/python-copilot-docs-hook/python_copilot_docs_hook-0.1.10.tar.gz/python_copilot_docs_hook-0.1.10/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='python_copilot_docs_hook',
+    version='0.1.10',
+    description='A pre-commit hook to check Python documentation using GitHub Copilot',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Itai Ganot',
+    author_email='lel@lel.bz',
+    url='https://github.com/geek-kb/python_copilot_docs_hook',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'python-copilot-docs=python_copilot_docs_hook.main:main',
+        ],
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
