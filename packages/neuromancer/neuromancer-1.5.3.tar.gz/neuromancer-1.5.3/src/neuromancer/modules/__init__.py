b35deaaa1da856422f6914fc5440d3ab39322d5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-27 07:04:40 +0000

    Add package neuromancer/1.5.3/neuromancer-1.5.3.tar.gz

---
+++ packages/neuromancer/neuromancer-1.5.3.tar.gz/neuromancer-1.5.3/src/neuromancer/modules/__init__.py
@@ -0,0 +1,5 @@
+from neuromancer.modules import activations
+from neuromancer.modules import blocks
+from neuromancer.modules import functions
+from neuromancer.modules import solvers
+from neuromancer.modules import lopo
