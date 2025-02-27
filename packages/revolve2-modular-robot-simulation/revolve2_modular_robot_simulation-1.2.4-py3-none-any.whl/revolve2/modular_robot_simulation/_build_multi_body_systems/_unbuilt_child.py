0555db265a2300ba06e3f8b12cd6ac4f3d3c54b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-27 05:04:10 +0000

    Add package revolve2-modular-robot-simulation/1.2.4/revolve2_modular_robot_simulation-1.2.4-py3-none-any.whl

---
+++ packages/revolve2-modular-robot-simulation/revolve2_modular_robot_simulation-1.2.4-py3-none-any.whl/revolve2/modular_robot_simulation/_build_multi_body_systems/_unbuilt_child.py
@@ -0,0 +1,30 @@
+from dataclasses import dataclass, field
+
+from pyrr import Quaternion, Vector3
+
+from revolve2.modular_robot.body import Module
+from revolve2.modular_robot.body.sensors import Sensor
+from revolve2.simulation.scene import Pose, RigidBody
+
+
+@dataclass
+class UnbuiltChild:
+    """A dataclass to store unbuilt children for the builders."""
+
+    child_object: Module | Sensor
+    rigid_body: RigidBody
+    pose: Pose = field(init=False)
+
+    def make_pose(
+        self, position: Vector3, orientation: Quaternion = Quaternion()
+    ) -> None:
+        """
+        Make the pose of the unbuilt child.
+
+        :param position: The position argument from the parent.
+        :param orientation: The orientation of the attachment on the parent.
+        """
+        self.pose = Pose(
+            position,
+            orientation * self.child_object.orientation,
+        )
