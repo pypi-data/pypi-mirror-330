9154dde87f0eb32472ed3097faf6595ea1677faa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-27 14:05:39 +0000

    Add package bpp-iplweb/202502.1159/bpp_iplweb-202502.1159-py3-none-any.whl

---
+++ packages/bpp-iplweb/bpp_iplweb-202502.1159-py3-none-any.whl/pbn_api/exceptions.py
@@ -0,0 +1,110 @@
+import json
+
+
+class AlreadyEnqueuedError(Exception):
+    pass
+
+
+class CharakterFormalnyNieobslugiwanyError(Exception):
+    pass
+
+
+class TlumaczDyscyplinException(ValueError):
+    pass
+
+
+class BrakZdefiniowanegoObiektuUczelniaWSystemieError(Exception):
+    pass
+
+
+class PraceSerwisoweException(Exception):
+    def __str__(self):
+        return "Po stronie PBN trwają prace serwisowe. Prosimy spróbować później. "
+
+
+class HttpException(Exception):
+    def __init__(self, status_code, url, content):
+        self.status_code = status_code
+        self.url = url
+        self.content = content
+        try:
+            self.json = json.loads(content[:4096])
+        except (json.JSONDecodeError, ValueError, TypeError):
+            self.json = None
+
+
+class ResourceLockedException(HttpException):
+    pass
+
+
+class AccessDeniedException(Exception):
+    def __init__(self, url, content):
+        self.url = url
+        self.content = content
+
+
+class BrakIDPracyPoStroniePBN(HttpException):
+    pass
+
+
+class SciencistDoesNotExist(Exception):
+    pass
+
+
+class AuthenticationConfigurationError(Exception):
+    pass
+
+
+class AuthenticationResponseError(Exception):
+    pass
+
+
+class IntegracjaWylaczonaException(Exception):
+    pass
+
+
+class SameDataUploadedRecently(Exception):
+    pass
+
+
+class WillNotExportError(Exception):
+    pass
+
+
+class DOIorWWWMissing(WillNotExportError):
+    pass
+
+
+class LanguageMissingPBNUID(WillNotExportError):
+    pass
+
+
+class StatementsMissing(WillNotExportError):
+    pass
+
+
+class PKZeroExportDisabled(WillNotExportError):
+    pass
+
+
+class CharakterFormalnyMissingPBNUID(WillNotExportError):
+    pass
+
+
+class StatementDeletionError(Exception):
+    def __init__(self, status_code, url, content):
+        self.status_code = status_code
+        self.url = url
+        self.content = content
+
+
+class NeedsPBNAuthorisationException(HttpException):
+    pass
+
+
+class NoFeeDataException(ValueError):
+    pass
+
+
+class NoPBNUIDException(ValueError):
+    pass
