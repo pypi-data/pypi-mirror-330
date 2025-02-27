85993db4c04413f27954eee0c5469a64323e004d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-27 17:04:34 +0000

    Add package rlgym-learn/0.1.5/rlgym_learn-0.1.5-cp311-cp311-manylinux_2_17_s390x.manylinux2014_s390x.whl

---
+++ packages/rlgym-learn/rlgym_learn-0.1.5-cp311-cp311-manylinux_2_17_s390x.manylinux2014_s390x.whl/rlgym_learn/api/typing.py
@@ -0,0 +1,6 @@
+from typing import TypeVar
+
+AgentControllerConfig = TypeVar("AgentControllerConfig")
+AgentControllerData = TypeVar("AgentControllerData")
+StateMetrics = TypeVar("StateMetrics")
+ActionAssociatedLearningData = TypeVar("ActionAssociatedLearningData")
