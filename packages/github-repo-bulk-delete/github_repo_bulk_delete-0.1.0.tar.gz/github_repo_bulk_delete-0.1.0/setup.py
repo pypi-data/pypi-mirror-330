7038b0a7e273df355872fcb3227a9b7b881db3d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-27 04:03:40 +0000

    Add package github-repo-bulk-delete/0.1.0/github_repo_bulk_delete-0.1.0.tar.gz

---
+++ packages/github-repo-bulk-delete/github_repo_bulk_delete-0.1.0.tar.gz/github_repo_bulk_delete-0.1.0/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="github-repo-bulk-delete",
+    version="0.1.0",
+    author="conficiusa",
+    author_email="your.email@example.com",
+    description="A CLI tool to interactively select and delete multiple GitHub repositories",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/conficiusa/github-repo-bulk-delete",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=[
+        "requests",
+        "blessed",
+    ],
+    entry_points={
+        "console_scripts": [
+            "github-repo-delete=github_repo_bulk_deleter:cli_main",
+        ],
+    },
+)
