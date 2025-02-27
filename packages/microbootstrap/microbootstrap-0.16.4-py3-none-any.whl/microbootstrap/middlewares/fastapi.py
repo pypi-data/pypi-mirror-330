7d1f73ba154085a70814c53a883ce1e2921276ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-27 03:10:47 +0000

    Add package microbootstrap/0.16.4/microbootstrap-0.16.4-py3-none-any.whl

---
+++ packages/microbootstrap/microbootstrap-0.16.4-py3-none-any.whl/microbootstrap/middlewares/fastapi.py
@@ -0,0 +1,39 @@
+import time
+import typing
+
+import fastapi
+from fastapi import status
+from starlette.middleware.base import BaseHTTPMiddleware, RequestResponseEndpoint
+
+from microbootstrap.instruments.logging_instrument import fill_log_message
+
+
+def build_fastapi_logging_middleware(
+    exclude_endpoints: typing.Iterable[str],
+) -> type[BaseHTTPMiddleware]:
+    class FastAPILoggingMiddleware(BaseHTTPMiddleware):
+        async def dispatch(
+            self,
+            request: fastapi.Request,
+            call_next: RequestResponseEndpoint,
+        ) -> fastapi.Response:
+            request_path: typing.Final = request.url.path.removesuffix("/")
+            should_log: typing.Final = not any(
+                exclude_endpoint == request_path for exclude_endpoint in exclude_endpoints
+            )
+            start_time: typing.Final = time.perf_counter_ns()
+            try:
+                response = await call_next(request)
+            except Exception:  # noqa: BLE001
+                response = fastapi.Response(status_code=500)
+
+            if should_log:
+                fill_log_message(
+                    "exception" if response.status_code == status.HTTP_500_INTERNAL_SERVER_ERROR else "info",
+                    request,
+                    response.status_code,
+                    start_time,
+                )
+            return response
+
+    return FastAPILoggingMiddleware
