72937ab309f4753fb1419a06ca71dc7621b5af65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-27 05:04:13 +0000

    Add package digitalkin-proto/0.0.1/digitalkin_proto-0.0.1-py3-none-any.whl

---
+++ packages/digitalkin-proto/digitalkin_proto-0.0.1-py3-none-any.whl/digitalkin_proto/__init__.py
@@ -0,0 +1,12 @@
+"""digitalkin_proto - Python Generated gRPC client and server interfaces.
+
+This package contains generated Python code from Protocol Buffers and gRPC definitions
+for interacting with Digitalkin services.
+"""
+
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version("digitalkin_proto")
+except PackageNotFoundError:
+    __version__ = "0.0.1"
