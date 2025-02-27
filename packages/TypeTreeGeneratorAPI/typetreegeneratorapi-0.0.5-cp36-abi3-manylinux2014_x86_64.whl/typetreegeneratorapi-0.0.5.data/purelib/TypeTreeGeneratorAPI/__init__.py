cecba0430593989dc5e9e7d050783b488bb409bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-27 14:04:53 +0000

    Add package TypeTreeGeneratorAPI/0.0.5/typetreegeneratorapi-0.0.5-cp36-abi3-manylinux2014_x86_64.whl

---
+++ packages/TypeTreeGeneratorAPI/typetreegeneratorapi-0.0.5-cp36-abi3-manylinux2014_x86_64.whl/typetreegeneratorapi-0.0.5.data/purelib/TypeTreeGeneratorAPI/__init__.py
@@ -0,0 +1,7 @@
+from .TypeTreeGenerator import (
+    TypeTreeGenerator as TypeTreeGenerator,
+    TypeTreeNode as TypeTreeNode,
+    TypeTreeNodeNative as TypeTreeNodeNative,
+)
+
+version = __version__ = "0.0.5"
