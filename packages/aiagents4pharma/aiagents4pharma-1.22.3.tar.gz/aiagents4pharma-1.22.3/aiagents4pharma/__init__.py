98f09ed0613042accd7c5d274070d261cce04741
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-27 06:04:40 +0000

    Add package aiagents4pharma/1.22.3/aiagents4pharma-1.22.3.tar.gz

---
+++ packages/aiagents4pharma/aiagents4pharma-1.22.3.tar.gz/aiagents4pharma-1.22.3/aiagents4pharma/__init__.py
@@ -0,0 +1,10 @@
+"""
+This file is used to import aiagents4pharma modules.
+"""
+
+from . import (
+    talk2biomodels,
+    talk2cells,
+    talk2scholars,
+    talk2knowledgegraphs,
+)
---
+++ packages/aiagents4pharma/aiagents4pharma-1.22.3.tar.gz/aiagents4pharma-1.22.3/aiagents4pharma/talk2scholars/agents/__init__.py
@@ -0,0 +1,6 @@
+'''
+This file is used to import all the modules in the package.
+'''
+
+from . import main_agent
+from . import s2_agent
