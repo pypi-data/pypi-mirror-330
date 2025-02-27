581610522902f3574768deeb842b41b62d0d980c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-27 06:04:28 +0000

    Add package pathsim/0.5.3/pathsim-0.5.3.tar.gz

---
+++ packages/pathsim/pathsim-0.5.3.tar.gz/pathsim-0.5.3/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="pathsim",
+    version="0.5.3",
+    author="Milan Rother",
+    author_email="milan.rother@gmx.de",
+    description="A differentiable block based hybrid system simulation framework.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/milanofthe/pathsim",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=[
+        "numpy",
+        "matplotlib",
+        "scipy",
+    ],
+)
