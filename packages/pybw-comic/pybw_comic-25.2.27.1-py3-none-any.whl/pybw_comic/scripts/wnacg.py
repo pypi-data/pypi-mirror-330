7b37ea628e5331f0fc5461bb6d24cc0ea8248fdd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-27 15:06:41 +0000

    Add package pybw-comic/25.2.27.1/pybw_comic-25.2.27.1-py3-none-any.whl

---
+++ packages/pybw-comic/pybw_comic-25.2.27.1-py3-none-any.whl/pybw_comic/scripts/wnacg.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+"""
+author: Bowei Pu at 2025.02.26
+version: 2025.02.27
+
+Download comic for wnacg.com website.
+"""
+
+import os, time
+from pathlib import Path
+from tqdm import tqdm
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from pybw_comic.engines.copymanga import ImageDownloader
+
+
+def parse_url(url):
+    """
+    """
+    if 'index' in url:
+        url_index = url
+        url_slide = url.replace('index', 'slide')
+    elif 'slide' in url:
+        url_index = url.replace('slide', 'index')
+        url_slide = url
+    return url_index, url_slide
+
+
+def download_one_mange(url):
+    """
+    """
+    ## ------ Prepare ------
+    url_index, url_slide = parse_url(url)
+    
+    d = webdriver.Edge()
+    
+    d.get(url_index)
+
+    # dire = d.title
+    # dire = dire.replace(' - 紳士漫畫-專註分享漢化本子|邪惡漫畫', '')
+    
+    x_title = '/html/body/div[4]/h2'
+    find = d.find_element(By.XPATH, x_title)
+    
+    dire = '{} {}'.format(time.strftime('%y%m%d_%H%M'), find.text)
+
+    print('\nTitle: {}\n'.format(dire))
+    os.makedirs(dire)
+    
+    
+    d.get(url_slide)
+    time.sleep(1)
+
+    x_links = '/html/body/div[7]/div/img'
+    finds = d.find_elements(By.XPATH, x_links)
+    links = [i.get_property('src') for i in finds]
+
+    d.close()
+    
+    ## ------ Program Begin ------
+    for link in tqdm(links):
+        name = Path(link).name
+        ImageDownloader(link).download(r'{}/{}'.format(dire, name))
+    return
+
+
+def main():
+    while True:
+        print('\n{}'.format('-' * 50))
+        url = input('\nInput url:\n>> ')
+        download_one_mange(url)
+
+
+if __name__ == '__main__':
+    ## ------ user settings ------
+    
+    
+    ## ------ Program Begin ------
+    main()
+
+
+
