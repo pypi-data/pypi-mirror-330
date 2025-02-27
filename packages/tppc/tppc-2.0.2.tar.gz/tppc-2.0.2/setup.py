b083c550f97c1d9fd7da1d6688eddafe8ef796f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-27 16:08:31 +0000

    Add package tppc/2.0.2/tppc-2.0.2.tar.gz

---
+++ packages/tppc/tppc-2.0.2.tar.gz/tppc-2.0.2/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="tppc",
+    version="2.0.2",
+    author="Sean Tichenor",
+    author_email="sean.tichenor@olivia-tgdk.com",
+    description="TPPC Interpreter for T++ Language",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/seantichenor/tppc",
+    license="MIT",
+    packages=find_packages(),
+    python_requires=">=3.6",
+    entry_points={
+        "console_scripts": [
+            "tppc=tppc.tppc:main"
+        ],
+    },
+)
