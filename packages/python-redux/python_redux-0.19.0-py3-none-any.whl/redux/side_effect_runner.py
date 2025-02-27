32b18fa139f9f7f3fb9ffe180e57148ab8cea65f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-27 14:05:31 +0000

    Add package python-redux/0.19.0/python_redux-0.19.0-py3-none-any.whl

---
+++ packages/python-redux/python_redux-0.19.0-py3-none-any.whl/redux/side_effect_runner.py
@@ -0,0 +1,62 @@
+"""Redux store for managing state and side effects."""
+
+from __future__ import annotations
+
+import asyncio
+import contextlib
+import threading
+import weakref
+from asyncio import Handle, iscoroutine
+from collections.abc import Callable
+from inspect import signature
+from typing import TYPE_CHECKING, Any, Generic, cast
+
+from redux.basic_types import Event, EventHandler
+
+if TYPE_CHECKING:
+    import queue
+
+
+class SideEffectRunnerThread(threading.Thread, Generic[Event]):
+    """Thread for running side effects."""
+
+    def __init__(
+        self: SideEffectRunnerThread,
+        *,
+        task_queue: queue.Queue[tuple[EventHandler[Event], Event] | None],
+    ) -> None:
+        """Initialize the side effect runner thread."""
+        super().__init__()
+        self.task_queue = task_queue
+        self.loop = asyncio.get_event_loop()
+        self._handles: set[Handle] = set()
+        self.create_task = lambda coro: self._handles.add(
+            self.loop.call_soon_threadsafe(self.loop.create_task, coro),
+        )
+
+    def run(self: SideEffectRunnerThread[Event]) -> None:
+        """Run the side effect runner thread."""
+        while True:
+            task = self.task_queue.get()
+            if task is None:
+                self.task_queue.task_done()
+                break
+            try:
+                event_handler_, event = task
+                if isinstance(event_handler_, weakref.ref):
+                    event_handler = event_handler_()
+                    if event_handler is None:
+                        continue
+                else:
+                    event_handler = event_handler_
+                parameters = 1
+                with contextlib.suppress(Exception):
+                    parameters = len(signature(event_handler).parameters)
+                if parameters == 1:
+                    result = cast(Callable[[Event], Any], event_handler)(event)
+                else:
+                    result = cast(Callable[[], Any], event_handler)()
+                if iscoroutine(result):
+                    self.create_task(result)
+            finally:
+                self.task_queue.task_done()
