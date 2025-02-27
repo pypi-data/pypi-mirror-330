84a72570f8ee1e1b43c790e4aeb798c0684b7de5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-27 05:04:16 +0000

    Add package carconnectivity/0.4a5/carconnectivity-0.4a5-py3-none-any.whl

---
+++ packages/carconnectivity/carconnectivity-0.4a5-py3-none-any.whl/carconnectivity/position.py
@@ -0,0 +1,31 @@
+"""Module for position classes."""
+from __future__ import annotations
+from typing import TYPE_CHECKING
+
+from enum import Enum
+
+from carconnectivity.objects import GenericObject
+from carconnectivity.attributes import EnumAttribute, FloatAttribute
+
+if TYPE_CHECKING:
+    from typing import Optional
+
+
+class Position(GenericObject):  # pylint: disable=too-many-instance-attributes
+    """
+    A class to represent a position.
+    """
+    def __init__(self, parent: Optional[GenericObject] = None) -> None:
+        super().__init__(object_id='position', parent=parent)
+        self.position_type: EnumAttribute = EnumAttribute("position_type", self, tags={'carconnectivity'})
+        self.latitude: FloatAttribute = FloatAttribute("latitude", self, tags={'carconnectivity'})
+        self.longitude: FloatAttribute = FloatAttribute("longitude", self, tags={'carconnectivity'})
+
+    class PositionType(Enum):
+        """
+        Enum for position type.
+        """
+        PARKING = 'parking'
+        DRIVING = 'driving'
+        INVALID = 'invalid'
+        UNKNOWN = 'unknown'
