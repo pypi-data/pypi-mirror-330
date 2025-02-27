ea86cf348684261e6016ec3e5d09d04df6d1ff98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-27 04:04:04 +0000

    Add package nemoai-package/0.1.0/nemoai_package-0.1.0.tar.gz

---
+++ packages/nemoai-package/nemoai_package-0.1.0.tar.gz/nemoai_package-0.1.0/nemoai_package/sample/math.py
@@ -0,0 +1,13 @@
+def add(a: str, b: str):
+    """
+    입력받은 두 정수를 더하는 함수입니다.
+
+    :param a: 첫 번째 정수
+    :param b: 두 번째 정수
+    :return: 두 정수와 두 정수의 합을 반환합니다.
+    :rtype: int
+    """
+    a_int = int(a)
+    b_int = int(b)
+    result = a_int + b_int
+    return result
