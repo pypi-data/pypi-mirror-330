2283e2e93151ae1b3f6769dde1fbdd0b2fb6d68b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-27 03:11:29 +0000

    Add package mats/0.74.5/mats-0.74.5-py3-none-any.whl

---
+++ packages/mats/mats-0.74.5-py3-none-any.whl/mats/__init__.py
@@ -0,0 +1,28 @@
+"""
+Manufacturing Automated Test System (MATS)
+
+MATS takes care of all of the low-level tedium that every
+manufacturing automated test must implement, such as:
+
+ - test runner
+ - test definition
+ - auto saving of data
+ - hardware setup / teardown
+ - automatic GUI integration
+"""
+
+import coloredlogs
+import logging
+
+from mats.archiving import ArchiveManager
+
+from mats.test import Test
+from mats.test_sequence import TestSequence
+from mats.tkwidgets import MatsFrame
+from mats.version import __version__
+
+__all__ = ["Test", "TestSequence", "ArchiveManager", "MatsFrame", "__version__"]
+
+coloredlogs.install(level="DEBUG")
+
+logger = logging.getLogger(__name__)
