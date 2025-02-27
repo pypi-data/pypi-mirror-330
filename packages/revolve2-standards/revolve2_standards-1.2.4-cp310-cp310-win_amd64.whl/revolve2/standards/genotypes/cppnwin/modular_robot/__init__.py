b38d899e9892a477a4779f18558202de998f0b6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-27 05:04:10 +0000

    Add package revolve2-standards/1.2.4/revolve2_standards-1.2.4-cp310-cp310-win_amd64.whl

---
+++ packages/revolve2-standards/revolve2_standards-1.2.4-cp310-cp310-win_amd64.whl/revolve2/standards/genotypes/cppnwin/modular_robot/__init__.py
@@ -0,0 +1,11 @@
+"""CPPNWIN genotypes for modular robots."""
+
+from ._brain_cpg_network_neighbor import BrainCpgNetworkNeighbor
+from ._brain_genotype_cpg import BrainGenotypeCpg
+from ._brain_genotype_cpg_orm import BrainGenotypeCpgOrm
+
+__all__ = [
+    "BrainCpgNetworkNeighbor",
+    "BrainGenotypeCpg",
+    "BrainGenotypeCpgOrm",
+]
