edbacfc178eacdebdbaebf713ccf8bfa17b6d342
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-27 03:10:26 +0000

    Add package stookwijzer/1.6.0/stookwijzer-1.6.0.tar.gz

---
+++ packages/stookwijzer/stookwijzer-1.6.0.tar.gz/stookwijzer-1.6.0/setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+setup(
+  name = 'stookwijzer',
+  packages = ['stookwijzer'],
+  version = '1.6.0',
+  license='MIT',
+  description = 'Stookwijzer package',
+  long_description_content_type="text/markdown",
+  long_description='Stookwijzer package',
+  author = 'fwestenberg',
+  author_email = '',
+  url = 'https://github.com/fwestenberg/stookwijzer',
+  download_url = 'https://github.com/fwestenberg/stookwijzer/releases/latest',
+  keywords = ['Stookwijzer', 'Home-Assistant'],
+  install_requires=[
+          'aiohttp', 'pytz'
+      ],
+  classifiers=[
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.12',
+    'Programming Language :: Python :: 3.13'
+  ],
+)
