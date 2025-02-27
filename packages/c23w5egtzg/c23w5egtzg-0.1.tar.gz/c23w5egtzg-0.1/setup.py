fe587a1613ea20931298db621c6166b57abdc58f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-27 17:04:44 +0000

    Add package c23w5egtzg/0.1/c23w5egtzg-0.1.tar.gz

---
+++ packages/c23w5egtzg/c23w5egtzg-0.1.tar.gz/c23w5egtzg-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='c23w5egtzg',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['c23w5egtzg'],
+      author='sergey.winston',
+      author_email='sergey.winston@bk.ru',
+      zip_safe=False)
