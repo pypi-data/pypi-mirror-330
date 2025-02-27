b0277096ca8b88d8dfb6ecb44701b27791633f32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-27 14:05:01 +0000

    Add package FunctionGUI/0.5.0/FunctionGUI-0.5.0-py3-none-any.whl

---
+++ packages/FunctionGUI/FunctionGUI-0.5.0-py3-none-any.whl/FunctionGUI/__init__.py
@@ -0,0 +1,19 @@
+# __init__.py
+
+from .FunctionGUI import (
+    Window,
+    Title,
+    Label,
+    Place,
+    Font,
+    BGImage,
+    Button,
+    Entry,
+    GetEntry,
+    Run,
+    BulleanVar,
+    ChexBox,
+    add,
+    Design
+
+)
