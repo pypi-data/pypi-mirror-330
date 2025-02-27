b6e4a455b98c5ff2c979ac7e2b7af6c125c93319
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-27 17:04:55 +0000

    Add package PistolMagazine/0.8.3/PistolMagazine-0.8.3-py3-none-any.whl

---
+++ packages/PistolMagazine/PistolMagazine-0.8.3-py3-none-any.whl/pistol_magazine/data_exporter/json_ex.py
@@ -0,0 +1,19 @@
+import json
+from .exporter import Exporter
+
+
+# class JSONExporter(Exporter):
+#     def export(self, data, filename):
+#         with open(filename, 'a') as output_file:
+#             output_file.write(json.dumps(data))
+
+
+class JSONExporter(Exporter):
+    def export(self, data, filename):
+        if isinstance(data, str):
+            try:
+                data = json.loads(data)
+            except json.JSONDecodeError:
+                raise ValueError("The provided string is not a valid JSON format")
+        with open(filename, 'w') as output_file:
+            json.dump(data, output_file, indent=4)
