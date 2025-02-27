f2a9fc726586290c5cd57ea8311d24f39e4045c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-27 16:08:53 +0000

    Add package dexvstuff/0.0.3/dexvstuff-0.0.3-py3-none-any.whl

---
+++ packages/dexvstuff/dexvstuff-0.0.3-py3-none-any.whl/dexvstuff/jsdomruntime.py
@@ -0,0 +1,21 @@
+from javascript import require
+import json
+
+class JsdomRuntime:
+    def __init__(self) -> None:
+        self.jsdom = require('jsdom')
+        self.vm = require("vm").Script
+        self.runtime = self.jsdom.JSDOM(
+            "<title>jsdom</title>", {
+                "runScripts": "dangerously"
+            }
+        ).getInternalVMContext()
+        
+    def eval(self, data: str, promise: bool = False, byte_array: bool = False) -> str:
+        script = (
+            f"{data}.then(r => JSON.stringify(Array.from(r)))" if byte_array and promise else
+            f"JSON.stringify(Array.from(({data})))" if byte_array else
+            f"{data}.then(r => JSON.stringify(r))" if promise else data
+        )
+        result = self.vm(script).runInContext(self.runtime)
+        return json.loads(result) if byte_array else result
