719f7b7de3c1e25479b5b5deb1479f41322502f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-28 15:05:48 +0000

    Add package pybaselib/0.3.4/pybaselib-0.3.4.tar.gz

---
+++ packages/pybaselib/pybaselib-0.3.4.tar.gz/pybaselib-0.3.4/pybaselib/utils/dataObject/string_type.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# @Author: maoyongfan
+# @email: maoyongfan@163.com
+# @Date: 2025/2/19 19:33
+
+def string_to_bytes_length(string: str) -> int:
+    """
+    字符串对应字节长度
+    :param string:
+    :return:
+    """
+    return len(string.encode('utf-8'))
+
+
+if __name__ == "__main__":
+    print(string_to_bytes_length("FFFFFF0700010000C0A8017A"))
