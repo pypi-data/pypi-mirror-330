b677e78e7b94499770a3be6c07b7d4a4e115d7aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-28 04:05:04 +0000

    Add package linflex/0.2.2/linflex-0.2.2.tar.gz

---
+++ packages/linflex/linflex-0.2.2.tar.gz/linflex-0.2.2/tests/test_example.py
@@ -0,0 +1,15 @@
+from linflex import Vec2
+
+
+def test_example():
+    a = Vec2(3, 4)
+    b = Vec2(2, -1)
+
+    assert a + b == Vec2(5, 3)
+    assert a - b == Vec2(1, 5)
+    assert a.length() == 5
+    assert -Vec2(2, -3) == Vec2(-2, 3)
+
+    c = Vec2(1, 1)
+    c += Vec2(0, 1)
+    assert c == Vec2(1, 2)
