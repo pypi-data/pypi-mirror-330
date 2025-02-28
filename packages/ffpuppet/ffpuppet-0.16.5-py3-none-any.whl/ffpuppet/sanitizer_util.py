6de1d2e25f3dd000109b5f4475c455b49f2e57c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-28 07:05:04 +0000

    Add package ffpuppet/0.16.5/ffpuppet-0.16.5-py3-none-any.whl

---
+++ packages/ffpuppet/ffpuppet-0.16.5-py3-none-any.whl/ffpuppet/sanitizer_util.py
@@ -0,0 +1,126 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+"""ffpuppet sanitizer utilities"""
+from __future__ import annotations
+
+from logging import getLogger
+from os.path import exists
+from re import compile as re_compile
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from collections.abc import Generator
+
+LOG = getLogger(__name__)
+
+__author__ = "Tyson Smith"
+__all__ = ("SanitizerOptions",)
+
+
+class SanitizerOptions:  # pylint: disable=missing-docstring
+    re_delim = re_compile(r":(?![\\|/])")
+
+    __slots__ = ("_options",)
+
+    def __init__(self, options: str | None = None) -> None:
+        self._options: dict[str, str] = {}
+        if options is not None:
+            self.load_options(options)
+
+    def __contains__(self, item: str) -> bool:
+        return item in self._options
+
+    def __iter__(self) -> Generator[tuple[str, str]]:
+        yield from self._options.items()
+
+    def __len__(self) -> int:
+        return len(self._options)
+
+    def __str__(self) -> str:
+        return ":".join("=".join(kv) for kv in self)
+
+    def add(self, flag: str, value: str, overwrite: bool = False) -> None:
+        """Add sanitizer flag.
+
+        Args:
+            flag: Flags to set.
+            value: Value to use.
+            overwrite: Overwrite existing value.
+
+        Returns:
+            None
+        """
+        assert flag
+        if ":" in value or " " in value:
+            assert self.is_quoted(value), f"{value} ({flag}) must be quoted"
+        if flag not in self._options or overwrite:
+            self._options[flag] = value
+
+    def check_path(self, flag: str) -> bool:
+        """Check path exists on disk.
+        Only indicate failure if flag exists and path does not.
+
+        Args:
+            flag: Flags to set.
+
+        Returns:
+            False if the flag exists and the path does not otherwise False
+        """
+        if flag in self._options:
+            value = self._options[flag]
+            if self.is_quoted(value):
+                value = value[1:-1]
+            return exists(value)
+        return True
+
+    def get(self, flag: str) -> str | None:
+        """Get sanitizer flag.
+
+        Args:
+            flag: Flags to retrieve.
+
+        Returns:
+            Value of given flag or None
+        """
+        return self._options.get(flag, None)
+
+    @staticmethod
+    def is_quoted(token: str) -> bool:
+        """Check if token is quoted.
+
+        Args:
+            token: Value to check.
+
+        Returns:
+            True if token is quoted otherwise False.
+        """
+        return len(token) > 1 and token[0] == token[-1] and token[0] in ('"', "'")
+
+    def load_options(self, options: str | None) -> None:
+        """Load flags from *SAN_OPTIONS in env.
+
+        Args:
+            options: Colon separated list of `flag=value` pairs.
+
+        Returns:
+            None
+        """
+        self._options.clear()
+        if options:
+            for option in self.re_delim.split(options):
+                try:
+                    self.add(*option.split("=", maxsplit=1))
+                except TypeError:  # noqa: PERF203
+                    LOG.warning("Malformed option %r", option)
+
+    def pop(self, flag: str) -> str | None:
+        """Pop sanitizer flag.
+
+        Args:
+            flag: Flags to retrieve.
+
+        Returns:
+            Value of given flag or None
+        """
+        return self._options.pop(flag, None)
---
+++ packages/ffpuppet/ffpuppet-0.16.5-py3-none-any.whl/ffpuppet/test_sanitizer_util.py
@@ -0,0 +1,93 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
+"""ffpuppet sanitizer_util tests"""
+
+from pytest import mark
+
+from .sanitizer_util import SanitizerOptions
+
+
+@mark.parametrize(
+    "init, add, result, overwrite",
+    [
+        # do nothing
+        ("", {}, [""], False),
+        # add single option
+        ("", {"a": "1"}, ["a=1"], False),
+        # add multiple options
+        ("", {"b": "2", "a": "1"}, ["a=1", "b=2"], False),
+        # existing
+        ("a=1", {}, ["a=1"], False),
+        # add to existing
+        ("a=1", {"b": "2"}, ["a=1", "b=2"], False),
+        # no overwrite existing
+        ("a=1", {"a": "2"}, ["a=1"], False),
+        # overwrite existing
+        ("a=1", {"a": "2"}, ["a=2"], True),
+        # parse quoted
+        (
+            "a='C:\\test\\':b=\"/dev/null\"",
+            {},
+            ["a='C:\\test\\'", 'b="/dev/null"'],
+            False,
+        ),
+    ],
+)
+def test_sanitizer_options_01(init, add, result, overwrite):
+    """test SanitizerOptions() - parsing and adding"""
+    opts = SanitizerOptions(init)
+    for key, value in add.items():
+        opts.add(key, value, overwrite=overwrite)
+    # test __str__
+    split_opts = SanitizerOptions.re_delim.split(str(opts))
+    assert len(split_opts) == len(result)
+    if opts:
+        # test __len___
+        assert len(opts) == len(result)
+        for opt in split_opts:
+            assert opt in result
+        # test __iter__
+        for opt, value in opts:
+            assert f"{opt}={value}" in result
+        # test __contains___
+        for opt in result:
+            assert opt.split("=")[0] in opts
+    else:
+        assert not result[-1]
+
+
+def test_sanitizer_options_02():
+    """test SanitizerOptions() - get() and pop()"""
+    opts = SanitizerOptions()
+    assert opts.get("missing") is None
+    assert opts.pop("missing") is None
+    opts.add("exists", "1")
+    assert opts.pop("exists") == "1"
+    assert opts.get("exists") is None
+
+
+def test_sanitizer_options_03(tmp_path):
+    """test SanitizerOptions() - check_path()"""
+    opts = SanitizerOptions()
+    # test missing key
+    assert opts.check_path("file")
+    # test exists
+    file = tmp_path / "file.bin"
+    file.touch()
+    opts.add("file", f"'{file}'")
+    assert opts.check_path("file")
+    # test missing file
+    file.unlink()
+    assert not opts.check_path("file")
+
+
+def test_sanitizer_options_04():
+    """test SanitizerOptions.is_quoted()"""
+    assert SanitizerOptions.is_quoted("'quoted'")
+    assert SanitizerOptions.is_quoted('"quoted"')
+    assert not SanitizerOptions.is_quoted("not'quoted")
+    assert not SanitizerOptions.is_quoted("'not'quoted")
+    assert not SanitizerOptions.is_quoted("not'quoted'")
+    assert not SanitizerOptions.is_quoted("'test\"")
+    assert not SanitizerOptions.is_quoted("'")
