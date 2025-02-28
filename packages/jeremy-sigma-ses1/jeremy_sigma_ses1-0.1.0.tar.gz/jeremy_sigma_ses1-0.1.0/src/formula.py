d86317bff9a716091b3a6f59004689f40ac9c89d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-28 15:05:57 +0000

    Add package jeremy-sigma-ses1/0.1.0/jeremy_sigma_ses1-0.1.0.tar.gz

---
+++ packages/jeremy-sigma-ses1/jeremy_sigma_ses1-0.1.0.tar.gz/jeremy_sigma_ses1-0.1.0/src/formula.py
@@ -0,0 +1,15 @@
+class formula:
+    def add(a,b):
+        return a+b
+    def subtract(a,b):
+        return a-b
+    def divide(a,b):
+        return a/b
+    def multi(a,b):
+        return a*b
+    def mod(a,b):
+        return a%b
+    def power(a,b):
+        return a**b   
+    def halo():
+        return "halo"
