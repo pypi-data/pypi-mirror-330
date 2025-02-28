7e3ab1028516c1f0e08449c2aa99f19a519b47fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-28 03:11:37 +0000

    Add package brightway25/1.1.0/brightway25-1.1.0-py3-none-any.whl

---
+++ packages/brightway25/brightway25-1.1.0-py3-none-any.whl/brightway25/__init__.py
@@ -0,0 +1,9 @@
+__all__ = (
+    "__version__",
+)
+
+__version__ = "1.1.0"
+
+import warnings
+
+warnings.warn("The Brightway25 package doesn't provide anything; import the constituent libraries separately")
