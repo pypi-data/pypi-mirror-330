22fd92a5b8f39e197af4b9c45a627856bef1eaa1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-28 07:05:19 +0000

    Add package python3-clamav/1.0.0/python3_clamav-1.0.0.tar.gz

---
+++ packages/python3-clamav/python3_clamav-1.0.0.tar.gz/python3_clamav-1.0.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="python3-clamav",
+    version="1.0.0",
+    author="Gianluigi Tiesi",
+    author_email="sherpya@netfarm.it",
+    maintainer="STAN-TAB CORP. LTD",
+    maintainer_email="support@stantabcorp.com",
+    license="BSD",
+    keywords="python3, clamav, antivirus, scanner, virus, libclamav",
+    py_modules=["clamav"],
+)
