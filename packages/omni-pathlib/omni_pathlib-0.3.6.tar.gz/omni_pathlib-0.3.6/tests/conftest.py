b03c8c21fe67093640f02900c432b96f9f653146
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-28 03:11:49 +0000

    Add package omni-pathlib/0.3.6/omni_pathlib-0.3.6.tar.gz

---
+++ packages/omni-pathlib/omni_pathlib-0.3.6.tar.gz/omni_pathlib-0.3.6/tests/conftest.py
@@ -0,0 +1,8 @@
+import asyncio
+import sys
+
+if sys.platform == 'win32':
+    from asyncio import WindowsSelectorEventLoopPolicy
+    asyncio.set_event_loop_policy(WindowsSelectorEventLoopPolicy())
+else:
+    asyncio.set_event_loop_policy(asyncio.DefaultEventLoopPolicy())  # Fallback for other platforms
