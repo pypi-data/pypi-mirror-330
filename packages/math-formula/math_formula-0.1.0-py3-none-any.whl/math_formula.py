b71d4d65843a1f1f44f696bfbb8288812c952f0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-28 15:05:53 +0000

    Add package math-formula/0.1.0/math_formula-0.1.0-py3-none-any.whl

---
+++ packages/math-formula/math_formula-0.1.0-py3-none-any.whl/math_formula.py
@@ -0,0 +1,18 @@
+import math as mt
+pi = mt.pi
+
+class MathFormula:
+    def plus(a, b):
+        return a+b
+
+    def subtract(a,b):
+        return a-b
+    
+    def multiply(a,b):
+        return a*b
+    
+    def divide(a,b):
+        return a/b
+
+    def cirlceArea(r):    
+        return pi * r * r
