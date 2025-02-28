44969e268ec631ab96666ab74496b979c3182a92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-28 16:05:46 +0000

    Add package microhhpy/0.0.2/microhhpy-0.0.2.tar.gz

---
+++ packages/microhhpy/microhhpy-0.0.2.tar.gz/microhhpy-0.0.2/microhhpy/__init__.py
@@ -0,0 +1,9 @@
+# Expose some main function as `microhhpy.some_function()` instead of having to use `microhhpy.subdir.some_function()`.
+#from .main.initial_fields import create_initial_fields
+
+# Expose sub-directories as `import microhhpy; microhhpy.subdir.some_function()`
+# NOTE: this only exposes what is defined in the subdirectory `__init__.py`.
+from .spatial import *
+from .thermo import *
+from .interpolate import *
+from .emission import *
