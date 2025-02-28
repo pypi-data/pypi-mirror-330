67b59ea3789d7834e1568c925181367def4ca534
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-28 03:11:25 +0000

    Add package matteridge/0.2.0/matteridge-0.2.0-py3-none-any.whl

---
+++ packages/matteridge/matteridge-0.2.0-py3-none-any.whl/matteridge/__init__.py
@@ -0,0 +1,7 @@
+from slidge.util.util import get_version  # noqa: F401
+
+from . import config, contact, gateway, group, session
+
+__all__ = ("config", "contact", "gateway", "group", "session")
+
+__version__ = "v0.2.0"
