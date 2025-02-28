7b1b591690936c44a8227c946de4a7d53fbab068
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-28 15:05:27 +0000

    Add package llm-context/0.2.13/llm_context-0.2.13-py3-none-any.whl

---
+++ packages/llm-context/llm_context-0.2.13-py3-none-any.whl/llm_context/highlighter/language_mapping.py
@@ -0,0 +1,51 @@
+from dataclasses import dataclass
+from importlib import resources
+from typing import Optional
+
+
+def to_language(filename: str) -> Optional[str]:
+    ext_to_lang = {
+        "c": "c",
+        "cc": "cpp",
+        "cs": "csharp",
+        "cpp": "cpp",
+        "el": "elisp",
+        "ex": "elixir",
+        "elm": "elm",
+        "go": "go",
+        "java": "java",
+        "js": "javascript",
+        "mjs": "javascript",
+        "php": "php",
+        "py": "python",
+        "rb": "ruby",
+        "rs": "rust",
+        "ts": "typescript",
+    }
+    extension = filename.split(".")[-1]
+    return ext_to_lang.get(extension)
+
+
+def to_query_file_name(lang: str) -> Optional[str]:
+    return f"tree-sitter-{lang}-tags.scm" if lang else None
+
+
+@dataclass(frozen=True)
+class TagQuery:
+    def get_query(self, language: str) -> str:
+        if language == "typescript":
+            return self._read_query("javascript") + self._read_query("typescript")
+        return self._read_query(language)
+
+    def _read_query(self, language: str) -> str:
+        query_file_name = to_query_file_name(language)
+        if not query_file_name:
+            raise ValueError(f"Unsupported language: {language}")
+        try:
+            return (
+                resources.files("llm_context.highlighter.tag-qry")
+                .joinpath(query_file_name)
+                .read_text()
+            )
+        except FileNotFoundError:
+            raise ValueError(f"Query file not found for language: {language}")
