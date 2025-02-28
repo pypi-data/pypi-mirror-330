3cf46ac5391ee924a3608e734cc173a42494a7d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-28 14:06:06 +0000

    Add package observability-testing-tool/1.0.0/observability_testing_tool-1.0.0-py3-none-any.whl

---
+++ packages/observability-testing-tool/observability_testing_tool-1.0.0-py3-none-any.whl/observability_testing_tool/main.py
@@ -0,0 +1,19 @@
+from observability_testing_tool.config.common import info_log
+
+from observability_testing_tool.config.executor import prepare, run_logging_jobs, create_metrics_descriptors, run_monitoring_jobs
+
+def main():
+    info_log(">>> Obs Test Tool - Getting things going...")
+    prepare()
+    info_log(">>> Obs Test Tool - Done with preparation. Now proceeding with logging tasks...")
+    p1 = run_logging_jobs()
+    info_log(">>> Obs Test Tool - Done with logging tasks. Now proceeding with monitoring tasks...")
+    create_metrics_descriptors()
+    p2 = run_monitoring_jobs()
+    info_log(">>> Obs Test Tool - Done with monitoring tasks. Now waiting for live jobs to terminate...")
+    if p1 is not None: p1.join()
+    if p2 is not None: p2.join()
+
+
+if __name__ == '__main__':
+    main()
