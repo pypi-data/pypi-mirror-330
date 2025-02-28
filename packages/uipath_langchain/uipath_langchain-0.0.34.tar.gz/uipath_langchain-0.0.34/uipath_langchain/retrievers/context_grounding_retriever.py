a9eca378ec71ae1218786ac5a0445d8eb7f0deda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-28 04:06:09 +0000

    Add package uipath_langchain/0.0.34/uipath_langchain-0.0.34.tar.gz

---
+++ packages/uipath_langchain/uipath_langchain-0.0.34.tar.gz/uipath_langchain-0.0.34/uipath_langchain/retrievers/context_grounding_retriever.py
@@ -0,0 +1,33 @@
+from typing import List, Optional
+
+from langchain_core.callbacks import CallbackManagerForRetrieverRun
+from langchain_core.documents import Document
+from langchain_core.retrievers import BaseRetriever
+from uipath_sdk import UiPathSDK  # type: ignore
+
+
+class ContextGroundingRetriever(BaseRetriever):
+    index_name: str
+    uipath_sdk: Optional[UiPathSDK] = None
+    number_of_results: Optional[int] = 10
+
+    def _get_relevant_documents(
+        self, query: str, *, run_manager: CallbackManagerForRetrieverRun
+    ) -> List[Document]:
+        """Sync implementations for retriever calls context_grounding API to search the requested index."""
+
+        sdk = self.uipath_sdk if self.uipath_sdk is not None else UiPathSDK()
+        results = sdk.context_grounding.search(
+            self.index_name, query, self.number_of_results
+        )
+
+        return [
+            Document(
+                page_content=x["content"],
+                metadata={
+                    "source": x["source"],
+                    "page_number": x["page_number"],
+                },
+            )
+            for x in results
+        ]
