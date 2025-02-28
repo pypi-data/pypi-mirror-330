0251fd222dcabeb961535c2a925260f23730ecfb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-28 16:06:20 +0000

    Add package negative-binom/0.1.4/negative_binom-0.1.4.tar.gz

---
+++ packages/negative-binom/negative_binom-0.1.4.tar.gz/negative_binom-0.1.4/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (
+    (this_directory / "README.md")
+    .read_text()
+    .replace("$`", "$")
+    .replace("`$", "$")
+    .replace("```math", "$$")
+    .replace("```", "$$")
+)
+
+setup(
+    name="negative-binom",
+    version="0.1.4",
+    packages=find_packages(),
+    install_requires=[],
+    author="Kağan Özer",
+    description="A binomial function that extends the domain of combinations to include negative integers.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kaganozer/negative-binom",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
