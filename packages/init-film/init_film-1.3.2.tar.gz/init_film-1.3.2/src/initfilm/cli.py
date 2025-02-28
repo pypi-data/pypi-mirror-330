12e7381f4ed5f32cb71d4587308927e9b3785c83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-28 03:11:17 +0000

    Add package init-film/1.3.2/init_film-1.3.2.tar.gz

---
+++ packages/init-film/init_film-1.3.2.tar.gz/init_film-1.3.2/src/initfilm/cli.py
@@ -0,0 +1,77 @@
+import sys, importlib.metadata
+import initfilm.main
+import initfilm.shortcut
+import initfilm.config
+import initfilm.templates
+
+
+def main():
+    version = importlib.metadata.version("init-film")
+
+
+    if len(sys.argv) > 1:
+        argument = sys.argv[1]
+
+        if argument == "--help" or argument == "-h":
+            print(f'''\
+------------------
+Init-Film v{version}
+By Andreas Delabie
+------------------
+
+Usage: init-film [option]
+e.g. init-film --set-number-style double
+
+Options:
+    --set-number-style <style>      Set number style to default (1. PROJECT FILES) or double (01. PROJECT FILES)
+    --set-separator-style <style>   Set separator style to dot (default), underscore, parenthesis or space
+    --set-templates-path <path>     Set templates path (source path) to python (.../site-packages/initfilm/templates) or custom path (ex. C:/Users/Spielberg/Videos/templates). ALWAYS USE FORWARD SLASHES!
+    --show-templates-path           Print templates path
+    --show-config                   Print current configuration
+
+    --add-shortcut                  Adds the Init-Film shortcut to the Windows context menu (right click menu)
+    --remove-shortcut               Removes the Init-Film shortcut to the Windows context menu (right click menu)
+
+    -v --version                    Print current version
+    -h --help                       Shows this screen
+''')
+        
+        elif argument == "--set-number-style":
+            if len(sys.argv) > 2:
+                initfilm.config.setNumberStyle(sys.argv[2])
+            else:
+                print("Please specify a valid number style like 'default' or 'double'.")
+        
+        elif argument == "--set-separator-style":
+            if len(sys.argv) > 2:
+                initfilm.config.setSeparatorStyle(sys.argv[2])
+            else:
+                print("Please specify a valid separator style like 'dot', 'underscore', 'parenthesis' or 'space'.")
+        
+        elif argument == "--set-templates-path":
+            if len(sys.argv) > 2:
+                initfilm.config.set("templates", "path", sys.argv[2])
+            else:
+                print("Please specify a templates path like python (.../site-packages/initfilm/templates) of a custom path (ex. C:/Users/Spielberg/Videos/templates).")
+        
+        elif argument == "--show-templates-path":
+            print(initfilm.templates.template_path)
+        
+        elif argument == "--show-config":
+            initfilm.config.show()
+
+        elif argument == "--add-shortcut":
+            initfilm.shortcut.add()
+
+        elif argument == "--remove-shortcut":
+            initfilm.shortcut.remove()
+        
+        elif argument == "--version" or argument == "-v":
+            print(version)
+
+        else:
+            print("Unknown command, use --help or -h for help")
+
+
+    else:
+        initfilm.main.createFolder_level0()
