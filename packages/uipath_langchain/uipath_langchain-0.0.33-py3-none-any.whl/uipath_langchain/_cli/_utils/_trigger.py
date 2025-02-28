a36e6927bf97c4c85908be9e81efd50f50784b8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-28 03:11:45 +0000

    Add package uipath_langchain/0.0.33/uipath_langchain-0.0.33-py3-none-any.whl

---
+++ packages/uipath_langchain/uipath_langchain-0.0.33-py3-none-any.whl/uipath_langchain/_cli/_utils/_trigger.py
@@ -0,0 +1,12 @@
+class ResumeTriggerType:
+    """
+    Constants representing different types of resume job triggers in the system.
+    """
+
+    NONE = "None"
+    QUEUE_ITEM = "QueueItem"
+    JOB = "Job"
+    ACTION = "Task"
+    TIMER = "Timer"
+    INBOX = "Inbox"
+    API = "Api"
