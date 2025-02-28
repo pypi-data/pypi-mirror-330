899c61c6f04038f650ccd936e2d5ed6e080bab0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-28 15:05:56 +0000

    Add package sigma-project/0.1.0/sigma_project-0.1.0-py3-none-any.whl

---
+++ packages/sigma-project/sigma_project-0.1.0-py3-none-any.whl/geometry/geometry.py
@@ -0,0 +1,15 @@
+import math as mt
+pi = mt.pi
+
+class GeometryFormula():
+    def square(a):
+        return a * a
+    
+    def triangle(a, b):
+        return a * b / 2
+    
+    def circle(r):
+        return pi * r * r
+    
+    def rectangle(a, b):
+        return a * b
