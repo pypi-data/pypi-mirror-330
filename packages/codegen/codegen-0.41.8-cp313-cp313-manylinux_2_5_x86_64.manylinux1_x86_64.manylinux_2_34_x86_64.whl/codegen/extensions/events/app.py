87b153fc4f3587e35762f991d5f09c07176864fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-28 14:05:44 +0000

    Add package codegen/0.41.8/codegen-0.41.8-cp313-cp313-manylinux_2_5_x86_64.manylinux1_x86_64.manylinux_2_34_x86_64.whl

---
+++ packages/codegen/codegen-0.41.8-cp313-cp313-manylinux_2_5_x86_64.manylinux1_x86_64.manylinux_2_34_x86_64.whl/codegen/extensions/events/app.py
@@ -0,0 +1,26 @@
+import modal  # deptry: ignore
+
+from codegen.extensions.events.github import GitHub
+from codegen.extensions.events.linear import Linear
+from codegen.extensions.events.slack import Slack
+from codegen.shared.logging.get_logger import get_logger
+
+logger = get_logger(__name__)
+
+
+class CodegenApp(modal.App):
+    linear: Linear
+    slack: Slack
+    github: GitHub
+
+    def __init__(self, name: str, modal_api_key: str, image: modal.Image):
+        self._modal_api_key = modal_api_key
+        self._image = image
+        self._name = name
+
+        super().__init__(name=name, image=image)
+
+        # Expose attributes that provide event decorators for different providers.
+        self.linear = Linear(self)
+        self.slack = Slack(self)
+        self.github = GitHub(self)
