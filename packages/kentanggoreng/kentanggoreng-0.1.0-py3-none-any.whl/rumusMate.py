1dd02f550b4dfc9936ca1b945baffde6c6112dac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-28 15:05:18 +0000

    Add package kentanggoreng/0.1.0/kentanggoreng-0.1.0-py3-none-any.whl

---
+++ packages/kentanggoreng/kentanggoreng-0.1.0-py3-none-any.whl/rumusMate.py
@@ -0,0 +1,19 @@
+class rumusMatematika:
+    def bagi(a,b):
+        return a/b
+    
+    def kali(a,b):
+        return a*b
+    
+    def tambah(a,b):
+        return a+b
+    
+    def kurang(a,b):
+        return a-b
+    
+    def luaspersegi(a):
+        return a*a
+
+    def perimeterpersegi(a):
+        return 4*a
+    
