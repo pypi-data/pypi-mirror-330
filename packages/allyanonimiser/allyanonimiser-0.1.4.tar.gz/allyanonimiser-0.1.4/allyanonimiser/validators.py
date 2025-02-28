0f2d78baa8ecc4871795d3ac13dc9f49ca1c529e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-28 14:05:51 +0000

    Add package allyanonimiser/0.1.4/allyanonimiser-0.1.4.tar.gz

---
+++ packages/allyanonimiser/allyanonimiser-0.1.4.tar.gz/allyanonimiser-0.1.4/allyanonimiser/validators.py
@@ -0,0 +1,27 @@
+"""
+Validators for the Allyanonimiser package - stub for testing.
+"""
+
+def validate_regex(pattern):
+    """Stub function for testing."""
+    return True, ""
+
+def validate_spacy_pattern(pattern):
+    """Stub function for testing."""
+    return True, ""
+
+def validate_context_list(context_list):
+    """Stub function for testing."""
+    return True, ""
+
+def validate_entity_type(entity_type):
+    """Stub function for testing."""
+    return True, ""
+
+def validate_pattern_definition(pattern_def):
+    """Stub function for testing."""
+    return True, ""
+
+def test_pattern_against_examples(pattern, positive_examples, negative_examples=None):
+    """Stub function for testing."""
+    return True, ""
