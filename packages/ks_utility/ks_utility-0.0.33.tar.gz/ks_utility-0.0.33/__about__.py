7af59f758ab4303496ed9ebd4090a276e90b9836
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-28 15:05:20 +0000

    Add package ks_utility/0.0.33/ks_utility-0.0.33.tar.gz

---
+++ packages/ks_utility/ks_utility-0.0.33.tar.gz/ks_utility-0.0.33/__about__.py
@@ -0,0 +1,5 @@
+# SPDX-FileCopyrightText: 2024-present liuyanzhi08 <702368372@qq.com>
+#
+# SPDX-License-Identifier: MIT
+# __version__ = "0.0.31" # 增加zmq支持
+__version__ = "0.0.33" # 抽取subscribe和publisher的原型
