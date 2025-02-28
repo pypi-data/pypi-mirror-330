497c32fc1113dfe238783311fc5886389df26ca6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-28 03:11:23 +0000

    Add package hud-sdk/0.3.0/hud_sdk-0.3.0-cp311-cp311-musllinux_1_2_x86_64.whl

---
+++ packages/hud-sdk/hud_sdk-0.3.0-cp311-cp311-musllinux_1_2_x86_64.whl/hud_sdk/__init__.py
@@ -0,0 +1,11 @@
+from .hook import set_hook as set_hook
+from .load import load_hud
+from .logging import internal_logger as internal_logger
+from .version import version as __version__
+from .worker.worker import init_hud_thread as init
+
+load_hud()
+
+del internal_logger
+
+__all__ = ["__version__", "set_hook", "init"]
