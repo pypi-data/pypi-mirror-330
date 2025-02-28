aa6392b76ea6c9bf783d15f9f370b1504524a796
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-28 15:05:44 +0000

    Add package mercadoradar/2.0.11/mercadoradar-2.0.11.tar.gz

---
+++ packages/mercadoradar/mercadoradar-2.0.11.tar.gz/mercadoradar-2.0.11/src/mercadoradar/models/_product_attribute_value.py
@@ -0,0 +1,36 @@
+from datetime import datetime
+from typing import Optional, Any
+
+from pydantic import BaseModel
+
+
+class ProductAttributeValue(BaseModel):
+    created_at: datetime
+    updated_at: datetime
+    is_deleted: Optional[bool] = None
+    deleted_at: Optional[datetime] = None
+    id: int
+    product_id: int
+    attribute_value: Any
+    attribute_type: Any
+    value: Optional[str] = None
+    value_type: Optional[Any] = None
+    unit: Optional[str] = None
+    iso_value: Optional[str] = None
+    iso_value_type: Optional[Any] = None
+    iso_unit: Optional[str] = None
+    is_verified: Optional[bool] = None
+
+    def to_str(self) -> str:
+        data = self.model_dump()
+        sorted_items = [('id', data.pop('id'))] if 'id' in data else []
+        sorted_items += sorted(data.items())
+        fields = "\n    ".join(f"{key}={value}" for key, value in sorted_items)
+
+        return f"<{self.__class__.__name__}\n    {fields}\n>"
+
+    def __str__(self) -> str:
+        return self.to_str()
+
+    def __repr__(self) -> str:
+        return self.to_str()
