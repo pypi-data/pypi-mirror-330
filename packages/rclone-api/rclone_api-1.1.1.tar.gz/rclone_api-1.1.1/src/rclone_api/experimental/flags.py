852d8efa653c6115595fee22d79bee9cfe1fcdb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-28 14:05:47 +0000

    Add package rclone-api/1.1.1/rclone_api-1.1.1.tar.gz

---
+++ packages/rclone-api/rclone_api-1.1.1.tar.gz/rclone_api-1.1.1/src/rclone_api/experimental/flags.py
@@ -0,0 +1,121 @@
+from dataclasses import dataclass, fields, is_dataclass
+from typing import Type, TypeVar
+
+T = TypeVar("T")
+
+
+def _merge(cls: Type[T], dataclass_a: T, dataclass_b: T) -> T:
+    if not is_dataclass(dataclass_a) or not is_dataclass(dataclass_b):
+        raise ValueError("Both inputs must be dataclass instances")
+    if type(dataclass_a) is not type(dataclass_b):
+        raise ValueError("Dataclass instances must be of the same type")
+
+    merged_kwargs = {}
+    for field in fields(dataclass_a):
+        a_value = getattr(dataclass_a, field.name)
+        b_value = getattr(dataclass_b, field.name)
+
+        if is_dataclass(a_value) and is_dataclass(b_value):
+            merged_kwargs[field.name] = _merge(type(a_value), a_value, b_value)
+        else:
+            merged_kwargs[field.name] = b_value if b_value is not None else a_value
+
+    return cls(**merged_kwargs)
+
+
+def _field_name_to_flag(field_name: str) -> str:
+    return f"--{field_name.replace('_', '-')}"
+
+
+@dataclass
+class BaseFlags:
+    def to_args(self) -> list[str]:
+        args = []
+        for field in fields(self):
+            value = getattr(self, field.name)
+            if value is None:
+                continue
+            # If the field value is a nested dataclass that supports to_args, use it.
+            if is_dataclass(value) and hasattr(value, "to_args"):
+                to_args = getattr(value, "to_args")
+                args.extend(to_args())
+            elif isinstance(value, bool):
+                # Only include the flag if the boolean is True.
+                if value:
+                    args.append(_field_name_to_flag(field.name))
+            else:
+                args.append(_field_name_to_flag(field.name))
+                if isinstance(value, list):
+                    # Join list values with a comma.
+                    args.append(",".join(map(str, value)))
+                else:
+                    args.append(str(value))
+        return args
+
+    def merge(self, other: "BaseFlags") -> "BaseFlags":
+        # Use the type of self, so merging CopyFlags returns a CopyFlags instance.
+        return _merge(type(self), self, other)
+
+    def __repr__(self):
+        return str(self.to_args())
+
+
+@dataclass(repr=False)
+class CopyFlags(BaseFlags):
+    check_first: bool | None = None
+    checksum: bool | None = False
+    compare_dest: list[str] | None = None
+    copy_dest: list[str] | None = None
+    cutoff_mode: str | None = None
+    ignore_case_sync: bool | None = None
+    ignore_checksum: bool | None = None
+    ignore_existing: bool | None = None
+    ignore_size: bool | None = None
+    ignore_times: bool | None = None
+    immutable: bool | None = None
+    inplace: bool | None = None
+    links: bool | None = None
+    max_backlog: int | None = None
+    max_duration: str | None = None
+    max_transfer: str | None = None
+    metadata: bool | None = None
+    modify_window: str | None = None
+    multi_thread_chunk_size: str | None = None
+    multi_thread_cutoff: str | None = None
+    multi_thread_streams: int | None = None
+    multi_thread_write_buffer_size: str | None = None
+    no_check_dest: bool | None = None
+    no_traverse: bool | None = None
+    no_update_dir_modtime: bool | None = None
+    no_update_modtime: bool | None = None
+    order_by: str | None = None
+    partial_suffix: str | None = None
+    refresh_times: bool | None = None
+    server_side_across_configs: bool | None = None
+    size_only: bool | None = None
+    streaming_upload_cutoff: str | None = None
+    update: bool | None = None
+
+
+@dataclass(repr=False)
+class Flags(BaseFlags):
+    copy: CopyFlags | None = None
+
+
+def unit_test() -> None:
+    copy_flags_a = CopyFlags(compare_dest=["a", "b"])
+    copy_flags_b = CopyFlags(checksum=False)
+    flags_a = copy_flags_a.merge(copy_flags_b)
+    print("A:", flags_a)
+
+    copy_flags_c = CopyFlags(checksum=True)
+    copy_flags_d = CopyFlags(checksum=False)
+
+    merged_c_d = copy_flags_c.merge(copy_flags_d)
+    print("B:", merged_c_d)
+    merged_d_c = copy_flags_d.merge(copy_flags_c)
+    print("C:", merged_d_c)
+
+
+if __name__ == "__main__":
+    unit_test()
---
+++ packages/rclone-api/rclone_api-1.1.1.tar.gz/rclone_api-1.1.1/src/rclone_api/util.py
@@ -0,0 +1,175 @@
+import os
+import shutil
+import subprocess
+import time
+import warnings
+from pathlib import Path
+from tempfile import TemporaryDirectory
+from typing import Any
+
+from rclone_api.config import Config
+from rclone_api.dir import Dir
+from rclone_api.remote import Remote
+from rclone_api.rpath import RPath
+from rclone_api.types import S3PathInfo
+
+# from .rclone import Rclone
+
+
+def to_path(item: Dir | Remote | str, rclone: Any) -> RPath:
+    from rclone_api.rclone import Rclone
+
+    assert isinstance(rclone, Rclone)
+    # if str then it will be remote:path
+    if isinstance(item, str):
+        # return RPath(item)
+        # remote_name: str = item.split(":")[0]
+        parts = item.split(":")
+        remote_name = parts[0]
+        path = ":".join(parts[1:])
+        remote = Remote(name=remote_name, rclone=rclone)
+        out = RPath(
+            remote=remote,
+            path=path,
+            name="",
+            size=0,
+            mime_type="",
+            mod_time="",
+            is_dir=True,
+        )
+        out.set_rclone(rclone)
+        return out
+    elif isinstance(item, Dir):
+        return item.path
+    elif isinstance(item, Remote):
+        out = RPath(
+            remote=item,
+            path=str(item),
+            name=str(item),
+            size=0,
+            mime_type="inode/directory",
+            mod_time="",
+            is_dir=True,
+        )
+        out.set_rclone(rclone)
+        return out
+    else:
+        raise ValueError(f"Invalid type for item: {type(item)}")
+
+
+def get_verbose(verbose: bool | None) -> bool:
+    if verbose is not None:
+        return verbose
+    # get it from the environment
+    return bool(int(os.getenv("RCLONE_API_VERBOSE", "0")))
+
+
+def get_check(check: bool | None) -> bool:
+    if check is not None:
+        return check
+    # get it from the environment
+    return bool(int(os.getenv("RCLONE_API_CHECK", "1")))
+
+
+def get_rclone_exe(rclone_exe: Path | None) -> Path:
+    if rclone_exe is None:
+
+        rclone_which_path = shutil.which("rclone")
+        if rclone_which_path is None:
+            raise ValueError("rclone executable not found")
+        return Path(rclone_which_path)
+    return rclone_exe
+
+
+def rclone_execute(
+    cmd: list[str],
+    rclone_conf: Path | Config,
+    rclone_exe: Path,
+    check: bool,
+    capture: bool | None = None,
+    verbose: bool | None = None,
+) -> subprocess.CompletedProcess:
+    tempdir: TemporaryDirectory | None = None
+    verbose = get_verbose(verbose)
+    capture = capture if isinstance(capture, bool) else True
+    assert verbose is not None
+
+    try:
+        if isinstance(rclone_conf, Config):
+            tempdir = TemporaryDirectory()
+            tmpfile = Path(tempdir.name) / "rclone.conf"
+            tmpfile.write_text(rclone_conf.text, encoding="utf-8")
+            rclone_conf = tmpfile
+        cmd = (
+            [str(rclone_exe.resolve())] + ["--config", str(rclone_conf.resolve())] + cmd
+        )
+        if verbose:
+            cmd_str = subprocess.list2cmdline(cmd)
+            print(f"\nRunning: {cmd_str}")
+        cp = subprocess.run(
+            cmd, capture_output=capture, encoding="utf-8", check=False, shell=False
+        )
+        if cp.returncode != 0:
+            cmd_str = subprocess.list2cmdline(cmd)
+            warnings.warn(
+                f"Error running: {cmd_str}, returncode: {cp.returncode}\n{cp.stdout}\n{cp.stderr}"
+            )
+            if check:
+                raise subprocess.CalledProcessError(
+                    cp.returncode, cmd, cp.stdout, cp.stderr
+                )
+        return cp
+    finally:
+        if tempdir:
+            try:
+                tempdir.cleanup()
+            except Exception as e:
+                print(f"Error cleaning up tempdir: {e}")
+
+
+def wait_for_mount(path: Path, mount_process: Any, timeout: int = 10) -> None:
+    from rclone_api.process import Process
+
+    assert isinstance(mount_process, Process)
+    expire_time = time.time() + timeout
+    while time.time() < expire_time:
+        rtn = mount_process.poll()
+        if rtn is not None:
+            cmd_str = subprocess.list2cmdline(mount_process.cmd)
+            raise subprocess.CalledProcessError(rtn, cmd_str)
+        if path.exists():
+            # how many files?
+            dircontents = os.listdir(str(path))
+            if len(dircontents) > 0:
+                print(f"Mount point {path}, waiting 5 seconds for files to appear.")
+                time.sleep(5)
+                return
+        time.sleep(1)
+
+
+def split_s3_path(path: str) -> S3PathInfo:
+    if ":" not in path:
+        raise ValueError(f"Invalid S3 path: {path}")
+
+    prts = path.split(":", 1)
+    remote = prts[0]
+    path = prts[1]
+    parts: list[str] = []
+    for part in path.split("/"):
+        part = part.strip()
+        if part:
+            parts.append(part)
+    if len(parts) < 2:
+        raise ValueError(f"Invalid S3 path: {path}")
+    bucket = parts[0]
+    key = "/".join(parts[1:])
+    assert bucket
+    assert key
+    return S3PathInfo(remote=remote, bucket=bucket, key=key)
+
+
+def random_str(length: int) -> str:
+    import random
+    import string
+
+    return "".join(random.choices(string.ascii_lowercase + string.digits, k=length))
