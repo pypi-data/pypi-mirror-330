f15ecbc344f7f7612826e8a2961389e88509a6af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-28 15:05:55 +0000

    Add package rumus-mat-mokel/0.1.0/rumus_mat_mokel-0.1.0-py3-none-any.whl

---
+++ packages/rumus-mat-mokel/rumus_mat_mokel-0.1.0-py3-none-any.whl/rumus_matematika.py
@@ -0,0 +1,13 @@
+# rumus_matematika.py
+class RumusMatematika:
+    def tambah(a, b):
+        return a + b
+
+    def kurang(a, b):
+        return a - b
+
+    def kali(a, b):
+        return a * b
+
+    def bagi(a, b):
+        return a / b
