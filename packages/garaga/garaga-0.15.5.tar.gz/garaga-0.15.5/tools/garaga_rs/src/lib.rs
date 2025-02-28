b8d035916bf1af4be3261ad66abf2c1f7075ab8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-28 17:05:24 +0000

    Add package garaga/0.15.5/garaga-0.15.5.tar.gz

---
+++ packages/garaga/garaga-0.15.5.tar.gz/garaga-0.15.5/tools/garaga_rs/src/lib.rs
@@ -0,0 +1,18 @@
+pub mod algebra;
+pub mod calldata;
+pub mod crypto;
+pub mod definitions;
+pub mod ecip;
+pub mod frobenius;
+pub mod io;
+pub mod pairing;
+
+pub mod poseidon_transcript;
+
+// automatically excludes python bindings on wasm32-unknown-unknown (pyo3 not supported)
+#[cfg(all(feature = "python", not(target_arch = "wasm32")))]
+pub mod python_bindings;
+
+// automatically includes wasm bindings on wasm32-unknown-unknown
+#[cfg(any(feature = "wasm", target_arch = "wasm32"))]
+pub mod wasm_bindings;
