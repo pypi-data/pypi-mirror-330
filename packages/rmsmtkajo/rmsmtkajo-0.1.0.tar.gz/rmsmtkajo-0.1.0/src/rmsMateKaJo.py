be9b4dab6019691e8bfb17bd2ea67acce91b4269
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-28 15:05:26 +0000

    Add package rmsmtkajo/0.1.0/rmsmtkajo-0.1.0.tar.gz

---
+++ packages/rmsmtkajo/rmsmtkajo-0.1.0.tar.gz/rmsmtkajo-0.1.0/src/rmsMateKaJo.py
@@ -0,0 +1,12 @@
+class rumus_MTK:
+    def tambah(a,b):
+        return a+b
+    
+    def kurang(a,b):
+        return a-b
+    
+    def kali (a,b):
+        return a*b
+    
+    def bagi (a,b):
+        return a/b
