16e7e89300c9e2527146c8a515a98014eddbde68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-28 15:05:35 +0000

    Add package notification-typing/0.1.0/notification_typing-0.1.0-py3-none-any.whl

---
+++ packages/notification-typing/notification_typing-0.1.0-py3-none-any.whl/notification_typing/NotificationCondition.py
@@ -0,0 +1,16 @@
+from enum import Enum
+
+class NotificationCondition(Enum):
+    GROW_BY_3_PCT = 1, # grew by 3% a day
+    DEC_BY_3_PCT = 2, # decrease by 3% a day
+    GROW_BY_5_PCT = 1, # grew by 5% a day
+    DEC_BY_5_PCT = 2, # decrease by 5% a day
+    @classmethod
+    def from_string(cls, s):
+        try:
+            return cls[s]
+        except KeyError:
+            raise ValueError(f"'{s}' is not a valid NotificationCondition")
+
+def valid_frequency(condition):
+    return condition in NotificationCondition
