1e8cff506bb6e9fcb9a9a881834b4ac8f7312285
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-28 03:11:18 +0000

    Add package kafi/0.0.45/kafi-0.0.45.tar.gz

---
+++ packages/kafi/kafi-0.0.45.tar.gz/kafi-0.0.45/kafi/kafka/kafka_consumer.py
@@ -0,0 +1,82 @@
+from kafi.storage_consumer import StorageConsumer
+
+# Constants
+
+ALL_MESSAGES = -1
+
+#
+
+class KafkaConsumer(StorageConsumer):
+    def __init__(self, kafka_obj, *topics, **kwargs):
+        super().__init__(kafka_obj, *topics, **kwargs)
+
+    #
+
+    def foldl(self, foldl_function, initial_acc, n=ALL_MESSAGES, commit_after_processing=None, **kwargs):
+        n_int = n
+        #
+        if n_int == 0:
+            return initial_acc
+        #
+        commit_after_processing_bool = self.storage_obj.commit_after_processing() if commit_after_processing is None else commit_after_processing
+        #
+        consume_batch_size_int = kwargs["consume_batch_size"] if "consume_batch_size" in kwargs else self.storage_obj.consume_batch_size()
+        if n != ALL_MESSAGES and consume_batch_size_int > n_int:
+            consume_batch_size_int = n_int
+        #
+        break_function = kwargs["break_function"] if "break_function" in kwargs else lambda _, _1: False
+        #
+        message_counter_int = 0
+        #
+        acc = initial_acc
+        break_bool = False
+        while True:
+            message_dict_list = self.consume_impl(n=consume_batch_size_int, **kwargs)
+            if not message_dict_list:
+                break
+            #
+            topic_str_offsets_dict_dict = {}
+            for message_dict in message_dict_list:
+                topic_str = message_dict["topic"]
+                partition_int = message_dict["partition"]
+                offset_int = message_dict["offset"]
+                #
+                if topic_str not in topic_str_offsets_dict_dict:
+                    topic_str_offsets_dict_dict[topic_str] = {}
+                #
+                offsets_dict = topic_str_offsets_dict_dict[topic_str]
+                #
+                offsets_dict[partition_int] = offset_int + 1
+                #
+                if break_function(acc, message_dict):
+                    break_bool = True
+                    break
+                #
+                acc = foldl_function(acc, message_dict)
+                message_counter_int += 1
+                #
+                if self.topic_str_end_offsets_dict_dict is not None and topic_str in self.topic_str_end_offsets_dict_dict and offset_int == self.topic_str_end_offsets_dict_dict[topic_str][partition_int]:
+                    break_bool = True
+                    break
+                #
+                if n_int != ALL_MESSAGES and message_counter_int >= n_int:
+                    break_bool = True
+                    break
+            #
+            if not self.enable_auto_commit_bool and commit_after_processing_bool:
+                self.commit(topic_str_offsets_dict_dict)
+            #
+            if break_bool:
+                break
+        #
+        return acc
+
+    #
+
+    def consume(self, n=ALL_MESSAGES, **kwargs):
+        def foldl_function(message_dict_list, message_dict):
+            message_dict_list.append(message_dict)
+            #
+            return message_dict_list
+        #
+        return self.foldl(foldl_function, [], n, commit_after_processing=False, **kwargs)
