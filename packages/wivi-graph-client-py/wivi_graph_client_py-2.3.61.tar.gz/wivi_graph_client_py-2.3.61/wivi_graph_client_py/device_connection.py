a1e9ae4a750cae085809d5e4f6f98594443bf35f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-28 03:11:46 +0000

    Add package wivi-graph-client-py/2.3.61/wivi_graph_client_py-2.3.61.tar.gz

---
+++ packages/wivi-graph-client-py/wivi_graph_client_py-2.3.61.tar.gz/wivi_graph_client_py-2.3.61/wivi_graph_client_py/device_connection.py
@@ -0,0 +1,46 @@
+class Device_Connections_Query:
+    get_device_connection_query = """
+        query deviceConnection($input: DeviceConnectionFilterInput) {
+            deviceConnection(input: $input) {
+                configuration {
+                    deviceId
+                    vehicleId
+                    organizationId
+                    fleetId
+                }
+            }
+        }
+    """
+
+
+class Device_Connections_Mutation:
+
+    upsert_device_connection_mutation = '''
+        mutation createDeviceConnection($input: CreateDeviceConnectionInput) {
+            createDeviceConnection(input: $input) {
+                configuration {
+                    vehicleId
+                    fleetId
+                    organizationId
+                    deviceId
+                }
+                bytesReceived
+                bytesSent
+                networkProvider
+                network
+                networkType
+                version
+                startTime
+                endTime
+                time
+            }
+        }
+    '''
+
+    delete_device_connection_mutation = '''
+        mutation DeleteDeviceConnection($input: DeleteDeviceConnectionInput) {
+            deleteDeviceConnection(input: $input) {
+                DeviceConnection
+            }
+        }
+    '''
