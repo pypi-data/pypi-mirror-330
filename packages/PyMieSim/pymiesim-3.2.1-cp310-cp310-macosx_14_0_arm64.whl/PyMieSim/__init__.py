91e2cfeecd8b26e2af7274e41fcd6e9b37af7ca0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-28 04:05:39 +0000

    Add package PyMieSim/3.2.1/pymiesim-3.2.1-cp310-cp310-macosx_14_0_arm64.whl

---
+++ packages/PyMieSim/pymiesim-3.2.1-cp310-cp310-macosx_14_0_arm64.whl/PyMieSim/__init__.py
@@ -0,0 +1,9 @@
+from PyMieSim.units import Quantity  # God knows why removing this line make the test fails on mac x86_64! DO NOT REMOVE!
+
+try:
+    from ._version import version as __version__
+
+except ImportError:
+    __version__ = "0.0.0"
+
+# -
