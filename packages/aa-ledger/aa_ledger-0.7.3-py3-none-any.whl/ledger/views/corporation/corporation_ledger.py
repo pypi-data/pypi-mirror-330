b81c4ef0ab3be1a5316ba12ca85fd03a90ccf530
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-28 06:06:17 +0000

    Add package aa-ledger/0.7.3/aa_ledger-0.7.3-py3-none-any.whl

---
+++ packages/aa-ledger/aa_ledger-0.7.3-py3-none-any.whl/ledger/views/corporation/corporation_ledger.py
@@ -0,0 +1,46 @@
+"""PvE Views"""
+
+from datetime import datetime
+
+# Django
+from django.contrib.auth.decorators import login_required, permission_required
+from django.shortcuts import render
+
+from ledger.hooks import get_extension_logger
+
+# Ledger
+from ledger.view_helpers.core import add_info_to_context
+
+logger = get_extension_logger(__name__)
+
+
+@login_required
+@permission_required("ledger.basic_access", "ledger.advanced_access")
+def corporation_ledger(request, corporation_pk):
+    """
+    Corporation Ledger
+    """
+    # pylint: disable=duplicate-code
+    current_year = datetime.now().year
+    years = [current_year - i for i in range(6)]
+
+    context = {
+        "years": years,
+        "entity_pk": corporation_pk,
+        "entity_type": "corporation",
+    }
+    context = add_info_to_context(request, context)
+    return render(request, "ledger/corpledger/corporation_ledger.html", context=context)
+
+
+@login_required
+@permission_required("ledger.basic_access", "ledger.advanced_access")
+def corporation_admin(request):
+    """
+    Corporation Admin
+    """
+    context = {}
+    context = add_info_to_context(request, context)
+    return render(
+        request, "ledger/corpledger/admin/corporation_admin.html", context=context
+    )
