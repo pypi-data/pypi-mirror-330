0938f86a5fb4cc6756788ff664a0ff00e72186c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-28 06:06:11 +0000

    Add package fezrs/0.1.0/fezrs-0.1.0.tar.gz

---
+++ packages/fezrs/fezrs-0.1.0.tar.gz/fezrs-0.1.0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="fezrs",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "matplotlib",
+        "scikit-image",
+        "scikit-learn",
+        "fastapi",
+        "opencv-python" 
+        
+    ],
+    author="Mahdi Farmahinifarahani, Hooman Mirzaee, Mahdi Nedaee, Mohammad Hossein Kiani Fayz Abadi",
+    author_email="aradfarahani@aol.com",
+    description="Feature Extraction and Zoning for Remote Sensing (FEZrs)",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/FEZtool-team/FEZrs",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",
+)
