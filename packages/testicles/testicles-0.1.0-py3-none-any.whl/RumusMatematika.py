3123e4d47d1fcced358561e4b60912a1f981c461
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-28 15:05:55 +0000

    Add package testicles/0.1.0/testicles-0.1.0-py3-none-any.whl

---
+++ packages/testicles/testicles-0.1.0-py3-none-any.whl/RumusMatematika.py
@@ -0,0 +1,18 @@
+class RumusMatematika:
+    def __init__():
+        pass
+
+    def tambah(a, b):
+        return a + b
+
+    def kurang(a,b):
+        return a-b
+    
+    def kali(a, b):
+        return a*b
+
+    def bagi(a, b):
+        return a/b
+
+    def luas_persegi(a):
+        return a
