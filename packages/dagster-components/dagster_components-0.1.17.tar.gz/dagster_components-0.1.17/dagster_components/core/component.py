1da7f6ed8f47ecf35633e180fe421bb526a2e543
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-28 15:05:32 +0000

    Add package dagster-components/0.1.17/dagster_components-0.1.17.tar.gz

---
+++ packages/dagster-components/dagster_components-0.1.17.tar.gz/dagster_components-0.1.17/dagster_components/core/component.py
@@ -0,0 +1,307 @@
+import dataclasses
+import importlib
+import importlib.metadata
+import inspect
+import sys
+import textwrap
+from abc import ABC, abstractmethod
+from collections.abc import Iterable, Mapping, Sequence
+from dataclasses import dataclass
+from pathlib import Path
+from types import ModuleType
+from typing import Any, Callable, Optional, TypedDict, TypeVar, Union
+
+from dagster import _check as check
+from dagster._core.definitions.definitions_class import Definitions
+from dagster._core.errors import DagsterError
+from dagster._utils import pushd
+from typing_extensions import Self
+
+from dagster_components.core.component_key import ComponentKey
+from dagster_components.core.component_scaffolder import (
+    ComponentScaffolder,
+    ComponentScaffolderUnavailableReason,
+    DefaultComponentScaffolder,
+)
+from dagster_components.core.schema.base import ResolvableSchema
+from dagster_components.core.schema.context import ResolutionContext
+from dagster_components.utils import format_error_message
+
+
+class ComponentsEntryPointLoadError(DagsterError):
+    pass
+
+
+class ComponentDeclNode(ABC):
+    @abstractmethod
+    def load(self, context: "ComponentLoadContext") -> Sequence["Component"]: ...
+
+
+class Component(ABC):
+    @classmethod
+    def get_schema(cls) -> Optional[type[ResolvableSchema]]:
+        return None
+
+    @classmethod
+    def get_scaffolder(cls) -> Union[ComponentScaffolder, ComponentScaffolderUnavailableReason]:
+        """Subclasses should implement this method to override scaffolding behavior. If this component
+        is not meant to be scaffolded it returns a ComponentScaffolderUnavailableReason with a message
+        This can be determined at runtime based on the environment or configuration. For example,
+        if scaffolders are optionally installed as extras (for example to avoid heavy dependencies in production),
+        this method should return a ComponentScaffolderUnavailableReason with a message explaining
+        how to install the necessary extras.
+        """
+        return DefaultComponentScaffolder()
+
+    @classmethod
+    def get_additional_scope(cls) -> Mapping[str, Any]:
+        return {}
+
+    @abstractmethod
+    def build_defs(self, context: "ComponentLoadContext") -> Definitions: ...
+
+    @classmethod
+    def load(cls, attributes: Optional[ResolvableSchema], context: "ComponentLoadContext") -> Self:
+        return attributes.resolve_as(cls, context.resolution_context) if attributes else cls()
+
+    @classmethod
+    def get_metadata(cls) -> "ComponentTypeInternalMetadata":
+        docstring = cls.__doc__
+        clean_docstring = _clean_docstring(docstring) if docstring else None
+
+        scaffolder = cls.get_scaffolder()
+
+        if isinstance(scaffolder, ComponentScaffolderUnavailableReason):
+            raise DagsterError(
+                f"Component {cls.__name__} is not scaffoldable: {scaffolder.message}"
+            )
+
+        component_schema = cls.get_schema()
+        scaffold_params = scaffolder.get_schema()
+        return {
+            "summary": clean_docstring.split("\n\n")[0] if clean_docstring else None,
+            "description": clean_docstring if clean_docstring else None,
+            "scaffold_params_schema": None
+            if scaffold_params is None
+            else scaffold_params.model_json_schema(),
+            "component_schema": None
+            if component_schema is None
+            else component_schema.model_json_schema(),
+        }
+
+    @classmethod
+    def get_description(cls) -> Optional[str]:
+        return inspect.getdoc(cls)
+
+
+def _clean_docstring(docstring: str) -> str:
+    lines = docstring.strip().splitlines()
+    first_line = lines[0]
+    if len(lines) == 1:
+        return first_line
+    else:
+        rest = textwrap.dedent("\n".join(lines[1:]))
+        return f"{first_line}\n{rest}"
+
+
+class ComponentTypeInternalMetadata(TypedDict):
+    summary: Optional[str]
+    description: Optional[str]
+    scaffold_params_schema: Optional[Any]  # json schema
+    component_schema: Optional[Any]  # json schema
+
+
+class ComponentTypeMetadata(ComponentTypeInternalMetadata):
+    name: str
+    namespace: str
+
+
+def get_entry_points_from_python_environment(group: str) -> Sequence[importlib.metadata.EntryPoint]:
+    if sys.version_info >= (3, 10):
+        return importlib.metadata.entry_points(group=group)
+    else:
+        return importlib.metadata.entry_points().get(group, [])
+
+
+COMPONENTS_ENTRY_POINT_GROUP = "dagster.components"
+
+
+def load_component_type(component_key: ComponentKey) -> type[Component]:
+    module_name, attr = component_key.namespace, component_key.name
+    try:
+        module = importlib.import_module(module_name)
+        if not hasattr(module, attr):
+            raise DagsterError(f"Module `{module_name}` has no attribute `{attr}`.")
+        component_type = getattr(module, attr)
+        if not issubclass(component_type, Component):
+            raise DagsterError(
+                f"Attribute `{attr}` in module `{module_name}` is not a subclass of `dagster_components.Component`."
+            )
+        return component_type
+    except ModuleNotFoundError as e:
+        raise DagsterError(f"Module `{module_name}` not found.") from e
+    except ImportError as e:
+        raise DagsterError(f"Error loading module `{module_name}`.") from e
+
+
+def discover_entry_point_component_types() -> dict[ComponentKey, type[Component]]:
+    """Discover component types registered in the Python environment via the
+    `dagster_components` entry point group.
+
+    `dagster-components` itself registers multiple component entry points. We call these
+    "builtin" component libraries. The `dagster_components` entry point resolves to published
+    component types and is loaded by default. Other entry points resolve to various sets of test
+    component types. This method will only ever load one builtin component library.
+    """
+    component_types: dict[ComponentKey, type[Component]] = {}
+    entry_points = get_entry_points_from_python_environment(COMPONENTS_ENTRY_POINT_GROUP)
+
+    for entry_point in entry_points:
+        try:
+            root_module = entry_point.load()
+        except Exception as e:
+            raise ComponentsEntryPointLoadError(
+                format_error_message(f"""
+                    Error loading entry point `{entry_point.name}` in group `{COMPONENTS_ENTRY_POINT_GROUP}`.
+                    Please fix the error or uninstall the package that defines this entry point.
+                """)
+            ) from e
+
+        if not isinstance(root_module, ModuleType):
+            raise DagsterError(
+                f"Invalid entry point {entry_point.name} in group {COMPONENTS_ENTRY_POINT_GROUP}. "
+                f"Value expected to be a module, got {root_module}."
+            )
+        for name, component_type in get_component_types_in_module(root_module):
+            key = ComponentKey(name=name, namespace=entry_point.value)
+            component_types[key] = component_type
+    return component_types
+
+
+def discover_component_types(modules: Sequence[str]) -> dict[ComponentKey, type[Component]]:
+    component_types: dict[ComponentKey, type[Component]] = {}
+    for extra_module in modules:
+        for name, component_type in get_component_types_in_module(
+            importlib.import_module(extra_module)
+        ):
+            key = ComponentKey(name=name, namespace=extra_module)
+            component_types[key] = component_type
+    return component_types
+
+
+def get_component_types_in_module(
+    module: ModuleType,
+) -> Iterable[tuple[str, type[Component]]]:
+    from dagster._core.definitions.module_loaders.load_assets_from_modules import (
+        find_subclasses_in_module,
+    )
+
+    for name, component in find_subclasses_in_module(module, (Component,)):
+        if not inspect.isabstract(component):
+            yield name, component
+
+
+T = TypeVar("T")
+
+
+@dataclass
+class ComponentLoadContext:
+    module_name: str
+    resources: Mapping[str, object]
+    decl_node: Optional[ComponentDeclNode]
+    resolution_context: ResolutionContext
+
+    @staticmethod
+    def for_test(
+        *,
+        resources: Optional[Mapping[str, object]] = None,
+        decl_node: Optional[ComponentDeclNode] = None,
+    ) -> "ComponentLoadContext":
+        return ComponentLoadContext(
+            module_name="test",
+            resources=resources or {},
+            decl_node=decl_node,
+            resolution_context=ResolutionContext.default(),
+        )
+
+    @property
+    def path(self) -> Path:
+        from dagster_components.core.component_decl_builder import (
+            PythonComponentDecl,
+            YamlComponentDecl,
+        )
+
+        if not isinstance(self.decl_node, (YamlComponentDecl, PythonComponentDecl)):
+            check.failed(f"Unsupported decl_node type {type(self.decl_node)}")
+
+        return self.decl_node.path
+
+    def with_rendering_scope(self, rendering_scope: Mapping[str, Any]) -> "ComponentLoadContext":
+        return dataclasses.replace(
+            self,
+            resolution_context=self.resolution_context.with_scope(**rendering_scope),
+        )
+
+    def for_decl_node(self, decl_node: ComponentDeclNode) -> "ComponentLoadContext":
+        return dataclasses.replace(self, decl_node=decl_node)
+
+    def resolve(self, value: ResolvableSchema, as_type: type[T]) -> T:
+        return self.resolution_context.resolve_value(value, as_type=as_type)
+
+    def normalize_component_type_str(self, type_str: str) -> str:
+        return f"{self.module_name}{type_str}" if type_str.startswith(".") else type_str
+
+    def load_component_relative_python_module(self, file_path: Path) -> ModuleType:
+        """Load a python module relative to the component's context path. This is useful for loading code
+        the resides within the component directory, loaded during `build_defs` method of a component.
+
+        Example:
+            .. code-block:: python
+
+                def build_defs(self, context: ComponentLoadContext) -> Definitions:
+                    return load_definitions_from_module(
+                        context.load_component_relative_python_module(
+                            Path(self.definitions_path) if self.definitions_path else Path("definitions.py")
+                        )
+                    )
+
+        In a typical setup you end up with module names such as "a_project.components.my_component.my_python_file".
+
+        This handles "__init__.py" files by ending the module name at the parent directory
+        (e.g "a_project.components.my_component") if the file resides at "a_project/defs/my_component/__init__.py".
+
+        This calls importlib.import_module with that module name, going through the python module import system.
+
+        It is as if one typed "import a_project.components.my_component.my_python_file" in the python interpreter.
+        """
+        abs_file_path = file_path.absolute()
+        with pushd(str(self.path)):
+            abs_context_path = self.path.absolute()
+
+            # Problematic
+            # See https://linear.app/dagster-labs/issue/BUILD-736/highly-suspect-hardcoding-of-components-string-is-component-relative
+            component_module_relative_path = abs_context_path.parts[
+                abs_context_path.parts.index("defs") + 2 :
+            ]
+            component_module_name = ".".join([self.module_name, *component_module_relative_path])
+            if abs_file_path.name != "__init__.py":
+                component_module_name = f"{component_module_name}.{abs_file_path.stem}"
+
+            return importlib.import_module(component_module_name)
+
+
+COMPONENT_LOADER_FN_ATTR = "__dagster_component_loader_fn"
+
+
+T_Component = TypeVar("T_Component", bound=Component)
+
+
+def component(
+    fn: Callable[[ComponentLoadContext], T_Component],
+) -> Callable[[ComponentLoadContext], T_Component]:
+    setattr(fn, COMPONENT_LOADER_FN_ATTR, True)
+    return fn
+
+
+def is_component_loader(obj: Any) -> bool:
+    return getattr(obj, COMPONENT_LOADER_FN_ATTR, False)
---
+++ packages/dagster-components/dagster_components-0.1.17.tar.gz/dagster_components-0.1.17/dagster_components/utils.py
@@ -0,0 +1,231 @@
+import importlib.util
+import subprocess
+import sys
+import textwrap
+from collections.abc import Iterable, Iterator, Mapping, Sequence
+from contextlib import contextmanager
+from dataclasses import dataclass
+from pathlib import Path
+from types import ModuleType
+from typing import Any, Optional, TypeVar
+
+import click
+from dagster._core.definitions.asset_key import AssetKey
+from dagster._core.definitions.asset_spec import AssetSpec
+from dagster._core.definitions.declarative_automation.automation_condition import (
+    AutomationCondition,
+)
+from dagster._core.definitions.tags import build_kind_tag
+from dagster._core.errors import DagsterError
+
+from dagster_components.core.schema.context import ResolutionContext
+from dagster_components.core.schema.objects import AssetAttributesSchema
+
+T = TypeVar("T")
+
+
+def ensure_dagster_components_tests_import() -> None:
+    from dagster_components import __file__ as dagster_components_init_py
+
+    dagster_components_package_root = (Path(dagster_components_init_py) / ".." / "..").resolve()
+    assert (
+        dagster_components_package_root / "dagster_components_tests"
+    ).exists(), "Could not find dagster_components_tests where expected"
+    sys.path.append(dagster_components_package_root.as_posix())
+
+
+def exit_with_error(error_message: str) -> None:
+    click.echo(click.style(error_message, fg="red"))
+    sys.exit(1)
+
+
+def format_error_message(message: str) -> str:
+    # width=10000 unwraps any hardwrapping
+    dedented = textwrap.dedent(message).strip()
+    paragraphs = [textwrap.fill(p, width=10000) for p in dedented.split("\n\n")]
+    return "\n\n".join(paragraphs)
+
+
+# Temporarily places a path at the front of sys.path, ensuring that any modules in that path are
+# importable.
+@contextmanager
+def ensure_loadable_path(path: Path) -> Iterator[None]:
+    orig_path = sys.path.copy()
+    sys.path.insert(0, str(path))
+    try:
+        yield
+    finally:
+        sys.path = orig_path
+
+
+def get_path_for_package(package_name: str) -> str:
+    spec = importlib.util.find_spec(package_name)
+    if not spec:
+        raise DagsterError(f"Cannot find package: {package_name}")
+    # file_path = spec.origin
+    submodule_search_locations = spec.submodule_search_locations
+    if not submodule_search_locations:
+        raise DagsterError(f"Package does not have any locations for submodules: {package_name}")
+    return submodule_search_locations[0]
+
+
+@dataclass
+class TranslatorResolvingInfo:
+    obj_name: str
+    asset_attributes: AssetAttributesSchema
+    resolution_context: ResolutionContext
+
+    def get_resolved_attribute(self, attribute: str, obj: Any, default_method) -> Any:
+        resolved_attributes = self.resolution_context.with_scope(
+            **{self.obj_name: obj}
+        ).resolve_value(self.asset_attributes)
+        return (
+            resolved_attributes[attribute]
+            if attribute in resolved_attributes
+            else default_method(obj)
+        )
+
+    def get_asset_spec(self, base_spec: AssetSpec, context: Mapping[str, Any]) -> AssetSpec:
+        """Returns an AssetSpec that combines the base spec with attributes resolved using the provided context.
+
+        Usage:
+
+        ```python
+        class WrappedDagsterXTranslator(DagsterXTranslator):
+            def __init__(self, *, base_translator, resolving_info: TranslatorResolvingInfo):
+                self.base_translator = base_translator
+                self.resolving_info = resolving_info
+
+            def get_asset_spec(self, base_spec: AssetSpec, x_params: Any) -> AssetSpec:
+                return self.resolving_info.get_asset_spec(
+                    base_spec, {"x_params": x_params}
+                )
+
+        ```
+        """
+        resolved_attributes = self.resolution_context.with_scope(**context).resolve_value(
+            self.asset_attributes
+        )
+        return base_spec.replace_attributes(**resolved_attributes)
+
+
+def get_wrapped_translator_class(translator_type: type):
+    """Temporary hack to allow wrapping of many methods of a given translator class. Will be removed
+    once all translators implement `get_asset_spec`.
+    """
+
+    class WrappedTranslator(translator_type):
+        def __init__(self, *, resolving_info: TranslatorResolvingInfo):
+            self.base_translator = translator_type()
+            self.resolving_info = resolving_info
+
+        def get_asset_key(self, obj: Any) -> AssetKey:
+            return self.resolving_info.get_resolved_attribute(
+                "key", obj, self.base_translator.get_asset_key
+            )
+
+        def get_group_name(self, obj: Any) -> Optional[str]:
+            return self.resolving_info.get_resolved_attribute(
+                "group_name", obj, self.base_translator.get_group_name
+            )
+
+        def get_tags(self, obj: Any) -> Mapping[str, str]:
+            tags = {}
+
+            base_kinds = (
+                self.base_translator.get_kinds
+                if hasattr(self.base_translator, "get_kinds")
+                else lambda *args: []
+            )
+            kinds = self.resolving_info.get_resolved_attribute("kinds", obj, base_kinds)
+            for kind in kinds:
+                tags.update(build_kind_tag(kind))
+
+            tags.update(
+                self.resolving_info.get_resolved_attribute(
+                    "tags", obj, self.base_translator.get_tags
+                )
+            )
+            return tags
+
+        def get_automation_condition(self, obj: Any) -> Optional[AutomationCondition]:
+            return self.resolving_info.get_resolved_attribute(
+                "automation_condition",
+                obj,
+                self.base_translator.get_automation_condition,
+            )
+
+        def get_metadata(self, obj: Any) -> Mapping[str, Any]:
+            return self.resolving_info.get_resolved_attribute(
+                "metadata", obj, self.base_translator.get_metadata
+            )
+
+        def get_owners(self, obj: Any) -> Sequence[str]:
+            return self.resolving_info.get_resolved_attribute(
+                "owners", obj, self.base_translator.get_owners
+            )
+
+        def get_code_version(self, obj: Any) -> Optional[str]:
+            version = self.resolving_info.get_resolved_attribute(
+                "code_version", obj, self.base_translator.get_code_version
+            )
+            return str(version) if version else None
+
+        def get_description(self, obj: Any) -> Optional[str]:
+            return self.resolving_info.get_resolved_attribute(
+                "description", obj, self.base_translator.get_description
+            )
+
+        def get_deps_asset_key(self, obj: Any) -> Iterable[AssetKey]:
+            return [
+                AssetKey.from_user_string(dep) if isinstance(dep, str) else dep
+                for dep in self.resolving_info.get_resolved_attribute(
+                    "deps", obj, self.base_translator.get_deps_asset_key
+                )
+            ]
+
+    return WrappedTranslator
+
+
+def load_module_from_path(module_name, path) -> ModuleType:
+    # Create a spec from the file path
+    spec = importlib.util.spec_from_file_location(module_name, path)
+    if spec is None:
+        raise ImportError(f"Cannot create a module spec from path: {path}")
+
+    # Create and load the module
+    module = importlib.util.module_from_spec(spec)
+    assert spec.loader, "Must have a loader"
+    spec.loader.exec_module(module)
+    return module
+
+
+# ########################
+# ##### PLATFORM
+# ########################
+
+
+def is_windows() -> bool:
+    return sys.platform == "win32"
+
+
+def is_macos() -> bool:
+    return sys.platform == "darwin"
+
+
+# ########################
+# ##### VENV
+# ########################
+
+
+def get_venv_executable(venv_dir: Path, executable: str = "python") -> Path:
+    if is_windows():
+        return venv_dir / "Scripts" / f"{executable}.exe"
+    else:
+        return venv_dir / "bin" / executable
+
+
+def install_to_venv(venv_dir: Path, install_args: list[str]) -> None:
+    executable = get_venv_executable(venv_dir)
+    command = ["uv", "pip", "install", "--python", str(executable), *install_args]
+    subprocess.run(command, check=True)
