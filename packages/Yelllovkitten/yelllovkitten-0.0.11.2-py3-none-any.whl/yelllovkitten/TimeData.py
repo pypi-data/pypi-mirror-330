1be5c7ce42ec59b655885fd4bbdcca66a373397f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-28 05:06:45 +0000

    Add package Yelllovkitten/0.0.11.2/yelllovkitten-0.0.11.2-py3-none-any.whl

---
+++ packages/Yelllovkitten/yelllovkitten-0.0.11.2-py3-none-any.whl/yelllovkitten/TimeData.py
@@ -0,0 +1,37 @@
+class TimeData:
+    def __init__(self, utc_edit_hours: int=0, time12: bool=True, mdy: bool=True, cdat: str=' | ', end: str='', data: bool=True, sec: bool=False):
+        self.utc_edit_hours = utc_edit_hours
+        self.time12 = time12
+        self.mdy = mdy
+        self.cdat = cdat
+        self.end = end
+        self.data = data
+        self.sec = sec
+
+def timedata(settings: TimeData=None, utc_edit_hours: int=0, time12: bool=True, mdy: bool=True, cdat: str=' | ', end: str='',data: bool=True,sec: bool=False):
+    '''
+    TimeData
+    :param settings: class yelllovkitten.TimeData.TimeData
+    :param utc_edit_hours: add or remove hours to utc
+    :param time12: twelve-hour format
+    :param mdy: date format MDY if True else YMD
+    :param cdat: text between date and time
+    :param end: the text at the end
+    :param data: show data if True
+    :param sec: show sec if True
+    :return:
+    '''
+    if settings is not None:
+        utc_edit_hours = settings.utc_edit_hours
+        time12 = settings.time12
+        mdy = settings.mdy
+        cdat = settings.cdat
+        end = settings.end
+        data = settings.data
+        sec = settings.sec
+    import datetime
+    time_now = datetime.datetime.now(datetime.timezone.utc)
+    time_now = time_now + datetime.timedelta(hours=utc_edit_hours)
+    if data: mdy_ymd = '%m-%d-%Y' if mdy else '%Y-%m-%d'
+    else: mdy_ymd = ''
+    return time_now.strftime(f'{mdy_ymd}{cdat}{'%I' if time12 else '%H'}:%M{':%S' if sec else ''}{'%p' if time12 else ''}{end}')
---
+++ packages/Yelllovkitten/yelllovkitten-0.0.11.2-py3-none-any.whl/yelllovkitten/__init__.py
@@ -0,0 +1,3 @@
+from yelllovkitten import text
+from yelllovkitten import protection
+from yelllovkitten import TimeData
