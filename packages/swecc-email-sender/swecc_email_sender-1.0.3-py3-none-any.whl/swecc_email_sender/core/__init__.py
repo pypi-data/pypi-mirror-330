33de880c68ccd49a23a4e6a969dfcd1f022d2d09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-28 15:05:28 +0000

    Add package swecc-email-sender/1.0.3/swecc_email_sender-1.0.3-py3-none-any.whl

---
+++ packages/swecc-email-sender/swecc_email_sender-1.0.3-py3-none-any.whl/swecc_email_sender/core/__init__.py
@@ -0,0 +1,8 @@
+"""
+Core functionality.
+"""
+
+from swecc_email_sender.core.loader import DataLoader
+from swecc_email_sender.core.sender import EmailSender
+
+__all__ = ["DataLoader", "EmailSender"]
