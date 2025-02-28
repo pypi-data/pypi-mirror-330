916ed5f43d1d167ce1e718b78fc1f244950f2ecc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-28 06:06:03 +0000

    Add package libkvikio-cu12/25.2.1/libkvikio_cu12-25.2.1-py3-none-manylinux_2_28_aarch64.whl

---
+++ packages/libkvikio-cu12/libkvikio_cu12-25.2.1-py3-none-manylinux_2_28_aarch64.whl/libkvikio/include/kvikio/error.hpp
@@ -0,0 +1,120 @@
+/*
+ * Copyright (c) 2021-2025, NVIDIA CORPORATION.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#pragma once
+
+#include <cstring>
+#include <exception>
+#include <system_error>
+
+#include <kvikio/shim/cuda.hpp>
+#include <kvikio/shim/cufile_h_wrapper.hpp>
+
+namespace kvikio {
+
+struct CUfileException : public std::runtime_error {
+  using std::runtime_error::runtime_error;
+};
+
+#ifndef CUDA_DRIVER_TRY
+#define CUDA_DRIVER_TRY(...)                                                   \
+  GET_CUDA_DRIVER_TRY_MACRO(__VA_ARGS__, CUDA_DRIVER_TRY_2, CUDA_DRIVER_TRY_1) \
+  (__VA_ARGS__)
+#define GET_CUDA_DRIVER_TRY_MACRO(_1, _2, NAME, ...) NAME
+#define CUDA_DRIVER_TRY_2(_call, _exception_type)                                  \
+  do {                                                                             \
+    kvikio::detail::cuda_driver_try_2<_exception_type>(_call, __LINE__, __FILE__); \
+  } while (0)
+#define CUDA_DRIVER_TRY_1(_call) CUDA_DRIVER_TRY_2(_call, kvikio::CUfileException)
+#endif
+
+#ifndef CUFILE_TRY
+#define CUFILE_TRY(...)                                         \
+  GET_CUFILE_TRY_MACRO(__VA_ARGS__, CUFILE_TRY_2, CUFILE_TRY_1) \
+  (__VA_ARGS__)
+#define GET_CUFILE_TRY_MACRO(_1, _2, NAME, ...) NAME
+#define CUFILE_TRY_2(_call, _exception_type)                                  \
+  do {                                                                        \
+    kvikio::detail::cufile_try_2<_exception_type>(_call, __LINE__, __FILE__); \
+  } while (0)
+#define CUFILE_TRY_1(_call) CUFILE_TRY_2(_call, kvikio::CUfileException)
+#endif
+
+#ifndef CUFILE_CHECK_BYTES_DONE
+#define CUFILE_CHECK_BYTES_DONE(...)                                   \
+  GET_CUFILE_CHECK_BYTES_DONE_MACRO(                                   \
+    __VA_ARGS__, CUFILE_CHECK_BYTES_DONE_2, CUFILE_CHECK_BYTES_DONE_1) \
+  (__VA_ARGS__)
+#define GET_CUFILE_CHECK_BYTES_DONE_MACRO(_1, _2, NAME, ...) NAME
+#define CUFILE_CHECK_BYTES_DONE_2(_nbytes_done, _exception_type)                                  \
+  do {                                                                                            \
+    kvikio::detail::cufile_check_bytes_done_2<_exception_type>(_nbytes_done, __LINE__, __FILE__); \
+  } while (0)
+#define CUFILE_CHECK_BYTES_DONE_1(_call) CUFILE_CHECK_BYTES_DONE_2(_call, kvikio::CUfileException)
+#endif
+
+namespace detail {
+template <typename Exception>
+void cuda_driver_try_2(CUresult error, int line_number, const char* filename)
+{
+  if (error == CUDA_ERROR_STUB_LIBRARY) {
+    throw Exception{std::string{"CUDA error at: "} + std::string(filename) + ":" +
+                    KVIKIO_STRINGIFY(line_number) +
+                    ": CUDA_ERROR_STUB_LIBRARY("
+                    "The CUDA driver loaded is a stub library)"};
+  }
+  if (error != CUDA_SUCCESS) {
+    const char* err_name     = nullptr;
+    const char* err_str      = nullptr;
+    CUresult err_name_status = cudaAPI::instance().GetErrorName(error, &err_name);
+    CUresult err_str_status  = cudaAPI::instance().GetErrorString(error, &err_str);
+    if (err_name_status == CUDA_ERROR_INVALID_VALUE) { err_name = "unknown"; }
+    if (err_str_status == CUDA_ERROR_INVALID_VALUE) { err_str = "unknown"; }
+    throw Exception{std::string{"CUDA error at: "} + filename + ":" +
+                    KVIKIO_STRINGIFY(line_number) + ": " + std::string(err_name) + "(" +
+                    std::string(err_str) + ")"};
+  }
+}
+
+template <typename Exception>
+void cufile_try_2(CUfileError_t error, int line_number, const char* filename)
+{
+  if (error.err != CU_FILE_SUCCESS) {
+    if (error.err == CU_FILE_CUDA_DRIVER_ERROR) {
+      CUresult const cuda_error = error.cu_err;
+      CUDA_DRIVER_TRY(cuda_error);
+    }
+    throw Exception{std::string{"cuFile error at: "} + filename + ":" +
+                    KVIKIO_STRINGIFY(line_number) + ": " +
+                    cufileop_status_error((CUfileOpError)std::abs(error.err))};
+  }
+}
+
+template <typename Exception>
+void cufile_check_bytes_done_2(ssize_t nbytes_done, int line_number, const char* filename)
+{
+  if (nbytes_done < 0) {
+    auto const err = std::abs(nbytes_done);
+    auto const msg = (err > CUFILEOP_BASE_ERR)
+                       ? std::string(cufileop_status_error((CUfileOpError)err))
+                       : std::string(std::strerror(err));
+    throw Exception{std::string{"cuFile error at: "} + filename + ":" +
+                    KVIKIO_STRINGIFY(line_number) + ": " + msg};
+  }
+}
+
+}  // namespace detail
+
+}  // namespace kvikio
---
+++ packages/libkvikio-cu12/libkvikio_cu12-25.2.1-py3-none-manylinux_2_28_aarch64.whl/libkvikio/include/kvikio/utils.hpp
@@ -0,0 +1,266 @@
+/*
+ * Copyright (c) 2021-2025, NVIDIA CORPORATION.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#pragma once
+
+#include <chrono>
+#include <cstring>
+#include <future>
+#include <optional>
+#include <stdexcept>
+#include <tuple>
+#include <type_traits>
+
+#ifdef KVIKIO_CUDA_FOUND
+#include <nvtx3/nvtx3.hpp>
+#endif
+
+#include <kvikio/shim/cuda.hpp>
+
+namespace kvikio {
+
+// cuFile defines a page size to 4 KiB
+inline constexpr std::size_t page_size = 4096;
+
+[[nodiscard]] off_t convert_size2off(std::size_t x);
+
+[[nodiscard]] ssize_t convert_size2ssize(std::size_t x);
+
+[[nodiscard]] CUdeviceptr convert_void2deviceptr(const void* devPtr);
+
+/**
+ * @brief Help function to convert value to 64 bit signed integer
+ */
+template <typename T, std::enable_if_t<std::is_integral_v<T>>* = nullptr>
+[[nodiscard]] std::int64_t convert_to_64bit(T value)
+{
+  if constexpr (std::numeric_limits<T>::max() > std::numeric_limits<std::int64_t>::max()) {
+    if (value > std::numeric_limits<std::int64_t>::max()) {
+      throw std::overflow_error("convert_to_64bit(x): x too large to fit std::int64_t");
+    }
+  }
+  return std::int64_t(value);
+}
+
+/**
+ * @brief Help function to convert value to 64 bit float
+ */
+template <typename T, std::enable_if_t<std::is_floating_point_v<T>>* = nullptr>
+[[nodiscard]] double convert_to_64bit(T value)
+{
+  return double(value);
+}
+
+/**
+ * @brief Check if `ptr` points to host memory (as opposed to device memory)
+ *
+ * In this context, managed memory counts as device memory
+ *
+ * @param ptr Memory pointer to query
+ * @return The boolean answer
+ */
+#ifdef KVIKIO_CUDA_FOUND
+bool is_host_memory(const void* ptr);
+#else
+constexpr bool is_host_memory(const void* ptr) { return true; }
+#endif
+
+/**
+ * @brief Return the device owning the pointer
+ *
+ * @param ptr Device pointer to query
+ * @return The device ordinal
+ */
+[[nodiscard]] int get_device_ordinal_from_pointer(CUdeviceptr dev_ptr);
+
+/**
+ * @brief Given a device ordinal, return the primary context of the device.
+ *
+ * This function caches the primary contexts retrieved until program exit
+ *
+ * @param ordinal Device ordinal - an integer between 0 and the number of CUDA devices
+ * @return Primary CUDA context
+ */
+[[nodiscard]] KVIKIO_EXPORT CUcontext get_primary_cuda_context(int ordinal);
+
+/**
+ * @brief Return the CUDA context associated the given device pointer, if any.
+ *
+ * @param dev_ptr Device pointer to query
+ * @return Usable CUDA context, if one were found.
+ */
+[[nodiscard]] std::optional<CUcontext> get_context_associated_pointer(CUdeviceptr dev_ptr);
+
+/**
+ * @brief Check if the current CUDA context can access the given device pointer
+ *
+ * @param dev_ptr Device pointer to query
+ * @return The boolean answer
+ */
+[[nodiscard]] bool current_context_can_access_pointer(CUdeviceptr dev_ptr);
+
+/**
+ * @brief Return a CUDA context that can be used with the given device pointer
+ *
+ * For robustness, we look for an usabale context in the following order:
+ *   1) If a context has been associated with `devPtr`, it is returned.
+ *   2) If the current context exists and can access `devPtr`, it is returned.
+ *   3) Return the primary context of the device that owns `devPtr`. We assume the
+ *      primary context can access `devPtr`, which might not be true in the exceptional
+ *      disjoint addressing cases mention in the CUDA docs[1]. In these cases, the user
+ *      has to set an usable current context before reading/writing using KvikIO.
+ *
+ * [1] <https://docs.nvidia.com/cuda/cuda-driver-api/group__CUDA__UNIFIED.html>
+ *
+ * @param devPtr Device pointer to query
+ * @return Usable CUDA context
+ */
+[[nodiscard]] CUcontext get_context_from_pointer(const void* devPtr);
+
+/**
+ * @brief Push CUDA context on creation and pop it on destruction
+ */
+class PushAndPopContext {
+ private:
+  CUcontext _ctx;
+
+ public:
+  PushAndPopContext(CUcontext ctx);
+  PushAndPopContext(const PushAndPopContext&)            = delete;
+  PushAndPopContext& operator=(PushAndPopContext const&) = delete;
+  PushAndPopContext(PushAndPopContext&&)                 = delete;
+  PushAndPopContext&& operator=(PushAndPopContext&&)     = delete;
+  ~PushAndPopContext();
+};
+
+// Find the base and offset of the memory allocation `devPtr` is in
+std::tuple<void*, std::size_t, std::size_t> get_alloc_info(const void* devPtr,
+                                                           CUcontext* ctx = nullptr);
+
+template <typename T>
+bool is_future_done(const T& future)
+{
+  return future.wait_for(std::chrono::seconds(0)) != std::future_status::timeout;
+}
+
+#ifdef KVIKIO_CUDA_FOUND
+/**
+ * @brief Tag type for libkvikio's NVTX domain.
+ */
+struct libkvikio_domain {
+  static constexpr char const* name{"libkvikio"};
+};
+
+// Macro to concatenate two tokens x and y.
+#define KVIKIO_CONCAT_HELPER(x, y) x##y
+#define KVIKIO_CONCAT(x, y)        KVIKIO_CONCAT_HELPER(x, y)
+
+// Macro to create a static, registered string that will not have a name conflict with any
+// registered string defined in the same scope.
+#define KVIKIO_REGISTER_STRING(msg)                                        \
+  [](const char* a_msg) -> auto& {                                         \
+    static nvtx3::registered_string_in<libkvikio_domain> a_reg_str{a_msg}; \
+    return a_reg_str;                                                      \
+  }(msg)
+
+// Macro overloads of KVIKIO_NVTX_FUNC_RANGE
+#define KVIKIO_NVTX_FUNC_RANGE_IMPL() NVTX3_FUNC_RANGE_IN(libkvikio_domain)
+
+#define KVIKIO_NVTX_SCOPED_RANGE_IMPL(msg, val)                                        \
+  nvtx3::scoped_range_in<libkvikio_domain> KVIKIO_CONCAT(_kvikio_nvtx_range, __LINE__) \
+  {                                                                                    \
+    nvtx3::event_attributes                                                            \
+    {                                                                                  \
+      KVIKIO_REGISTER_STRING(msg), nvtx3::payload { convert_to_64bit(val) }            \
+    }                                                                                  \
+  }
+
+#define KVIKIO_NVTX_MARKER_IMPL(msg, val) \
+  nvtx3::mark_in<libkvikio_domain>(       \
+    nvtx3::event_attributes{KVIKIO_REGISTER_STRING(msg), nvtx3::payload{convert_to_64bit(val)}})
+
+#endif
+
+/**
+ * @brief Convenience macro for generating an NVTX range in the `libkvikio` domain
+ * from the lifetime of a function.
+ *
+ * Takes no argument. The name of the immediately enclosing function returned by `__func__` is used
+ * as the message.
+ *
+ * Example:
+ * ```
+ * void some_function(){
+ *    KVIKIO_NVTX_FUNC_RANGE();  // The name `some_function` is used as the message
+ *    ...
+ * }
+ * ```
+ */
+#ifdef KVIKIO_CUDA_FOUND
+#define KVIKIO_NVTX_FUNC_RANGE() KVIKIO_NVTX_FUNC_RANGE_IMPL()
+#else
+#define KVIKIO_NVTX_FUNC_RANGE(...) \
+  do {                              \
+  } while (0)
+#endif
+
+/**
+ * @brief Convenience macro for generating an NVTX scoped range in the `libkvikio` domain to
+ * annotate a time duration.
+ *
+ * Takes two arguments (message, payload).
+ *
+ * Example:
+ * ```
+ * void some_function(){
+ *    KVIKIO_NVTX_SCOPED_RANGE("my function", 42);
+ *    ...
+ * }
+ * ```
+ */
+#ifdef KVIKIO_CUDA_FOUND
+#define KVIKIO_NVTX_SCOPED_RANGE(msg, val) KVIKIO_NVTX_SCOPED_RANGE_IMPL(msg, val)
+#else
+#define KVIKIO_NVTX_SCOPED_RANGE(msg, val) \
+  do {                                     \
+  } while (0)
+#endif
+
+/**
+ * @brief Convenience macro for generating an NVTX marker in the `libkvikio` domain to annotate a
+ * certain time point.
+ *
+ * Takes two arguments (message, payload). Use this macro to annotate asynchronous I/O operations,
+ * where the payload refers to the I/O size.
+ *
+ * Example:
+ * ```
+ * std::future<void> some_function(){
+ *     size_t io_size{2077};
+ *     KVIKIO_NVTX_MARKER("I/O operation", io_size);
+ *     perform_async_io_operation(io_size);
+ *     ...
+ * }
+ * ```
+ */
+#ifdef KVIKIO_CUDA_FOUND
+#define KVIKIO_NVTX_MARKER(message, payload) KVIKIO_NVTX_MARKER_IMPL(message, payload)
+#else
+#define KVIKIO_NVTX_MARKER(message, payload) \
+  do {                                       \
+  } while (0)
+#endif
+
+}  // namespace kvikio
