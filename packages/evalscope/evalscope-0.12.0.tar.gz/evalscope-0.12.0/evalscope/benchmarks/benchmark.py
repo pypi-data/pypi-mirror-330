3c493b1d8645cecb9f4c85046f0e2a55e7eca84e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-28 04:05:03 +0000

    Add package evalscope/0.12.0/evalscope-0.12.0.tar.gz

---
+++ packages/evalscope/evalscope-0.12.0.tar.gz/evalscope-0.12.0/evalscope/benchmarks/benchmark.py
@@ -0,0 +1,78 @@
+import copy
+from dataclasses import dataclass, field
+from typing import TYPE_CHECKING, Dict, List, Optional
+
+if TYPE_CHECKING:
+    from evalscope.benchmarks import DataAdapter
+
+from evalscope.models import BaseModelAdapter
+
+BENCHMARK_MAPPINGS = {}
+
+
+@dataclass
+class BenchmarkMeta:
+    name: str
+    dataset_id: str
+    data_adapter: 'DataAdapter'
+    model_adapter: BaseModelAdapter
+    subset_list: List[str] = field(default_factory=list)
+    metric_list: List[str] = field(default_factory=list)
+    few_shot_num: int = 0
+    few_shot_random: bool = False
+    train_split: Optional[str] = None
+    eval_split: Optional[str] = None
+    prompt_template: Optional[str] = None
+    system_prompt: Optional[str] = None
+    query_template: Optional[str] = None
+    pretty_name: Optional[str] = None
+
+    def _update(self, args: dict):
+        if args.get('local_path'):
+            self.dataset_id = args['local_path']
+            del args['local_path']
+        self.__dict__.update(args)
+
+    def to_dict(self) -> dict:
+        return self.__dict__
+
+    def to_string_dict(self) -> dict:
+        cur_dict = copy.deepcopy(self.__dict__)
+        # cur_dict['data_adapter'] = self.data_adapter.__name__
+        # cur_dict['model_adapter'] = self.model_adapter.__name__
+        # cur_dict['metric_list'] = [metric['name'] for metric in self.metric_list]
+        del cur_dict['data_adapter']
+        del cur_dict['model_adapter']
+        return cur_dict
+
+    def get_data_adapter(self, config: dict = {}) -> 'DataAdapter':
+        if config:
+            self._update(config)
+
+        data_adapter = self.data_adapter(**self.to_dict())
+        return data_adapter
+
+
+class Benchmark:
+
+    def __init__(self):
+        pass
+
+    @classmethod
+    def get(cls, name: str) -> 'BenchmarkMeta':
+        if name not in BENCHMARK_MAPPINGS:
+            raise Exception(f'Unknown benchmark: {name}. Available tasks: {list(BENCHMARK_MAPPINGS.keys())}')
+        benchmark = BENCHMARK_MAPPINGS[name]
+        return benchmark
+
+    @classmethod
+    def register(cls, name: str, dataset_id: str, model_adapter: BaseModelAdapter, **kwargs):
+
+        def register_wrapper(data_adapter):
+            if name in BENCHMARK_MAPPINGS:
+                raise Exception(f'Benchmark {name} already registered')
+            BENCHMARK_MAPPINGS[name] = BenchmarkMeta(
+                name=name, data_adapter=data_adapter, model_adapter=model_adapter, dataset_id=dataset_id, **kwargs)
+            return data_adapter
+
+        return register_wrapper
---
+++ packages/evalscope/evalscope-0.12.0.tar.gz/evalscope-0.12.0/tests/cli/test_run.py
@@ -0,0 +1,254 @@
+# Copyright (c) Alibaba, Inc. and its affiliates.
+import os
+import subprocess
+import torch
+import unittest
+
+from evalscope.constants import EvalType
+from evalscope.run import run_task
+from evalscope.utils import is_module_installed, test_level_list
+from evalscope.utils.logger import get_logger
+
+os.environ['LOG_LEVEL'] = 'DEBUG'
+
+logger = get_logger()
+
+
+class TestRun(unittest.TestCase):
+
+    def setUp(self) -> None:
+        logger.info('Init env for evalscope native run UTs ...\n')
+        self._check_env('evalscope')
+
+    def tearDown(self) -> None:
+        pass
+
+    @staticmethod
+    def _check_env(module_name: str):
+        if is_module_installed(module_name):
+            logger.info(f'{module_name} is installed.')
+        else:
+            raise ModuleNotFoundError(f'run: pip install {module_name}')
+
+    @unittest.skipUnless(0 in test_level_list(), 'skip test in current test level')
+    def test_run_simple_eval(self):
+        model = 'qwen/Qwen2-0.5B-Instruct'
+        datasets = 'arc'  # arc ceval
+        limit = 10
+
+        cmd_simple = f'evalscope eval ' \
+                     f'--model {model} ' \
+                     f'--datasets {datasets} ' \
+                     f'--limit {limit}'
+
+        logger.info(f'Start to run command: {cmd_simple}')
+        run_res = subprocess.run(cmd_simple, text=True, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+        assert run_res.returncode == 0, f'Failed to run command: {cmd_simple}'
+        logger.info(f'>>test_run_simple_eval stdout: {run_res.stdout}')
+        logger.error(f'>>test_run_simple_eval stderr: {run_res.stderr}')
+
+    @unittest.skipUnless(0 in test_level_list(), 'skip test in current test level')
+    def test_run_eval_with_args(self):
+        model = 'qwen/Qwen2-0.5B-Instruct'
+        datasets = 'arc'  # arc ceval
+        limit = 5
+        dataset_args = '{"ceval": {"few_shot_num": 0, "few_shot_random": false}}'
+
+        cmd_with_args = f'evalscope eval ' \
+                        f'--model {model} ' \
+                        f'--datasets {datasets} ' \
+                        f'--limit {limit} ' \
+                        f'--generation-config do_sample=false,temperature=0.0 ' \
+                        f"""--dataset-args \'{dataset_args}\' """
+
+        logger.info(f'Start to run command: {cmd_with_args}')
+        run_res = subprocess.run(cmd_with_args, text=True, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+        assert run_res.returncode == 0, f'Failed to run command: {cmd_with_args}'
+        logger.info(f'>>test_run_eval_with_args stdout: {run_res.stdout}')
+        logger.error(f'>>test_run_eval_with_args stderr: {run_res.stderr}')
+
+    @unittest.skipUnless(0 in test_level_list(), 'skip test in current test level')
+    def test_run_task(self):
+        task_cfg = {'model': 'qwen/Qwen2-0.5B-Instruct',
+                    'datasets': [
+                        # 'mmlu_pro',
+                        # 'bbh',
+                        # 'hellaswag',
+                        'gsm8k',
+                        # 'arc',
+                        # 'race',
+                        # 'ifeval',
+                        # 'truthful_qa',
+                        # 'trivia_qa',
+                        ],
+                    'limit': 2,
+                    'eval_batch_size': 2,
+                    'debug': True}
+        run_task(task_cfg=task_cfg)
+
+
+    @unittest.skipUnless(0 in test_level_list(), 'skip test in current test level')
+    def test_run_custom_task(self):
+        from evalscope.config import TaskConfig
+
+        task_cfg = TaskConfig(
+            model='qwen/Qwen2-0.5B-Instruct',
+            datasets=['general_mcq', 'general_qa'],  # 数据格式，选择题格式固定为 'ceval'
+            dataset_args={
+                'general_mcq': {
+                    'local_path': 'custom_eval/text/mcq',  # 自定义数据集路径
+                    'subset_list': [
+                        'example'  # 评测数据集名称，上述 *_dev.csv 中的 *
+                    ],
+                    'query_template': 'Question: {question}\n{choices}\nAnswer: {answer}'  # 问题模板
+                },
+                'general_qa': {
+                    'local_path': 'custom_eval/text/qa',  # 自定义数据集路径
+                    'subset_list': [
+                        'example'  # 评测数据集名称，上述 *_dev.csv 中的 *
+                    ]
+                }
+            },
+        )
+        res = run_task(task_cfg=task_cfg)
+        print(res)
+
+    @unittest.skipUnless(0 in test_level_list(), 'skip test in current test level')
+    def test_run_humaneval(self):
+        from evalscope.config import TaskConfig
+
+        task_cfg = TaskConfig(
+            model='qwen/Qwen2-0.5B-Instruct',
+            datasets=[
+                # 'math_500',
+                # 'aime24',
+                'competition_math'
+            ],
+            dataset_args={
+                'competition_math': {
+                    'subset_list': ['Level 4', 'Level 5']
+                }
+            },
+            limit=5
+        )
+
+        run_task(task_cfg=task_cfg)
+
+    @unittest.skipUnless(0 in test_level_list(), 'skip test in current test level')
+    def test_run_server_model(self):
+        from evalscope.config import TaskConfig
+
+        task_cfg = TaskConfig(
+            model='Qwen2.5-0.5B-Instruct',
+            api_url='http://127.0.0.1:8801/v1',
+            api_key='EMPTY',
+            eval_type=EvalType.SERVICE,
+            datasets=[
+                'iquiz',
+                # 'ifeval',
+                # 'mmlu',
+                # 'mmlu_pro',
+                # 'musr',
+                # 'process_bench',
+                # 'race',
+                # 'trivia_qa',
+                # 'cmmlu',
+                # 'humaneval',
+                # 'gsm8k',
+                # 'bbh',
+                # 'competition_math',
+                # 'math_500',
+                # 'aime24',
+                # 'gpqa',
+                # 'arc',
+                # 'ceval',
+                # 'hellaswag',
+            ],
+            dataset_args={
+                'mmlu': {
+                    'subset_list': ['elementary_mathematics'],
+                    'few_shot_num': 0
+                },
+                'mmlu_pro': {
+                    'subset_list': ['math', 'health'],
+                    'few_shot_num': 4
+                },
+                'ceval': {
+                    'subset_list': [
+                        'computer_network', 'operating_system', 'computer_architecture'
+                    ],
+                    'few_shot_num': 0
+                },
+                'cmmlu': {
+                    'subset_list': ['elementary_chinese'],
+                    'few_shot_num': 0
+                },
+                'bbh': {
+                    'subset_list': ['word_sorting', 'movie_recommendation'],
+                },
+                'gpqa': {
+                    'subset_list': ['gpqa_diamond'],
+                    'few_shot_num': 0
+                },
+                'humaneval': {
+                    'metric_list': ['Pass@1', 'Pass@2', 'Pass@5'],
+                },
+                'competition_math': {
+                    'subset_list': ['Level 1']
+                },
+                'process_bench': {
+                    'subset_list': ['gsm8k'],
+                },
+                'musr': {
+                    'subset_list': ['murder_mysteries']
+                },
+            },
+            eval_batch_size=5,
+            limit=5,
+            debug=True,
+            stream=True,
+            generation_config={
+                'temperature': 0.7,
+                'n': 1,
+                'max_tokens': 512,
+            },
+            # use_cache='/mnt/data/data/user/maoyunlin.myl/eval-scope/outputs/20250212_150525',
+        )
+
+        run_task(task_cfg=task_cfg)
+
+
+    @unittest.skipUnless(0 in test_level_list(), 'skip test in current test level')
+    def test_run_batch_eval(self):
+        from evalscope.config import TaskConfig
+
+        task_cfg = TaskConfig(
+            model='LLM-Research/Llama-3.2-1B-Instruct',
+            datasets=[
+                # 'math_500',
+                # 'aime24',
+                # 'competition_math'
+                # 'arc',
+                'gsm8k'
+                # 'truthful_qa'
+            ],
+            dataset_args={
+                'competition_math': {
+                    'subset_list': ['Level 4', 'Level 5']
+                }
+            },
+            eval_batch_size=2,
+            limit=5,
+            generation_config={
+                'max_new_tokens': 2048,
+                'temperature': 0.7,
+                'num_return_sequences': 2,
+            }
+        )
+
+        run_task(task_cfg=task_cfg)
+
+if __name__ == '__main__':
+    unittest.main()
