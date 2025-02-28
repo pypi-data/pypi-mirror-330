5fdf064d745ed30f1a69e85789e71ac286a502d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-28 14:05:49 +0000

    Add package Weather-API-MASI/0.2/Weather_API_MASI-0.2.tar.gz

---
+++ packages/Weather-API-MASI/Weather_API_MASI-0.2.tar.gz/Weather_API_MASI-0.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Weather_API_MASI',
+    version='0.2',
+    packages=find_packages(),
+    install_requires=[
+        'requests>=2.31.0',
+        'selenium>=4.28.1',
+        'geopy>=2.4.1'
+    ],
+    entry_points={
+        "console_scripts": ["Weather-API-MASI = Weather_API_MASI.main:main"]
+    }
+)
