af2b7fc9348ce964f3161ea7079c84d513d74cde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-28 04:05:34 +0000

    Add package pytenter/0.1.0/pytenter-0.1.0.tar.gz

---
+++ packages/pytenter/pytenter-0.1.0.tar.gz/pytenter-0.1.0/setup.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""Builds package for release"""
+
+from setuptools import setup, find_packages
+
+setup(
+    name="pytenter",
+    version="0.1.0",  
+    description="pdfjinja fork with updated libraries",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    author="Timothy",
+    url="https://github.com/boiledsteak/pytenter",
+    packages=find_packages(),
+    install_requires=[
+        "jinja2==3.1.5",
+        "pdfminer.six==20240706",
+        "pypdf==5.3.0",
+        "Pillow==11.1.0",
+        "reportlab==4.3.1",
+        "fdfgen==0.16.1"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
