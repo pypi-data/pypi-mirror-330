9b90348a5ce252de5dd3c281b8328ce549445d2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-28 16:06:17 +0000

    Add package mood/0.0.11/mood-0.0.11.tar.gz

---
+++ packages/mood/mood-0.0.11.tar.gz/mood-0.0.11/mood/sentiment.py
@@ -0,0 +1,19 @@
+"""Sentiment analysis tools"""
+
+import json
+from dol import Pipe
+
+from mood.util import add_egress
+
+
+@add_egress(json.loads)
+def sentiment_analysis(headlines):
+    """Get sentiment analysis for headlines."""
+    from oa import ask
+
+    if not isinstance(headlines, str):  # Pattern: ingress
+        headlines = "\n\n".join(
+            headlines
+        )  # join with two newlines to separate headlines
+    headline_sentiments = ask.ai.sentiment_analysis(headlines)
+    return headline_sentiments
