4e2bf1a05557386811d4606267a23a1011dec3f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-28 14:05:55 +0000

    Add package briton/0.4.5.dev1/briton-0.4.5.dev1.tar.gz

---
+++ packages/briton/briton-0.4.5.dev1.tar.gz/briton-0.4.5.dev1/briton/proto/__init__.py
@@ -0,0 +1,32 @@
+from .briton_pb2 import (
+    AddedToken,
+    AddedTokens,
+    Batch,
+    BritonConfig,
+    InferenceRequest,
+    InferenceAnswerPart,
+    StatesToTokens,
+    TokenToNextState,
+    Tensor,
+    DataType,
+    FinishReason,
+    LookaheadDecodingConfig,
+)
+from .briton_pb2_grpc import BritonStub
+
+
+__all__ = [
+    "BritonStub",
+    "StatesToTokens",
+    "TokenToNextState",
+    "InferenceRequest",
+    "InferenceAnswerPart",
+    "Batch",
+    "BritonConfig",
+    "AddedToken",
+    "AddedTokens",
+    "Tensor",
+    "DataType",
+    "FinishReason",
+    "LookaheadDecodingConfig",
+]
