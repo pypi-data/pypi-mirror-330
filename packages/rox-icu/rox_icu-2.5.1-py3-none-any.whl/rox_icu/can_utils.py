516f7b3ca5bf52ff46f00d1c9ec5f42acd984990
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-28 04:04:55 +0000

    Add package rox-icu/2.5.1/rox_icu-2.5.1-py3-none-any.whl

---
+++ packages/rox-icu/rox_icu-2.5.1-py3-none-any.whl/rox_icu/can_utils.py
@@ -0,0 +1,117 @@
+#!/usr/bin/env python3
+"""
+ supporting functions for working with can
+
+ Copyright (c) 2024 ROX Automation - Jev Kuznetsov
+"""
+
+import os
+import struct
+from typing import List, NamedTuple, Type
+from can.interfaces.socketcan import SocketcanBus
+from can.interfaces.udp_multicast import UdpMulticastBus
+from cantools.database import Database, Message, Signal, dump_file
+
+import rox_icu.can_protocol as canp
+
+
+def is_ci_environment() -> bool:
+    """Check if the code is running in a CI environment"""
+    return os.getenv("CI") == "true"
+
+
+def get_can_bus(bus_type: str = "env") -> UdpMulticastBus | SocketcanBus:
+    """Get a CAN bus instance"""
+
+    match bus_type:
+        case "udp_multicast":
+            return UdpMulticastBus("224.0.0.1", interface="udp_multicast")
+
+        case "env":
+
+            channel = os.getenv("CAN_CHANNEL")
+            interface = os.getenv("CAN_INTERFACE", "socketcan")
+            if channel is None:
+                raise ValueError(
+                    "Missing CAN_CHANNEL environment variable, set to can0 or similar"
+                )
+            if interface == "socketcan":
+                return SocketcanBus(
+                    channel=channel, interface="socketcan", receive_own_messages=False
+                )
+            elif interface == "udp_multicast":
+                return UdpMulticastBus(channel, interface="udp_multicast")
+            else:
+                raise ValueError(f"Unsupported CAN interface: {interface}")
+
+        case _:  # just use the bus_type as the channel
+
+            return SocketcanBus(
+                channel=bus_type, interface="socketcan", receive_own_messages=False
+            )
+
+
+# -----------------Database Functions-----------------
+def byte_def_to_size(struct_byte_def: str) -> int:
+    """Convert byte definition to number of bytes using struct.calcsize()."""
+    return struct.calcsize(struct_byte_def)
+
+
+def create_signals_from_namedtuple(
+    namedtuple_type: Type[NamedTuple], format_string: str
+) -> List[Signal]:
+
+    signals = []
+    offset = 0
+
+    # Remove byte order character for processing individual formats
+    if format_string[0] in "@=<>!":
+        format_string = format_string[1:]
+
+    for field_name, fmt in zip(namedtuple_type._fields, format_string):  # type: ignore
+        # Calculate the bit length of the field
+        bit_length = struct.calcsize(fmt) * 8
+
+        # Determine if the field is signed
+        is_signed = fmt.islower() and fmt not in "?"
+
+        # Create the Signal object
+        signal = Signal(
+            name=field_name,
+            start=offset,
+            length=bit_length,
+            byte_order="little_endian",  # Adjust based on your format string
+            is_signed=is_signed,
+        )
+        signals.append(signal)
+
+        # Update the offset for the next field
+        offset += bit_length
+
+    return signals
+
+
+def create_dbc(node_id: int, filename: str | None = None) -> Database:
+    """Create a DBC file for a given node ID"""
+
+    # Create a new database
+    db = Database(version=str(canp.VERSION))
+
+    for msg in canp.MSG_DEFS.keys():
+        opcode, byte_def = canp.get_opcode_and_bytedef(msg)
+        msg_id = canp.generate_message_id(node_id, opcode)
+        signals = create_signals_from_namedtuple(msg, byte_def)
+
+        db_msg = Message(
+            frame_id=msg_id,
+            name="ICU_" + msg.__name__,
+            length=byte_def_to_size(byte_def),
+            signals=signals,
+        )
+
+        db.messages.append(db_msg)
+
+    if filename is not None:
+        dump_file(db, filename)
+
+    return db
---
+++ packages/rox-icu/rox_icu-2.5.1-py3-none-any.whl/rox_icu/cli.py
@@ -0,0 +1,127 @@
+#!/usr/bin/env python3
+"""
+rox_icu CLI
+"""
+
+
+from pathlib import Path
+import os
+import can
+import click
+
+import rox_icu.can_protocol as canp
+from rox_icu import __version__
+from rox_icu.can_utils import get_can_bus
+from rox_icu.utils import run_main
+
+
+@click.group()
+@click.version_option(version=__version__)
+def cli() -> None:
+    pass  # pragma: no cover
+
+
+@cli.command()
+def info() -> None:
+    """Print package info"""
+    print(__version__)
+
+
+@cli.command()
+@click.option("--node-id", default=1, help="device ID")
+@click.option("--channel", "-c", help="CAN bus name", default="vcan0")
+def mock(
+    node_id: int,
+    channel: str,
+):
+    # set env variable for can bus
+    os.environ["CAN_CHANNEL"] = channel
+
+    import rox_icu.mock as mock
+
+    run_main(lambda: mock.main(node_id))
+
+
+@cli.command()
+def monitor():
+    """Monitor ICU devices on CAN bus"""
+    import curses
+
+    import rox_icu.monitor as icu_monitor
+
+    curses.wrapper(icu_monitor.main)
+
+
+@cli.command()
+@click.argument("node_id", type=int)
+@click.option("--channel", "-c", default="vcan0")
+def inspect(node_id: int, channel: str) -> None:
+    """Inspect ICU messages on CAN bus for a specific node"""
+    from candbg import inspector
+
+    from rox_icu.can_utils import create_dbc
+
+    # note: tried to pass dbc object directly but for some reason that
+    # does not work. Spent 1.5 hours trying to figure out why, but no luck.
+    # So, for now, just create a temporary DBC file and use that.
+    tmp_file = "/tmp/icu.dbc"
+
+    create_dbc(node_id, filename=tmp_file)
+    dbc_pth = Path(tmp_file)
+    assert dbc_pth.exists(), f"Failed to create DBC file: {dbc_pth}"
+
+    try:
+        inspector.main(dbc_pth, channel=channel)
+    except KeyboardInterrupt:
+        pass
+
+
+@cli.command()
+@click.argument("node_id", type=int)
+@click.argument("hex_input")
+def output(node_id: int, hex_input: str) -> None:
+    """Set output state, provide hex value"""
+    try:
+        state = int(hex_input, 16)
+
+    except ValueError:
+        print("Invalid hex input. Please provide a valid hexadecimal value.")
+        return
+
+    # for now, simply put packet on the bus, without checking if device is alive
+
+    print(f"Setting ICU_{node_id} output state to {state:08b}")
+
+    with get_can_bus() as bus:
+        arb_id, data = canp.encode_message(canp.SetIoStateMessage(state), node_id)
+        msg = can.Message(arbitration_id=arb_id, data=data, is_extended_id=False)
+        bus.send(msg)
+
+
+@cli.command()
+@click.argument("node_id", type=int)
+@click.option("--channel", "-c", default="env")
+def clear_errors(node_id: int, channel: str) -> None:
+    """Clear errors on a specific ICU device"""
+    with get_can_bus(channel) as bus:
+        arb_id, data = canp.encode_message(
+            canp.CommandMessage(canp.Commands.CLEAR_ERRORS), node_id
+        )
+        msg = can.Message(arbitration_id=arb_id, data=data, is_extended_id=False)
+        bus.send(msg)
+
+
+@cli.command()
+@click.argument("node_id", type=int)
+@click.argument("command", type=int)
+@click.option("--channel", "-c", default="env")
+def command(node_id: int, command: int, channel: str) -> None:
+    """send numeric command"""
+    with get_can_bus(channel) as bus:
+        arb_id, data = canp.encode_message(canp.CommandMessage(command), node_id)
+        msg = can.Message(arbitration_id=arb_id, data=data, is_extended_id=False)
+        bus.send(msg)
+
+
+if __name__ == "__main__":
+    cli()  # pragma: no cover
