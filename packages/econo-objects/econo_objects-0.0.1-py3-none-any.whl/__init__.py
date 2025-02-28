3eceb68e5cd43f79b12fb2e50f622c92f16efa27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-28 14:06:16 +0000

    Add package econo-objects/0.0.1/econo_objects-0.0.1-py3-none-any.whl

---
+++ packages/econo-objects/econo_objects-0.0.1-py3-none-any.whl/__init__.py
@@ -0,0 +1,9 @@
+"""
+This module intializes the econo-objects package and all its modules
+"""
+from.econo_objects import Keys
+from.econo_objects import FredObject
+from.econo_objects import TimeSeriesAnalysisObject
+from.econo_objects import CurrencyPairObject
+from.econo_objects import YieldSpreadObject
+from.econo_objects import CompositeIndexObject
