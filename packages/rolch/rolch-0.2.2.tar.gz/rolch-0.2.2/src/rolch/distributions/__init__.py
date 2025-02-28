94300c42dd2edaf530300541bbdb92e52371d060
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-28 05:04:54 +0000

    Add package rolch/0.2.2/rolch-0.2.2.tar.gz

---
+++ packages/rolch/rolch-0.2.2.tar.gz/rolch-0.2.2/src/rolch/distributions/__init__.py
@@ -0,0 +1,11 @@
+from .gamma import DistributionGamma
+from .johnsonsu import DistributionJSU
+from .normal import DistributionNormal
+from .studentt import DistributionT
+
+__all__ = [
+    "DistributionNormal",
+    "DistributionT",
+    "DistributionJSU",
+    "DistributionGamma",
+]
