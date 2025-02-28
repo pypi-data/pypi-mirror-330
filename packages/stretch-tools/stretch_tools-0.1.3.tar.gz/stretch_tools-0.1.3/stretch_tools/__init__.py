33ff700e7e3cb2bad29b7f19d125f8306f46906c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-28 14:05:38 +0000

    Add package stretch-tools/0.1.3/stretch_tools-0.1.3.tar.gz

---
+++ packages/stretch-tools/stretch_tools-0.1.3.tar.gz/stretch_tools-0.1.3/stretch_tools/__init__.py
@@ -0,0 +1,8 @@
+"""Initialize the smmap package"""
+
+__author__ = "Morgan Goeller"
+__contact__ = "stretch@morgan.gg"
+__homepage__ = "https://github.com/the-stretch"
+
+# make everything available in root package for convenience
+from .tools import *
