b071794452a9b12bc3590b55119af829527f77b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-28 06:06:01 +0000

    Add package stretch-tools/0.1.1/stretch_tools-0.1.1.tar.gz

---
+++ packages/stretch-tools/stretch_tools-0.1.1.tar.gz/stretch_tools-0.1.1/hello.py
@@ -0,0 +1,14 @@
+# /// script
+# requires-python = ">=3.12"
+# dependencies = [
+#     "elasticsearch",
+#     "elasticsearch-dsl",
+#     "pandas",
+# ]
+# ///
+def main():
+    print("Hello from the-stretch-tools!")
+
+
+if __name__ == "__main__":
+    main()
