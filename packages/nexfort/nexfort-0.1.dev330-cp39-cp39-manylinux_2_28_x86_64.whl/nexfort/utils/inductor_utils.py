bc51e3914651c750e250d03aa5d77915834d6698
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-28 05:05:19 +0000

    Add package nexfort/0.1.dev330/nexfort-0.1.dev330-cp39-cp39-manylinux_2_28_x86_64.whl

---
+++ packages/nexfort/nexfort-0.1.dev330-cp39-cp39-manylinux_2_28_x86_64.whl/nexfort/utils/inductor_utils.py
@@ -0,0 +1,7 @@
+def reset_async_compile_process_pool():
+    from torch._inductor.codecache import AsyncCompile
+    if AsyncCompile.process_pool.cache_info().currsize > 0:
+        pool = AsyncCompile.process_pool()
+        pool.shutdown(wait=True)
+        AsyncCompile.process_pool.cache_clear()
+        AsyncCompile.warm_pool()
---
+++ packages/nexfort/nexfort-0.1.dev330-cp39-cp39-manylinux_2_28_x86_64.whl/nexfort/utils/types.py
@@ -0,0 +1,10 @@
+import torch
+
+def is_type(dtype, t):
+    return getattr(torch, t, None) == dtype
+
+def is_fp8_type(dtype):
+    return dtype.is_floating_point and dtype.itemsize == 1
+
+def is_8bit_type(dtype):
+    return dtype.itemsize == 1
