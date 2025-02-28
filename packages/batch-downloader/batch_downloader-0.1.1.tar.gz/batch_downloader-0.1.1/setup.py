e683c9660318b19d78423547d2dd3f579ac56691
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-28 04:05:51 +0000

    Add package batch-downloader/0.1.1/batch_downloader-0.1.1.tar.gz

---
+++ packages/batch-downloader/batch_downloader-0.1.1.tar.gz/batch_downloader-0.1.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+setup(
+    name="batch_downloader",
+    version="0.1.1",
+    author="zhenpingzhan",
+    author_email="stallzhan@gmail.com",
+    description="A simple batch downloader with chunked downloads",
+    long_description=Path("README.md").read_text(encoding="utf-8"),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Zzhenping/batch_downloader",
+    packages=find_packages(),
+    install_requires=[
+        "requests>=2.25.1",
+        "aiohttp>=3.8.0",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+)
