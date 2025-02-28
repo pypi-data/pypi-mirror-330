0e6262687495da183f03e8c4937724281e9618ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-28 06:05:46 +0000

    Add package native-sparse-attention-pytorch/0.0.57/native_sparse_attention_pytorch-0.0.57-py3-none-any.whl

---
+++ packages/native-sparse-attention-pytorch/native_sparse_attention_pytorch-0.0.57-py3-none-any.whl/native_sparse_attention_pytorch/__init__.py
@@ -0,0 +1,7 @@
+from native_sparse_attention_pytorch.native_sparse_attention import (
+    SparseAttention
+)
+
+from native_sparse_attention_pytorch.triton_native_sparse_attention import (
+    native_sparse_attend
+)
