2ccfb4d5746bc870aa7e60f079e7fbb94719e288
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-28 03:11:42 +0000

    Add package modelviz/2.0.5/modelviz-2.0.5.tar.gz

---
+++ packages/modelviz/modelviz-2.0.5.tar.gz/modelviz-2.0.5/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+import re
+
+
+def get_version():
+    with open("modelviz/__init__.py", "r") as f:
+        version_line = next((line for line in f if line.startswith("__version__")), None)
+        if version_line:
+            return re.search(r"\"(.*?)\"", version_line).group(1)
+        raise RuntimeError("Version not found in modelviz/__init__.py")
+
+
+setup(
+    name="modelviz",
+    version=get_version(),
+    author="Gary Hutson",
+    author_email="hutsons-hacks@outlook.com",
+    description="A package for EDA and Sci-Kit Learn visualisations and utilities",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/StatsGary/modelviz",
+    packages=find_packages(),
+    install_requires=[
+        "pandas>=1.0",        # For DataFrame operations
+        "numpy>=1.20",        # For numerical computations
+        "scikit-learn>=0.24", # For machine learning utilities
+        "seaborn"
+    ],
+    extras_require={
+        "dev": ["pytest>=7.0"]  # Development and testing dependencies
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.9",
+)
+
