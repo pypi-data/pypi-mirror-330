794e2c83dbd580f3c601af73363d5fab516b56f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-28 15:05:48 +0000

    Add package mercadoradar/2.1.2/mercadoradar-2.1.2-py3-none-any.whl

---
+++ packages/mercadoradar/mercadoradar-2.1.2-py3-none-any.whl/mercadoradar/models/_user.py
@@ -0,0 +1,31 @@
+from datetime import datetime
+from typing import Optional, Any
+
+from pydantic import BaseModel
+
+
+class User(BaseModel):
+    created_at: datetime
+    updated_at: datetime
+    is_deleted: Optional[bool] = None
+    deleted_at: Optional[datetime] = None
+    id: int
+    name: str
+    first_name: str
+    last_name: str
+    email: str
+    account: Any
+
+    def to_str(self) -> str:
+        data = self.model_dump()
+        sorted_items = [('id', data.pop('id'))] if 'id' in data else []
+        sorted_items += sorted(data.items())
+        fields = "\n    ".join(f"{key}={value}" for key, value in sorted_items)
+
+        return f"<{self.__class__.__name__}\n    {fields}\n>"
+
+    def __str__(self) -> str:
+        return self.to_str()
+
+    def __repr__(self) -> str:
+        return self.to_str()
