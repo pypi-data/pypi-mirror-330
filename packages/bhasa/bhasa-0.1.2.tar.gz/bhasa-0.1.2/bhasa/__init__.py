73dd77f8dcb2fbcdf3cb7c98bc0d18d58b52d054
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-28 06:05:57 +0000

    Add package bhasa/0.1.2/bhasa-0.1.2.tar.gz

---
+++ packages/bhasa/bhasa-0.1.2.tar.gz/bhasa-0.1.2/bhasa/__init__.py
@@ -0,0 +1,5 @@
+__version__ = "1.1.1"
+
+from bhasa.ops.selective_scan_interface import selective_scan_fn, mamba_inner_fn
+from bhasa.modules.mamba_simple import Mamba
+from bhasa.models.mixer_seq_simple import MambaLMHeadModel
