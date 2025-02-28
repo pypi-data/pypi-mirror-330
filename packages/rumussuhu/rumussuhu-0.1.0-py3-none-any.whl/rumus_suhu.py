92d3dc164b5b49dc7db80a9c11d5abe0711a0a9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-28 16:06:35 +0000

    Add package rumussuhu/0.1.0/rumussuhu-0.1.0-py3-none-any.whl

---
+++ packages/rumussuhu/rumussuhu-0.1.0-py3-none-any.whl/rumus_suhu.py
@@ -0,0 +1,9 @@
+class RumusSuhu:
+    def to_reamur(self):
+        return self * 4/5
+    
+    def to_fahrenheit(self):
+        return (self * 9/5) + 32
+    
+    def to_kelvin(suhu):
+        return suhu + 273.15
