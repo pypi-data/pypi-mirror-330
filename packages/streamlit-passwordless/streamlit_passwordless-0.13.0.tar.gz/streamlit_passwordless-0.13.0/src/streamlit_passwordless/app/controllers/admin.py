b9594a9d59e2e23b3394efc69b11c283b7f3974b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-28 06:06:11 +0000

    Add package streamlit-passwordless/0.13.0/streamlit_passwordless-0.13.0.tar.gz

---
+++ packages/streamlit-passwordless/streamlit_passwordless-0.13.0.tar.gz/streamlit_passwordless-0.13.0/src/streamlit_passwordless/app/controllers/admin.py
@@ -0,0 +1,29 @@
+r"""The controller of the admin page."""
+
+# Local
+import streamlit_passwordless.database as db
+from streamlit_passwordless.app.views.admin import sidebar, title
+from streamlit_passwordless.bitwarden_passwordless import BitwardenPasswordlessClient
+from streamlit_passwordless.components import create_user_form
+from streamlit_passwordless.models import User
+
+
+def controller(client: BitwardenPasswordlessClient, db_session: db.Session, user: User) -> None:
+    r"""Render the admin page.
+
+    Parameters
+    ----------
+    client : streamlit_passwordless.BitwardenPasswordlessClient
+        An instance of the Bitwarden Passwordless client to
+        communicate with the backend API.
+
+    db_session : streamlit_passwordless.db.Session
+        An active session to the Streamlit Passwordless database.
+
+    user : streamlit_passwordless.User
+        The signed in admin user.
+    """
+
+    title()
+    sidebar(user=user)
+    create_user_form(db_session=db_session, with_ad_username=True)
