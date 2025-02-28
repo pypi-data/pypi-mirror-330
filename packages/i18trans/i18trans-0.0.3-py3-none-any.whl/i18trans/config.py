363cbb731d52d391b3dee5fa56a17f9df762b077
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-28 06:05:41 +0000

    Add package i18trans/0.0.3/i18trans-0.0.3-py3-none-any.whl

---
+++ packages/i18trans/i18trans-0.0.3-py3-none-any.whl/i18trans/config.py
@@ -0,0 +1,85 @@
+
+APP_NAME = "i18Next Translator"
+APP_SIZE = "1430x800"
+APP_BG_COLOR = "#18bc9c"
+TAB_LIST = ["2 Tab Space","3 Tab Space","4 Tab Space"]
+
+LABEL_WIDTH = 70
+LABEL_HIGHT = 2
+
+BOX_WIDTH = 80
+BOX_HIGHT = 45
+
+BUTTON_WIDTH = 25
+MENU_WIDTH = 35
+
+LIST_WIDTH = 35
+LIST_HIGHT = 14
+
+LANGUAGE = {
+  "Afrikaans": "af",
+  "Albanian": "sq",
+  "Arabic": "ar",
+  "Azerbaijani": "az",
+  "Basque": "eu",
+  "Bengali": "bn",
+  "Belarusian": "be",
+  "Bulgarian": "bg",
+  "Catalan": "ca",
+  "Chinese Simplified": "zh-CN",
+  "Chinese Traditional": "zh-TW",
+  "Croatian": "hr",
+  "Czech": "cs",
+  "Danish": "da",
+  "Dutch": "nl",
+  "English": "en",
+  "Esperanto": "eo",
+  "Estonian": "et",
+  "Filipino": "tl",
+  "Finnish": "fi",
+  "French": "fr",
+  "Galician": "gl",
+  "Georgian": "ka",
+  "German": "de",
+  "Greek": "el",
+  "Gujarati": "gu",
+  "Haitian Creole": "ht",
+  "Hebrew": "he",
+  "Hindi": "hi",
+  "Hungarian": "hu",
+  "Icelandic": "is",
+  "Indonesian": "id",
+  "Irish": "ga",
+  "Italian": "it",
+  "Japanese": "ja",
+  "Kannada": "kn",
+  "Korean": "ko",
+  "Latin": "la",
+  "Latvian": "lv",
+  "Lithuanian": "lt",
+  "Macedonian": "mk",
+  "Malay": "ms",
+  "Maltese": "mt",
+  "Norwegian": "no",
+  "Persian": "fa",
+  "Polish": "pl",
+  "Portuguese": "pt",
+  "Romanian": "ro",
+  "Russian": "ru",
+  "Serbian": "sr",
+  "Slovak": "sk",
+  "Slovenian": "sl",
+  "Spanish": "es",
+  "Swahili": "sw",
+  "Swedish": "sv",
+  "Tamil": "ta",
+  "Telugu": "te",
+  "Thai": "th",
+  "Turkish": "tr",
+  "Ukrainian": "uk",
+  "Urdu": "ur",
+  "Vietnamese": "vi",
+  "Welsh": "cy",
+  "Yiddish": "yi"
+}
+
