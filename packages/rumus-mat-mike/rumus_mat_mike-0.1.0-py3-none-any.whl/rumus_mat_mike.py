5417ef6484f4d1d51c2686bf910fc62352a79aa5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-28 15:05:57 +0000

    Add package rumus-mat-mike/0.1.0/rumus_mat_mike-0.1.0-py3-none-any.whl

---
+++ packages/rumus-mat-mike/rumus_mat_mike-0.1.0-py3-none-any.whl/rumus_mat_mike.py
@@ -0,0 +1,16 @@
+class RumusMatematika:
+    @staticmethod
+    def tambah(a, b):
+        return a + b
+
+    @staticmethod
+    def kurang(a, b):
+        return a - b
+    
+    @staticmethod
+    def kali(a, b):
+        return a * b
+
+    @staticmethod
+    def bagi(a, b):
+        return a / b
