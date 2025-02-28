4b3bb46696ebe6dc30b26fc1c19c95dd4479ed29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-28 15:05:13 +0000

    Add package pedophilia/0.1.0/pedophilia-0.1.0-py3-none-any.whl

---
+++ packages/pedophilia/pedophilia-0.1.0-py3-none-any.whl/main.py
@@ -0,0 +1,10 @@
+from rumus_matematika import RumusMatematika
+    
+
+def main():
+    hasil = RumusMatematika.luas_persegi(3);
+    print(hasil)
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/pedophilia/pedophilia-0.1.0-py3-none-any.whl/rumus_matematika.py
@@ -0,0 +1,15 @@
+class RumusMatematika:
+    def tambah(a,b) :
+        return a+b
+    
+    def kurang(a,b) :
+        return a-b
+
+    def kali(a,b) :
+        return a*b
+
+    def bagi(a,b) :
+        return a/b
+
+    def luas_persegi(sisi):
+        return sisi * sisi
