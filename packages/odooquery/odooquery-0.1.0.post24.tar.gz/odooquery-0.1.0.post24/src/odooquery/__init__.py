51fbd0b305c74088babc731df873afa24cf62560
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-28 03:11:42 +0000

    Add package odooquery/0.1.0.post24/odooquery-0.1.0.post24.tar.gz

---
+++ packages/odooquery/odooquery-0.1.0.post24.tar.gz/odooquery-0.1.0.post24/src/odooquery/__init__.py
@@ -0,0 +1,19 @@
+from .query import OdooQuery
+from . import (
+    types,
+    products,
+    order,
+    partner,
+    messages,
+    mass_mailing
+)
+
+__all__ = [
+    'OdooQuery',
+    'types',
+    'products',
+    'order',
+    'partner',
+    'messages',
+    'mass_mailing'
+]
