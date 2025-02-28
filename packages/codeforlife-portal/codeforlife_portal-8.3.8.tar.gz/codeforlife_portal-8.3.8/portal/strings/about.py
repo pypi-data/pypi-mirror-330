1e7de88bf5310601834ec219d16d5665cb01def0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-28 05:06:24 +0000

    Add package codeforlife-portal/8.3.8/codeforlife_portal-8.3.8.tar.gz

---
+++ packages/codeforlife-portal/codeforlife_portal-8.3.8.tar.gz/codeforlife_portal-8.3.8/portal/strings/about.py
@@ -0,0 +1,18 @@
+ABOUT_BANNER = {
+    "title": "About Code for Life",
+    "subtitle": "Code For Life gives everyone the ability to shape technology’s future",
+    "text": "",
+    "image_class": "banner--picture--about",
+}
+
+GETINVOLVED_BANNER = {
+    "title": "Get involved",
+    "subtitle": "How you can get involved with the creation of Code for Life products and resources",
+    "image_class": "banner--picture--getinvolved",
+}
+
+CONTRIBUTE_BANNER = {
+    "title": "Contribute to Code for Life",
+    "subtitle": "How you can get involved with Code for Life products and resources and help us help others",
+    "image_class": "banner--picture--contribute",
+}
