8ac493fb36487cbf8ef96efdbe1d67c7956369de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-28 16:05:22 +0000

    Add package xlin/0.1.8/xlin-0.1.8.tar.gz

---
+++ packages/xlin/xlin-0.1.8.tar.gz/xlin-0.1.8/xlin/terminal_color.py
@@ -0,0 +1,10 @@
+
+blue = "\x1b[34m"
+cyan = "\x1b[36;21m"
+green = "\x1b[32;21m"
+orange = "\x1b[33;21m"
+grey = "\x1b[38;21m"
+yellow = "\x1b[33;21m"
+red = "\x1b[31;21m"
+bold_red = "\x1b[31;1m"
+reset = "\x1b[0m"
