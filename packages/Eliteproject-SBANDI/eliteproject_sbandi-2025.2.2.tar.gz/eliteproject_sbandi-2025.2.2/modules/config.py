b7e7f2520248f31652a02185eae97bc99b6ec07d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-28 05:05:40 +0000

    Add package Eliteproject-SBANDI/2025.2.2/eliteproject_sbandi-2025.2.2.tar.gz

---
+++ packages/Eliteproject-SBANDI/eliteproject_sbandi-2025.2.2.tar.gz/eliteproject_sbandi-2025.2.2/modules/config.py
@@ -0,0 +1,14 @@
+# config.py
+
+import webbrowser
+import time
+import pandas as pd
+import plotly.graph_objects as go
+import dash_bootstrap_components as dbc
+import re
+import os
+import tarfile
+import bz2
+import shutil
+import xml.etree.ElementTree as ET
+import csv
