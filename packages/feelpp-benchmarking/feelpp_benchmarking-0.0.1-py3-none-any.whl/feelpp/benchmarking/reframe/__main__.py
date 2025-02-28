502f8a562dd3fc838e03ea188aaff99df56fd047
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-28 03:11:33 +0000

    Add package feelpp-benchmarking/0.0.1/feelpp_benchmarking-0.0.1-py3-none-any.whl

---
+++ packages/feelpp-benchmarking/feelpp_benchmarking-0.0.1-py3-none-any.whl/feelpp/benchmarking/reframe/__main__.py
@@ -0,0 +1,88 @@
+import os, json, subprocess, shutil
+from feelpp.benchmarking.reframe.parser import Parser
+from feelpp.benchmarking.reframe.config.configReader import ConfigReader, FileHandler
+from feelpp.benchmarking.reframe.config.configSchemas import ConfigFile
+from feelpp.benchmarking.reframe.config.configMachines import MachineConfig
+from feelpp.benchmarking.reframe.reporting import WebsiteConfig
+from feelpp.benchmarking.reframe.commandBuilder import CommandBuilder
+
+
+def main_cli():
+    parser = Parser()
+    parser.printArgs()
+
+    machine_reader = ConfigReader(parser.args.machine_config,MachineConfig,"machine",dry_run=parser.args.dry_run)
+
+    #Sets the cachedir and tmpdir directories for containers
+    for platform, dirs in machine_reader.config.containers.items():
+        if platform=="apptainer":
+            if dirs.cachedir:
+                os.environ["APPTAINER_CACHEDIR"] = dirs.cachedir
+            if dirs.tmpdir:
+                os.environ["APPTAINER_TMPDIR"] = dirs.cachedir
+        elif platform=="docker":
+            raise NotImplementedError("Docker container directories configuration is not implemented")
+
+    cmd_builder = CommandBuilder(machine_reader.config,parser)
+
+    os.environ["MACHINE_CONFIG_FILEPATH"] = parser.args.machine_config
+
+    website_config = WebsiteConfig(machine_reader.config.reports_base_dir)
+
+    for config_filepath in parser.args.benchmark_config:
+        os.environ["APP_CONFIG_FILEPATH"] = config_filepath
+
+
+        configs = [config_filepath]
+        if parser.args.plots_config:
+            configs += [parser.args.plots_config]
+        app_reader = ConfigReader(configs,ConfigFile,"app",dry_run=parser.args.dry_run,additional_readers=[machine_reader])
+
+        executable_name = os.path.basename(app_reader.config.executable).split(".")[0]
+        report_folder_path = cmd_builder.createReportFolder(executable_name,app_reader.config.use_case_name)
+
+        reframe_cmd = cmd_builder.buildCommand( app_reader.config.timeout)
+
+        exit_code = subprocess.run(reframe_cmd, shell=True)
+
+        #============ CREATING RESULT ITEM ================#
+        with open(os.path.join(report_folder_path,"plots.json"),"w") as f:
+            f.write(json.dumps([p.model_dump() for p in app_reader.config.plots]))
+
+        #Copy use case description if existant
+        FileHandler.copyFile(
+            os.path.join(report_folder_path,"partials"),
+            "description",
+            app_reader.config.additional_files.description_filepath
+        )
+
+        if parser.args.move_results:
+            if not os.path.exists(parser.args.move_results):
+                os.makedirs(parser.args.move_results)
+            os.rename(os.path.join(report_folder_path,"reframe_report.json"),os.path.join(parser.args.move_results,"reframe_report.json"))
+            os.rename(os.path.join(report_folder_path,"plots.json"),os.path.join(parser.args.move_results,"plots.json"))
+        #======================================================#
+
+        #============== UPDATE WEBSITE CONFIG FILE ==============#
+        common_itempath = (parser.args.move_results or report_folder_path).split("/")
+        common_itempath = "/".join(common_itempath[:-1 - (common_itempath[-1] == "")])
+
+        website_config.updateExecutionMapping(
+            executable_name, machine_reader.config.machine, app_reader.config.use_case_name,
+            report_itempath = common_itempath
+        )
+
+        website_config.updateMachine(machine_reader.config.machine)
+        website_config.updateUseCase(app_reader.config.use_case_name)
+        website_config.updateApplication(executable_name)
+
+        website_config.save()
+        #======================================================#
+
+    if parser.args.website:
+        subprocess.run(["render-benchmarks","--config-file", website_config.config_filepath])
+        subprocess.run(["npm","run","antora"])
+        subprocess.run(["npm","run","start"])
+
+    # return exit_code.returncode
+    return 0
---
+++ packages/feelpp-benchmarking/feelpp_benchmarking-0.0.1-py3-none-any.whl/feelpp/benchmarking/reframe/config/configSchemas.py
@@ -0,0 +1,175 @@
+from pydantic import BaseModel, field_validator, model_validator, RootModel, ConfigDict
+from typing import Literal, Union, Optional, List, Dict
+from feelpp.benchmarking.reframe.config.configParameters import Parameter
+from feelpp.benchmarking.reframe.config.configPlots import Plot
+import os, re
+
+class Sanity(BaseModel):
+    success:List[str]
+    error:List[str]
+
+class Stage(BaseModel):
+    name:str
+    filepath:str
+    format:Literal["csv","tsv","json"]
+    variables_path:Optional[Union[str,List[str]]] = []
+    units: Optional[Dict[str,str]] = {}
+
+    @field_validator("units",mode="before")
+    @classmethod
+    def parseUnits(cls,v):
+        v["*"] = v.get("*","s")
+        return v
+
+    @model_validator(mode="after")
+    def checkFormatOptions(self):
+        if self.format == "json":
+            if not self.variables_path:
+                raise ValueError("variables_path must be specified if format == json")
+            if type(self.variables_path) == str:
+                self.variables_path = [self.variables_path]
+        elif self.format != "json":
+            if self.variables_path:
+                raise ValueError("variables_path cannot be specified with other format than json")
+        return self
+
+class CustomVariable(BaseModel):
+    name:str
+    columns:List[str]
+    op: Literal["sum","min","max","mean"]
+    unit: str
+
+class Scalability(BaseModel):
+    directory: str
+    stages: List[Stage]
+    custom_variables:Optional[List[CustomVariable]] = []
+    clean_directory: Optional[bool] = False
+
+
+
+class Image(BaseModel):
+    protocol:Optional[Literal["oras","docker","library","local"]] = None
+    name:str
+
+    @model_validator(mode="before")
+    def extractProtocol(self):
+        """ Extracts the image protocol (oras, docker, etc..) or if a local image is provided.
+        If local, checks if the image exists """
+
+        self["protocol"] = self["name"].split("://")[0] if "://" in self["name"] else "local"
+
+        if self["protocol"] not in ["oras","docker","library","local"]:
+            raise ValueError("Unkown Protocol")
+
+        return self
+
+    @field_validator("name", mode="after")
+    @classmethod
+    def checkImage(cls,v,info):
+        if info.data["protocol"] == "local" and not ("{{"  in v  or "}}" in v) :
+            if not os.path.exists(v):
+                if info.context and info.context.get("dry_run", False):
+                   print(f"Dry Run: Skipping image check for {v}")
+                else:
+                    raise FileNotFoundError(f"Cannot find image {v}")
+
+        return v
+
+
+class Platform(BaseModel):
+    image:Optional[Image] = None
+    input_dir:Optional[str] = None
+    options:Optional[List[str]]= []
+    append_app_options:Optional[List[str]]= []
+
+class AdditionalFiles(BaseModel):
+    description_filepath: Optional[str] = None
+    parameterized_descriptions_filepath: Optional[str] = None
+    custom_logs: Optional[List[str]] = []
+
+
+class Resources(BaseModel):
+    tasks: Optional[Union[str,int]] = None
+    tasks_per_node: Optional[Union[str,int]] = None
+    gpus_per_node: Optional[Union[str,int]] = None
+    nodes: Optional[Union[str,int]] = None
+    memory: Optional[Union[str,int]] = 0
+    exclusive_access: Optional[Union[str,bool]] = True
+
+    @model_validator(mode="after")
+    def validateResources(self):
+        assert (
+            self.tasks and self.tasks_per_node and not self.nodes or
+            self.tasks_per_node and self.nodes and not self.tasks or
+            self.tasks and not self.tasks_per_node and not self.nodes
+        ), "Tasks - tasks_per_node - nodes combination is not supported"
+        return self
+
+class ConfigFile(BaseModel):
+    executable: str
+    timeout: str
+    resources: Resources
+    platforms:Optional[Dict[str,Platform]] = {"builtin":Platform()}
+    output_directory:Optional[str] = ""
+    use_case_name: str
+    options: List[str]
+    env_variables:Optional[Dict] = {}
+    input_file_dependencies: Optional[Dict[str,str]] = {}
+    scalability: Scalability
+    sanity: Sanity
+    parameters: List[Parameter]
+    additional_files: Optional[AdditionalFiles] = AdditionalFiles()
+    plots: Optional[List[Plot]] = []
+
+    model_config = ConfigDict( extra='allow' )
+    def __getattr__(self, item):
+        if item in self.model_extra:
+            return self.model_extra[item]
+        raise AttributeError(f"'{self.__class__.__name__}' object has no attribute '{item}'")
+
+
+    @field_validator("timeout",mode="before")
+    @classmethod
+    def validateTimeout(cls,v):
+        pattern = r'^\d+-\d{1,2}:\d{1,2}:\d{1,2}$'
+        if not re.match(pattern, v):
+            raise ValueError(f"Time is not properly formatted (<days>-<hours>:<minutes>:<seconds>) : {v}")
+        days,time = v.split("-")
+        hours,minutes,seconds = time.split(":")
+
+        assert int(days) >= 0
+        assert 24>int(hours)>=0
+        assert 60>int(minutes)>=0
+        assert 60>int(seconds)>=0
+
+        return v
+
+    @model_validator(mode="after")
+    def checkPlotAxisParameters(self):
+        """ Checks that the plot axis parameter field corresponds to existing parameters"""
+        parameter_names = []
+        for outer in self.parameters:
+            if outer.zip:
+                for inner in outer.zip:
+                    parameter_names.append(f"{outer.name}.{inner.name}")
+            elif outer.sequence and all(type(s)==dict and s.keys() for s in outer.sequence):
+                for inner in outer.sequence[0].keys():
+                    parameter_names.append(f"{outer.name}.{inner}")
+
+        parameter_names += [outer.name for outer in self.parameters if outer] + ["performance_variable"]
+        for plot in self.plots:
+            for ax in [plot.xaxis,plot.secondary_axis,plot.yaxis,plot.color_axis]:
+                if ax and ax.parameter:
+                    assert ax.parameter in parameter_names, f"Parameter not found {ax.parameter} in {parameter_names}"
+
+        return self
+
+    @field_validator("platforms",mode="before")
+    @classmethod
+    def checkPlatforms(cls,v):
+        accepted_platforms = ["builtin","apptainer","docker"]
+        for k in v.keys():
+            if k not in accepted_platforms:
+                raise ValueError(f"{k} not implemented")
+        return v
+
