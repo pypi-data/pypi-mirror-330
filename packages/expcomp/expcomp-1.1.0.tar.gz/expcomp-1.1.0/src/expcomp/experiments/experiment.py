40acf5a3723bae852aad6959b6a2884c0d08e7f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-28 05:05:21 +0000

    Add package expcomp/1.1.0/expcomp-1.1.0.tar.gz

---
+++ packages/expcomp/expcomp-1.1.0.tar.gz/expcomp-1.1.0/src/expcomp/experiments/experiment.py
@@ -0,0 +1,10 @@
+from dataclasses import dataclass
+from typing import List
+from expcomp.evaluation import Metric
+from .experiment_config import ExperimentConfig
+
+
+@dataclass
+class Experiment:
+    config: ExperimentConfig
+    metrics: List[Metric]
