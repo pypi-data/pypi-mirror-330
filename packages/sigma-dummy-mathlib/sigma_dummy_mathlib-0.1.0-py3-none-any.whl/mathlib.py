4d03aec502bd3f52cc7ca6cc3c9db6b0987ace43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-28 15:05:55 +0000

    Add package sigma-dummy-mathlib/0.1.0/sigma_dummy_mathlib-0.1.0-py3-none-any.whl

---
+++ packages/sigma-dummy-mathlib/sigma_dummy_mathlib-0.1.0-py3-none-any.whl/mathlib.py
@@ -0,0 +1,12 @@
+class Math:
+    def add(a: int, b: int) -> int:
+        return a + b
+
+    def mul(a: int, b: int) -> int:
+        return a * b
+
+    def div(a: int, b: int) -> int:
+        return a / b
+
+    def square(side: int) -> int:
+        return side * side
