4741e3778528f297dedc7d0fb5f013154b0c151d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-28 15:05:30 +0000

    Add package slac-alarm-manager/1.4.0/slac_alarm_manager-1.4.0.tar.gz

---
+++ packages/slac-alarm-manager/slac_alarm_manager-1.4.0.tar.gz/slac_alarm_manager-1.4.0/docs/index.md
@@ -0,0 +1,25 @@
+# SLAC Alarm Manager
+
+The [SLAC Alarm Manager](https://github.com/slaclab/slac-alarm-manager) is a python package providing an interface to the [NALMS alarm system](https://slaclab.github.io/nalms/).
+It contains two main ways of interacting with NALMS. There is a standalone python application for
+viewing and interacting with alarms described in greater detail [here](alarm_manager.md). And
+a [PyDM data plugin](pydm.md) that will allow for building PyDM displays containing summary alarms.
+
+## Installation
+
+The easiest installation method is using pip to install from PyPI into a virtual environment:
+
+`pip install slac-alarm-manager`
+
+It can also be installed from source using pip from the repository here: https://github.com/slaclab/slac-alarm-manager
+
+## Setting up Alarm Configurations
+
+The alarm hierarchies displayed in this system are defined using xml files. As an example, we can look at this file:
+
+https://github.com/pcdshub/pcds-nalms/blob/master/XML/KFE/GMDXGMD.xml
+
+The top level config name, `nalms-kfe-GMDXGMD`, represents the system being monitored. It will also be referred to
+as the alarm topic or kafka topic in the rest of this documentation. A component is a way of grouping PVs together
+in whatever way makes sense. For more information about the valid options for each pv, please see the description
+of these at the NALMS documentation here: https://slaclab.github.io/nalms/configuration/.
---
+++ packages/slac-alarm-manager/slac_alarm_manager-1.4.0.tar.gz/slac_alarm_manager-1.4.0/slam/kafka_reader.py
@@ -0,0 +1,46 @@
+import json
+from kafka import KafkaConsumer
+from kafka.consumer.fetcher import ConsumerRecord
+from qtpy.QtCore import QObject, Signal
+from typing import Callable, List
+
+
+class KafkaReader(QObject):
+    """
+    KafkaReader is a simple class for reading message from kafka topics and sending them to the main qt thread.
+
+    Parameters
+    ----------
+    topics : List[str]
+        A list of topics representing the alarm configs to listen to
+    bootstrap_servers : List[str]
+        A list containing one or more urls for kafka bootstrap servers
+    new_message_slot : Callable
+        The slot function that each message will end up at
+    """
+
+    new_message_signal = Signal(ConsumerRecord)  # Emitted for every new message received
+
+    def __init__(self, topics: List[str], bootstrap_servers: List[str], new_message_slot: Callable):
+        self.topics = topics
+        self.main_consumer = KafkaConsumer(
+            *topics,
+            bootstrap_servers=bootstrap_servers,
+            auto_offset_reset="earliest",
+            enable_auto_commit=False,
+            key_deserializer=lambda x: x.decode("utf-8"),
+            value_deserializer=self.value_decode,
+        )
+
+        super(KafkaReader, self).__init__()
+        self.new_message_slot = new_message_slot
+        self.new_message_signal.connect(self.new_message_slot)
+
+    def value_decode(self, x):
+        if x is not None:
+            return json.loads(x.decode("utf-8"))
+        return None
+
+    def run(self):
+        for message in self.main_consumer:
+            self.new_message_signal.emit(message)
