d628fc11663b83d268587b7bbed0c1192662d576
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-28 03:11:18 +0000

    Add package PyPaf/0.8.0/pypaf-0.8.0-py3-none-any.whl

---
+++ packages/PyPaf/pypaf-0.8.0-py3-none-any.whl/paf/immutable.py
@@ -0,0 +1,12 @@
+"""Immutable Mixin"""
+
+from dataclasses import FrozenInstanceError
+
+class ImmutableMixin():
+    """Prevent manipulation of object attributes"""
+
+    def __setattr__(self, *_):
+        raise FrozenInstanceError
+
+    def __delattr__(self, *_):
+        raise FrozenInstanceError
