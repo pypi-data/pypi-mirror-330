8f434dde3340f27da622af436dd442ea69cd3827
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-28 03:11:40 +0000

    Add package macrosynergy/1.1.2/macrosynergy-1.1.2-py3-none-any.whl

---
+++ packages/macrosynergy/macrosynergy-1.1.2-py3-none-any.whl/macrosynergy/learning/__init__.py
@@ -0,0 +1,106 @@
+from .splitters import (
+    ExpandingKFoldPanelSplit,
+    RollingKFoldPanelSplit,
+    RecencyKFoldPanelSplit,
+    ExpandingIncrementPanelSplit,
+    ExpandingFrequencyPanelSplit,
+)
+from .cv_tools import panel_cv_scores
+
+from .preprocessing import (
+    LassoSelector,
+    LarsSelector,
+    MapSelector,
+    ZnScoreAverager,
+    PanelMinMaxScaler,
+    PanelStandardScaler,
+    BasePanelScaler,
+    BasePanelSelector,
+    PanelPCA,
+)
+from .model_evaluation import (
+    neg_mean_abs_corr,
+    panel_significance_probability,
+    sharpe_ratio,
+    sortino_ratio,
+    regression_accuracy,
+    regression_balanced_accuracy,
+    correlation_coefficient,
+    create_panel_metric,
+    regression_mcc,
+)
+from .sequential import SignalOptimizer, BetaEstimator
+
+from .forecasting import (
+    LADRegressor,
+    NaiveRegressor,
+    SignWeightedLinearRegression,
+    TimeWeightedLinearRegression,
+    SignWeightedLADRegressor,
+    TimeWeightedLADRegressor,
+    ModifiedLinearRegression,
+    ModifiedSignWeightedLinearRegression,
+    ModifiedTimeWeightedLinearRegression,
+    LinearRegressionSystem,
+    LADRegressionSystem,
+    RidgeRegressionSystem,
+    CorrelationVolatilitySystem,
+    KNNClassifier,
+)
+
+from .random_effects import RandomEffects
+
+__all__ = [
+    # splitters
+    "ExpandingKFoldPanelSplit",
+    "RollingKFoldPanelSplit",
+    "RecencyKFoldPanelSplit",
+    "ExpandingIncrementPanelSplit",
+    "ExpandingFrequencyPanelSplit",
+    "BasePanelSplit",
+    # cv_tools
+    "panel_cv_scores",
+    # preprocessing
+    "BasePanelSelector",
+    "LassoSelector",
+    "LarsSelector",
+    "MapSelector",
+    # transformers
+    "BasePanelScaler",
+    "PanelMinMaxScaler",
+    "PanelStandardScaler",
+    "ZnScoreAverager",
+    "PanelPCA",
+    # metrics
+    "neg_mean_abs_corr",
+    "panel_significance_probability",
+    "sharpe_ratio",
+    "sortino_ratio",
+    "regression_accuracy",
+    "regression_balanced_accuracy",
+    "create_panel_metric",
+    "correlation_coefficient",
+    "regression_mcc",
+    # signal_optimizer
+    "SignalOptimizer",
+    # forecasting
+    "NaiveRegressor",
+    "LADRegressor",
+    "KNNClassifier",
+    "SignWeightedLADRegressor",
+    "TimeWeightedLADRegressor",
+    "SignWeightedLinearRegression",
+    "TimeWeightedLinearRegression",
+    "ModifiedLinearRegression",
+    "ModifiedSignWeightedLinearRegression",
+    "ModifiedTimeWeightedLinearRegression",
+    # market beta estimation
+    "BetaEstimator",
+    # regression system
+    "LADRegressionSystem",
+    "RidgeRegressionSystem",
+    "LinearRegressionSystem",
+    "CorrelationVolatilitySystem",
+    # random effects
+    "RandomEffects",
+]
