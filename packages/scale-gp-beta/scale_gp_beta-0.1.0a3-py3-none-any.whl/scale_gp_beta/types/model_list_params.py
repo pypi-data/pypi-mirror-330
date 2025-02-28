7ae12e4362a4b5172f215c56b38796849488717a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-28 14:05:55 +0000

    Add package scale-gp-beta/0.1.0a3/scale_gp_beta-0.1.0a3-py3-none-any.whl

---
+++ packages/scale-gp-beta/scale_gp_beta-0.1.0a3-py3-none-any.whl/scale_gp_beta/types/model_list_params.py
@@ -0,0 +1,33 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from typing import Optional
+from typing_extensions import Literal, TypedDict
+
+__all__ = ["ModelListParams"]
+
+
+class ModelListParams(TypedDict, total=False):
+    ending_before: Optional[str]
+
+    limit: int
+
+    model_vendor: Optional[
+        Literal[
+            "openai",
+            "cohere",
+            "vertex_ai",
+            "anthropic",
+            "azure",
+            "gemini",
+            "launch",
+            "llmengine",
+            "model_zoo",
+            "bedrock",
+        ]
+    ]
+
+    name: Optional[str]
+
+    starting_after: Optional[str]
