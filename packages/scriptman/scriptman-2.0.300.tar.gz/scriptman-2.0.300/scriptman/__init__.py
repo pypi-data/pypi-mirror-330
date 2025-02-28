4cf1979cc8beef87eb4a2a6d9423f42cebe8219b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-28 16:06:13 +0000

    Add package scriptman/2.0.300/scriptman-2.0.300.tar.gz

---
+++ packages/scriptman/scriptman-2.0.300.tar.gz/scriptman-2.0.300/scriptman/__init__.py
@@ -0,0 +1,20 @@
+from scriptman import powers
+from scriptman.core.config import config
+
+"""
+######################################################################################
+##                                                                                  ##
+## #######  #######  #######  ######   ####### ########  ##   ##  #######  ###  ##  ##
+##                        ##    ##          ##    ##     ### ###       ##  #### ##  ##
+## #######  ##       #######    ##     #######    ##     #######  #######  ## ####  ##
+##      ##  ##       ##  ##     ##     ##         ##     ## # ##  ##   ##  ##  ###  ##
+## #######  #######  ##   ##  ######   ##         ##     ##   ##  ##   ##  ##   ##  ##
+##                                                                                  ##
+######################################################################################
+"""
+__all__: list[str] = ["powers", "config"]
+
+# TODO: Scriptman not running files with imports
+# TODO: Script aliases
+# TODO: Exit from scriptman while running things on multiple threads
+# TODO: Setting Literals on config leading to errors
