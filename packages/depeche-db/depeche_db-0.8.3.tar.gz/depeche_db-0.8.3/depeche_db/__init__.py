422ed2c7f874c07e5fd58f2dddf563db33029d1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-28 07:05:33 +0000

    Add package depeche-db/0.8.3/depeche_db-0.8.3.tar.gz

---
+++ packages/depeche-db/depeche_db-0.8.3.tar.gz/depeche_db-0.8.3/depeche_db/__init__.py
@@ -0,0 +1,35 @@
+from ._aggregated_stream import AggregatedStream, StreamProjector  # noqa: F401
+from ._exceptions import MessageNotFound, OptimisticConcurrencyError  # noqa: F401
+from ._executor import Executor  # noqa: F401
+from ._factories import AggregatedStreamFactory, SubscriptionFactory  # noqa: F401
+from ._interfaces import (  # noqa: F401
+    CallMiddleware,
+    ErrorAction,
+    HandlerDescriptor,
+    LockProvider,
+    MessageHandlerRegisterProtocol,
+    MessagePartitioner,
+    MessagePosition,
+    MessageProtocol,
+    MessageSerializer,
+    RunOnNotification,
+    StoredMessage,
+    StreamPartitionStatistic,
+    SubscriptionErrorHandler,
+    SubscriptionMessage,
+    SubscriptionStartPoint,
+    SubscriptionState,
+    SubscriptionStateProvider,
+)
+from ._message_handler import MessageHandler, MessageHandlerRegister  # noqa: F401
+from ._message_store import MessageStore, MessageStoreReader  # noqa: F401
+from ._storage import Storage  # noqa: F401
+from ._subscription import (  # noqa: F401
+    ExitSubscriptionErrorHandler,  # move somewhere else
+    LogAndIgnoreSubscriptionErrorHandler,  # move somewhere else
+    StartAtNextMessage,
+    StartAtPointInTime,
+    Subscription,
+    SubscriptionMessageHandler,
+    SubscriptionRunner,
+)
