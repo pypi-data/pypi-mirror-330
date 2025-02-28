2b78356947f3b44effb11b84e9eb8d30585fdc8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-28 07:05:37 +0000

    Add package sagace/0.1.0/sagace-0.1.0.tar.gz

---
+++ packages/sagace/sagace-0.1.0.tar.gz/sagace-0.1.0/sagace/exceptions/domain/domain_error.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+    --------------------------------------------------------------------------------------------------------------------
+
+    Description:
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    Obs.:
+
+    Author:           @diego.yosiura
+    Last Update:      27/02/2025 16:05
+    Created:          27/02/2025 16:05
+    Copyright:        (c) Ampere Consultoria Ltda
+    Original Project: sagace-v2-package
+    IDE:              PyCharm
+"""
+class DomainError(Exception):
+    """Exceção base para erros no domínio."""
+    pass
