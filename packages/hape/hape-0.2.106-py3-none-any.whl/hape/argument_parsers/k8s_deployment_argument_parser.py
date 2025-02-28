daf4f97e594c6a39d6969409eedbac833c8fe6de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-28 05:05:27 +0000

    Add package hape/0.2.106/hape-0.2.106-py3-none-any.whl

---
+++ packages/hape/hape-0.2.106-py3-none-any.whl/hape/argument_parsers/k8s_deployment_argument_parser.py
@@ -0,0 +1,13 @@
+from hape.base.model_argument_parser import ModelArgumentParser
+from hape.models.k8s_deployment_model import K8SDeployment
+from hape.controllers.k8s_deployment_controller import K8SDeploymentController
+
+class K8SDeploymentArgumentParser(ModelArgumentParser):
+    def __init__(self):
+        super().__init__(K8SDeployment, K8SDeploymentController)
+
+    def extend_subparser(self):
+        pass
+    
+    def extend_actions(self):
+        pass
