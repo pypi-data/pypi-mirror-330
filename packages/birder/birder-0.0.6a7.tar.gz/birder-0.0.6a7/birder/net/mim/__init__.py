b267c9177446cba9b3286701eb17f0f44f28f57f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-28 15:05:30 +0000

    Add package birder/0.0.6a7/birder-0.0.6a7.tar.gz

---
+++ packages/birder/birder-0.0.6a7.tar.gz/birder-0.0.6a7/birder/net/mim/__init__.py
@@ -0,0 +1,11 @@
+from birder.net.mim.fcmae import FCMAE
+from birder.net.mim.mae_hiera import MAE_Hiera
+from birder.net.mim.mae_vit import MAE_ViT
+from birder.net.mim.simmim import SimMIM
+
+__all__ = [
+    "FCMAE",
+    "MAE_Hiera",
+    "MAE_ViT",
+    "SimMIM",
+]
