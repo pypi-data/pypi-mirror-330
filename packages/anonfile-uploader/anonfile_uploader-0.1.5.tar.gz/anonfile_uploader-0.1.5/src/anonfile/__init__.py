29690d0dcf173f4c0a77d454b4cba71a7e2e650c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-28 16:06:14 +0000

    Add package anonfile-uploader/0.1.5/anonfile_uploader-0.1.5.tar.gz

---
+++ packages/anonfile-uploader/anonfile_uploader-0.1.5.tar.gz/anonfile_uploader-0.1.5/src/anonfile/__init__.py
@@ -0,0 +1,18 @@
+
+
+"""
+@author: RknDeveloper
+@contact: https://t.me/RknDeveloperr
+@license MIT License, see LICENSE file
+
+Copyright (C) 2025
+"""
+
+
+__version__ = "0.1.5"
+__author__ = "RknDeveloper"
+__license__ = "MIT License "
+__copyright__ = "Copyright (C) 2025-present RknDeveloper <https://github.com/RknDeveloper>"
+
+from .anonfile import Anonfile
+from .exceptions import AnonfileError
