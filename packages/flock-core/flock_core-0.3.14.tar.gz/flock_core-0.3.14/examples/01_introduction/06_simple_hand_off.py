977656076e2fe6e7b6ea0a5ed29b86888213e859
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-28 16:06:10 +0000

    Add package flock-core/0.3.14/flock_core-0.3.14.tar.gz

---
+++ packages/flock-core/flock_core-0.3.14.tar.gz/flock_core-0.3.14/examples/01_introduction/06_simple_hand_off.py
@@ -0,0 +1,42 @@
+from flock.core import Flock, FlockFactory
+from flock.routers.agent.agent_router import AgentRouter, AgentRouterConfig
+from flock.routers.default.default_router import DefaultRouter, DefaultRouterConfig
+from flock.routers.llm.llm_router import LLMRouter, LLMRouterConfig
+
+
+
+flock = Flock(enable_logging=True)
+
+idea_agent = FlockFactory.create_default_agent(
+    name="idea_agent",
+    input="query",
+    output="a_fun_software_project_idea",
+    enable_rich_tables=True,
+    wait_for_input=True,
+)
+
+project_plan_agent = FlockFactory.create_default_agent(
+    name="project_plan_agent",
+    input="a_fun_software_project_idea",
+    output="catchy_project_name, project_pitch, techstack, project_implementation_plan",
+    enable_rich_tables=True,
+    wait_for_input=True,
+)
+
+# Default router = handoff to specific agent
+#idea_agent.handoff_router = DefaultRouter(config=DefaultRouterConfig(hand_off=project_plan_agent.name))
+
+# Agent router = handoff to agent based on agent's output
+idea_agent.handoff_router = AgentRouter(config=AgentRouterConfig(with_output=True))
+
+# LLM router = handoff to agent based on LLM's decision
+#idea_agent.handoff_router = LLMRouter(config=LLMRouterConfig())
+
+
+flock.run(
+    input={"query": "fun software project idea about ducks"},
+    start_agent=idea_agent,
+    agents=[idea_agent,project_plan_agent]
+)
+
+
---
+++ packages/flock-core/flock_core-0.3.14.tar.gz/flock_core-0.3.14/examples/README.md
@@ -0,0 +1,24 @@
+# Flock examples
+
+This part of the repository tries to explain how to use Flock to.
+
+## 01 Introduction
+
+Basic usage of flock
+
+## 02 Cook Book
+
+Recipes to let Flock cook or cook with Flock!
+
+## 03 Misc
+
+Additional Topics like modules and evaluators
+
+## 04 Fake It Til You Make It
+
+We re-implement examples of other agent framework.
+For the migrating birds.
+
+## notebooks
+
+Examples as jupyter notebooks
