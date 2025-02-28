1719135517d4a1c33c4dec8062a2257eadefe9d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-28 17:05:21 +0000

    Add package tabsdata/0.9.2/tabsdata-0.9.2-py312-none-win_amd64.whl

---
+++ packages/tabsdata/tabsdata-0.9.2-py312-none-win_amd64.whl/td_interceptor/interceptor_test.py
@@ -0,0 +1,24 @@
+#
+#  Copyright 2024 Tabs Data Inc.
+#
+
+import os
+from abc import ABC
+
+from ta_interceptor.api.api_test import InterceptorTestPlugin
+
+
+class InterceptorTest(InterceptorTestPlugin, ABC):
+    name = "Interceptor Test Plugin (Standard)"
+    version = "0.9.2"
+
+    @classmethod
+    def instance(cls) -> "InterceptorTest":
+        return instance
+
+    def check_test_examples(self, folder):
+        assert os.path.exists(os.path.join(folder, "spanish.jsonl"))
+        assert os.path.exists(os.path.join(folder, "french.jsonl"))
+
+
+instance = InterceptorTest()
