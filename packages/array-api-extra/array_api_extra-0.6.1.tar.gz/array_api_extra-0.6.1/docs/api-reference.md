de94f0241dd1a2bc28925f6224cc46260e27ba2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-28 05:06:44 +0000

    Add package array-api-extra/0.6.1/array_api_extra-0.6.1.tar.gz

---
+++ packages/array-api-extra/array_api_extra-0.6.1.tar.gz/array_api_extra-0.6.1/docs/api-reference.md
@@ -0,0 +1,21 @@
+# API Reference
+
+```{eval-rst}
+.. currentmodule:: array_api_extra
+.. autosummary::
+    :nosignatures:
+    :toctree: generated
+
+    at
+    atleast_nd
+    broadcast_shapes
+    cov
+    create_diagonal
+    expand_dims
+    isclose
+    kron
+    nunique
+    pad
+    setdiff1d
+    sinc
+```
---
+++ packages/array-api-extra/array_api_extra-0.6.1.tar.gz/array_api_extra-0.6.1/src/array_api_extra/__init__.py
@@ -0,0 +1,34 @@
+"""Extra array functions built on top of the array API standard."""
+
+from ._delegation import isclose, pad
+from ._lib._at import at
+from ._lib._funcs import (
+    atleast_nd,
+    broadcast_shapes,
+    cov,
+    create_diagonal,
+    expand_dims,
+    kron,
+    nunique,
+    setdiff1d,
+    sinc,
+)
+
+__version__ = "0.6.1"
+
+# pylint: disable=duplicate-code
+__all__ = [
+    "__version__",
+    "at",
+    "atleast_nd",
+    "broadcast_shapes",
+    "cov",
+    "create_diagonal",
+    "expand_dims",
+    "isclose",
+    "kron",
+    "nunique",
+    "pad",
+    "setdiff1d",
+    "sinc",
+]
