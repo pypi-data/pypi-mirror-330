fe18ad8f4c71a0c4125c33c088bf0702505928aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-28 05:05:27 +0000

    Add package OSToolBox/1.8.2/OSToolBox-1.8.2.tar.gz

---
+++ packages/OSToolBox/OSToolBox-1.8.2.tar.gz/OSToolBox-1.8.2/README.md
@@ -0,0 +1,10 @@
+# OStoolBox 
+version 1.8.1
+ 
+This package regroups usefuls functions for file managment, GIS and point cloud coding with python. 
+
+Source of improved functions : 
+- Original code for point clouds reading : https://github.com/HuguesTHOMAS/KPConv-PyTorch
+- Original code for point clouds transform : https://github.com/PRBonn/segcontrast
+- Original code for Tensor cubifying : https://stackoverflow.com/questions/42297115/numpy-split-cube-into-cubes
+- Original code for natural key sorting : http://nedbatchelder.com/blog/200712/human_sorting.html
---
+++ packages/OSToolBox/OSToolBox-1.8.2.tar.gz/OSToolBox-1.8.2/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Date: 
+    Thu Jun 13 10:47 2019
+Author: 
+    Olivier Stocker 
+    NanBlanc
+Project: 
+    OSToolBox    
+"""
+
+from setuptools import setup
+
+setup(name='OSToolBox',
+      version='1.8.2',
+      description='implement many basic function and tools for GIS, Point clouds and General coding with python',
+      url='https://github.com/NanBlanc/OSToolBox',
+      author='Olivier Stocker',
+      author_email='stocker.olivier@gmail.com',
+      license='MIT',
+      install_requires=['numpy','matplotlib','scikit-learn'], #FOR TEST.PYPI: DEPENDENCIES CHECK CAN NOT CARRIED AS TEST.PYPI ISNT MAINTAINED FOR DEPENDENCIES
+      packages=['OSToolBox','GDALToolBox'],
+      zip_safe=False)
