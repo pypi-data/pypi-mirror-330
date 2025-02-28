dec7476800fd4f8842840f92b07ad93f6ebabc5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-28 06:05:56 +0000

    Add package NeditGD/0.2.3/neditgd-0.2.3-py3-none-any.whl

---
+++ packages/NeditGD/neditgd-0.2.3-py3-none-any.whl/NeditGD/__init__.py
@@ -0,0 +1,7 @@
+from NeditGD.object_gd import Object
+from NeditGD.editor_gd import Editor
+from NeditGD.Dictionaries.PropertyHSV import HSV
+from NeditGD.Nextra import group_pool
+
+__version__ = "0.2.3"
+__author__ = 'Nemo2510 (Boris Filin)'
