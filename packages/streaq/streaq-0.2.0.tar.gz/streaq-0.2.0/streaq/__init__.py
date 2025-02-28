469586dc8c25e4d87946876534742b48230c0672
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-28 14:05:14 +0000

    Add package streaq/0.2.0/streaq-0.2.0.tar.gz

---
+++ packages/streaq/streaq-0.2.0.tar.gz/streaq-0.2.0/streaq/__init__.py
@@ -0,0 +1,22 @@
+import logging
+
+VERSION = "0.2.0"
+__version__ = VERSION
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+# ruff: noqa: E402
+
+from .task import TaskStatus
+from .types import WrappedContext
+from .utils import StreaqError
+from .worker import StreaqRetry, Worker
+
+__all__ = [
+    "StreaqError",
+    "StreaqRetry",
+    "TaskStatus",
+    "Worker",
+    "WrappedContext",
+]
