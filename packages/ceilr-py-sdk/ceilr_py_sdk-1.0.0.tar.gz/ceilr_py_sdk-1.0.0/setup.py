05c0407b267d224c7329cb80233eb3bed833e00e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-28 07:04:59 +0000

    Add package ceilr-py-sdk/1.0.0/ceilr_py_sdk-1.0.0.tar.gz

---
+++ packages/ceilr-py-sdk/ceilr_py_sdk-1.0.0.tar.gz/ceilr_py_sdk-1.0.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ceilr-py-sdk",
+    version="1.0.0",
+    packages=find_packages(),
+    install_requires=["requests"],
+    description="CeilR Python SDK for feature access, usage tracking, and entitlements",
+    author="Mani Kumar Gouni",
+    author_email="support@ceilr.com",
+    license="MIT",
+    url="https://github.com/GouniManikumar12/ceilr-py-sdk",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
