4818cb2ba8908e3470a7631a09c3aee37538dbf1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-28 14:05:22 +0000

    Add package pyorthogonalrouting/1.4.0/pyorthogonalrouting-1.4.0-py3-none-any.whl

---
+++ packages/pyorthogonalrouting/pyorthogonalrouting-1.4.0-py3-none-any.whl/pyorthogonalrouting/OrthogonalConnectorByProduct.py
@@ -0,0 +1,31 @@
+
+from typing import List
+from typing import NewType
+
+from dataclasses import dataclass
+from dataclasses import field
+
+from pyorthogonalrouting.Common import Integers
+from pyorthogonalrouting.Common import integerListFactory
+
+from pyorthogonalrouting.Line import Lines
+from pyorthogonalrouting.Line import linesFactory
+
+from pyorthogonalrouting.Point import Points
+from pyorthogonalrouting.Point import pointsFactory
+
+from pyorthogonalrouting.Rectangle import Rectangles
+from pyorthogonalrouting.Rectangle import rectanglesFactory
+
+
+@dataclass
+class OrthogonalConnectorByProduct:
+
+    hRulers:     Integers   = field(default_factory=integerListFactory)
+    vRulers:     Integers   = field(default_factory=integerListFactory)
+    spots:       Points     = field(default_factory=pointsFactory)
+    grid:        Rectangles = field(default_factory=rectanglesFactory)
+    connections: Lines      = field(default_factory=linesFactory)
+
+
+OrthogonalConnectorByProducts = NewType('OrthogonalConnectorByProducts', List[OrthogonalConnectorByProduct])
