168e58fc31a2d2a445afcccc410e71bee9dd70e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-28 15:05:38 +0000

    Add package deep-apply/1.0.3/deep_apply-1.0.3-py3-none-any.whl

---
+++ packages/deep-apply/deep_apply-1.0.3-py3-none-any.whl/deep_apply/helpers.py
@@ -0,0 +1,35 @@
+from deep_apply import constants
+
+
+def add_key_to_depth_key(key: str, depth_key: str | None) -> str:
+    """
+    Add key to depth key.
+
+    :param key: The current key.
+    :param depth_key: The current depth key.
+    """
+
+    if depth_key:
+        new_depth_key = f"{depth_key}:{key}"
+    else:
+        new_depth_key = key
+
+    return new_depth_key
+
+
+def can_handle_object_type(
+    allowed_types: list[constants.SUPPORTED_TYPES],
+    type_to_check: constants.SUPPORTED_TYPES,
+) -> bool:
+    """
+    Check if object type is allowed to be handled.
+    An empty list as allowed_types means all types are allowed.
+
+    :param allowed_types: A list of types allowed.
+    :param type_to_check: Tye type to check.
+    """
+
+    if not allowed_types or (allowed_types and type_to_check in allowed_types):
+        return True
+
+    return False
