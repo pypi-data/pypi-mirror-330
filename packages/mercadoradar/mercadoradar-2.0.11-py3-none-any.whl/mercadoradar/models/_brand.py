78063a52bb34f63fa3d7407e87a4da8d8959f351
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-28 15:05:44 +0000

    Add package mercadoradar/2.0.11/mercadoradar-2.0.11-py3-none-any.whl

---
+++ packages/mercadoradar/mercadoradar-2.0.11-py3-none-any.whl/mercadoradar/models/_brand.py
@@ -0,0 +1,28 @@
+from datetime import datetime
+from typing import Optional
+
+from pydantic import BaseModel
+
+
+class Brand(BaseModel):
+    created_at: datetime
+    updated_at: datetime
+    is_deleted: Optional[bool] = None
+    deleted_at: Optional[datetime] = None
+    id: int
+    value: str
+    name: str
+
+    def to_str(self) -> str:
+        data = self.model_dump()
+        sorted_items = [('id', data.pop('id'))] if 'id' in data else []
+        sorted_items += sorted(data.items())
+        fields = "\n    ".join(f"{key}={value}" for key, value in sorted_items)
+
+        return f"<{self.__class__.__name__}\n    {fields}\n>"
+
+    def __str__(self) -> str:
+        return self.to_str()
+
+    def __repr__(self) -> str:
+        return self.to_str()
