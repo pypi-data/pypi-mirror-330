6fdc6ecce65849f0cd4cedd82120777a4c35e27d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-28 15:05:24 +0000

    Add package rumusmatematikatesting/0.1.0/rumusmatematikatesting-0.1.0-py3-none-any.whl

---
+++ packages/rumusmatematikatesting/rumusmatematikatesting-0.1.0-py3-none-any.whl/rumus.py
@@ -0,0 +1,16 @@
+class rumusMatematika:
+    def tambah(a,b):
+        return a+b
+
+    def kurang(a,b):
+        return a-b
+    
+    def kali(a,b):
+        return a*b
+    
+    def bagi(a,b):
+        return a/b
+
+    def persegi(sisi):
+        return sisi*sisi
+    
