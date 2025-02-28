c29caa738fd9fcc74ba78a10c56313c844581d57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-28 16:05:07 +0000

    Add package rnet/1.5.2/rnet-1.5.2.tar.gz

---
+++ packages/rnet/rnet-1.5.2.tar.gz/rnet-1.5.2/src/param/request.rs
@@ -0,0 +1,125 @@
+use crate::types::{Body, CookieMap, HeaderMap, IpAddr, Json, Multipart, Version};
+use pyo3::prelude::*;
+use pyo3_stub_gen::derive::gen_stub_pyclass;
+
+/// The parameters for a request.
+///
+/// # Examples
+///
+/// ```python
+/// import rnet
+/// from rnet import Impersonate, Version
+///
+/// params = rnet.RequestParams(
+///     impersonate=Impersonate.Chrome100,
+///     version=Version.HTTP_2,
+///     user_agent="Mozilla/5.0",
+///     headers={"Content-Type": "application/json"},
+///     timeout=10,
+///     connect_timeout=5,
+///     read_timeout=15,
+///     local_address="192.168.1.188",
+///     no_keepalive=True,
+///     no_proxy=False,
+///     http1_only=False,
+///     http2_only=True,
+///     referer=True
+/// )
+///
+/// response = await rnet.get("https://www.rust-lang.org", **params)
+/// body = await response.text()
+/// print(body)
+/// ```
+#[gen_stub_pyclass]
+#[pyclass(get_all, set_all)]
+#[derive(Default, Debug)]
+pub struct RequestParams {
+    /// The proxy to use for the request.
+    pub proxy: Option<String>,
+
+    /// Bind to a local IP Address.
+    pub local_address: Option<IpAddr>,
+
+    /// Bind to an interface by `SO_BINDTODEVICE`.
+    pub interface: Option<String>,
+
+    /// The timeout to use for the request.
+    pub timeout: Option<u64>,
+
+    /// The read timeout to use for the request.
+    pub read_timeout: Option<u64>,
+
+    /// The HTTP version to use for the request.
+    pub version: Option<Version>,
+
+    /// The headers to use for the request.
+    pub headers: Option<HeaderMap>,
+
+    /// The cookies to use for the request.
+    pub cookies: Option<CookieMap>,
+
+    /// Whether to allow redirects.
+    pub allow_redirects: Option<bool>,
+
+    /// The maximum number of redirects to follow.
+    pub max_redirects: Option<usize>,
+
+    /// The authentication to use for the request.
+    pub auth: Option<String>,
+
+    /// The bearer authentication to use for the request.
+    pub bearer_auth: Option<String>,
+
+    /// The basic authentication to use for the request.
+    pub basic_auth: Option<(String, Option<String>)>,
+
+    /// The query parameters to use for the request.
+    pub query: Option<Vec<(String, String)>>,
+
+    /// The form parameters to use for the request.
+    pub form: Option<Vec<(String, String)>>,
+
+    /// The JSON body to use for the request.
+    pub json: Option<Json>,
+
+    /// The body to use for the request.
+    pub body: Option<Body>,
+
+    /// The multipart form to use for the request.
+    pub multipart: Option<Py<Multipart>>,
+}
+
+macro_rules! extract_option {
+    ($ob:expr, $params:expr, $field:ident) => {
+        if let Ok(value) = $ob.get_item(stringify!($field)) {
+            $params.$field = value.extract()?;
+        }
+    };
+}
+
+impl<'py> FromPyObject<'py> for RequestParams {
+    fn extract_bound(ob: &Bound<'py, PyAny>) -> PyResult<RequestParams> {
+        let mut params = Self::default();
+        extract_option!(ob, params, proxy);
+        extract_option!(ob, params, local_address);
+        extract_option!(ob, params, interface);
+        extract_option!(ob, params, timeout);
+        extract_option!(ob, params, read_timeout);
+
+        extract_option!(ob, params, version);
+        extract_option!(ob, params, headers);
+        extract_option!(ob, params, cookies);
+        extract_option!(ob, params, allow_redirects);
+        extract_option!(ob, params, max_redirects);
+        extract_option!(ob, params, auth);
+        extract_option!(ob, params, bearer_auth);
+        extract_option!(ob, params, basic_auth);
+        extract_option!(ob, params, query);
+        extract_option!(ob, params, form);
+        extract_option!(ob, params, json);
+        extract_option!(ob, params, body);
+        extract_option!(ob, params, multipart);
+
+        Ok(params)
+    }
+}
---
+++ packages/rnet/rnet-1.5.2.tar.gz/rnet-1.5.2/src/types/multipart/part.rs
@@ -0,0 +1,142 @@
+use crate::{
+    error::{stream_consumed_error, wrap_io_error, MIMEParseError},
+    types::PyIterator,
+};
+use arc_swap::ArcSwapOption;
+use bytes::Bytes;
+use futures_util::StreamExt;
+use pyo3::prelude::*;
+use pyo3_stub_gen::{
+    derive::{gen_stub_pyclass, gen_stub_pymethods},
+    PyStubType, TypeInfo,
+};
+use std::{fmt::Debug, path::PathBuf, pin::Pin, sync::Arc};
+
+/// A part of a multipart form.
+#[gen_stub_pyclass]
+#[pyclass]
+pub struct Part {
+    pub name: Option<String>,
+    pub inner: Option<rquest::multipart::Part>,
+}
+
+/// The data for a part of a multipart form.
+pub enum PartData {
+    Text(String),
+    Bytes(Vec<u8>),
+    File(PathBuf),
+    Iterator(Arc<ArcSwapOption<Box<dyn Iterator<Item = Vec<u8>> + Send + Sync + 'static>>>),
+    Stream(
+        Arc<
+            ArcSwapOption<
+                Pin<Box<dyn futures_util::Stream<Item = PyObject> + Send + Sync + 'static>>,
+            >,
+        >,
+    ),
+}
+
+impl Debug for PartData {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Text(inner) => write!(f, "PartData::Text({:?})", inner),
+            Self::Bytes(inner) => write!(f, "PartData::Bytes({:?})", inner),
+            Self::File(inner) => write!(f, "PartData::File({:?})", inner),
+            Self::Iterator(_) => write!(f, "PartData::Iterator(...)"),
+            Self::Stream(_) => write!(f, "PartData::Stream(...)"),
+        }
+    }
+}
+
+impl PyStubType for PartData {
+    fn type_output() -> TypeInfo {
+        TypeInfo::any()
+    }
+}
+
+#[gen_stub_pymethods]
+#[pymethods]
+impl Part {
+    /// Creates a new part.
+    #[new]
+    #[pyo3(signature = (name, value, filename = None, mime = None))]
+    pub fn new(
+        py: Python,
+        name: String,
+        value: PartData,
+        filename: Option<String>,
+        mime: Option<&str>,
+    ) -> PyResult<Part> {
+        py.allow_threads(|| {
+            // Create the inner part
+            let mut inner = match value {
+                PartData::Text(text) => rquest::multipart::Part::text(text),
+                PartData::Bytes(bytes) => rquest::multipart::Part::bytes(bytes),
+                PartData::File(path) => pyo3_async_runtimes::tokio::get_runtime()
+                    .block_on(rquest::multipart::Part::file(path))
+                    .map_err(wrap_io_error)?,
+                PartData::Iterator(iterator) => iterator
+                    .swap(None)
+                    .and_then(Arc::into_inner)
+                    .map(|iter| iter.map(|item| Ok::<_, PyErr>(Bytes::from(item))))
+                    .map(futures_util::stream::iter)
+                    .map(rquest::Body::wrap_stream)
+                    .map(rquest::multipart::Part::stream)
+                    .ok_or_else(stream_consumed_error)?,
+                PartData::Stream(stream) => stream
+                    .swap(None)
+                    .and_then(Arc::into_inner)
+                    .map(|stream| {
+                        stream.map(|item| Python::with_gil(|py| item.extract::<Vec<u8>>(py)))
+                    })
+                    .map(rquest::Body::wrap_stream)
+                    .map(rquest::multipart::Part::stream)
+                    .ok_or_else(stream_consumed_error)?,
+            };
+
+            // Set the filename and MIME type if provided
+            if let Some(filename) = filename {
+                inner = inner.file_name(filename);
+            }
+
+            // Set the MIME type if provided
+            if let Some(mime) = mime {
+                inner = inner.mime_str(mime).map_err(|e| {
+                    MIMEParseError::new_err(format!("Cannot parse MIME type: {:?}", e))
+                })?;
+            }
+
+            Ok(Part {
+                name: Some(name),
+                inner: Some(inner),
+            })
+        })
+    }
+}
+
+impl FromPyObject<'_> for PartData {
+    fn extract_bound(ob: &Bound<'_, PyAny>) -> PyResult<Self> {
+        if let Ok(text) = ob.extract::<String>() {
+            Ok(Self::Text(text))
+        } else if let Ok(bytes) = ob.extract::<Vec<u8>>() {
+            Ok(Self::Bytes(bytes))
+        } else if let Ok(path) = ob.extract::<PathBuf>() {
+            Ok(Self::File(path))
+        } else if let Ok(iter) = ob.extract::<PyObject>() {
+            Ok(Self::Iterator(Arc::new(ArcSwapOption::from_pointee(
+                Box::new(PyIterator::new(iter))
+                    as Box<dyn Iterator<Item = Vec<u8>> + Send + Sync + 'static>,
+            ))))
+        } else {
+            pyo3_async_runtimes::tokio::into_stream_v2(ob.to_owned())
+                .map(|s| {
+                    Box::pin(s)
+                        as Pin<
+                            Box<dyn futures_util::Stream<Item = PyObject> + Send + Sync + 'static>,
+                        >
+                })
+                .map(ArcSwapOption::from_pointee)
+                .map(Arc::new)
+                .map(Self::Stream)
+        }
+    }
+}
