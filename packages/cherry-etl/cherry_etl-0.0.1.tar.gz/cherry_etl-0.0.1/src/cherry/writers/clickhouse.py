68f92f0654cc563679511046620435e8b1f76a02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-28 03:11:25 +0000

    Add package cherry-etl/0.0.1/cherry_etl-0.0.1.tar.gz

---
+++ packages/cherry-etl/cherry_etl-0.0.1.tar.gz/cherry_etl-0.0.1/src/cherry/writers/clickhouse.py
@@ -0,0 +1,16 @@
+import logging
+from typing import Dict
+import pyarrow as pa
+from ..writers.base import DataWriter
+from ..config import ClickHouseWriterConfig
+
+logger = logging.getLogger(__name__)
+
+
+class Writer(DataWriter):
+    def __init__(self, config: ClickHouseWriterConfig):
+        self.client = config.client
+
+    async def push_data(self, data: Dict[str, pa.RecordBatch]) -> None:
+        for table_name, table_data in data.items():
+            self.client.insert_arrow(table_name, pa.Table.from_batches([table_data]))
