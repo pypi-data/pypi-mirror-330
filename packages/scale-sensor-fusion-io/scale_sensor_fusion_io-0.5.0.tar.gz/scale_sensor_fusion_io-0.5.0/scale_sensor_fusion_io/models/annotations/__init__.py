98fbb3da93c3d61f327982c59d6e15dcb747f3a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-28 06:06:32 +0000

    Add package scale-sensor-fusion-io/0.5.0/scale_sensor_fusion_io-0.5.0.tar.gz

---
+++ packages/scale-sensor-fusion-io/scale_sensor_fusion_io-0.5.0.tar.gz/scale_sensor_fusion_io-0.5.0/scale_sensor_fusion_io/models/annotations/__init__.py
@@ -0,0 +1,21 @@
+from typing import Union
+
+from .attributes import *
+from .box_2d import *
+from .cuboid import *
+from .event import *
+from .labeled_points import *
+from .localization_adjustment import *
+from .object import *
+from .polygon import *
+
+Annotation = Union[
+    AttributesAnnotation,
+    Box2DAnnotation,
+    CuboidAnnotation,
+    EventAnnotation,
+    LabeledPointsAnnotation,
+    LocalizationAdjustmentAnnotation,
+    ObjectAnnotation,
+    PolygonAnnotation,
+]
