14dba1f5a2e710ce79fa654530ff882f99ddd742
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-28 05:05:06 +0000

    Add package scvi-tools/1.3.0/scvi_tools-1.3.0.tar.gz

---
+++ packages/scvi-tools/scvi_tools-1.3.0.tar.gz/scvi_tools-1.3.0/src/scvi/module/base/__init__.py
@@ -0,0 +1,26 @@
+from ._base_module import (
+    BaseMinifiedModeModuleClass,
+    BaseModuleClass,
+    JaxBaseModuleClass,
+    LossOutput,
+    PyroBaseModuleClass,
+    TrainStateWithState,
+)
+from ._decorators import auto_move_data, flax_configure
+from ._embedding_mixin import EmbeddingModuleMixin
+from ._priors import GaussianPrior, MogPrior, VampPrior
+
+__all__ = [
+    "BaseModuleClass",
+    "LossOutput",
+    "PyroBaseModuleClass",
+    "auto_move_data",
+    "flax_configure",
+    "JaxBaseModuleClass",
+    "TrainStateWithState",
+    "BaseMinifiedModeModuleClass",
+    "EmbeddingModuleMixin",
+    "GaussianPrior",
+    "MogPrior",
+    "VampPrior",
+]
