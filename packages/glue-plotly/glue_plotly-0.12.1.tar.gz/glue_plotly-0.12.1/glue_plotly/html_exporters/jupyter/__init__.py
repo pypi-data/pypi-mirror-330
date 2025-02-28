b0ee669b627d7e342943e259683f04701b14b98b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-28 07:05:27 +0000

    Add package glue-plotly/0.12.1/glue_plotly-0.12.1.tar.gz

---
+++ packages/glue-plotly/glue_plotly-0.12.1.tar.gz/glue_plotly-0.12.1/glue_plotly/html_exporters/jupyter/__init__.py
@@ -0,0 +1,6 @@
+from . import histogram  # noqa
+from . import image  # noqa
+from . import profile  # noqa
+from . import scatter2d  # noqa
+from . import scatter3d # noqa
+from . import volume  # noqa
