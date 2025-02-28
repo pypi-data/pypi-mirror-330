10eb184a01bac4f2a48e54ad8ae05a0795b6011e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-28 06:05:06 +0000

    Add package netbox-old-search/1.1.0/netbox_old_search-1.1.0-py3-none-any.whl

---
+++ packages/netbox-old-search/netbox_old_search-1.1.0-py3-none-any.whl/netbox_old_search/__init__.py
@@ -0,0 +1,15 @@
+from netbox.plugins import PluginConfig
+from .middleware import SearchRedirectMiddleware
+
+
+class NetboxOldSearch(PluginConfig):
+    name = "netbox_old_search"
+    verbose_name = "Old Search"
+    description = "Netbox Old Search"
+    version = "1.1.0"
+    base_url = "old-search"
+    default_settings = {"show_menu": False, "replace_search": True}
+    middleware = ["netbox_old_search.middleware.SearchRedirectMiddleware"]
+
+
+config = NetboxOldSearch
