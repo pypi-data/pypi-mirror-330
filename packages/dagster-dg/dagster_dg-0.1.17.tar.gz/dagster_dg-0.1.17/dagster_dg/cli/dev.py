3e82552ab4f72acb4746be70104ba7a34f4ec298
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-28 15:05:32 +0000

    Add package dagster-dg/0.1.17/dagster_dg-0.1.17.tar.gz

---
+++ packages/dagster-dg/dagster_dg-0.1.17.tar.gz/dagster_dg-0.1.17/dagster_dg/cli/dev.py
@@ -0,0 +1,218 @@
+import os
+import signal
+import subprocess
+import sys
+import time
+from collections.abc import Iterator, Mapping, Sequence
+from contextlib import contextmanager, nullcontext
+from pathlib import Path
+from tempfile import NamedTemporaryFile
+from typing import Optional, TypeVar
+
+import click
+import psutil
+import yaml
+
+from dagster_dg.cli.global_options import dg_global_options
+from dagster_dg.config import normalize_cli_config
+from dagster_dg.context import DgContext
+from dagster_dg.error import DgError
+from dagster_dg.utils import DgClickCommand, exit_with_error, pushd
+
+T = TypeVar("T")
+
+_CHECK_SUBPROCESS_INTERVAL = 5
+
+
+@click.command(name="dev", cls=DgClickCommand)
+@click.option(
+    "--code-server-log-level",
+    help="Set the log level for code servers spun up by dagster services.",
+    show_default=True,
+    default="warning",
+    type=click.Choice(["critical", "error", "warning", "info", "debug"], case_sensitive=False),
+)
+@click.option(
+    "--log-level",
+    help="Set the log level for dagster services.",
+    show_default=True,
+    default="info",
+    type=click.Choice(["critical", "error", "warning", "info", "debug"], case_sensitive=False),
+)
+@click.option(
+    "--log-format",
+    type=click.Choice(["colored", "json", "rich"], case_sensitive=False),
+    show_default=True,
+    required=False,
+    default="colored",
+    help="Format of the logs for dagster services",
+)
+@click.option(
+    "--port",
+    "-p",
+    type=int,
+    help="Port to use for the Dagster webserver.",
+    required=False,
+)
+@click.option(
+    "--host",
+    "-h",
+    type=str,
+    help="Host to use for the Dagster webserver.",
+    required=False,
+)
+@click.option(
+    "--live-data-poll-rate",
+    help="Rate at which the dagster UI polls for updated asset data (in milliseconds)",
+    type=int,
+    default=2000,
+    show_default=True,
+    required=False,
+)
+@dg_global_options
+def dev_command(
+    code_server_log_level: str,
+    log_level: str,
+    log_format: str,
+    port: Optional[int],
+    host: Optional[str],
+    live_data_poll_rate: int,
+    **global_options: Mapping[str, object],
+) -> None:
+    """Start a local instance of Dagster.
+
+    If run inside a workspace directory, this command will launch all projects in the
+    workspace. If launched inside a project directory, it will launch only that project.
+    """
+    cli_config = normalize_cli_config(global_options, click.get_current_context())
+    dg_context = DgContext.for_workspace_or_project_environment(Path.cwd(), cli_config)
+
+    forward_options = [
+        *format_forwarded_option("--code-server-log-level", code_server_log_level),
+        *format_forwarded_option("--log-level", log_level),
+        *format_forwarded_option("--log-format", log_format),
+        *format_forwarded_option("--port", port),
+        *format_forwarded_option("--host", host),
+        *format_forwarded_option("--live-data-poll-rate", live_data_poll_rate),
+    ]
+
+    # In a project context, we can just run `dagster dev` directly, using `dagster` from the
+    # project's environment.
+    if dg_context.is_project:
+        cmd_location = dg_context.get_executable("dagster")
+        if dg_context.use_dg_managed_environment:
+            cmd = ["uv", "run", "dagster", "dev", *forward_options]
+        else:
+            cmd = [cmd_location, "dev", *forward_options]
+        temp_workspace_file_cm = nullcontext()
+
+    # In a workspace context, dg dev will construct a temporary
+    # workspace file that points at all defined projects and invoke:
+    #
+    #     uv tool run --with dagster-webserver dagster dev
+    #
+    # The `--with dagster-webserver` is necessary here to ensure that dagster-webserver is
+    # installed in the isolated environment that `uv` will install `dagster` in.
+    # `dagster-webserver` is not a dependency of `dagster` but is required to run the `dev`
+    # command.
+    elif dg_context.is_workspace:
+        cmd = [
+            "uv",
+            "tool",
+            "run",
+            "--with",
+            "dagster-webserver",
+            "dagster",
+            "dev",
+            *forward_options,
+        ]
+        cmd_location = "ephemeral dagster dev"
+        temp_workspace_file_cm = temp_workspace_file(dg_context)
+    else:
+        exit_with_error("This command must be run inside a project or workspace directory.")
+
+    with pushd(dg_context.root_path), temp_workspace_file_cm as workspace_file:
+        print(f"Using {cmd_location}")  # noqa: T201
+        if workspace_file:  # only non-None deployment context
+            cmd.extend(["--workspace", workspace_file])
+        uv_run_dagster_dev_process = _open_subprocess(cmd)
+        try:
+            while True:
+                time.sleep(_CHECK_SUBPROCESS_INTERVAL)
+                if uv_run_dagster_dev_process.poll() is not None:
+                    raise DgError(
+                        f"dagster-dev process shut down unexpectedly with return code {uv_run_dagster_dev_process.returncode}."
+                    )
+        except KeyboardInterrupt:
+            click.secho(
+                "Received keyboard interrupt. Shutting down dagster-dev process.", fg="yellow"
+            )
+        finally:
+            # For reasons not fully understood, directly interrupting the `uv run` process does not
+            # work as intended. The interrupt signal is not correctly propagated to the `dagster
+            # dev` process, and so that process never shuts down. Therefore, we send the signal
+            # directly to the `dagster dev` process (the only child of the `uv run` process). This
+            # will cause `dagster dev` to terminate which in turn will cause `uv run` to terminate.
+            dagster_dev_pid = _get_child_process_pid(uv_run_dagster_dev_process)
+            _interrupt_subprocess(dagster_dev_pid)
+
+            try:
+                uv_run_dagster_dev_process.wait(timeout=10)
+            except subprocess.TimeoutExpired:
+                click.secho("`dagster dev` did not terminate in time. Killing it.")
+                uv_run_dagster_dev_process.kill()
+
+
+@contextmanager
+def temp_workspace_file(dg_context: DgContext) -> Iterator[str]:
+    with NamedTemporaryFile(mode="w+", delete=True) as temp_workspace_file:
+        entries = []
+        for project_name in dg_context.get_project_names():
+            project_root = dg_context.get_project_path(project_name)
+            project_context: DgContext = dg_context.with_root_path(project_root)
+            entry = {
+                "working_directory": str(dg_context.workspace_root_path),
+                "relative_path": str(project_context.definitions_path),
+                "location_name": project_context.project_name,
+            }
+            if project_context.use_dg_managed_environment:
+                entry["executable_path"] = str(project_context.project_python_executable)
+            entries.append({"python_file": entry})
+        yaml.dump({"load_from": entries}, temp_workspace_file)
+        temp_workspace_file.flush()
+        yield temp_workspace_file.name
+
+
+def format_forwarded_option(option: str, value: object) -> list[str]:
+    return [] if value is None else [option, str(value)]
+
+
+def _get_child_process_pid(proc: "subprocess.Popen") -> int:
+    children = psutil.Process(proc.pid).children(recursive=False)
+    if len(children) != 1:
+        raise ValueError(f"Expected exactly one child process, but found {len(children)}")
+    return children[0].pid
+
+
+# Windows subprocess termination utilities. See here for why we send CTRL_BREAK_EVENT on Windows:
+# https://stefan.sofa-rockers.org/2013/08/15/handling-sub-process-hierarchies-python-linux-os-x/
+
+
+def _interrupt_subprocess(pid: int) -> None:
+    """Send CTRL_BREAK_EVENT on Windows, SIGINT on other platforms."""
+    if sys.platform == "win32":
+        os.kill(pid, signal.CTRL_BREAK_EVENT)
+    else:
+        os.kill(pid, signal.SIGINT)
+
+
+def _open_subprocess(command: Sequence[str]) -> "subprocess.Popen":
+    """Sets the correct flags to support graceful termination."""
+    creationflags = 0
+    if sys.platform == "win32":
+        creationflags = subprocess.CREATE_NEW_PROCESS_GROUP
+
+    return subprocess.Popen(
+        command,
+        creationflags=creationflags,
+    )
---
+++ packages/dagster-dg/dagster_dg-0.1.17.tar.gz/dagster_dg-0.1.17/dagster_dg/context.py
@@ -0,0 +1,481 @@
+import shlex
+import shutil
+import subprocess
+from collections.abc import Iterable, Mapping
+from functools import cached_property
+from pathlib import Path
+from typing import Final, Optional, Union
+
+import tomlkit
+import tomlkit.items
+from typing_extensions import Self
+
+from dagster_dg.cache import CachableDataType, DgCache, hash_paths
+from dagster_dg.component import RemoteComponentRegistry
+from dagster_dg.config import (
+    DgConfig,
+    DgRawCliConfig,
+    discover_config_file,
+    load_dg_root_file_config,
+    load_dg_workspace_file_config,
+)
+from dagster_dg.error import DgError
+from dagster_dg.utils import (
+    MISSING_DAGSTER_COMPONENTS_ERROR_MESSAGE,
+    NO_LOCAL_VENV_ERROR_MESSAGE,
+    NOT_COMPONENT_LIBRARY_ERROR_MESSAGE,
+    NOT_PROJECT_ERROR_MESSAGE,
+    NOT_WORKSPACE_ERROR_MESSAGE,
+    NOT_WORKSPACE_OR_PROJECT_ERROR_MESSAGE,
+    ensure_loadable_path,
+    exit_with_error,
+    generate_missing_dagster_components_in_local_venv_error_message,
+    get_path_for_module,
+    get_toml_value,
+    get_venv_executable,
+    has_toml_value,
+    is_package_installed,
+    pushd,
+    resolve_local_venv,
+    strip_activated_venv_from_env_vars,
+)
+
+# Project
+_DEFAULT_PROJECT_COMPONENTS_LIB_SUBMODULE: Final = "lib"
+_DEFAULT_PROJECT_COMPONENTS_SUBMODULE: Final = "defs"
+
+# Workspace
+_WORKSPACE_PROJECTS_DIR: Final = "projects"
+
+
+class DgContext:
+    root_path: Path
+    workspace_root_path: Optional[Path]
+    config: DgConfig
+    cli_opts: Optional[DgRawCliConfig] = None
+    _cache: Optional[DgCache] = None
+
+    # We need to preserve CLI options for the context to be able to derive new contexts, because
+    # cli_options override everything else. If we didn't maintain them we wouldn't be able to tell
+    # whether a given config option should be overridden in a new derived context.
+    def __init__(
+        self,
+        config: DgConfig,
+        root_path: Path,
+        workspace_root_path: Optional[Path] = None,
+        cli_opts: Optional[DgRawCliConfig] = None,
+    ):
+        self.config = config
+        self.root_path = root_path
+        self.workspace_root_path = workspace_root_path
+        self.cli_opts = cli_opts
+        if config.cli.disable_cache or not self.use_dg_managed_environment:
+            self._cache = None
+        else:
+            self._cache = DgCache.from_config(config)
+        self.component_registry = RemoteComponentRegistry.empty()
+
+    @classmethod
+    def for_workspace_environment(cls, path: Path, command_line_config: DgRawCliConfig) -> Self:
+        context = cls.from_file_discovery_and_command_line_config(path, command_line_config)
+
+        # Commands that operate on a workspace need to be run inside a workspace context.
+        if not context.is_workspace:
+            exit_with_error(NOT_WORKSPACE_ERROR_MESSAGE)
+        return context
+
+    @classmethod
+    def for_project_environment(cls, path: Path, command_line_config: DgRawCliConfig) -> Self:
+        context = cls.from_file_discovery_and_command_line_config(path, command_line_config)
+
+        # Commands that operate on a project need to be run (a) with dagster-components
+        # available; and (b) inside a Dagster project context.
+        _validate_dagster_components_availability(context)
+
+        if not context.is_project:
+            exit_with_error(NOT_PROJECT_ERROR_MESSAGE)
+        return context
+
+    @classmethod
+    def for_workspace_or_project_environment(
+        cls, path: Path, commmand_line_config: DgRawCliConfig
+    ) -> Self:
+        context = cls.from_file_discovery_and_command_line_config(path, commmand_line_config)
+
+        # Commands that operate on a workspace need to be run inside a workspace or project
+        # context.
+        if not (context.is_workspace or context.is_project):
+            exit_with_error(NOT_WORKSPACE_OR_PROJECT_ERROR_MESSAGE)
+        return context
+
+    @classmethod
+    def for_component_library_environment(
+        cls, path: Path, command_line_config: DgRawCliConfig
+    ) -> Self:
+        context = cls.from_file_discovery_and_command_line_config(path, command_line_config)
+
+        # Commands that operate on a component library need to be run (a) with dagster-components
+        # available; (b) in a component library context.
+        _validate_dagster_components_availability(context)
+
+        if not context.is_component_library:
+            exit_with_error(NOT_COMPONENT_LIBRARY_ERROR_MESSAGE)
+        return context
+
+    @classmethod
+    def for_defined_registry_environment(
+        cls, path: Path, command_line_config: DgRawCliConfig
+    ) -> Self:
+        context = cls.from_file_discovery_and_command_line_config(path, command_line_config)
+
+        # Commands that access the component registry need to be run with dagster-components
+        # available.
+        _validate_dagster_components_availability(context)
+        return context
+
+    @classmethod
+    def from_file_discovery_and_command_line_config(
+        cls,
+        path: Path,
+        command_line_config: DgRawCliConfig,
+    ) -> Self:
+        config_path = discover_config_file(path)
+        workspace_config_path = discover_config_file(
+            path, lambda x: bool(x.get("directory_type") == "workspace")
+        )
+
+        if config_path:
+            root_path = config_path.parent
+            root_file_config = load_dg_root_file_config(config_path)
+            if workspace_config_path:
+                workspace_root_path = workspace_config_path.parent
+
+                # If the workspace config is different from the root config, we need to load the
+                # workspace config.
+                container_workspace_file_config = (
+                    load_dg_workspace_file_config(workspace_config_path)
+                    if config_path != workspace_config_path
+                    else None
+                )
+
+            else:
+                container_workspace_file_config = None
+                workspace_root_path = None
+        else:
+            root_path = Path.cwd()
+            workspace_root_path = None
+            root_file_config = None
+            container_workspace_file_config = None
+        config = DgConfig.from_partial_configs(
+            root_file_config=root_file_config,
+            container_workspace_file_config=container_workspace_file_config,
+            command_line_config=command_line_config,
+        )
+
+        return cls(
+            config=config,
+            root_path=root_path,
+            workspace_root_path=workspace_root_path,
+            cli_opts=command_line_config,
+        )
+
+    @classmethod
+    def default(cls) -> Self:
+        return cls(DgConfig.default(), Path.cwd())
+
+    # Use to derive a new context for a project while preserving existing settings
+    def with_root_path(self, root_path: Path) -> Self:
+        if not root_path / "pyproject.toml":
+            raise DgError(f"Cannot find `pyproject.toml` at {root_path}")
+        return self.__class__.from_file_discovery_and_command_line_config(
+            root_path, self.cli_opts or {}
+        )
+
+    # ########################
+    # ##### CACHE METHODS
+    # ########################
+
+    @property
+    def cache(self) -> DgCache:
+        if not self._cache:
+            raise DgError("Cache is disabled")
+        return self._cache
+
+    @property
+    def has_cache(self) -> bool:
+        return self._cache is not None
+
+    # Allowing open-ended str data_type for now so we can do module names
+    def get_cache_key(self, data_type: Union[CachableDataType, str]) -> tuple[str, str, str]:
+        path_parts = [str(part) for part in self.root_path.parts if part != self.root_path.anchor]
+        paths_to_hash = [
+            self.root_path / "uv.lock",
+            *([self.default_components_library_path] if self.is_component_library else []),
+        ]
+        env_hash = hash_paths(paths_to_hash)
+        return ("_".join(path_parts), env_hash, data_type)
+
+    def get_cache_key_for_module(self, module_name: str) -> tuple[str, str, str]:
+        if module_name.startswith(self.root_module_name):
+            path = self.get_path_for_module(module_name)
+            env_hash = hash_paths([path], includes=["*.py"])
+            path_parts = [str(part) for part in path.parts if part != "/"]
+            return ("_".join(path_parts), env_hash, "local_component_registry")
+        else:
+            return self.get_cache_key(module_name)
+
+    # ########################
+    # ##### WORKSPACE METHODS
+    # ########################
+
+    @property
+    def is_workspace(self) -> bool:
+        return self.workspace_root_path is not None
+
+    def get_workspace_project_path(self, name: str) -> Path:
+        if not self.workspace_root_path:
+            raise DgError("`get_workspace_project_path` is only available in a workspace context")
+        return self.workspace_root_path / _WORKSPACE_PROJECTS_DIR / name
+
+    def has_project(self, name: str) -> bool:
+        if not self.is_workspace:
+            raise DgError("`has_project` is only available in a workspace context")
+
+        return self.get_workspace_project_path(name).is_dir()
+
+    @property
+    def project_root_path(self) -> Path:
+        if not self.workspace_root_path:
+            raise DgError("`project_root_path` is only available in a workspace context")
+        return self.workspace_root_path / _WORKSPACE_PROJECTS_DIR
+
+    def get_project_names(self) -> Iterable[str]:
+        return [loc.name for loc in sorted(self.project_root_path.iterdir())]
+
+    def get_project_path(self, name: str) -> Path:
+        return self.project_root_path / name
+
+    def get_project_root_module(self, name: str) -> Path:
+        return self.project_root_path / name
+
+    # ########################
+    # ##### GENERAL PYTHON PACKAGE METHODS
+    # ########################
+
+    @property
+    def root_module_name(self) -> str:
+        if self.config.project:
+            return self.config.project.root_module
+        elif self.is_component_library:
+            return self.default_components_library_module.split(".")[0]
+        else:
+            raise DgError("Cannot determine root package name")
+
+    # ########################
+    # ##### PROJECT METHODS
+    # ########################
+
+    @property
+    def is_project(self) -> bool:
+        return self.config.project is not None
+
+    @property
+    def project_name(self) -> str:
+        if not self.is_project:
+            raise DgError("`project_name` is only available in a Dagster project context")
+        return self.root_path.name
+
+    @property
+    def project_python_executable(self) -> Path:
+        if not self.is_project:
+            raise DgError(
+                "`project_python_executable` is only available in a Dagster project context"
+            )
+        return self.root_path / get_venv_executable(Path(".venv"))
+
+    @cached_property
+    def components_module_name(self) -> str:
+        if not self.config.project:
+            raise DgError("`components_module_name` is only available in a Dagster project context")
+        return (
+            self.config.project.components_module
+            or f"{self.root_module_name}.{_DEFAULT_PROJECT_COMPONENTS_SUBMODULE}"
+        )
+
+    @cached_property
+    def components_path(self) -> Path:
+        if not self.is_project:
+            raise DgError("`components_path` is only available in a Dagster project context")
+        return self.get_path_for_module(self.components_module_name)
+
+    def get_component_instance_names(self) -> Iterable[str]:
+        return [str(instance_path.name) for instance_path in self.components_path.iterdir()]
+
+    def get_component_instance_module(self, name: str) -> str:
+        return f"{self.components_module_name}.{name}"
+
+    def has_component_instance(self, name: str) -> bool:
+        return (self.components_path / name).is_dir()
+
+    @property
+    def definitions_module_name(self) -> str:
+        if not self.is_project:
+            raise DgError(
+                "`definitions_module_name` is only available in a Dagster project context"
+            )
+        return f"{self.root_module_name}.definitions"
+
+    @cached_property
+    def definitions_path(self) -> Path:
+        return self.get_path_for_module(self.definitions_module_name)
+
+    # ########################
+    # ##### COMPONENT LIBRARY METHODS
+    # ########################
+
+    # It is possible for a single package to define multiple entry points under the
+    # `dagster.components` entry point group. At present, `dg` only cares about the first one, which
+    # it uses for all component type scaffolding operations.
+
+    @property
+    def is_component_library(self) -> bool:
+        return bool(self._dagster_components_entry_points)
+
+    @cached_property
+    def default_components_library_module(self) -> str:
+        if not self._dagster_components_entry_points:
+            raise DgError(
+                "`default_components_library_module_name` is only available in a component library context"
+            )
+        return next(iter(self._dagster_components_entry_points.values()))
+
+    @cached_property
+    def default_components_library_path(self) -> Path:
+        if not self.is_component_library:
+            raise DgError("`components_lib_path` is only available in a component library context")
+        return self.get_path_for_module(self.default_components_library_module)
+
+    @cached_property
+    def _dagster_components_entry_points(self) -> Mapping[str, str]:
+        if not self.pyproject_toml_path.exists():
+            return {}
+        toml = tomlkit.parse(self.pyproject_toml_path.read_text())
+        if not has_toml_value(toml, ("project", "entry-points", "dagster.components")):
+            return {}
+        else:
+            return get_toml_value(
+                toml,
+                ("project", "entry-points", "dagster.components"),
+                (tomlkit.items.Table, tomlkit.items.InlineTable),
+            ).unwrap()
+
+    # ########################
+    # ##### HELPERS
+    # ########################
+
+    def external_components_command(self, command: list[str], log: bool = True) -> str:
+        executable_path = self.get_executable("dagster-components")
+        if self.use_dg_managed_environment:
+            # uv run will resolve to the same dagster-components as we resolve above
+            command = ["uv", "run", "dagster-components", *command]
+            env = strip_activated_venv_from_env_vars()
+        else:
+            command = [str(executable_path), *command]
+            env = None
+        with pushd(self.root_path):
+            if log:
+                print(f"Using {executable_path}")  # noqa: T201
+
+            # We don't capture stderr here-- it will print directly to the console, then we can
+            # add a clean error message at the end explanining what happened.
+            result = subprocess.run(command, stdout=subprocess.PIPE, env=env, check=False)
+            if result.returncode != 0:
+                exit_with_error(f"""
+                    An error occurred while executing a `dagster-components` command in the {self.environment_desc}.
+
+                    `{shlex.join(command)}` exited with code {result.returncode}. Aborting.
+                """)
+            else:
+                return result.stdout.decode("utf-8")
+
+    def ensure_uv_lock(self, path: Optional[Path] = None) -> None:
+        path = path or self.root_path
+        with pushd(path):
+            if not (path / "uv.lock").exists():
+                subprocess.run(["uv", "sync"], check=True, env=strip_activated_venv_from_env_vars())
+
+    @property
+    def use_dg_managed_environment(self) -> bool:
+        return self.config.cli.use_dg_managed_environment and self.is_project
+
+    @property
+    def has_venv(self) -> bool:
+        return resolve_local_venv(self.root_path) is not None
+
+    @cached_property
+    def venv_path(self) -> Path:
+        path = resolve_local_venv(self.root_path)
+        if not path:
+            raise DgError("Cannot find .venv")
+        return path
+
+    def has_executable(self, command: str) -> bool:
+        return self._resolve_executable(command) is not None
+
+    def get_executable(self, command: str) -> Path:
+        if not (executable := self._resolve_executable(command)):
+            raise DgError(f"Cannot find executable {command}")
+        return executable
+
+    def _resolve_executable(self, command: str) -> Optional[Path]:
+        if (
+            self.has_venv
+            and (venv_exec := get_venv_executable(self.venv_path, command))
+            and venv_exec.exists()
+        ):
+            return venv_exec
+        elif not self.use_dg_managed_environment and (global_exec := shutil.which(command)):
+            return Path(global_exec)
+        else:
+            return None
+
+    @property
+    def environment_desc(self) -> str:
+        if self.has_venv:
+            return f"Python environment at {self.venv_path}"
+        else:
+            return "ambient Python environment"
+
+    @property
+    def pyproject_toml_path(self) -> Path:
+        return self.root_path / "pyproject.toml"
+
+    def get_path_for_module(self, module_name: str) -> Path:
+        with ensure_loadable_path(self.root_path):
+            parts = module_name.split(".")
+            for i in range(len(parts)):
+                leading_module_name = ".".join(parts[: i + 1])
+                if not is_package_installed(leading_module_name):
+                    raise DgError(
+                        f"Module `{leading_module_name}` is not installed in the current environment."
+                    )
+            return Path(get_path_for_module(module_name))
+
+
+# ########################
+# ##### HELPERS
+# ########################
+
+
+def _validate_dagster_components_availability(context: DgContext) -> None:
+    if context.config.cli.require_local_venv:
+        if not context.has_venv:
+            exit_with_error(NO_LOCAL_VENV_ERROR_MESSAGE)
+        elif not get_venv_executable(context.venv_path, "dagster-components").exists():
+            exit_with_error(
+                generate_missing_dagster_components_in_local_venv_error_message(
+                    str(context.venv_path)
+                )
+            )
+    elif not context.has_executable("dagster-components"):
+        exit_with_error(MISSING_DAGSTER_COMPONENTS_ERROR_MESSAGE)
