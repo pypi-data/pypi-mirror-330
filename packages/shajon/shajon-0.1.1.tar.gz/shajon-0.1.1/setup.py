954b066b90c5f26dfd815b8c9eb93b301dc4dbc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-28 05:06:42 +0000

    Add package shajon/0.1.1/shajon-0.1.1.tar.gz

---
+++ packages/shajon/shajon-0.1.1.tar.gz/shajon-0.1.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="shajon",
+    version="0.1.1",
+    packages=find_packages(),
+    install_requires=[],
+    author="SHAH MAKHDUM SHAJON",
+    author_email="shahmakhdumshajon@gmail.com",
+    description="A module that checks and downloads required shared object files [JSON XD and others tool sequrity files].",
+    url="https://github.com/SHAJON-404/SHAJON",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
