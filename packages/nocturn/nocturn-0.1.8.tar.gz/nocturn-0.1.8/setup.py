0b29217ba7f287002919356beaf84d6df43126ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-28 04:05:39 +0000

    Add package nocturn/0.1.8/nocturn-0.1.8.tar.gz

---
+++ packages/nocturn/nocturn-0.1.8.tar.gz/nocturn-0.1.8/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="nocturn",
+    version="0.1.8",
+    author="Suryanshsinh Sisodiya",
+    author_email="suryanshsinhsisodiya@gmail.com",
+    description="A Python-based lightweight crypto wallet for Ethereum, Binance Smart Chain, and Polygon.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/suryanshsinh/nocturn",
+    packages=find_packages(),
+    install_requires=[
+        "web3",
+        "eth-account",
+        "mnemonic",
+        "bip32utils",
+        "requests",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    license="MIT",
+    python_requires=">=3.6",
+)
