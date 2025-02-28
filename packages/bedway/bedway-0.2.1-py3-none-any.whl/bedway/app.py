12e4e842556079c391623f90884bc9a714e2125a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-28 05:05:05 +0000

    Add package bedway/0.2.1/bedway-0.2.1-py3-none-any.whl

---
+++ packages/bedway/bedway-0.2.1-py3-none-any.whl/bedway/app.py
@@ -0,0 +1,44 @@
+import uvicorn
+from fastapi import FastAPI
+from fastapi.exceptions import RequestValidationError
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import PlainTextResponse
+
+from bedway.routers import chat, embeddings, model
+from bedway.setting import API_ROUTE_PREFIX, DESCRIPTION, SUMMARY, TITLE, VERSION
+
+config = {
+    "title": TITLE,
+    "description": DESCRIPTION,
+    "summary": SUMMARY,
+    "version": VERSION,
+}
+
+app = FastAPI(**config)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(model.router, prefix=API_ROUTE_PREFIX)
+app.include_router(chat.router, prefix=API_ROUTE_PREFIX)
+app.include_router(embeddings.router, prefix=API_ROUTE_PREFIX)
+
+
+@app.get("/health")
+async def health():
+    """For health check if needed"""
+    return {"status": "OK"}
+
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request, exc):
+    return PlainTextResponse(str(exc), status_code=400)
+
+
+if __name__ == "__main__":
+    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
