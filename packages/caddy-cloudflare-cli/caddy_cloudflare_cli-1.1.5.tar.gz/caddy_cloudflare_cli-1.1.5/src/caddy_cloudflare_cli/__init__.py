d5541b400b3015a6fc34eab9628bdb7dc2cd00ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-01 15:06:04 +0000

    Add package caddy-cloudflare-cli/1.1.5/caddy_cloudflare_cli-1.1.5.tar.gz

---
+++ packages/caddy-cloudflare-cli/caddy_cloudflare_cli-1.1.5.tar.gz/caddy_cloudflare_cli-1.1.5/src/caddy_cloudflare_cli/__init__.py
@@ -0,0 +1,7 @@
+"""
+Caddy Cloudflare CLI - Tool for setting up and managing Cloudflare DNS with Caddy
+"""
+
+__version__ = "1.1.4"
+__author__ = "Tadeas Fort"
+__email__ = "business@tadeasfort.com"
