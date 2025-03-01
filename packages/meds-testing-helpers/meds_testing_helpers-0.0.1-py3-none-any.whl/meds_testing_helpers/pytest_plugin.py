9055e62e98a94589f23475568ce4d30f1113c582
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-01 05:05:15 +0000

    Add package meds-testing-helpers/0.0.1/meds_testing_helpers-0.0.1-py3-none-any.whl

---
+++ packages/meds-testing-helpers/meds_testing_helpers-0.0.1-py3-none-any.whl/meds_testing_helpers/pytest_plugin.py
@@ -0,0 +1,69 @@
+"""Pytest plugin capabilities for loading sample MEDS datasets."""
+
+import subprocess
+import tempfile
+from pathlib import Path
+
+import pytest
+
+from .dataset import MEDSDataset
+from .static_sample_data import SIMPLE_STATIC_SHARDED_BY_SPLIT
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--generated-dataset-N",
+        type=int,
+        help="If using the generated-dataset dataset, how many patients?",
+        default=500,
+    )
+
+    parser.addoption(
+        "--generated-dataset-seed",
+        type=int,
+        help="If generating a dataset, what seed to use?",
+        default=1,
+    )
+
+
+@pytest.fixture
+def simple_static_MEDS() -> Path:
+    with tempfile.TemporaryDirectory() as data_dir:
+        data_dir = Path(data_dir)
+        data = MEDSDataset.from_yaml(SIMPLE_STATIC_SHARDED_BY_SPLIT)
+        data.write(data_dir)
+        yield data_dir
+
+
+def generate_MEDS(request, dataset_spec: str) -> Path:
+    N = request.config.getoption("--generated-dataset-N")
+    seed = request.config.getoption("--generated-dataset-seed")
+    with tempfile.TemporaryDirectory() as data_dir:
+        data_dir = Path(data_dir)
+
+        cmd_args = [
+            "build_sample_MEDS_dataset",
+            f"seed={seed}",
+            f"N_subjects={N}",
+            "do_overwrite=False",
+            f"output_dir={str(data_dir)}",
+            "dataset_spec/data_generator=sample",
+        ]
+
+        out = subprocess.run(cmd_args, shell=False, check=False, capture_output=True)
+
+        error_str = (
+            f"Command failed with return code {out.returncode}.\n"
+            f"Command stdout:\n{out.stdout.decode()}\n"
+            f"Command stderr:\n{out.stderr.decode()}"
+        )
+
+        if out.returncode != 0:
+            raise RuntimeError(error_str)
+
+        yield data_dir
+
+
+@pytest.fixture
+def generated_sample_MEDS(request) -> Path:
+    yield from generate_MEDS(request, "sample")
