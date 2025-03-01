03543a6b7f7c8cdb5811f7d1219c213aa9e74949
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-01 06:05:39 +0000

    Add package fastemriwaveforms/2.0.0rc1/fastemriwaveforms-2.0.0rc1-cp311-cp311-manylinux_2_28_aarch64.whl

---
+++ packages/fastemriwaveforms/fastemriwaveforms-2.0.0rc1-cp311-cp311-manylinux_2_28_aarch64.whl/few/waveform/__init__.py
@@ -0,0 +1,22 @@
+"""
+The waveform package houses the waveform generation classes, as well as the generic waveform
+generation interface `GenerateEMRIWaveform`.
+"""
+
+from .waveform import (
+    GenerateEMRIWaveform,
+    FastKerrEccentricEquatorialFlux,
+    FastSchwarzschildEccentricFlux,
+    FastSchwarzschildEccentricFluxBicubic,
+    SlowSchwarzschildEccentricFlux,
+    Pn5AAKWaveform,
+)
+
+__all__ = [
+    "GenerateEMRIWaveform",
+    "FastKerrEccentricEquatorialFlux",
+    "FastSchwarzschildEccentricFlux",
+    "FastSchwarzschildEccentricFluxBicubic",
+    "SlowSchwarzschildEccentricFlux",
+    "Pn5AAKWaveform",
+]
