28f588d04b23e681f89f5b4607d47a001495539d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-01 03:15:07 +0000

    Add package pancham/0.3.2/pancham-0.3.2-py3-none-any.whl

---
+++ packages/pancham/pancham-0.3.2-py3-none-any.whl/pancham/__main__.py
@@ -0,0 +1,29 @@
+from typing import Annotated, Optional
+
+import typer
+
+from .reporter_lib.halo_reporter import HaloReporter
+from .reporter import get_reporter
+from .runner import PanchamRunner
+from .pancham_configuration import OrderedPanchamConfiguration
+
+app = typer.Typer()
+
+@app.command()
+async def run(
+        configuration: Annotated[str, typer.Argument(help = "Path to the Pancham configuration file")],
+        data_configuration: Annotated[Optional[str], typer.Argument(help = "Path to the data mapping if individual files are being used")] = None
+):
+    pancham_configuration = OrderedPanchamConfiguration(configuration)
+
+    if pancham_configuration.reporter_name == 'spinner':
+        reporter = get_reporter(pancham_configuration.debug_status, HaloReporter())
+    else:
+        reporter = get_reporter(pancham_configuration.debug_status)
+
+    runner = PanchamRunner(pancham_configuration, reporter = reporter)
+
+    if len(pancham_configuration.mapping_files) == 0:
+        await runner.load_and_run(data_configuration)
+    else:
+        await runner.run_all()
