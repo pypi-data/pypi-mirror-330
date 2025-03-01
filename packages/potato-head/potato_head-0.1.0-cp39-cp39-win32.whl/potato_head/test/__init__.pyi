3b56e6c6f8b05fbeac7a6db41a230ab5708aaac8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-01 06:05:36 +0000

    Add package potato-head/0.1.0/potato_head-0.1.0-cp39-cp39-win32.whl

---
+++ packages/potato-head/potato_head-0.1.0-cp39-cp39-win32.whl/potato_head/test/__init__.pyi
@@ -0,0 +1,25 @@
+class LLMTestServer:
+    def __init__(self, cleanup: bool = True) -> None:
+        """Instantiates the test server.
+
+        When the test server is used as a context manager, it will start the server
+        in a background thread and set the appropriate env vars so that the client
+        can connect to the server. The server will be stopped when the context manager
+        exits and the env vars will be reset.
+
+        Args:
+            cleanup (bool, optional): Whether to cleanup the server after the test.
+            Defaults to True.
+        """
+
+    def start_server(self) -> None:
+        """Starts the test server."""
+
+    def stop_server(self) -> None:
+        """Stops the test server."""
+
+    def __enter__(self) -> "LLMTestServer":
+        """Starts the test server."""
+
+    def __exit__(self, exc_type, exc_value, traceback) -> None:
+        """Stops the test server."""
