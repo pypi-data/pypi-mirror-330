574f19bae0d926c97fe8d49dd4e7ad7fd58aa339
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-01 14:06:42 +0000

    Add package pip-cleanup/0.1.8/pip_cleanup-0.1.8.tar.gz

---
+++ packages/pip-cleanup/pip_cleanup-0.1.8.tar.gz/pip_cleanup-0.1.8/pip_cleanup/pipkill/__init__.py
@@ -0,0 +1,11 @@
+"""
+pipkill - A tool to find and remove unused pip packages
+"""
+
+from .cli import main
+
+__version__ = "0.1.0"
+__author__ = "Samso9th"
+
+if __name__ == "__main__":
+    main() 
