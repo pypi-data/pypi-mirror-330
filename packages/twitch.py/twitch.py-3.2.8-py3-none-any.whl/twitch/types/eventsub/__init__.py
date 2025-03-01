fdedf97f04cc42cd179e0ba4f2a61f06f6afdbef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-01 14:06:32 +0000

    Add package twitch.py/3.2.8/twitch.py-3.2.8-py3-none-any.whl

---
+++ packages/twitch.py/twitch.py-3.2.8-py3-none-any.whl/twitch/types/eventsub/__init__.py
@@ -0,0 +1,32 @@
+"""
+twitch.types.eventsub
+
+Typing for Twitch.
+
+:copyright: (c) 2025-present Snifo
+:license: MIT, see LICENSE for more details.
+"""
+
+from __future__ import annotations
+
+from . import activity, bits, channels, chat, interaction, moderation, streams, users
+from typing import TYPE_CHECKING, TypedDict
+
+if TYPE_CHECKING:
+    from typing import TypeVar, Dict, Any
+    T = TypeVar('T')
+
+
+class Subscription(TypedDict):
+    id: str
+    type: str
+    version: str
+    status: str
+    cost: int
+    condition: Dict[str, Any]
+    created_at: str
+
+
+class Data(TypedDict):
+    subscription: Subscription
+    event: T
