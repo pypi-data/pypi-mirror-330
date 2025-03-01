0f83ff17f4de726b9e6823bcae281582e775cf80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-01 04:05:56 +0000

    Add package auto-easy/0.1.0/auto_easy-0.1.0.tar.gz

---
+++ packages/auto-easy/auto_easy-0.1.0.tar.gz/auto_easy-0.1.0/auto_easy/core/win_supper_res.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+from auto_core.ai.model_mgr_v2 import ModelMgrV2, AIModelBase
+from auto_core.ai.superres.ai_supper_res import SuperRes
+from auto_core.window.windows import Window
+
+
