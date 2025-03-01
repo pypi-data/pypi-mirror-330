c620f58677680053e91b8bc7a875fb85369a2134
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-01 05:05:43 +0000

    Add package jf1uids/0.2.3/jf1uids-0.2.3-py3-none-any.whl

---
+++ packages/jf1uids/jf1uids-0.2.3-py3-none-any.whl/jf1uids/__init__.py
@@ -0,0 +1,16 @@
+# setup
+from jf1uids.option_classes.simulation_config import SimulationConfig
+from jf1uids.data_classes.simulation_helper_data import get_helper_data
+from jf1uids.option_classes.simulation_params import SimulationParams
+from jf1uids.fluid_equations.registered_variables import get_registered_variables
+
+# module-setup
+from jf1uids._physics_modules._stellar_wind.stellar_wind_options import WindParams
+
+# run
+from jf1uids.time_stepping.time_integration import time_integration
+
+# postprocessing
+from jf1uids.shock_finder.shock_finder import strongest_shock_radius
+# units
+from jf1uids.units import CodeUnits
