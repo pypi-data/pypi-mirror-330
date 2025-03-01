91a4e299a07b00159af81bb018812f1a48855de8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-01 06:05:32 +0000

    Add package InclusionMap/1.5.0/InclusionMap-1.5.0-py3-none-any.whl

---
+++ packages/InclusionMap/InclusionMap-1.5.0-py3-none-any.whl/inc_map/readable_path.py
@@ -0,0 +1,14 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from typing import Collection
+
+from pathlib import Path
+
+def readable_path(root_dirs: Collection[Path], file: Path) -> Path:
+    for root in root_dirs:
+        if file.is_relative_to(root):
+            if len(root_dirs) == 1:
+                return file.relative_to(root)
+            return Path(root.name, file.relative_to(root))
+    raise ValueError(f'Unknown file "{file}"')
