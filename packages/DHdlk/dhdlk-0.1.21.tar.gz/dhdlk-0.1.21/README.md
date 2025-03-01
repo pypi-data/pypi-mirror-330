dfa0785cfa5ff9847626c1eac948c10a85dfdcc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-01 06:05:47 +0000

    Add package DHdlk/0.1.21/dhdlk-0.1.21.tar.gz

---
+++ packages/DHdlk/dhdlk-0.1.21.tar.gz/dhdlk-0.1.21/README.md
@@ -0,0 +1,8 @@
+# My Python Package
+
+Bu proje, basit bir Python modülü içerir. Modül, belirli işlevleri yerine getirmek için kullanılır.
+
+## Kurulum
+
+### Pip ile Kurulum
+
---
+++ packages/DHdlk/dhdlk-0.1.21.tar.gz/dhdlk-0.1.21/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="DHdlk",
+    version="0.1.21",
+    packages=find_packages(include=["xntwoger*", "pyarmor_runtime*"]),
+    include_package_data=True,  # MANIFEST.in dosyasını kullan
+    package_data={
+        "": ["*.py", "*.pyd", "*.pyc"],
+        "pyarmor_runtime": ["*"],
+        "xntwoger/dist/pyarmor_runtime_000000": ["*"],
+    },
+)
