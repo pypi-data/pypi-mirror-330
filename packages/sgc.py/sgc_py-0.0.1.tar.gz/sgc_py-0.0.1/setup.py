40dac44eb2f104ea56a25228272b0bfb80752768
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-01 14:06:29 +0000

    Add package sgc.py/0.0.1/sgc_py-0.0.1.tar.gz

---
+++ packages/sgc.py/sgc_py-0.0.1.tar.gz/sgc_py-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sgc.py',
+    version="0.0.1",
+    author='Mochagod123',
+    url="https://github.com/mochagod123/sgc.py",
+    license='GPL 3.0 license',
+    classifiers=[
+        "Development Status :: 1 - Planning"
+    ],
+    packages=find_packages()
+)
