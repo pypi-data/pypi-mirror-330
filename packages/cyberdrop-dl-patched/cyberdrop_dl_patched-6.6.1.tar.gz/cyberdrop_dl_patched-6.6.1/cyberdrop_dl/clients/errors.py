fe10e8c4980016e3741dcfe9da75dbd338014526
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-01 06:06:21 +0000

    Add package cyberdrop-dl-patched/6.6.1/cyberdrop_dl_patched-6.6.1.tar.gz

---
+++ packages/cyberdrop-dl-patched/cyberdrop_dl_patched-6.6.1.tar.gz/cyberdrop_dl_patched-6.6.1/cyberdrop_dl/clients/errors.py
@@ -0,0 +1,243 @@
+from __future__ import annotations
+
+from functools import singledispatch
+from http import HTTPStatus
+from pathlib import Path
+from typing import TYPE_CHECKING, NamedTuple
+
+from yaml import YAMLError
+from yarl import URL
+
+from cyberdrop_dl.utils.constants import VALIDATION_ERROR_FOOTER
+
+if TYPE_CHECKING:
+    from requests import Response
+
+    from cyberdrop_dl.scraper.crawler import ScrapeItem
+    from cyberdrop_dl.utils.data_enums_classes.url_objects import MediaItem
+
+
+# See: https://developers.cloudflare.com/support/troubleshooting/cloudflare-errors/troubleshooting-cloudflare-5xx-errors/
+CLOUDFLARE_ERRORS = {
+    520: "Unexpected Response",
+    521: "Web Server Down",
+    522: "Connection Timeout",
+    523: "Origin Is Unreachable",
+    524: "Response Timeout",
+    525: "SSL Handshake Failed",
+    526: "Untrusted",
+    530: "IP Banned / Restricted",
+}
+
+
+class CDLBaseError(Exception):
+    """Base exception for cyberdrop-dl errors."""
+
+    def __init__(
+        self,
+        ui_message: str = "Something went wrong",
+        *,
+        message: str | None = None,
+        status: str | int | None = None,
+        origin: ScrapeItem | MediaItem | URL | Path | None = None,
+    ) -> None:
+        self.ui_message = ui_message
+        self.message = message or ui_message
+        self.origin = get_origin(origin)
+        super().__init__(self.message)
+        if status:
+            self.status = status
+            super().__init__(self.status)
+
+    def __str__(self):
+        return self.format(self.ui_message, self.message)
+
+    @classmethod
+    def format(cls, ui_message: str, message: str) -> str:
+        if ui_message == message:
+            return message
+        return f"{ui_message} - {message}"
+
+
+class InvalidContentTypeError(CDLBaseError):
+    def __init__(self, *, message: str | None = None, origin: ScrapeItem | MediaItem | URL | None = None) -> None:
+        """This error will be thrown when the content type isn't as expected."""
+        ui_message = "Invalid Content Type"
+        super().__init__(ui_message, message=message, origin=origin)
+
+
+class NoExtensionError(CDLBaseError):
+    def __init__(self, *, message: str | None = None, origin: ScrapeItem | MediaItem | URL | None = None) -> None:
+        """This error will be thrown when no extension is given for a file."""
+        ui_message = "No File Extension"
+        super().__init__(ui_message, message=message, origin=origin)
+
+
+class InvalidExtensionError(NoExtensionError):
+    def __init__(self, *, message: str | None = None, origin: ScrapeItem | MediaItem | URL | None = None) -> None:
+        """This error will be thrown when no extension is given for a file."""
+        super().__init__(message=message, origin=origin)
+        self.ui_message = "Invalid File Extension"
+
+
+class PasswordProtectedError(CDLBaseError):
+    def __init__(self, message: str | None = None, *, origin: ScrapeItem | MediaItem | URL | None = None) -> None:
+        """This error will be thrown when a file is password protected."""
+        ui_message = "Password Protected"
+        msg = message or "File/Folder is password protected"
+        super().__init__(ui_message, message=msg, origin=origin)
+
+
+class MaxChildrenError(CDLBaseError):
+    def __init__(self, message: str | None = None, *, origin: ScrapeItem | MediaItem | URL | None = None) -> None:
+        """This error will be thrown when an scrape item reaches its max number or children."""
+        ui_message = "Max Children Reached"
+        msg = message or "Max number of children reached"
+        super().__init__(ui_message, message=msg, origin=origin)
+
+
+class DDOSGuardError(CDLBaseError):
+    def __init__(self, message: str | None = None, *, origin: ScrapeItem | MediaItem | URL | None = None) -> None:
+        """This error will be thrown when DDoS-Guard is detected."""
+        ui_message = "DDoS-Guard"
+        msg = message or "DDoS-Guard detected"
+        super().__init__(ui_message, message=msg, origin=origin)
+
+
+class DownloadError(CDLBaseError):
+    def __init__(
+        self,
+        status: str | int,
+        message: str | None = None,
+        origin: ScrapeItem | MediaItem | URL | None = None,
+        retry: bool = False,
+    ) -> None:
+        """This error will be thrown when a download fails."""
+        ui_message = create_error_msg(status)
+        msg = message
+        self.retry = retry
+        super().__init__(ui_message, message=msg, status=status, origin=origin)
+
+
+class SlowDownloadError(DownloadError):
+    def __init__(self, origin: ScrapeItem | MediaItem | URL | None = None) -> None:
+        """This error will be thrown when a file will be skipped do to a low download speed."""
+        ui_message = "Slow Download"
+        super().__init__(ui_message, origin=origin)
+
+
+class InsufficientFreeSpaceError(CDLBaseError):
+    def __init__(self, origin: ScrapeItem | MediaItem | URL | None = None) -> None:
+        """This error will be thrown when no enough storage is available."""
+        ui_message = "Insufficient Free Space"
+        super().__init__(ui_message, origin=origin)
+
+
+class RestrictedFiletypeError(CDLBaseError):
+    def __init__(self, origin: ScrapeItem | MediaItem | URL | None = None) -> None:
+        """This error will be thrown when has a filytpe not allowed by config."""
+        ui_message = "Restricted Filetype"
+        super().__init__(ui_message, origin=origin)
+
+
+class DurationError(CDLBaseError):
+    def __init__(self, origin: ScrapeItem | MediaItem | URL | None = None) -> None:
+        """THis error will be thrown when the file duration is not allowed by the config."""
+        ui_message = "Duration Not Allowed"
+        super().__init__(ui_message, origin=origin)
+
+
+class MediaFireError(CDLBaseError):
+    def __init__(
+        self, status: str | int, message: str | None = None, origin: ScrapeItem | MediaItem | URL | None = None
+    ) -> None:
+        """This error will be thrown when a scrape fails."""
+        ui_message = f"{status} MediaFire Error"
+        super().__init__(ui_message, message=message, status=status, origin=origin)
+
+
+class RealDebridError(CDLBaseError):
+    """Base RealDebrid API error."""
+
+    def __init__(self, response: Response, code: int, message: str) -> None:
+        url = URL(response.url)
+        self.path = url.path
+        msg = message.capitalize()
+        ui_message = f"{code} RealDebrid Error"
+        super().__init__(ui_message, message=msg, status=code, origin=url)
+
+
+class ScrapeError(CDLBaseError):
+    def __init__(
+        self, status: str | int, message: str | None = None, origin: ScrapeItem | MediaItem | URL | None = None
+    ) -> None:
+        """This error will be thrown when a scrape fails."""
+        ui_message = create_error_msg(status)
+        super().__init__(ui_message, message=message, status=status, origin=origin)
+
+
+class InvalidURLError(ScrapeError):
+    def __init__(
+        self, message: str | None = None, origin: ScrapeItem | MediaItem | URL | None = None, url: URL | str = ""
+    ) -> None:
+        """This error will be thrown when parsed URL is not valid."""
+        ui_message = "Invalid URL"
+        self.url = url
+        super().__init__(ui_message, message=message, origin=origin)
+
+
+class LoginError(CDLBaseError):
+    def __init__(self, message: str | None = None, *, origin: ScrapeItem | MediaItem | URL | None = None) -> None:
+        """This error will be thrown when the login fails for a site."""
+        ui_message = "Failed Login"
+        super().__init__(ui_message, message=message, origin=origin)
+
+
+class JDownloaderError(CDLBaseError):
+    """This error will be thrown for any Jdownloader error."""
+
+
+class InvalidYamlError(CDLBaseError):
+    def __init__(self, file: Path, e: Exception) -> None:
+        """This error will be thrown when a yaml config file has invalid values."""
+        file_path = file.resolve()
+        msg = f"Unable to read file '{file_path}'"
+        if isinstance(e, YAMLError):
+            msg = f"File '{file_path}' is not a valid YAML file"
+        mark = getattr(e, "problem_mark", None)
+        if mark:
+            msg += f"\n\nThe error was found in this line: \n {mark}"
+
+        problem = getattr(e, "problem", str(e))
+        msg += f"\n\n{problem.capitalize()}"
+        msg += f"\n\n{VALIDATION_ERROR_FOOTER}"
+        super().__init__("Invalid YAML", message=msg, origin=file)
+
+
+@singledispatch
+def create_error_msg(error: int) -> str:
+    try:
+        msg = HTTPStatus(error).phrase
+        return f"{error} {msg}"
+    except ValueError:
+        cloudflare_error = CLOUDFLARE_ERRORS.get(error)
+        if cloudflare_error:
+            return f"{error} {cloudflare_error}"
+    return f"{error} HTTP Error"
+
+
+@create_error_msg.register
+def _(error: str) -> str:
+    return error
+
+
+def get_origin(origin: ScrapeItem | Path | MediaItem | URL | None = None) -> Path | URL | None:
+    if origin and not isinstance(origin, URL | Path):
+        return origin.parents[0] if origin.parents else None
+    return origin
+
+
+class ErrorLogMessage(NamedTuple):
+    ui_msg: str
+    main_log_msg: str
+    csv_log_msg: str
---
+++ packages/cyberdrop-dl-patched/cyberdrop_dl_patched-6.6.1.tar.gz/cyberdrop_dl_patched-6.6.1/cyberdrop_dl/managers/progress_manager.py
@@ -0,0 +1,174 @@
+from __future__ import annotations
+
+import time
+from dataclasses import field
+from datetime import timedelta
+from functools import partial
+from typing import TYPE_CHECKING
+
+from pydantic import ByteSize
+from rich.console import Group
+from rich.layout import Layout
+from rich.progress import Progress, SpinnerColumn, TextColumn
+
+from cyberdrop_dl.ui.progress.downloads_progress import DownloadsProgress
+from cyberdrop_dl.ui.progress.file_progress import FileProgress
+from cyberdrop_dl.ui.progress.hash_progress import HashProgress
+from cyberdrop_dl.ui.progress.scraping_progress import ScrapingProgress
+from cyberdrop_dl.ui.progress.sort_progress import SortProgress
+from cyberdrop_dl.ui.progress.statistic_progress import DownloadStatsProgress, ScrapeStatsProgress
+from cyberdrop_dl.utils.logger import log, log_spacer, log_with_color
+
+if TYPE_CHECKING:
+    from pathlib import Path
+
+    from rich.console import RenderableType
+
+    from cyberdrop_dl.managers.manager import Manager
+    from cyberdrop_dl.ui.progress.statistic_progress import UiFailureTotal
+
+log_cyan = partial(log_with_color, style="cyan", level=20)
+log_yellow = partial(log_with_color, style="yellow", level=20)
+log_green = partial(log_with_color, style="green", level=20)
+log_red = partial(log_with_color, style="red", level=20)
+
+
+class ProgressManager:
+    def __init__(self, manager: Manager) -> None:
+        # File Download Bars
+        self.manager = manager
+        self.file_progress: FileProgress = FileProgress(
+            manager.config_manager.global_settings_data.ui_options.downloading_item_limit,
+            manager,
+        )
+
+        # Scraping Printout
+        self.scraping_progress: ScrapingProgress = ScrapingProgress(
+            manager.config_manager.global_settings_data.ui_options.scraping_item_limit,
+            manager,
+        )
+
+        # Overall Progress Bars & Stats
+        self.download_progress: DownloadsProgress = DownloadsProgress(manager)
+        self.download_stats_progress: DownloadStatsProgress = DownloadStatsProgress()
+        self.scrape_stats_progress: ScrapeStatsProgress = ScrapeStatsProgress()
+        self.hash_progress: HashProgress = HashProgress(manager)
+        self.sort_progress: SortProgress = SortProgress(1, manager)
+
+        self.ui_refresh_rate = manager.config_manager.global_settings_data.ui_options.refresh_rate
+
+        self.fullscreen_layout: Layout = field(init=False)
+        self.hash_remove_layout: RenderableType = field(init=False)
+        self.hash_layout: RenderableType = field(init=False)
+        self.sort_layout: RenderableType = field(init=False)
+
+    def startup(self) -> None:
+        """Startup process for the progress manager."""
+        progress_layout = Layout()
+        progress_layout.split_column(
+            Layout(name="upper", ratio=2, minimum_size=8),
+            Layout(renderable=self.scraping_progress.get_renderable(), name="Scraping", ratio=2),
+            Layout(renderable=self.file_progress.get_renderable(), name="Downloads", ratio=2),
+        )
+        progress_layout["upper"].split_row(
+            Layout(renderable=self.download_progress.get_progress(), name="Files", ratio=1),
+            Layout(renderable=self.scrape_stats_progress.get_progress(), name="Scrape Failures", ratio=1),
+            Layout(renderable=self.download_stats_progress.get_progress(), name="Download Failures", ratio=1),
+        )
+
+        spinner = SpinnerColumn(style="green", spinner_name="dots"), TextColumn("Running Cyberdrop-DL")
+        actvity_placeholder = Progress(*spinner)
+        actvity_placeholder.add_task("running with no UI", total=100, completed=0)
+
+        simple_layout = Group(actvity_placeholder, self.download_progress.simple_progress)
+
+        self.activity_layout = actvity_placeholder
+        self.simple_layout = simple_layout
+        self.fullscreen_layout = progress_layout
+        self.hash_remove_layout = self.hash_progress.get_removed_progress()
+        self.hash_layout = self.hash_progress.get_renderable()
+        self.sort_layout = self.sort_progress.get_renderable()
+
+    def print_stats(self, start_time: float) -> None:
+        """Prints the stats of the program."""
+        if not self.manager.parsed_args.cli_only_args.print_stats:
+            return
+        end_time = time.perf_counter()
+        runtime = timedelta(seconds=int(end_time - start_time))
+        data_size = ByteSize(self.file_progress.downloaded_data).human_readable(decimal=True)
+
+        log_spacer(20)
+        log("Printing Stats...\n", 20)
+        config_path = self.manager.path_manager.config_folder / self.manager.config_manager.loaded_config
+        config_path_text = get_console_hyperlink(config_path, text=self.manager.config_manager.loaded_config)
+        input_file_text = get_input(self.manager)
+        log_folder_text = get_console_hyperlink(self.manager.path_manager.log_folder)
+        log_cyan(f"Run Stats (config: {config_path_text}):", markup=True)
+        log_yellow(f"  Log Folder: {log_folder_text}", markup=True)
+        log_yellow(f"  Input File: {input_file_text}", markup=True)
+        log_yellow(f"  Input URLs: {self.manager.scrape_mapper.count:,}")
+        log_yellow(f"  Input URL Groups: {self.manager.scrape_mapper.group_count:,}")
+        log_yellow(f"  Total Runtime: {runtime}")
+        log_yellow(f"  Total Downloaded Data: {data_size}")
+
+        log_spacer(20, "")
+        log_cyan("Download Stats:")
+        log_green(f"  Downloaded: {self.download_progress.completed_files:,} files")
+        log_yellow(f"  Skipped (By Config): {self.download_progress.skipped_files:,} files")
+        log_yellow(f"  Skipped (Previously Downloaded): {self.download_progress.previously_completed_files:,} files")
+        log_red(f"  Failed: {self.download_stats_progress.failed_files:,} files")
+
+        log_spacer(20, "")
+        log_cyan("Unsupported URLs Stats:")
+        log_yellow(f"  Sent to Jdownloader: {self.scrape_stats_progress.sent_to_jdownloader:,}")
+        log_yellow(f"  Skipped: {self.scrape_stats_progress.unsupported_urls_skipped:,}")
+
+        log_spacer(20, "")
+        log_cyan("Dupe Stats:")
+        log_yellow(f"  Newly Hashed: {self.hash_progress.hashed_files:,} files")
+        log_yellow(f"  Previously Hashed: {self.hash_progress.prev_hashed_files:,} files")
+        log_yellow(f"  Removed (Downloads): {self.hash_progress.removed_files:,} files")
+
+        log_spacer(20, "")
+        log_cyan("Sort Stats:")
+        log_green(f"  Audios: {self.sort_progress.audio_count:,}")
+        log_green(f"  Images: {self.sort_progress.image_count:,}")
+        log_green(f"  Videos: {self.sort_progress.video_count:,}")
+        log_green(f"  Other Files: {self.sort_progress.other_count:,}")
+
+        last_padding = log_failures(self.scrape_stats_progress.return_totals(), "Scrape Failures:")
+        log_failures(self.download_stats_progress.return_totals(), "Download Failures:", last_padding)
+
+
+def log_failures(failures: list[UiFailureTotal], title: str = "Failures:", last_padding: int = 0) -> int:
+    log_spacer(20, "")
+    log_cyan(title)
+    if not failures:
+        log_green("  None")
+        return 0
+    error_padding = last_padding
+    error_codes = [f.error_code for f in failures if f.error_code is not None]
+    if error_codes:
+        error_padding = max(len(str(max(error_codes))), error_padding)
+    for f in failures:
+        error = f.error_code if f.error_code is not None else ""
+        log_red(f"  {error:>{error_padding}}{' ' if error_padding else ''}{f.msg}: {f.count:,}")
+    return error_padding
+
+
+def get_input(manager: Manager) -> str:
+    if manager.parsed_args.cli_only_args.retry_all:
+        return "--retry-all"
+    if manager.parsed_args.cli_only_args.retry_failed:
+        return "--retry-failed"
+    if manager.parsed_args.cli_only_args.retry_maintenance:
+        return "--retry-maintenance"
+    if manager.scrape_mapper.using_input_file:
+        return get_console_hyperlink(manager.path_manager.input_file)
+    return "--links (CLI args)"
+
+
+def get_console_hyperlink(file_path: Path, text: str = "") -> str:
+    full_path = file_path.resolve()
+    show_text = text or full_path
+    return f"[link=file://{full_path}]{show_text}[/link]"
