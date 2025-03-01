55a24c80ca42e9a99afda32bde545f76ccfc1e4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-01 04:05:38 +0000

    Add package profile-fox/1.0.3/profile-fox-1.0.3.tar.gz

---
+++ packages/profile-fox/profile-fox-1.0.3.tar.gz/profile-fox-1.0.3/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+import distutils.core
+
+setuptools.setup(
+    name='profile-fox',
+    version="1.0.3",
+    author='readwithai',
+    long_description_content_type='text/markdown',
+    author_email='talwrii@gmail.com',
+    description='Open things in a particular firefox profile from the command line',
+    license='MIT',
+    keywords='firefox,tabs,profile',
+    url='https://github.com/talwrii/profile-fox',
+    packages=["profile_fox"],
+    long_description=open('README.md').read(),
+    entry_points={
+        'console_scripts': ['profile-fox=profile_fox.main:main']
+    },
+)
