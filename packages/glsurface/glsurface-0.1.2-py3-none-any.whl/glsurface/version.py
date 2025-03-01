6411f6a56e3bc433a45a415d681f75057ce10680
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-01 14:06:40 +0000

    Add package glsurface/0.1.2/glsurface-0.1.2-py3-none-any.whl

---
+++ packages/glsurface/glsurface-0.1.2-py3-none-any.whl/glsurface/version.py
@@ -0,0 +1,9 @@
+PROJECT_NAME     = 'glsurface'
+
+VER_MAJOR        = 0
+VER_MINOR        = 1
+VER_RELEASE      = 2
+
+VER_FLAGS        = ""
+
+__version__ = f'{VER_MAJOR}.{VER_MINOR}.{VER_RELEASE}{VER_FLAGS}'
