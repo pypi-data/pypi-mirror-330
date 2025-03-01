c01f0f7990e3638e6df387e92dbb9b72e65697dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-01 14:06:38 +0000

    Add package refactool/0.1.0/refactool-0.1.0-py3-none-any.whl

---
+++ packages/refactool/refactool-0.1.0-py3-none-any.whl/api/version.py
@@ -0,0 +1,16 @@
+"""Controle de versão do projeto."""
+
+__version__ = "3.1.0"
+__version_info__ = tuple(map(int, __version__.split(".")))
+
+# Informações adicionais
+__title__ = "refactool"
+__description__ = "Analisador de código com cache distribuído e alta performance"
+__author__ = "RefacTool Team"
+__license__ = "MIT"
+__copyright__ = "Copyright 2024 RefacTool Team"
+
+# Metadados da versão
+VERSION_MAJOR = __version_info__[0]  # Mudanças incompatíveis
+VERSION_MINOR = __version_info__[1]  # Novas funcionalidades
+VERSION_PATCH = __version_info__[2]  # Correções de bugs 
