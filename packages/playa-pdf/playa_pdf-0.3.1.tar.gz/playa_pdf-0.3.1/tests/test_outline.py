3ebe328bc95afa070bd82b71398fc264b7f83369
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-01 03:15:18 +0000

    Add package playa-pdf/0.3.1/playa_pdf-0.3.1.tar.gz

---
+++ packages/playa-pdf/playa_pdf-0.3.1.tar.gz/playa_pdf-0.3.1/tests/test_outline.py
@@ -0,0 +1,80 @@
+"""
+Test document outline.
+"""
+
+from typing import List
+
+import playa
+import pytest
+from playa.exceptions import PDFEncryptionError
+from playa.outline import Outline
+
+from .data import ALLPDFS, PASSWORDS, TESTDIR, XFAILS
+
+
+def expand_titles(outline: Outline) -> List:
+    def expand_one(child):
+        out = [child.title]
+        for c in child:
+            out.append(expand_one(c))
+        return out
+
+    out = []
+    for child in outline:
+        out.extend(expand_one(child))
+    return out
+
+
+def test_outline():
+    """Test basic outline functionality."""
+    with playa.open(TESTDIR / "pdf_structure.pdf") as pdf:
+        titles = expand_titles(pdf.outline)
+        assert titles == ["Titre 1", ["Titre 2", ["Tableau"]]]
+
+
+def expand(outline: Outline) -> List:
+    def expand_one(child):
+        out = [child.title, child.destination, child.element]
+        for c in child:
+            out.append(expand_one(c))
+        return out
+
+    out = []
+    for child in outline:
+        out.extend(expand_one(child))
+    return out
+
+
+@pytest.mark.parametrize("path", ALLPDFS, ids=str)
+def test_outlines(path) -> None:
+    """Verify that we can read outlines when they exist."""
+    if path.name in XFAILS:
+        pytest.xfail("Intentionally corrupt file: %s" % path.name)
+    passwords = PASSWORDS.get(path.name, [""])
+    for password in passwords:
+        try:
+            with playa.open(path, password=password) as pdf:
+                outline = pdf.outline
+                if outline is not None:
+                    print(expand(outline))
+        except PDFEncryptionError:
+            pytest.skip("password incorrect or cryptography package not installed")
+
+
+@pytest.mark.parametrize("path", ALLPDFS, ids=str)
+def test_destinations(path) -> None:
+    """Verify that we can read destinations when they exist."""
+    if path.name in XFAILS:
+        pytest.xfail("Intentionally corrupt file: %s" % path.name)
+    passwords = PASSWORDS.get(path.name, [""])
+    for password in passwords:
+        try:
+            with playa.open(path, password=password) as pdf:
+                for k in pdf.destinations:
+                    print(k, pdf.destinations[k])
+        except PDFEncryptionError:
+            pytest.skip("password incorrect or cryptography package not installed")
+
+
+if __name__ == "__main__":
+    test_outline()
