9426e93ee434b62f632f88cda6e491ce69306fee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-01 04:05:36 +0000

    Add package eventbus-sdk/1.0.2/eventbus_sdk-1.0.2.tar.gz

---
+++ packages/eventbus-sdk/eventbus_sdk-1.0.2.tar.gz/eventbus_sdk-1.0.2/eventbus/worker.py
@@ -0,0 +1,27 @@
+import os
+from celery import Celery
+from kombu import Queue, Exchange
+from dotenv import load_dotenv
+
+from eventbus.routes import ROUTES
+
+load_dotenv()
+
+SERVICE_NAME = os.environ["SERVICE_NAME"]
+
+RABBIT_HOST = os.environ["AWS_MQ_RABBITMQ_URL"]
+RABBIT_PORT = os.getenv("AWS_MQ_RABBITMQ_PORT", "5671")
+RABBIT_USER = os.getenv("AWS_MQ_RABBITMQ_USER", "guest")
+RABBIT_PASS = os.getenv("AWS_MQ_RABBITMQ_PASSWORD", "guest")
+
+BROKER_URL = f'amqps://{RABBIT_USER}:{RABBIT_PASS}@{RABBIT_HOST}:{RABBIT_PORT}'
+
+app = Celery('eventbus')
+app.conf.update({
+    'broker_url': BROKER_URL,
+    'task_routes': ROUTES,
+    'task_queues': (
+        Queue('broadcast_events', Exchange('broadcast_events', type='fanout', durable=True),
+              routing_key='broadcast_events'),
+    ),
+})
