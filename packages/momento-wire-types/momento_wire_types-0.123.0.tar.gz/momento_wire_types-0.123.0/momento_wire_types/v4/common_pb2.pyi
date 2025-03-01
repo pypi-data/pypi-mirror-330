a529ca9eb211ff540302002f7c5cf5fd239051e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-01 14:05:56 +0000

    Add package momento-wire-types/0.123.0/momento_wire_types-0.123.0.tar.gz

---
+++ packages/momento-wire-types/momento_wire_types-0.123.0.tar.gz/momento_wire_types-0.123.0/momento_wire_types/v4/common_pb2.pyi
@@ -0,0 +1,45 @@
+from google.protobuf import descriptor as _descriptor
+from google.protobuf import message as _message
+from typing import ClassVar as _ClassVar, Optional as _Optional
+
+DESCRIPTOR: _descriptor.FileDescriptor
+
+class Absent(_message.Message):
+    __slots__ = []
+    def __init__(self) -> None: ...
+
+class AbsentOrEqual(_message.Message):
+    __slots__ = ["value_to_check"]
+    VALUE_TO_CHECK_FIELD_NUMBER: _ClassVar[int]
+    value_to_check: bytes
+    def __init__(self, value_to_check: _Optional[bytes] = ...) -> None: ...
+
+class Equal(_message.Message):
+    __slots__ = ["value_to_check"]
+    VALUE_TO_CHECK_FIELD_NUMBER: _ClassVar[int]
+    value_to_check: bytes
+    def __init__(self, value_to_check: _Optional[bytes] = ...) -> None: ...
+
+class NotEqual(_message.Message):
+    __slots__ = ["value_to_check"]
+    VALUE_TO_CHECK_FIELD_NUMBER: _ClassVar[int]
+    value_to_check: bytes
+    def __init__(self, value_to_check: _Optional[bytes] = ...) -> None: ...
+
+class Present(_message.Message):
+    __slots__ = []
+    def __init__(self) -> None: ...
+
+class PresentAndNotEqual(_message.Message):
+    __slots__ = ["value_to_check"]
+    VALUE_TO_CHECK_FIELD_NUMBER: _ClassVar[int]
+    value_to_check: bytes
+    def __init__(self, value_to_check: _Optional[bytes] = ...) -> None: ...
+
+class _Empty(_message.Message):
+    __slots__ = []
+    def __init__(self) -> None: ...
+
+class _Unbounded(_message.Message):
+    __slots__ = []
+    def __init__(self) -> None: ...
