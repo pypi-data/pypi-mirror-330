0767842ba2bc8b91cf999160b4cf21a75e91b5ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-01 05:05:39 +0000

    Add package glvkn-preproc/0.1.0/glvkn_preproc-0.1.0.tar.gz

---
+++ packages/glvkn-preproc/glvkn_preproc-0.1.0.tar.gz/glvkn_preproc-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="glvkn_preproc",
+    version="0.1.0",
+    author="asd dasd",
+    description="zxczxczxc",
+    packages=find_packages(),
+    python_requires=">=3.6",
+    install_requires=["nltk", "spacy", "pymorphy3", "langedetect", "tokenizer", "emoji"], 
+)
