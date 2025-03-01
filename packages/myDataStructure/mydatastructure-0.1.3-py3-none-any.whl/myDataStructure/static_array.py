0efd4aecf943d608235e0b093b087316f37d3fad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-01 15:05:16 +0000

    Add package myDataStructure/0.1.3/mydatastructure-0.1.3-py3-none-any.whl

---
+++ packages/myDataStructure/mydatastructure-0.1.3-py3-none-any.whl/myDataStructure/static_array.py
@@ -0,0 +1,10 @@
+class StaticArray:
+    def __init__(self, n):
+        self.data = []*n
+    
+    def get_at(self,i):
+        if not(0 <= i < len(self.data)) : raise IndexError
+        return self.data[i]
+    def set_at(self, i, x):
+        if not(0 <= i < len(self.data)) : raise IndexError
+        self.data = x
