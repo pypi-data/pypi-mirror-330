e624931a1695c9f63d774be07b28b5f2baac2d64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-01 06:05:42 +0000

    Add package arcstack-django-api/0.1.1/arcstack_django_api-0.1.1-py3-none-any.whl

---
+++ packages/arcstack-django-api/arcstack_django_api-0.1.1-py3-none-any.whl/arcstack_api/__init__.py
@@ -0,0 +1,11 @@
+from .api import arcstack_api
+from .decorators import api_endpoint
+from .endpoint import Endpoint
+from .errors import APIError
+
+
+# isort: off
+
+__version__ = '0.1.1'
+
+__all__ = ['arcstack_api', 'Endpoint', 'APIError', 'api_endpoint']
