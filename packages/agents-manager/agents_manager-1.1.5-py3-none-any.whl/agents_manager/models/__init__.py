1c3994f0cb82b9e0d72b7034df514c0da9d8ca88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-01 04:06:37 +0000

    Add package agents-manager/1.1.5/agents_manager-1.1.5-py3-none-any.whl

---
+++ packages/agents-manager/agents_manager-1.1.5-py3-none-any.whl/agents_manager/models/__init__.py
@@ -0,0 +1,8 @@
+
+from agents_manager.models.OpenAi import OpenAi
+from agents_manager.models.Grok import Grok
+from agents_manager.models.DeepSeek import DeepSeek
+from agents_manager.models.Anthropic import Anthropic
+from agents_manager.models.Llama import Llama
+
+__all__ = ["OpenAi", "Grok", "DeepSeek", "Anthropic", "Llama"]
