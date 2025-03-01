93a802fad6a599282480e83fbb0d9c8e3440a8a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-01 06:05:41 +0000

    Add package naptha-sdk/1.0.5/naptha_sdk-1.0.5-py3-none-any.whl

---
+++ packages/naptha-sdk/naptha_sdk-1.0.5-py3-none-any.whl/naptha_sdk/modules/agent.py
@@ -0,0 +1,18 @@
+from naptha_sdk.client.node import NodeClient, UserClient
+from naptha_sdk.schemas import AgentDeployment, AgentRunInput
+from naptha_sdk.utils import get_logger
+
+logger = get_logger(__name__)
+
+class Agent:
+    async def create(self, deployment: AgentDeployment, *args, **kwargs):
+        logger.info(f"Creating agent on worker node {deployment.node}")
+        node = UserClient(deployment.node)
+        agent_deployment = await node.create(module_type="agent", module_request=deployment)
+        return agent_deployment
+
+    async def run(self, module_run_input: AgentRunInput, *args, **kwargs):
+        logger.info(f"Running agent on worker node {module_run_input.deployment.node}")
+        node = NodeClient(module_run_input.deployment.node)
+        agent_run = await node.run_module(module_type="agent", run_input=module_run_input)
+        return agent_run
