91f5298c1c12af9fab6f420b3e44a8a044e1f2c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-01 16:05:11 +0000

    Add package mm-aptos/0.2.2/mm_aptos-0.2.2-py3-none-any.whl

---
+++ packages/mm-aptos/mm_aptos-0.2.2-py3-none-any.whl/mm_aptos/ans.py
@@ -0,0 +1,34 @@
+from mm_crypto_utils import Proxies, random_proxy
+from mm_std import Err, Ok, Result, hr
+
+
+# noinspection DuplicatedCode
+def address_to_primary_name(address: str, timeout: int = 5, proxies: Proxies = None, attempts: int = 3) -> Result[str | None]:
+    result: Result[str | None] = Err("not_started")
+    url = f"https://www.aptosnames.com/api/mainnet/v1/primary-name/{address}"
+    for _ in range(attempts):
+        res = hr(url, proxy=random_proxy(proxies), timeout=timeout)
+        data = res.to_dict()
+        try:
+            if res.code == 200 and res.json == {}:
+                return Ok(None, data=data)
+            return Ok(res.json["name"], data=data)
+        except Exception as e:
+            result = Err(e, data=data)
+    return result
+
+
+# noinspection DuplicatedCode
+def address_to_name(address: str, timeout: int = 5, proxies: Proxies = None, attempts: int = 3) -> Result[str | None]:
+    result: Result[str | None] = Err("not_started")
+    url = f"https://www.aptosnames.com/api/mainnet/v1/name/{address}"
+    for _ in range(attempts):
+        res = hr(url, proxy=random_proxy(proxies), timeout=timeout)
+        data = res.to_dict()
+        try:
+            if res.code == 200 and res.json == {}:
+                return Ok(None, data=data)
+            return Ok(res.json["name"], data=data)
+        except Exception as e:
+            result = Err(e, data=data)
+    return result
