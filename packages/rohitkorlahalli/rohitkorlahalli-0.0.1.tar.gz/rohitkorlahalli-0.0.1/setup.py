aa440e6fb0a168ddeaa08188d8b8293a0b49bfb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-01 03:15:06 +0000

    Add package rohitkorlahalli/0.0.1/rohitkorlahalli-0.0.1.tar.gz

---
+++ packages/rohitkorlahalli/rohitkorlahalli-0.0.1.tar.gz/rohitkorlahalli-0.0.1/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="rohitkorlahalli",  # Replace with your own username
+    version="0.0.1",
+    author="Rohit Korlahalli",
+    author_email="rohit.korlahalli21@gmail.com",
+    description="A small example numbers package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    install_requires=[],  # Add required dependencies here
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
+
