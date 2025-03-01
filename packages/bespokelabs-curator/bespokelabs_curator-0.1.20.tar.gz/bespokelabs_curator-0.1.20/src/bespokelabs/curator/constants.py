6e024d3bbae369ff40a142e5023eb083ef6938b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-01 06:05:47 +0000

    Add package bespokelabs-curator/0.1.20/bespokelabs_curator-0.1.20.tar.gz

---
+++ packages/bespokelabs-curator/bespokelabs_curator-0.1.20.tar.gz/bespokelabs_curator-0.1.20/src/bespokelabs/curator/constants.py
@@ -0,0 +1,8 @@
+"""Defaults used across curator."""
+
+BATCH_REQUEST_ID_TAG = "custom_id"
+_CURATOR_DEFAULT_CACHE_DIR = "~/.cache/curator"
+_DEFAULT_CACHE_DIR = "~/.cache"
+BASE_CLIENT_URL = "https://api.bespokelabs.ai/v0/viewer"
+PUBLIC_CURATOR_VIEWER_HOME_URL = "https://curator.bespokelabs.ai"
+PUBLIC_CURATOR_VIEWER_DATASET_URL = PUBLIC_CURATOR_VIEWER_HOME_URL + "/datasets"
