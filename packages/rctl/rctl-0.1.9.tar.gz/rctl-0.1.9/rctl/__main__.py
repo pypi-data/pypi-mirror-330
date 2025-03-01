31dbfea83658ddfcebee13dc151bdacb45ee6fcf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-01 15:05:40 +0000

    Add package rctl/0.1.9/rctl-0.1.9.tar.gz

---
+++ packages/rctl/rctl-0.1.9.tar.gz/rctl-0.1.9/rctl/__main__.py
@@ -0,0 +1,11 @@
+import typer
+
+
+def main():
+    from .cli._main import app
+
+    app()
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/rctl/rctl-0.1.9.tar.gz/rctl-0.1.9/rctl/core.py
@@ -0,0 +1,56 @@
+from rctl.base2 import StepDataExtension
+from .scanner import scan_files
+
+
+def scan(from_file: str = None, from_dir: str = None):
+    if from_dir:
+        return scan_files(from_dir, include=["*.yml", "*.yaml"])
+
+    if from_file:
+        return [from_file]
+
+    raise Exception()
+
+
+def apply_resource(from_file: str = None, from_dir: str = None):
+    for f in scan(from_file, from_dir):
+        step = StepDataExtension.from_file(f)
+        step.apply()
+
+
+def create_resource(from_file: str = None, from_dir: str = None):
+    for f in scan(from_file, from_dir):
+        step = StepDataExtension.from_file(f).override(state="created")
+        step.apply()
+
+
+def exists_resource(from_file: str = None, from_dir: str = None):
+    for f in scan(from_file, from_dir):
+        step = StepDataExtension.from_file(f).override(state="exists")
+        step.apply()
+
+
+def absent_resource(from_file: str = None, from_dir: str = None):
+    for f in scan(from_file, from_dir):
+        step = StepDataExtension.from_file(f).override(state="absent")
+        step.apply()
+
+
+def delete_resource(from_file: str = None, from_dir: str = None):
+    for f in scan(from_file, from_dir):
+        step = StepDataExtension.from_file(f).override(state="deleted")
+        step.apply()
+
+
+def recreate_resource(from_file: str = None, from_dir: str = None):
+    for f in scan(from_file, from_dir):
+        step = StepDataExtension.from_file(f).override(state="recreated")
+        step.apply()
+
+
+def scan_resource(from_file: str = None, from_dir: str = "."):
+    for f in scan(from_file, from_dir):
+        print(f)
+
+
+def apply_manifest(): ...
