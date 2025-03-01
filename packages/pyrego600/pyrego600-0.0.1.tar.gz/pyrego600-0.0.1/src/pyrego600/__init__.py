113a22b5305f651776b472f80cd502ba30eac0bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-01 16:05:20 +0000

    Add package pyrego600/0.0.1/pyrego600-0.0.1.tar.gz

---
+++ packages/pyrego600/pyrego600-0.0.1.tar.gz/pyrego600-0.0.1/src/pyrego600/__init__.py
@@ -0,0 +1,7 @@
+from .group import Group
+from .heatpump import HeatPump
+from .identifier import Identifier
+from .register import Register
+from .serial_connection import SerialConnection
+
+__all__ = ["Group", "HeatPump", "Identifier", "Register", "SerialConnection"]
