3a544b18fa2ca2f70fbdf4a6a4bdb3ef01f55521
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-01 15:05:53 +0000

    Add package timekan/0.1.1/timekan-0.1.1-py3-none-any.whl

---
+++ packages/timekan/timekan-0.1.1-py3-none-any.whl/timekan/layers/__init__.py
@@ -0,0 +1,9 @@
+from .chebkan import ChebyKANLayer
+from .fourierkan import NaiveFourierKANLayer
+from .kan import KANLinear
+
+__all__ = [
+    'ChebyKANLayer',
+    'NaiveFourierKANLayer',
+    'KANLinear',
+]
