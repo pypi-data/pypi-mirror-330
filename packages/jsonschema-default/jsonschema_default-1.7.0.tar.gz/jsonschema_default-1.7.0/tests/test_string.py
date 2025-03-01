b97611d3c8c235e3fcc5123b2dd2151dff2faf02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-01 06:05:39 +0000

    Add package jsonschema-default/1.7.0/jsonschema_default-1.7.0.tar.gz

---
+++ packages/jsonschema-default/jsonschema_default-1.7.0.tar.gz/jsonschema_default-1.7.0/tests/test_string.py
@@ -0,0 +1,30 @@
+import json
+import re
+from pathlib import Path
+
+import jsonschema_default as js
+
+
+def test_min_length():
+    obj = js.create_from("./schemas/string/minLength.json")
+    val = obj["string"]
+    min_length = 2
+    assert isinstance(val, str) and len(val) >= min_length
+
+
+def test_max_length():
+    obj = js.create_from("./schemas/string/maxLength.json")
+    val = obj["string"]
+    max_length = 2
+    assert isinstance(val, str) and len(val) <= max_length
+
+
+def test_regex():
+    schema_path = "./schemas/string/regex.json"
+    schema = json.loads(Path(schema_path).read_text())
+    obj = js.create_from(schema_path)
+    result_string = obj["string"]
+    pattern = schema["properties"]["string"]["pattern"]
+    assert re.fullmatch(pattern=pattern, string=result_string), (
+        f"Pattern '{pattern}' does not match string: {result_string}"
+    )
