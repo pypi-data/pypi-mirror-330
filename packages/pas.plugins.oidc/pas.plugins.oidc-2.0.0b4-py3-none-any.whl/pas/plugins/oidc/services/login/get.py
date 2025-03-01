1f79951249d8bd9085a45c9b6c828ff5e39a5f21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-01 04:05:47 +0000

    Add package pas.plugins.oidc/2.0.0b4/pas.plugins.oidc-2.0.0b4-py3-none-any.whl

---
+++ packages/pas.plugins.oidc/pas.plugins.oidc-2.0.0b4-py3-none-any.whl/pas/plugins/oidc/services/login/get.py
@@ -0,0 +1,29 @@
+from pas.plugins.oidc.plugins import OIDCPlugin
+from plone import api
+from plone.base.interfaces import IPloneSiteRoot
+from plone.restapi.interfaces import ILoginProviders
+from zope.component import adapter
+from zope.interface import implementer
+
+
+@adapter(IPloneSiteRoot)
+@implementer(ILoginProviders)
+class OIDCLoginProviders:
+    def __init__(self, context):
+        self.context = context
+
+    def get_providers(self):
+        options = []
+        acl_users = api.portal.get_tool("acl_users")
+        for plugin in acl_users.objectValues():
+            if isinstance(plugin, OIDCPlugin):
+                options.append(
+                    {
+                        "id": plugin.getId(),
+                        "plugin": "oidc",
+                        "title": plugin.title,
+                        "url": f"{self.context.absolute_url()}/@login-oidc/{plugin.getId()}",
+                    }
+                )
+
+        return options
