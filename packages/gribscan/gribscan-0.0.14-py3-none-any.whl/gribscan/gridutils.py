cac2a6616c9b69e47dc112a4527621a9820ef2a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-01 04:06:09 +0000

    Add package gribscan/0.0.14/gribscan-0.0.14-py3-none-any.whl

---
+++ packages/gribscan/gribscan-0.0.14-py3-none-any.whl/gribscan/gridutils.py
@@ -0,0 +1,112 @@
+import numpy as np
+from scipy.special import roots_legendre
+
+
+class GribGrid:
+    _subclasses = []
+
+    def __init_subclass__(cls, **kwargs):
+        super().__init_subclass__(**kwargs)
+        cls._subclasses.append(cls)
+
+
+class GaussianReduced(GribGrid):
+    gridType = "reduced_gg"
+    params = ["pl"]
+
+    @classmethod
+    def compute_coords(cls, pl):
+        lons = np.concatenate([np.linspace(0, 360, nl, endpoint=False) for nl in pl])
+        single_lats = np.rad2deg(-np.arcsin(roots_legendre(len(pl))[0]))
+        lats = np.concatenate([[lat] * nl for nl, lat in zip(pl, single_lats)])
+        return {"lon": lons, "lat": lats}
+
+
+class LatLonReduced(GribGrid):
+    gridType = "reduced_ll"
+    params = ["pl"]
+
+    @classmethod
+    def compute_coords(cls, pl):
+        lons = np.concatenate([np.linspace(0, 360, nl, endpoint=False) for nl in pl])
+        single_lats = np.linspace(90, -90, len(pl), endpoint=True)
+        lats = np.concatenate([[lat] * nl for nl, lat in zip(pl, single_lats)])
+        return {"lon": lons, "lat": lats}
+
+
+class LatLonRegular(GribGrid):
+    gridType = "regular_ll"
+    params = [
+        "Ni",
+        "Nj",
+        "latitudeOfFirstGridPointInDegrees",
+        "longitudeOfFirstGridPointInDegrees",
+        "iDirectionIncrementInDegrees",
+        "jDirectionIncrementInDegrees",
+        "iScansNegatively",
+        "jScansPositively",
+    ]
+
+    @classmethod
+    def compute_coords(cls, **kwargs):
+        Ni = kwargs["Ni"]
+        Nj = kwargs["Nj"]
+        iInc = kwargs["iDirectionIncrementInDegrees"]
+        jInc = kwargs["jDirectionIncrementInDegrees"]
+        lonFirst = kwargs["longitudeOfFirstGridPointInDegrees"]
+        latFirst = kwargs["latitudeOfFirstGridPointInDegrees"]
+
+        iInc = -iInc if kwargs["iScansNegatively"] else iInc
+        jInc = jInc if kwargs["jScansPositively"] else -jInc
+
+        lons, lats = np.meshgrid(
+            (lonFirst + np.arange(Ni) * iInc + 180) % 360 - 180,
+            latFirst + np.arange(Nj) * jInc,
+        )
+
+        return {"lon": lons.ravel(), "lat": lats.ravel()}
+
+
+class HEALPix(GribGrid):
+    gridType = "healpix"
+    params = [
+        "orderingConvention",
+        "Nside",
+    ]
+
+    @classmethod
+    def compute_coords(cls, orderingConvention, Nside):
+        import healpy as hp
+
+        lons, lats = hp.pix2ang(
+            nside=Nside,
+            ipix=np.arange(hp.nside2npix(Nside)),
+            nest=orderingConvention == "nested",
+            lonlat=True,
+        )
+
+        return {"lon": lons, "lat": lats}
+
+
+class SphericalHarmonics(GribGrid):
+    gridType = "sh"
+    params = []
+
+    @classmethod
+    def compute_coords(cls):
+        return {}
+
+
+grids = {g.gridType: g for g in GribGrid._subclasses}
+
+
+def params_for_gridType(gridType):
+    if gridType in grids:
+        return grids[gridType].params
+    else:
+        return []
+
+
+def varinfo2coords(varinfo):
+    grid = grids[varinfo["attrs"]["gridType"]]
+    return grid.compute_coords(**{k: varinfo["extra"][k] for k in grid.params})
---
+++ packages/gribscan/gribscan-0.0.14-py3-none-any.whl/gribscan/tools.py
@@ -0,0 +1,142 @@
+import argparse
+import glob
+import json
+import logging
+import textwrap
+from functools import partial
+from pathlib import Path
+import multiprocessing as mp
+
+import gribscan
+from .magician import MAGICIANS
+
+
+def create_index():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("sources", metavar="GRIB", help="source gribfile(s)", nargs="+")
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        help="increase the logging level",
+        action="store_true",
+    )
+    parser.add_argument(
+        "-o",
+        "--outdir",
+        help="output directory to write index files",
+        type=str,
+        default=None,
+        nargs="?",
+    )
+    parser.add_argument(
+        "-f",
+        "--force",
+        help="overwrite existing index files",
+        action="store_true",
+    )
+    parser.add_argument(
+        "-n",
+        "--nprocs",
+        help="number of parallel processes",
+        type=int,
+        default=1,
+        nargs="?",
+    )
+    args = parser.parse_args()
+
+    logging.basicConfig()
+    if args.verbose:
+        logging.getLogger().setLevel(logging.DEBUG)
+
+    mapfunc = partial(gribscan.write_index, outdir=args.outdir, force=args.force)
+    with mp.Pool(args.nprocs) as pool:
+        pool.map(mapfunc, args.sources)
+
+
+def build_dataset():
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.RawTextHelpFormatter,
+    )
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        help="increase the logging level",
+        action="store_true",
+    )
+    parser.add_argument(
+        "indices",
+        metavar="GRIB.index",
+        help="source index files (JSONLines)",
+        nargs="*",
+    )
+    parser.add_argument(
+        "-g",
+        "--glob",
+        metavar="GLOB.index",
+        help="glob pattern to create list of index files (JSONLines)",
+        type=str,
+        nargs="?",
+        default=None,
+    )
+    parser.add_argument(
+        "-o",
+        "--output",
+        metavar="outdir",
+        default=".",
+        help="output directory to write reference filesystems (JSON)",
+        type=str,
+    )
+    parser.add_argument(
+        "--prefix",
+        metavar="template_prefix",
+        default=None,
+        help=textwrap.dedent("""\
+            Absolute path to the location of the dataset.
+
+            The prefix is prepended to the filename stored in the index files.
+            For full file paths, a sub-tree can be denoted using the '/./'
+            character. The following examples show how a prefix adds to
+            different filenames:
+
+            /prefix/ + filename.grb = /prefix/filename.grb
+            /prefix/ + path/filename.grb = /prefix/path/filename.grb
+            /prefix/ + path/./sub/tree/filename.grb = /prefix/sub/tree/filename.grb
+        """
+        ),
+        type=str,
+    )
+    parser.add_argument(
+        "-m",
+        "--magician",
+        metavar="magician",
+        default="monsoon",
+        help="use this magician for dataset assembly",
+        type=str,
+    )
+    args = parser.parse_args()
+
+    logging.basicConfig()
+    if args.verbose:
+        logging.getLogger().setLevel(logging.DEBUG)
+
+    magician = MAGICIANS[args.magician]()
+
+    if args.glob is None and not args.indices:
+        parser.error("You need to pass a glob pattern or a file list.")
+
+    if args.glob is not None and args.indices:
+        parser.error("It is not possible to pass a glob pattern and a file list.")
+
+    if args.glob is not None:
+        indices = glob.iglob(args.glob)
+
+    if args.indices:
+        indices = args.indices
+
+    refs = gribscan.grib_magic(
+        indices, magician=magician, global_prefix=args.prefix
+    )
+
+    for dataset, ref in refs.items():
+        with open(f"{args.output}/{dataset}.json", "w") as indexfile:
+            json.dump(ref, indexfile)
