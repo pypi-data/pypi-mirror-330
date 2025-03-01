80c8127591239e3820fcb1045cc92c298a4e6379
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-01 05:06:44 +0000

    Add package honeybees/1.1.1/honeybees-1.1.1-py3-none-any.whl

---
+++ packages/honeybees/honeybees-1.1.1-py3-none-any.whl/honeybees/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""This package is a framework for creating large-scale agent-based geographical models.
+
+Submodules
+==========
+
+
+"""
+
+__title__ = "honeybees"
+__version__ = "1.1.1"
+__email__ = "j.a.debruijn at outlook com"
+__status__ = "development"
