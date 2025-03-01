0fba2d9d1b19ed5cfbf257ee8eb0c6edc149e624
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-01 16:05:39 +0000

    Add package hhelper/0.1.2/hhelper-0.1.2.tar.gz

---
+++ packages/hhelper/hhelper-0.1.2.tar.gz/hhelper-0.1.2/src/hhelper/helpers/check_valid_journal.py
@@ -0,0 +1,18 @@
+import subprocess
+from functools import cache
+
+
+@cache
+def check_valid_journal(text):
+    result = subprocess.run(
+        ["hledger", "check", "-f", "-"],
+        input=text,
+        text=True,
+        capture_output=True,
+        check=False,
+    )
+    err = result.stderr
+
+    if err != "":
+        print(err)
+        exit()
