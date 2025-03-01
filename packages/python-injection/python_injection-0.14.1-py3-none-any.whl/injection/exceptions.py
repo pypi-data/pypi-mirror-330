8b613bf7217bac940f0fcc44b4c2c8cb32cbe779
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-01 06:05:42 +0000

    Add package python-injection/0.14.1/python_injection-0.14.1-py3-none-any.whl

---
+++ packages/python-injection/python_injection-0.14.1-py3-none-any.whl/injection/exceptions.py
@@ -0,0 +1,51 @@
+from typing import Any
+
+__all__ = (
+    "HookError",
+    "InjectionError",
+    "ModuleError",
+    "ModuleLockError",
+    "ModuleNotUsedError",
+    "NoInjectable",
+    "ScopeAlreadyDefinedError",
+    "ScopeError",
+    "ScopeUndefinedError",
+)
+
+
+class InjectionError(Exception): ...
+
+
+class NoInjectable[T](KeyError, InjectionError):
+    __slots__ = ("__class",)
+
+    __class: type[T]
+
+    def __init__(self, cls: type[T] | Any) -> None:
+        super().__init__(f"No injectable for `{cls}`.")
+        self.__class = cls
+
+    @property
+    def cls(self) -> type[T]:
+        return self.__class
+
+
+class ModuleError(InjectionError): ...
+
+
+class ModuleLockError(ModuleError): ...
+
+
+class ModuleNotUsedError(KeyError, ModuleError): ...
+
+
+class ScopeError(InjectionError): ...
+
+
+class ScopeUndefinedError(LookupError, ScopeError): ...
+
+
+class ScopeAlreadyDefinedError(ScopeError): ...
+
+
+class HookError(InjectionError): ...
