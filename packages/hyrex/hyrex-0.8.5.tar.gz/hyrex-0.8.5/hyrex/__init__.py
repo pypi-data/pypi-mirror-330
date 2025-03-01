f03f4de8addb8b96d337cae61d3bbb260413bd90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-01 14:05:55 +0000

    Add package hyrex/0.8.5/hyrex-0.8.5.tar.gz

---
+++ packages/hyrex/hyrex-0.8.5.tar.gz/hyrex-0.8.5/hyrex/__init__.py
@@ -0,0 +1,12 @@
+# from .app import DispatcherType, Hyrex
+# from .task_registry import TaskRegistry
+import logging
+
+from .hyrex_app import HyrexApp
+from .hyrex_cache import HyrexCache
+from .hyrex_context import HyrexContext, get_hyrex_context
+from .hyrex_queue import HyrexQueue
+from .hyrex_registry import HyrexRegistry
+
+# Set up null handler at library root level
+logging.getLogger("hyrex").addHandler(logging.NullHandler())
---
+++ packages/hyrex/hyrex-0.8.5.tar.gz/hyrex-0.8.5/hyrex/dispatcher/dispatcher_provider.py
@@ -0,0 +1,31 @@
+import os
+from enum import StrEnum
+
+from hyrex.env_vars import EnvVars
+
+from .dispatcher import Dispatcher
+from .platform_dispatcher import PlatformDispatcher
+from .postgres_dispatcher import PostgresDispatcher
+from .postgres_lite_dispatcher import PostgresLiteDispatcher
+
+
+class DispatcherType(StrEnum):
+    POSTGRES = "postgres"
+    POSTGRES_LITE = "postgres_lite"
+    PLATFORM = "platform"
+
+
+def get_dispatcher(worker: bool = False) -> Dispatcher:
+    api_key = os.environ.get(EnvVars.API_KEY)
+    conn_string = os.environ.get(EnvVars.DATABASE_URL)
+    if api_key:
+        return PlatformDispatcher(api_key=api_key)
+    elif conn_string:
+        if worker:
+            # Single-threaded dispatcher simplifies worker
+            return PostgresLiteDispatcher(conn_string=conn_string)
+        return PostgresDispatcher(conn_string=conn_string)
+    else:
+        raise ValueError(
+            f"Hyrex requires either {EnvVars.DATABASE_URL} or {EnvVars.API_KEY} to be set."
+        )
