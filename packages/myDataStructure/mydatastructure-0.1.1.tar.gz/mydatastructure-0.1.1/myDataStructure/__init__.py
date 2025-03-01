6a1482c2b4582963c5489f1b2b0743936c5a8f18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-01 15:05:36 +0000

    Add package myDataStructure/0.1.1/mydatastructure-0.1.1.tar.gz

---
+++ packages/myDataStructure/mydatastructure-0.1.1.tar.gz/mydatastructure-0.1.1/myDataStructure/__init__.py
@@ -0,0 +1,25 @@
+from .my_structure import MyDataStructure
+from .StaticArray import StaticArray
+
+__all__ = ["MyDataStructure", "StaticArray"]
+
+# from .queue import Queue
+# from .linked_list import LinkedList
+# from .binary_tree import BinaryTree
+# from .heap import Heap
+# from .hash_table import HashTable
+# from .graph import Graph
+# from .sorting import quicksort, mergesort
+# from .search import binary_search, linear_search
+
+# __all__ = [
+#     "Stack", "Queue", "LinkedList", "BinaryTree", "Heap",
+#     "HashTable", "Graph", "quicksort", "mergesort",
+#     "binary_search", "linear_search"
+# ]
+
+
+# Remove-Item -Recurse -Force dist build *.egg-info
+# py -m build
+# py -m twine upload dist/*
+# version="0.2.0"
---
+++ packages/myDataStructure/mydatastructure-0.1.1.tar.gz/mydatastructure-0.1.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="myDataStructure",
+    version="0.1.1",
+    packages=find_packages(),
+    install_requires=[],  # Add dependencies if needed
+    author="Imanol",
+    author_email="imanolsupo669@example.com",
+    description="A custom data structure package",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourusername/myDataStructure",  # Change if needed
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
+
