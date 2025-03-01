f9485fa259f43092eec826c4fad3620c85b41974
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-01 03:15:40 +0000

    Add package smsmobileapi-send-email/0.3.2/smsmobileapi_send_email-0.3.2.tar.gz

---
+++ packages/smsmobileapi-send-email/smsmobileapi_send_email-0.3.2.tar.gz/smsmobileapi_send_email-0.3.2/setup.py
@@ -0,0 +1,22 @@
+﻿from setuptools import setup, find_packages
+
+setup(
+    name="smsmobileapi-send-email",
+    version="0.3.2",
+    packages=find_packages(),
+    install_requires=[
+        "requests",
+    ],
+    author="Quest-Concept",
+    author_email="info@smsmobileapi.com",
+    description="A Python module for sending emails using SMSMobileAPI directly from mailbox account",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/SmsMobileApi/smsmobileapi-python-email",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
