17a45585d5989dcdb6f8515f06238d89cac45b4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-01 15:06:10 +0000

    Add package basedpyright/1.28.1/basedpyright-1.28.1.tar.gz

---
+++ packages/basedpyright/basedpyright-1.28.1.tar.gz/basedpyright-1.28.1/packages/pyright-internal/src/common/serviceProviderExtensions.ts
@@ -0,0 +1,129 @@
+/*
+ * serviceProviderExtensions.ts
+ * Copyright (c) Microsoft Corporation.
+ * Licensed under the MIT license.
+ *
+ * Shortcuts to common services.
+ */
+import { CacheManager } from '../analyzer/cacheManager';
+import { ISourceFileFactory } from '../analyzer/programTypes';
+import { IPythonMode, SourceFile, SourceFileEditMode } from '../analyzer/sourceFile';
+import { ServiceKeys } from './serviceKeys';
+import { CaseSensitivityDetector } from './caseSensitivityDetector';
+import { ConsoleInterface } from './console';
+import { FileSystem, TempFile } from './fileSystem';
+import { LogTracker } from './logTracker';
+import { ServiceProvider } from './serviceProvider';
+import { Uri } from './uri/uri';
+import { DocStringService, PyrightDocStringService } from './docStringService';
+import { CommandService, WindowService } from './languageServerInterface';
+import { SupportPartialStubs } from '../partialStubService';
+import { BaselineHandler } from '../baseline';
+
+declare module './serviceProvider' {
+    interface ServiceProvider {
+        fs(): FileSystem;
+        console(): ConsoleInterface;
+        tmp(): TempFile | undefined;
+        sourceFileFactory(): ISourceFileFactory;
+        partialStubs(): SupportPartialStubs;
+        cacheManager(): CacheManager | undefined;
+        docStringService(): DocStringService;
+    }
+}
+
+export function createServiceProvider(...services: any): ServiceProvider {
+    const sp = new ServiceProvider();
+
+    // For known interfaces, register the service.
+    services.forEach((service: any) => {
+        if (FileSystem.is(service)) {
+            sp.add(ServiceKeys.fs, service);
+        }
+        if (ConsoleInterface.is(service)) {
+            sp.add(ServiceKeys.console, service);
+        }
+        if (ISourceFileFactory.is(service)) {
+            sp.add(ServiceKeys.sourceFileFactory, service);
+        }
+        if (SupportPartialStubs.is(service)) {
+            sp.add(ServiceKeys.partialStubs, service);
+        }
+        if (TempFile.is(service)) {
+            sp.add(ServiceKeys.tempFile, service);
+        }
+        if (CaseSensitivityDetector.is(service)) {
+            sp.add(ServiceKeys.caseSensitivityDetector, service);
+        }
+        if (CacheManager.is(service)) {
+            sp.add(ServiceKeys.cacheManager, service);
+        }
+        if (DocStringService.is(service)) {
+            sp.add(ServiceKeys.docStringService, service);
+        }
+        if (WindowService.is(service)) {
+            sp.add(ServiceKeys.windowService, service);
+        }
+        if (CommandService.is(service)) {
+            sp.add(ServiceKeys.commandService, service);
+        }
+    });
+    return sp;
+}
+
+ServiceProvider.prototype.fs = function () {
+    return this.get(ServiceKeys.fs);
+};
+ServiceProvider.prototype.console = function () {
+    return this.get(ServiceKeys.console);
+};
+ServiceProvider.prototype.partialStubs = function () {
+    return this.get(ServiceKeys.partialStubs);
+};
+ServiceProvider.prototype.tmp = function () {
+    return this.tryGet(ServiceKeys.tempFile);
+};
+ServiceProvider.prototype.sourceFileFactory = function () {
+    const result = this.tryGet(ServiceKeys.sourceFileFactory);
+    return result || DefaultSourceFileFactory;
+};
+
+ServiceProvider.prototype.docStringService = function () {
+    const result = this.tryGet(ServiceKeys.docStringService);
+    return result || new PyrightDocStringService();
+};
+
+ServiceProvider.prototype.cacheManager = function () {
+    const result = this.tryGet(ServiceKeys.cacheManager);
+    return result;
+};
+
+const DefaultSourceFileFactory: ISourceFileFactory = {
+    createSourceFile(
+        serviceProvider: ServiceProvider,
+        fileUri: Uri,
+        moduleName: string,
+        isThirdPartyImport: boolean,
+        isThirdPartyPyTypedPresent: boolean,
+        editMode: SourceFileEditMode,
+        baselineHandler: BaselineHandler,
+        getCellIndex: () => number | undefined,
+        console?: ConsoleInterface,
+        logTracker?: LogTracker,
+        ipythonMode?: IPythonMode
+    ) {
+        return new SourceFile(
+            serviceProvider,
+            fileUri,
+            moduleName,
+            isThirdPartyImport,
+            isThirdPartyPyTypedPresent,
+            editMode,
+            baselineHandler,
+            getCellIndex,
+            console,
+            logTracker,
+            ipythonMode
+        );
+    },
+};
