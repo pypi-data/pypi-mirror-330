bbfdb79b94906c3a2fbdff2a45f7d05faf9fd4c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-01 14:06:30 +0000

    Add package zrb/1.1.0/zrb-1.1.0.tar.gz

---
+++ packages/zrb/zrb-1.1.0.tar.gz/zrb-1.1.0/src/zrb/input/float_input.py
@@ -0,0 +1,34 @@
+from zrb.attr.type import StrAttr
+from zrb.context.any_shared_context import AnySharedContext
+from zrb.input.base_input import BaseInput
+
+
+class FloatInput(BaseInput):
+    def __init__(
+        self,
+        name: str,
+        description: str | None = None,
+        prompt: str | None = None,
+        default_str: StrAttr = "0.0",
+        auto_render: bool = True,
+        allow_empty: bool = False,
+        allow_positional_parsing: bool = True,
+    ):
+        super().__init__(
+            name=name,
+            description=description,
+            prompt=prompt,
+            default_str=default_str,
+            auto_render=auto_render,
+            allow_empty=allow_empty,
+            allow_positional_parsing=allow_positional_parsing,
+        )
+
+    def to_html(self, ctx: AnySharedContext) -> str:
+        name = self.name
+        description = self.description
+        default = self.get_default_str(ctx)
+        return f'<input type="number" name="{name}" placeholder="{description}" value="{default}" step="any" />'  # noqa
+
+    def _parse_str_value(self, str_value: str) -> float:
+        return float(str_value)
