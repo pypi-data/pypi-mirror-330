75d91c1f494529ef2698072d0a16c81cdafbcaff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-01 03:15:07 +0000

    Add package pody/0.1.13/pody-0.1.13.tar.gz

---
+++ packages/pody/pody-0.1.13.tar.gz/pody-0.1.13/pody/cli/user.py
@@ -0,0 +1,73 @@
+import typer
+import rich
+from typing import Optional
+from pody.eng.user import UserDatabase
+import docker
+from ..eng.utils import parse_storage_size
+from ..eng.docker import ContainerAction, DockerController
+
+app = typer.Typer(
+    help = "Manage users in the system",
+    no_args_is_help=True
+    )
+
+@app.command()
+def add(
+    username: str,
+    password: str,
+    admin: bool = False,
+    ):
+    db = UserDatabase()
+    db.add_user(username, password, admin)
+
+@app.command()
+def update(
+    username: str, 
+    password: Optional[str] = None,
+    admin: Optional[bool] = None,
+    ):
+    db = UserDatabase()
+    db.update_user(username, password=password, is_admin=admin)
+
+@app.command(help="List users, optionally filter by username")
+def list(usernames: Optional[list[str]] = typer.Argument(None)):
+    console = rich.console.Console()
+    db = UserDatabase()
+    users = db.list_users(usernames)
+    for idx, user in enumerate(users):
+        console.print(f"{idx+1}. {user} {db.check_user_quota(user.name)}")
+
+@app.command()
+def update_quota(
+    username: str, 
+    max_pods: Optional[int] = None,
+    gpu_count: Optional[int] = None,
+    memory_limit: Optional[str] = None, 
+    storage_limit: Optional[str] = None
+    ):
+    db = UserDatabase()
+    db.update_user_quota(
+        username, max_pods=max_pods, gpu_count=gpu_count, 
+        memory_limit=parse_storage_size(memory_limit) if not memory_limit is None else None, 
+        storage_limit=parse_storage_size(storage_limit) if not storage_limit is None else None
+        )
+
+@app.command(help="Delete user")
+def delete(username: str):
+    db = UserDatabase()
+    db.delete_user(username)
+
+@app.command(help="Delete user and all related containers")
+def purge(
+    username: str, 
+    yes: bool= typer.Option(False, "--yes", "-y", help="Skip confirmation")
+    ):
+    if not yes:
+        typer.confirm(f"Are you sure to purge user {username}?", abort=True)
+    c = DockerController()
+    db = UserDatabase()
+    db.delete_user(username)
+    containers = c.list_docker_containers(filter_name=username + "-")
+    for container in containers:
+        c.container_action(container, ContainerAction.DELETE)
+        print(f"Container [{container}] removed")
---
+++ packages/pody/pody-0.1.13.tar.gz/pody-0.1.13/pody/config.py
@@ -0,0 +1,85 @@
+import os
+import toml
+import pathlib
+from dataclasses import dataclass
+
+"""
+DATA_HOME structure:
+    DATA_HOME
+        |- users.db
+        |- config.toml
+"""
+DATA_HOME = pathlib.Path(os.environ.get('PODY_HOME', os.path.expanduser('~/.pody')))
+SRC_HOME = pathlib.Path(__file__).parent
+
+def validate_name_part(part: str, reserved_kw: list[str] = []) -> tuple[bool, str]:
+    if not 3 <= len(part) <= 20:
+        return False, "Name part must be between 3 and 20 characters"
+    if part in reserved_kw:
+        return False, f"Name part '{part}' is reserved"
+    if not part.isidentifier():
+        return False, "Name part must be an identifier"
+    if '-' in part or ':' in part:
+        return False, "Name part cannot contain '-' or ':'"
+    if part.startswith('_') or part.endswith('_'):
+        return False, "Name part cannot start or end with '_'"
+    return True, ""
+
+@dataclass
+class Config:
+    @dataclass
+    class ImageConfig:
+        name: str           # e.g. "ubuntu2204-cuda121:latest"
+        ports: list[int]    # e.g. [22, 80, 443]
+        description: str = ""
+
+    name_prefix: str
+    available_ports: list[int | tuple[int, int]]
+    images: list[ImageConfig]
+    volume_mappings: list[str]
+
+def config():
+    def parse_ports(ports_str: str) -> list[int | tuple[int, int]]:
+        ports: list[int | tuple[int, int]] = []
+        ports_str_sp = ports_str.split(',')
+        for port in ports_str_sp:
+            if '-' in port:
+                start, end = port.split('-')
+                start, end = start.strip(), end.strip()
+                assert start.isdigit() and end.isdigit()
+                ports.append((int(start), int(end)))
+            else:
+                port = port.strip()
+                assert port.isdigit(), "Port must be an integer"
+                ports.append(int(port))
+        # validity check
+        for port in ports:
+            if isinstance(port, tuple):
+                assert port[0] < port[1], "Invalid port range"
+                assert port[0] >= 0 and port[1] <= 65535, "Port range must be between 0 and 65535"
+            else:
+                assert port >= 0 and port <= 65535, "Port must be between 0 and 65535"
+        return ports
+    
+    config_path = DATA_HOME / "config.toml"
+    def create_default_config():
+        nonlocal config_path
+        template_config_file = pathlib.Path(__file__).parent / "config.default.toml"
+        with template_config_file.open() as f:
+            with config_path.open('w') as f2:
+                f2.write(f.read())
+    
+    DATA_HOME.mkdir(exist_ok=True)
+    if not config_path.exists():
+        create_default_config()
+    
+    loaded = toml.load(config_path)
+    prefix_valid, reason = validate_name_part(loaded['name_prefix'])
+    if not prefix_valid:
+        raise ValueError(f"Invalid name prefix: {reason}")
+    return Config(
+        name_prefix=loaded['name_prefix'],
+        available_ports=parse_ports(loaded['available_ports']), 
+        images=[Config.ImageConfig(name=i['name'], ports=i['ports']) for i in loaded['images']], 
+        volume_mappings=loaded['volume_mappings'],
+        )
