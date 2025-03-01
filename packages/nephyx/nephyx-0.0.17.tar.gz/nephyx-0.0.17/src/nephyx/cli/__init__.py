28876d98e73d271ba29b751ac4d639917087e8a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-01 14:06:07 +0000

    Add package nephyx/0.0.17/nephyx-0.0.17.tar.gz

---
+++ packages/nephyx/nephyx-0.0.17.tar.gz/nephyx-0.0.17/src/nephyx/cli/__init__.py
@@ -0,0 +1,42 @@
+import json
+from pathlib import Path
+import typer
+from nephyx.cli.helper import import_app_entrypoint
+import sys
+import getpass
+
+from nephyx.cli.postgres import setup_database
+
+app = typer.Typer()
+
+
+@app.command()
+def init_db():
+    db_host = "localhost"
+    db_port = "5432"
+
+    db_admin_user = typer.prompt("Enter database admin user")
+    db_admin_password = getpass.getpass("Enter database admin password")
+
+    # take from config
+    db_name = typer.prompt("Enter database name")
+    db_user = typer.prompt("Enter database user")
+    db_password = getpass.getpass("Enter database password")
+
+    admin_config = {
+        "host": db_host,
+        "port": db_port,
+        "user": db_admin_user,
+        "password": db_admin_password,
+    }
+
+    setup_database(admin_config, db_name, db_user, db_password)
+
+
+@app.command()
+def export_openapi():
+    sys.path.insert(0, "")
+    app = import_app_entrypoint()
+    openapi = app.openapi()
+    with Path("openapi.json").open("w") as f:
+        json.dump(openapi, f, indent=2)
