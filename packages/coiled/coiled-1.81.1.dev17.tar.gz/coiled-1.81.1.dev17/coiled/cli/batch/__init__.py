cdd4cc66177af779204f0cf80d5c3192df98f41e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-01 05:05:46 +0000

    Add package coiled/1.81.1.dev17/coiled-1.81.1.dev17.tar.gz

---
+++ packages/coiled/coiled-1.81.1.dev17.tar.gz/coiled-1.81.1.dev17/coiled/cli/batch/__init__.py
@@ -0,0 +1,20 @@
+import click
+
+from ..utils import CONTEXT_SETTINGS
+from .run import batch_run_cli
+from .status import batch_status_cli
+from .wait import batch_wait_cli
+
+
+@click.group(name="batch", context_settings=CONTEXT_SETTINGS)
+def batch_group():
+    """
+    Commands for managing Coiled Batch Jobs.
+
+    Batch Jobs is currently an experimental feature.
+    """
+
+
+batch_group.add_command(batch_run_cli, "run")
+batch_group.add_command(batch_status_cli, "status")
+batch_group.add_command(batch_wait_cli, "wait")
