e068da25c79dce27439dcfe3188511e39018b365
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-01 05:05:52 +0000

    Add package auto-easy/0.1.4/auto_easy-0.1.4.tar.gz

---
+++ packages/auto-easy/auto_easy-0.1.4.tar.gz/auto_easy-0.1.4/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='auto_easy',  # 打包后的名称
+    version='0.1.4',   # 版本号
+    author='Jefung',
+    author_email='jefung865424525@gmail.com',
+    description='make automated script development easier',
+    long_description=open('README.md', encoding='utf-8').read(),  # 从README.md中读取详细描述
+    long_description_content_type='text/markdown',
+    url='https://github.com/Jefung/auto_easy',  # 项目的URL
+    packages=find_packages(),  # 自动发现所有的���
+    classifiers=[ # 根据您的许可证调整
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.9',  # 最低Python版本
+)
