60fac7ca72603330637c7371b1c2338c4d4bec34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-01 03:15:34 +0000

    Add package schema-alchemist/0.1.1/schema_alchemist-0.1.1.tar.gz

---
+++ packages/schema-alchemist/schema_alchemist-0.1.1.tar.gz/schema_alchemist-0.1.1/src/schema_alchemist/__init__.py
@@ -0,0 +1,18 @@
+from typing import Union
+
+from sqlalchemy import Engine, Connection
+
+from constants import SchemaTypeEnum
+from generators import SchemaGeneratorFactory
+from reflection import reflect, get_inspector
+
+
+def create_schema(
+    engine: Union[Engine, Connection], schema_type: SchemaTypeEnum, schema_name: str
+) -> str:
+    reflected_data = reflect(engine, schema_name)
+    inspector = get_inspector(engine)
+    sorted_tables = inspector.get_sorted_table_and_fkc_names(schema_name)
+    return SchemaGeneratorFactory(
+        reflected_data, sorted_tables, schema_type, schema_name
+    ).generate()
---
+++ packages/schema-alchemist/schema_alchemist-0.1.1.tar.gz/schema_alchemist-0.1.1/tests/test_generators/test_enum_generator.py
@@ -0,0 +1,68 @@
+from enum import Enum
+
+import pytest
+
+from sqlalchemy import Enum as SqlAlchemyEnum
+
+from generators import EnumGenerator
+from utils import DEFAULT_INDENTATION
+
+
+def test_enum_generator_init(import_path_resolver):
+    name = "status"
+    items = ["active", "inactive", "suspended"]
+
+    eg = EnumGenerator(name, items, import_path_resolver)
+
+    assert eg.name == name
+    assert eg.items == items
+    assert eg.import_path_resolver == import_path_resolver
+    assert eg.indentation == DEFAULT_INDENTATION
+
+    indentation = " "
+    eg = EnumGenerator(name, items, import_path_resolver, indentation)
+    assert eg.indentation == indentation
+
+
+@pytest.mark.parametrize(
+    "name, items, expected",
+    (
+        (
+            "status",
+            ["active", "inactive", "suspended"],
+            (
+                "class status(Enum):\n"
+                "    active = 'active'\n"
+                "    inactive = 'inactive'\n"
+                "    suspended = 'suspended'"
+            ),
+        ),
+        (
+            "SwitchEnum",
+            ["open", "closed"],
+            "class SwitchEnum(Enum):\n    open = 'open'\n    closed = 'closed'",
+        ),
+    ),
+)
+def test_enum_generator_generate(name, items, expected, import_path_resolver):
+    import_path_resolver.insert_many(Enum)
+    eg = EnumGenerator(name, items, import_path_resolver)
+    assert eg.generate() == expected
+
+
+def test_enum_generator_generate_with_usage(import_path_resolver):
+    name = "status"
+    items = ["active", "inactive", "suspended"]
+
+    expected = (
+        "class status(enum_Enum):\n"
+        "    active = 'active'\n"
+        "    inactive = 'inactive'\n"
+        "    suspended = 'suspended'"
+    )
+
+    import_path_resolver.insert_many(SqlAlchemyEnum, Enum)
+
+    eg = EnumGenerator(name, items, import_path_resolver)
+
+    assert eg.generate() == expected
