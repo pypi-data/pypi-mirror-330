c086add9f3408221c179c05245c0e4a8a28b5ad8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-01 05:05:27 +0000

    Add package baseapp-backend/0.0.19/baseapp-backend-0.0.19.tar.gz

---
+++ packages/baseapp-backend/baseapp-backend-0.0.19.tar.gz/baseapp-backend-0.0.19/testproject/graphql.py
@@ -0,0 +1,64 @@
+import graphene
+from baseapp_profiles.graphql.mutations import ProfilesMutations
+from baseapp_profiles.graphql.queries import ProfilesQueries
+from graphene import relay
+from graphene.relay.node import NodeField as RelayNodeField
+from graphene_django.debug import DjangoDebug
+
+from baseapp.activity_log.graphql.queries import ActivityLogQueries
+from baseapp_blocks.graphql.mutations import BlocksMutations
+from baseapp_comments.graphql.mutations import CommentsMutations
+from baseapp_comments.graphql.queries import CommentsQueries
+from baseapp_comments.graphql.subscriptions import CommentsSubscriptions
+from baseapp_core.graphql import DeleteNode
+from baseapp_follows.graphql.mutations import FollowsMutations
+from baseapp_notifications.graphql.mutations import NotificationsMutations
+from baseapp_notifications.graphql.subscriptions import NotificationsSubscription
+from baseapp_organizations.graphql.mutations import OrganizationsMutations
+from baseapp_organizations.graphql.queries import OrganizationsQueries
+from baseapp_pages.graphql.mutations import PagesMutations
+from baseapp_pages.graphql.queries import PagesQueries
+from baseapp_ratings.graphql.mutations import RatingsMutations
+from baseapp_ratings.graphql.queries import RatingsQueries
+from baseapp_reactions.graphql.mutations import ReactionsMutations
+from baseapp_reactions.graphql.queries import ReactionsQueries
+from baseapp_reports.graphql.mutations import ReportsMutations
+from testproject.testapp.graphql.queries import UsersQueries
+
+
+class Query(
+    graphene.ObjectType,
+    UsersQueries,
+    ProfilesQueries,
+    CommentsQueries,
+    ActivityLogQueries,
+    ReactionsQueries,
+    RatingsQueries,
+    PagesQueries,
+    OrganizationsQueries,
+):
+    node = RelayNodeField(relay.Node)
+    debug = graphene.Field(DjangoDebug, name="_debug")
+
+
+class Mutation(
+    graphene.ObjectType,
+    ProfilesMutations,
+    CommentsMutations,
+    ReactionsMutations,
+    ReportsMutations,
+    RatingsMutations,
+    FollowsMutations,
+    BlocksMutations,
+    PagesMutations,
+    NotificationsMutations,
+    OrganizationsMutations,
+):
+    delete_node = DeleteNode.Field()
+
+
+class Subscription(graphene.ObjectType, NotificationsSubscription, CommentsSubscriptions):
+    pass
+
+
+schema = graphene.Schema(query=Query, mutation=Mutation, subscription=Subscription)
