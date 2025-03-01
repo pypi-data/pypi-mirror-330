9c2f6c30945bfab0a4f0d072dd1fb87c76ed4573
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-01 05:05:28 +0000

    Add package Z0Z-tools/0.10.3/z0z_tools-0.10.3.tar.gz

---
+++ packages/Z0Z-tools/z0z_tools-0.10.3.tar.gz/z0z_tools-0.10.3/Z0Z_tools/optionalPyTorchAlternative.py
@@ -0,0 +1,11 @@
+"""If `torch` is not installed, this module prevents errors."""
+from collections.abc import Callable
+from numpy import ndarray, dtype, float64
+from typing import ParamSpec, TypeVar
+
+from collections.abc import Callable
+callableTargetParameters = ParamSpec('callableTargetParameters')
+callableReturnsNDArray = TypeVar('callableReturnsNDArray', bound=Callable[..., ndarray[tuple[int], dtype[float64]]])
+
+def def_asTensor(callableTarget: Callable[callableTargetParameters, ndarray[tuple[int], dtype[float64]]]) -> Callable[callableTargetParameters, ndarray[tuple[int], dtype[float64]]]:
+	return callableTarget
