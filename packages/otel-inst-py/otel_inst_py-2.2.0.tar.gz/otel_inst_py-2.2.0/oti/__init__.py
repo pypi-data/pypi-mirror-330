6e4c187100f32b031e9b4a44a4c8fba1f48fa889
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-01 06:05:42 +0000

    Add package otel-inst-py/2.2.0/otel_inst_py-2.2.0.tar.gz

---
+++ packages/otel-inst-py/otel_inst_py-2.2.0.tar.gz/otel_inst_py-2.2.0/oti/__init__.py
@@ -0,0 +1,15 @@
+"""
+The OTI module
+
+It makes simple to to configure and instrument the OTEL SDK for applications that run as standalone processes.
+"""
+
+from .oti import OTI
+from .config import (
+    OTIConfig,
+    ExporterConfig,
+    SamplingConfig,
+    PeriodicMetricReaderConfig,
+)
+
+__all__ = ["oti", "config"]
