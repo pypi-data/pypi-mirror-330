54f7689f69e4babb08b4f9954f0f2476fffa83a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-01 05:05:39 +0000

    Add package iris-sample-data/2.5.2/iris_sample_data-2.5.2-py3-none-any.whl

---
+++ packages/iris-sample-data/iris_sample_data-2.5.2-py3-none-any.whl/iris_sample_data/__init__.py
@@ -0,0 +1,8 @@
+"""Iris sample data."""
+
+import os
+
+from ._version import version as __version__
+
+# Root directory containing the iris sample data.
+path = os.path.join(os.path.abspath(os.path.dirname(__file__)), "sample_data")
