a4a46d759588ff74b6462734f7c828e861153147
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-01 06:05:55 +0000

    Add package polars-bio/0.6.3/polars_bio-0.6.3-cp38-abi3-macosx_10_12_x86_64.whl

---
+++ packages/polars-bio/polars_bio-0.6.3-cp38-abi3-macosx_10_12_x86_64.whl/polars_bio/__init__.py
@@ -0,0 +1,31 @@
+from polars_bio.polars_bio import InputFormat, ReadOptions, VcfReadOptions
+
+from .context import ctx, set_option
+from .io import read_bam, read_fasta, read_fastq, read_table, read_vcf
+from .polars_ext import PolarsRangesOperations as LazyFrame
+from .range_op import FilterOp, count_overlaps, merge, nearest, overlap
+from .range_viz import visualize_intervals
+
+POLARS_BIO_MAX_THREADS = "datafusion.execution.target_partitions"
+
+
+__version__ = "0.6.3"
+__all__ = [
+    "overlap",
+    "nearest",
+    "merge",
+    "count_overlaps",
+    "ctx",
+    "FilterOp",
+    "visualize_intervals",
+    "read_bam",
+    "read_vcf",
+    "read_fasta",
+    "read_fastq",
+    "read_table",
+    "InputFormat",
+    "LazyFrame",
+    "ReadOptions",
+    "VcfReadOptions",
+    "set_option",
+]
