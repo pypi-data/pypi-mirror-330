14af9586d32c126415ae0d042066832c5dbf6c61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-01 06:05:38 +0000

    Add package jsonschema-default/1.7.0/jsonschema_default-1.7.0-py3-none-any.whl

---
+++ packages/jsonschema-default/jsonschema_default-1.7.0-py3-none-any.whl/jsonschema_default/__init__.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+from typing import Union
+
+from . import main
+from .options import DefaultOptions
+
+
+def create_from(schema: Union[dict, str, Path], default_options: Union[DefaultOptions, None] = None) -> dict:
+    return main.JsonSchemaDefault(schema, parent=None).generate(options=default_options)
