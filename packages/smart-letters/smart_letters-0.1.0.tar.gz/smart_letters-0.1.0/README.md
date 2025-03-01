1c4bab1176f9533f24428a2592cefde0c56e3e15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-01 14:06:30 +0000

    Add package smart-letters/0.1.0/smart_letters-0.1.0.tar.gz

---
+++ packages/smart-letters/smart_letters-0.1.0.tar.gz/smart_letters-0.1.0/README.md
@@ -0,0 +1,266 @@
+[![Build Status](https://github.com/dusktreader/smart-letters/actions/workflows/push.yaml/badge.svg)](https://github.com/dusktreader/smart-letters/actions/workflows/push.yaml)
+![Python Versions](https://img.shields.io/pypi/pyversions/smart-letters?label=python-versions&logo=python&style=plastic)
+![PyPI Versions](https://img.shields.io/pypi/v/smart-letters?label=pypi-version&logo=python&style=plastic)
+![License](https://img.shields.io/pypi/l/smart-letters?style=plastic)
+
+> [!IMPORTANT]
+> I'm looking for a job right now! If you know of any openings that match my skill-set,
+> please let me know! You can read my resume over at my
+> [cv](https://github.com/dusktreader/cv) project. Thanks!!
+
+# Smart Letters
+
+[//]: # (Add an asciicast)
+
+`smart-letters` is a CLI application designed to help you write cover letters rapidly
+for specifc job postings. By harnessing the power of OpenAI and Python, we can generate
+a styled cover letter in PDF format in seconds. The slowest part is waiting for the
+request from OpenAI to return!
+
+
+## Quickstart
+
+### 1. Install `smart-letters`:
+
+```bash
+pip install smart-letters
+```
+
+### 2. Configure `smart-letters`:
+
+To see all the configuration options, run:
+
+```bash
+smart-letters config --help
+```
+
+The simplest working config would look like this:
+
+```bash
+smart-letters config bind --openai-api-key=<your-api-key> --resume-path=<path-to-resume-text> --candidate-name=<your-name>
+```
+
+### 3. Run `smart-letters`:
+
+To see all the generation options, run:
+
+```bash
+smart-letters generate --help
+```
+
+An example run command would look like this:
+
+```
+smart-letters generate --company=ACME --position="Senior Engineering Lead" https://github.com/dusktreader/smart-letters/blob/main/etc/fake-listing.md
+```
+
+Follow the prompts and get your ready-to-send cover letter!
+
+
+## Philosophy
+
+There's a lot of pain involved with job searching these days. There's the struggle to
+find postings that match your experience, expertise, and preferences. Then, there's the
+frustration of all the different web-apps that are used to gather applications. Of
+course, there's the rejection which is often automated; that is, if you get any
+rejection notice instead of just silence.
+
+However, probably the most frustrating part is the composition of cover letters. In the
+current marketplace for tech talent, you need to be blasting out lots and lots of
+applications. I've read horror stories on Reddit of seasoned devs that have sent out
+literal thousands of applications over a several month span as they searched for their
+next opportunity. Obviously, this is a process that needs to be fine tuned to maximize
+production and maintain quality. The cover letter is a wrench thrown directly into the
+machinery of the application process.
+
+I understand that hiring teams may feel that cover letters are a useful tool. It helps
+to screen out endless spam from automation. It's a way to ensure that the candidate has
+at least read the posting. Plus, it gives you a snapshot into the personality and
+motivation of the person applying. Sometimes I worry that the main reason that cover
+letters are requested is that it takes only ticking a single check-box in the submission
+form to many of these application SaaS platforms. In any case, writing the cover letter
+is the main hot-spot of the application process.
+
+Obviously, AI is a powerful tool for producing a lot of text very quickly. But, cover
+letters produced by AI...read like they were written by AI. If you really want to get
+good results, you need to tailor the letter to the job posting, refine your prompt, feed
+it with personal information, refine the output, and then edit it by hand afterward to
+get a good result.
+
+Even that is quite boring and time consuming. So, as most good Python engineers like to
+do, I decided to automate the boring stuff!
+
+
+## Configuration
+
+The `smart-letters` program stores its configuration in a file so that it can use
+the same settings for many runs without having a super-cluttered command line. You can
+check out the location where the config file is saved in the `config.py` module.
+
+#### Sub-commands
+
+There are several sub-commands that you can use to interact with your configuration:
+
+
+#### bind
+
+This is the core command. We used it in the Quickstart. This sub-command binds the
+provided configuration options to the `smart-letters` program by storing them in a
+config file.
+
+
+#### update
+
+Sometimes you want to just change one or two configuration options instead of supplying
+all of them again. For that, you would use the `update` subcommand:
+
+```bash
+smart-letters config update --filename-prefix=awesome-letter
+```
+
+In this case, we only updated the filename prefix setting and left all the others as
+they are.
+
+
+#### unset
+
+If you need to just unset one of the config options, you can do that with the `unset`
+subcommand. You don't need to provide any value, just pass the option:
+
+```bash
+smart-letters config unset --filename-prefix
+```
+
+
+#### show
+
+If you want to see the current configuration for `smart-letters`, use the show command:
+
+```bash
+smart-letters config show
+```
+
+It will produce some nicely formatted output that would look like this:
+
+```
+╭──────────────────────────── Current Configuration ───────────────────────────────────╮
+│                                                                                      │
+│   openai_api_key  -> <omitted>                                                       │
+│   resume_path     -> /home/coyote/git-repos/personal/cv/README.md                    │
+│   candidate_name  -> Wile E. Coyote                                                  │
+│   filename_prefix -> cover-letter                                                    │
+│   heading_path    -> /home/coyote/text/heading.md                                    │
+│   sig_path        -> /home/coyote/images/sig.png                                     │
+│                                                                                      │
+╰──────────────────────────────────────────────────────────────────────────────────────╯
+```
+
+
+#### clear
+
+If you want to just completely clear out your config, use the `clear` subcommand:
+
+```bash
+smart-letters config clear
+```
+
+
+### Settings
+
+There are some notable settings that weren't used in the Quickstart. Let's go over them
+here!
+
+
+#### --filename-prefix
+
+This one is really simple. By default, `smart-letters` will use the generic
+"cover-letter" prefix for your file names. If you want to change it to something else,
+you can set that here.
+
+
+#### --heading-path
+
+I, personally, like to include a heading in my letters that includes my name and some
+basic contact information. You might want to supply one as well. You should note that
+the contents of the letter are formatted as Markdown before they are rendered to a PDF,
+so if your heading is formatted as markdown, it will get rendered nicely. Here's an
+example heading:
+
+```markdown
+# Wile E. Coyote
+
+📍 [Tuscon, AZ](https://maps.app.goo.gl/5siCgp4pUymGPU499) /
+📧 [wile.e.coyote@gmail.com](wile.e.coyote@gmail.com) /
+🛠 [wile.e.coyote@github](https://github.com/wile.e.coyote)
+
+---
+```
+
+#### --sig-path
+
+I also like to include a signature image in my generated cover letter. If you include
+one with this option, it will be placed in between your closing and your name:
+
+
+```
+Best regards,
+
+[SIG HERE]
+
+Wile E. Coyote
+```
+
+
+## Generation
+
+There are a few options that are available on the `generate` sub-command that can be
+useful. Let's go over them quickly:
+
+
+### --company
+
+This is simply the name of the company for which the letter will be generated. It is
+used in creating the salutation and setting the filename.
+
+If it's omitted, the salutation will just be generic:
+
+```
+Dear Hiring Manager,
+```
+
+If you set it, however, the salutation will incorporate it:
+
+```
+To the Hiring Team at <company>:
+```
+
+
+### --position
+
+This option is only used for inclusion in the filename. It's useful to be able to
+distinguish between many letters that you've generated over time.
+
+
+### --dump-html
+
+This is only really meant to be used for debugging. The `smart-letters` program
+renders the letters in this order:
+
+```
+Generated Markdown -> HTML --> PDF
+```
+
+Sometimes its useful to see what was produced in the intermediary HTML. Use this option
+to take a peek.
+
+
+### --fake
+
+When you are debugging `smart-letters`, the most time-consuming part is waiting for
+OpenAI to generate the letter. If you want to skip that part, you can just pass this
+option, and it will use a pre-baked letter body instead of calling out to OpenAI.
+
+
+The `smart-letters` program stores its configuration in a file so that it can use
+the same settings for many runs without having a super-cluttered command line. You can
+check out the location where the config file is saved in the `config.py` module.
---
+++ packages/smart-letters/smart_letters-0.1.0.tar.gz/smart_letters-0.1.0/src/smart_letters/generate.py
@@ -0,0 +1,317 @@
+import json
+import os
+import subprocess
+import tempfile
+from importlib import resources
+from pathlib import Path
+from typing import Annotated, Any
+
+import httpx
+import typer
+from inflection import parameterize
+from loguru import logger
+from markdown import markdown
+from openai import OpenAI
+from weasyprint import HTML
+
+from smart_letters.config import attach_settings
+from smart_letters.exceptions import handle_abort
+from smart_letters.format import terminal_message, simple_message
+
+
+def generate_letter(
+    api_key: str,
+    posting_text: str,
+    resume_text: str,
+    example_text: str | None = None,
+    reprompts: list[tuple[str, str]] | None = None,
+) -> str:
+    client = OpenAI(api_key=api_key)
+
+    messages = []
+
+    messages.append(
+        dict(
+            role="developer",
+            content="""
+                You are a professional resume writer and career coach. Your task is to
+                generate a cover letter for a job application based on the provided job
+                posting and resume. You will be given a job posting and a resume, and
+                you need to create a tailored cover letter that highlights the
+                candidate's qualifications and suitability for the position.
+            """,
+        ),
+    )
+
+    messages.append(
+        dict(
+            role="developer",
+            content=f"""
+                This is the job posting. It is html scraped from a job board. It
+                contains a lot of html markup, but you should ignore that and focus on
+                the text content.
+
+                {posting_text}
+            """,
+        ),
+    )
+
+    messages.append(
+        dict(
+            role="developer",
+            content=f"""
+                Use this resume as you write the letter. It highlights the skills and
+                experience of the candidate.
+
+                {resume_text}
+            """,
+        ),
+    )
+
+    if example_text:
+        messages.append(
+            dict(
+                role="developer",
+                content=f"""
+                    You should use this cover letter as reference for what I would like
+                    the resulting cover letter to look like. It is a markdown document.
+
+                    {example_text}
+                """,
+            ),
+        )
+
+    messages.append(
+        dict(
+            role="developer",
+            content="""
+                The cover letter should not sound like it was written by a machine. It
+                should be direct, personal, and energetic. It should be professional,
+                but informal in an almost daring way.
+
+                Avoid sentences of the form, "With X, I will Y". Do not use any of the
+                following words:
+
+                - challenge
+                - opportunity
+                - leverage
+                - passionate
+
+                The letter should highlight the candidate's leadership, technical
+                excellence, and ambition.
+
+                Do not include a salutation or closing. Only include the body of the
+                letter. Don't include any other text. Just print the body of the letter
+                itself.
+
+                Again, do not provide any friendly text before the letter. Do not
+                explain what you are doing. Do not say "Here is your letter". Just print
+                the damn letter.
+            """,
+        )
+    )
+
+    messages.append(dict(role="user", content="Please write me a nice letter!"))
+
+    if reprompts is not None:
+        for old_letter, user_feedback in reprompts:
+            messages.append(dict(role="assistant", content=old_letter))
+            messages.append(dict(role="user", content=user_feedback))
+
+    kwargs = dict(
+        model="gpt-4o",
+        n=1,
+        messages=messages,
+    )
+    logger.debug(
+        f"Getting openai output using params: \n{json.dumps(kwargs, indent=2)}"
+    )
+
+    cmp = client.chat.completions.create(**kwargs)  # type: ignore
+    text = cmp.choices[0].message.content
+    assert text is not None
+
+    return text
+
+
+def assemble_letter(
+    text: str,
+    candidate_name: str,
+    company: str | None,
+    heading_text: str | None,
+    sig_path: Path | None,
+) -> str:
+    parts = []
+
+    if heading_text:
+        parts.append(heading_text)
+
+    if company:
+        parts.append(f"To the Hiring Team at {company}:")
+    else:
+        parts.append("Dear Hiring Manager,")
+
+    parts.append("")
+    parts.append(text)
+    parts.append("")
+    parts.append("Best regards,")
+    parts.append("")
+
+    if sig_path:
+        parts.append(f"![signature](file://{sig_path.absolute()})")
+        parts.append("")
+
+    parts.append(candidate_name)
+
+    return "\n".join(parts)
+
+
+def edit_letter(text: str) -> str:
+    logger.debug("Editing generated letter")
+    with tempfile.NamedTemporaryFile() as tmp_file:
+        tmp_path = Path(tmp_file.name)
+        tmp_path.write_text(text)
+        subprocess.run([os.environ["EDITOR"], str(tmp_path)])
+        return tmp_path.read_text()
+
+
+def asset_path(file_name: str):
+    return resources.files(f"{__package__}.assets") / file_name
+
+
+def write_letter(
+    text: str,
+    filename_prefix: str,
+    company: str | None,
+    position: str | None,
+    dump_html: bool,
+) -> Path:
+    logger.debug("Saving letter to file")
+    name = filename_prefix
+    if company:
+        name += f"--{parameterize(company)}"
+    if position:
+        name += f"--{parameterize(position)}"
+    pdf_path = Path(f"{name}.pdf")
+
+    html_content = markdown(text)
+
+    if dump_html:
+        html_path = Path(f"{name}.html")
+        logger.debug(f"Dumping html to {html_path}")
+        html_path.write_text(html_content)
+
+    css_paths = [asset_path("styles.css")]
+    html = HTML(string=html_content)
+    html.write_pdf(pdf_path, stylesheets=css_paths)
+    return pdf_path
+
+
+cli = typer.Typer()
+
+
+@cli.callback(invoke_without_command=True)
+@handle_abort
+@attach_settings
+def generate(
+    ctx: typer.Context,
+    posting_url: Annotated[str, typer.Argument(help="The URL of the job posting.")],
+    company: Annotated[
+        str | None, typer.Option(help="The name of the company.")
+    ] = None,
+    position: Annotated[str | None, typer.Option(help="The title for the job.")] = None,
+    example_letter: Annotated[
+        Path | None, typer.Option(help="An example letter to use as a reference.")
+    ] = None,
+    dump_html: Annotated[
+        bool, typer.Option(help="[FOR DEBUGGING] Dump generated HTML to a file.")
+    ] = False,
+    fake: Annotated[
+        bool,
+        typer.Option(
+            help="[FOR DEBUGGING] Use a fake letter body instead of fetching one from OpenAI."
+        ),
+    ] = False,
+):
+    settings = ctx.obj.settings
+    resume_path: Path = settings.resume_path
+    candidate_name = settings.candidate_name
+    filename_prefix = settings.filename_prefix
+    heading_path = settings.heading_path
+    sig_path = settings.sig_path
+
+    logger.debug(f"Loading resume from {resume_path}")
+    resume_text = resume_path.read_text()
+
+    heading_text: str | None = None
+    if heading_path:
+        logger.debug(f"Loading heading text from {heading_path}")
+        heading_text = heading_path.read_text()
+
+    example_text: str | None = None
+    if example_letter:
+        logger.debug(f"Loading example text from {example_letter}")
+        heading_text = example_letter.read_text()
+
+    logger.debug(f"Pulling posting from {posting_url}")
+    response = httpx.get(posting_url)
+    response.raise_for_status()
+    posting_text = response.text
+
+    kwargs: dict[str, Any] = dict()
+    if example_letter:
+        logger.debug(f"Loading example letter from {example_letter}")
+        kwargs["example_text"] = example_letter.read_text()
+
+    logger.debug("Generating letter")
+
+    api_key = ctx.obj.settings.openai_api_key
+
+    generate_args: list[Any] = [api_key, candidate_name, posting_text, resume_text]
+    generate_kwargs: dict[str, Any] = dict(
+        example_text=example_text,
+        sig_path=sig_path,
+    )
+
+    assemble_args = [
+        candidate_name,
+        company,
+        heading_text,
+        sig_path,
+    ]
+
+    if fake:
+        text = asset_path("fake.txt").read_text()
+    else:
+        text = generate_letter(*generate_args, **generate_kwargs)
+
+    full_text = assemble_letter(text, *assemble_args)
+
+    accepted = False
+    reprompts = []
+
+    while not accepted:
+        terminal_message(full_text, subject="Generated Letter", markdown=True)
+
+        accepted = typer.confirm("Are you satisfied with the letter?", default=True)
+
+        if not accepted:
+            reprompts.append(
+                (
+                    text,
+                    typer.prompt("What can I do to fix it?", default="Just try again"),
+                )
+            )
+            generate_kwargs["reprompts"] = reprompts
+
+            logger.debug("Regenerating letter based on feedback")
+            text = generate_letter(*generate_args, **kwargs)
+            full_text = assemble_letter(text, *assemble_args)
+
+    edit = typer.confirm("Would you like to edit the letter?", default=False)
+    if edit:
+        edit_letter(full_text)
+
+    pdf_path = write_letter(full_text, filename_prefix, company, position, dump_html)
+
+    simple_message(f"Cover letter saved to {pdf_path}")
