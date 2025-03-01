25457e48b3c8e5ab4ce727fcb25a828eeb6875ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-01 06:05:47 +0000

    Add package diff-dot/2025.2.28.1/diff_dot-2025.2.28.1.tar.gz

---
+++ packages/diff-dot/diff_dot-2025.2.28.1.tar.gz/diff_dot-2025.2.28.1/diff_dot/dot.py
@@ -0,0 +1,14 @@
+import subprocess
+
+from .cmd import is_tool
+from .error import fail
+
+
+def render_dot_file(input_dot_path, output_png_path):
+    if not is_tool("dot"):
+        fail("Dot is not installed, see [link=https://github.com/westonal/graph-diff#setup]README.md/setup[/link]")
+    command = ["dot", "-Tpng", input_dot_path, "-o", output_png_path]
+    return_code = subprocess.run(command).returncode
+    if return_code != 0:
+        join = ' '.join(map(lambda a: f"{a}", command))
+        fail(f"Dot failed return code {return_code} [cyan]{join}")
