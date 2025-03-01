6ff99acfbb925723d0ca3f052f541bd85749db4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-01 14:05:59 +0000

    Add package pangea-llm-translator/1.0.1/pangea_llm_translator-1.0.1-py3-none-any.whl

---
+++ packages/pangea-llm-translator/pangea_llm_translator-1.0.1-py3-none-any.whl/ai/__init__.py
@@ -0,0 +1,7 @@
+# Copyright 2021 Pangea Cyber Corporation
+# Author: Pangea Cyber Corporation
+from ai.model.model import PangeaRoles, PangeaAiInput, PangeaMessage
+from ai.translators.base import Translator
+from ai.translate import get_translator
+
+__all__ = ["PangeaRoles", "PangeaAiInput", "PangeaMessage", "get_translator", "Translator"]
