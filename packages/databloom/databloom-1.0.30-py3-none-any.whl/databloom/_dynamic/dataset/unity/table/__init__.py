7b269e0fa435b3a3ddce7431cb702e84610e73da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-01 16:05:25 +0000

    Add package databloom/1.0.30/databloom-1.0.30-py3-none-any.whl

---
+++ packages/databloom/databloom-1.0.30-py3-none-any.whl/databloom/_dynamic/dataset/unity/table/__init__.py
@@ -0,0 +1,2 @@
+from .trino_finance_data2 import *
+from .hainv4_campaign_2 import *
---
+++ packages/databloom/databloom-1.0.30-py3-none-any.whl/databloom/_dynamic/source/postgresql/neww222.py
@@ -0,0 +1,8 @@
+# --- render code block -----
+from databloom._core.postgres_core import PostgresqlBase
+
+class neww222(PostgresqlBase):
+    def __init__(self, get_credential_from_server) -> None:
+        self.id = "67bc07a2f1ba9a2bf4a678b2"
+        self.credential = get_credential_from_server(self.id)
+# --- render code block -----
