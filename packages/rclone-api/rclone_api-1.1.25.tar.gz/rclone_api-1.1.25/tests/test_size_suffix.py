d0675b21cb67fd28c299fde6db3f7cdeaf664f5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-01 06:05:45 +0000

    Add package rclone-api/1.1.25/rclone_api-1.1.25.tar.gz

---
+++ packages/rclone-api/rclone_api-1.1.25.tar.gz/rclone_api-1.1.25/tests/test_size_suffix.py
@@ -0,0 +1,29 @@
+"""
+Unit test file.
+"""
+
+import unittest
+
+from rclone_api import SizeSuffix
+
+
+class RcloneSuffixSize(unittest.TestCase):
+    """Test rclone functionality."""
+
+    def test_simple_suffix(self) -> None:
+        size_suffix = SizeSuffix(1024)
+        size_suffix = SizeSuffix("16MB")
+        size_int = size_suffix.as_int()
+        self.assertEqual(size_int, 16 * 1024 * 1024)
+
+    def test_float_suffix(self) -> None:
+        size_suffix = SizeSuffix("16.5M")
+        size_int = size_suffix.as_int()
+        self.assertEqual(size_int, int(16.5 * 1024 * 1024))
+        # now assert that the string value is the same as the input
+        out_str = str(size_suffix)
+        self.assertEqual(out_str, "16.5M")
+
+
+if __name__ == "__main__":
+    unittest.main()
