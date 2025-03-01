434accfe6d061deaef7a87051bc7a136bfb538ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-01 04:06:12 +0000

    Add package senphora/0.1.0/senphora-0.1.0.tar.gz

---
+++ packages/senphora/senphora-0.1.0.tar.gz/senphora-0.1.0/README.md
@@ -0,0 +1,7 @@
+# senphora
+Библиотека для работы с API copernicus
+
+# Установка в режиме редактирования
+```cmd
+pip install -e .
+```
---
+++ packages/senphora/senphora-0.1.0.tar.gz/senphora-0.1.0/src/senphora/loader/__init__.py
@@ -0,0 +1,8 @@
+__all__ = [
+    "TokenManager",
+    "products_by_coords",
+    "products_by_polygon",
+]
+
+from .token_manager import TokenManager
+from .find_product import products_by_coords, products_by_polygon
