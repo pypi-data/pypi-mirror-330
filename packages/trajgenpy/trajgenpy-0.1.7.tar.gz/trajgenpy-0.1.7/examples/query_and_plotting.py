ef05001c067ee18db78dbc64bb9a906ecd619415
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-01 04:06:16 +0000

    Add package trajgenpy/0.1.7/trajgenpy-0.1.7.tar.gz

---
+++ packages/trajgenpy/trajgenpy-0.1.7.tar.gz/trajgenpy-0.1.7/examples/query_and_plotting.py
@@ -0,0 +1,81 @@
+import geojson
+import matplotlib.pyplot as plt
+import shapely
+import trajgenpy.Logging
+from trajgenpy import Utils
+from trajgenpy.Geometries import GeoMultiPolygon, GeoMultiTrajectory, GeoPolygon
+from trajgenpy.Query import query_features
+
+log = trajgenpy.Logging.get_logger()
+
+if __name__ == "__main__":
+    # Download the water features data within the bounding box
+    tags = {
+        "natural": ["water", "wetland"],
+        # "natural": ["coastline"],
+        "landuse": ["farmland"],
+        "highway": [
+            "primary",
+            "secondary",
+            "secondary_link",
+            "tertiary",
+            "unclassified",
+            "road",
+            "service",
+            "path",
+            "track",
+        ],
+        "building": ["industrial", "yes", "storage_tank"],
+    }
+
+    # Amagerværket: shapely.Polygon([(12.620400,55.687962),(12.632788,55.691589),(12.637446,55.687689),(12.624924,55.683489)])
+    # Davinde: shapely.Polygon([(10.490913, 55.315346), (10.576744, 55.315346), (10.576744, 55.337417), (10.490913, 55.337417)])
+    polygon = GeoPolygon(
+        shapely.Polygon(
+            [
+                (10.490913, 55.315346),
+                (10.576744, 55.315346),
+                (10.576744, 55.337417),
+                (10.490913, 55.337417),
+            ]
+        ),
+        crs="WGS84",
+    )  # Download the water features data within the bounding box
+
+    features = query_features(polygon, tags)
+    polygon.set_crs("EPSG:2197")
+
+    polygon.plot(facecolor="none", edgecolor="black", linewidth=2)
+
+    # Plot highways
+    roads = GeoMultiTrajectory(features["highway"], crs="WGS84").set_crs("EPSG:2197")
+    roads.plot()
+
+    # Plot Buildings
+    buildings = GeoMultiPolygon(features["building"], crs="WGS84").set_crs("EPSG:2197")
+
+    buildings.plot(color="red")
+
+    # Plot natural features
+    # coastline = GeoMultiTrajectory(features["natural"], crs="WGS84").set_crs(
+    #     "EPSG:2197"
+    # )
+    # coastline.plot(color="green")
+
+    # Export the polygon and the buildings as a GeoJSON file
+    geojson_collection = geojson.FeatureCollection(
+        [
+            polygon.to_geojson(id="boundary"),
+            buildings.to_geojson(id="obstacles"),
+        ]
+    )
+
+    with open("environment.geojson", "w") as f:
+        geojson.dump(geojson_collection, f)
+
+    # Plot on a map
+    Utils.plot_basemap(crs="EPSG:2197")
+
+    # No axis on the plot
+    plt.axis("off")
+    plt.show()
---
+++ packages/trajgenpy/trajgenpy-0.1.7.tar.gz/trajgenpy-0.1.7/tests/test_bindings.py
@@ -0,0 +1,67 @@
+import pytest
+import trajgenpy.bindings as bindings
+
+
+def test_create_polygon():
+    # Construct a polygon from a list of points
+    points = [
+        bindings.Point_2(0, 0),
+        bindings.Point_2(1, 0),
+        bindings.Point_2(1, 1),
+        bindings.Point_2(0, 1),
+    ]
+    polygon = bindings.Polygon_2(points)
+
+    # Check if the polygon is simple and convex
+    assert polygon.is_simple()  # True
+    assert polygon.is_convex()  # True
+
+
+def test_create_sweeps():
+    # create the outer polygon with holes
+    outer_boundary = bindings.Polygon_2()
+    outer_boundary.push_back(bindings.Point_2(0, 0))
+    outer_boundary.push_back(bindings.Point_2(0, 10))
+    outer_boundary.push_back(bindings.Point_2(10, 10))
+    outer_boundary.push_back(bindings.Point_2(10, 0))
+
+    outer_poly = bindings.Polygon_with_holes_2(outer_boundary)
+
+    inner_boundary = bindings.Polygon_2()
+    inner_boundary.push_back(bindings.Point_2(2, 2))
+    inner_boundary.push_back(bindings.Point_2(2, 8))
+    inner_boundary.push_back(bindings.Point_2(8, 8))
+    inner_boundary.push_back(bindings.Point_2(8, 2))
+
+    outer_poly.add_hole(inner_boundary)
+
+    # create the list of polygons
+    poly_list = []
+    polygon1 = bindings.Polygon_2()
+    polygon1.push_back(bindings.Point_2(3, 3))
+    polygon1.push_back(bindings.Point_2(3, 5))
+    polygon1.push_back(bindings.Point_2(5, 5))
+    polygon1.push_back(bindings.Point_2(5, 3))
+    poly_list.append(polygon1)
+
+    polygon2 = bindings.Polygon_2()
+    polygon2.push_back(bindings.Point_2(6, 6))
+    polygon2.push_back(bindings.Point_2(6, 8))
+    polygon2.push_back(bindings.Point_2(8, 8))
+    polygon2.push_back(bindings.Point_2(8, 6))
+    poly_list.append(polygon2)
+
+    decomposed_polygons = bindings.decompose(outer_poly)
+    segments = []
+    for poly in decomposed_polygons:
+        if poly.is_convex():
+            segments.extend(bindings.generate_sweeps(poly, 0.5))
+        else:
+            msg = "not able to generate a plan for a non convex polygon"
+            raise Exception(msg)
+    assert len(segments) == 20
+
+
+# Run the tests
+if __name__ == "__main__":
+    pytest.main(["-v", "-x", "tests/test_bindings.py"])
