fab50828f83bce90876d92ca53e777732f242fc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-01 03:15:07 +0000

    Add package snakehdl/0.2.1/snakehdl-0.2.1.tar.gz

---
+++ packages/snakehdl/snakehdl-0.2.1.tar.gz/snakehdl-0.2.1/snakehdl/__init__.py
@@ -0,0 +1,5 @@
+from snakehdl.bops import (
+  BOp, BOps, BOpGroup,                        # noqa: F401
+  const_bits, input_bits, output, bit, join,  # noqa: F401
+  neg, conj, nand, disj, nor, xor, xnor,      # noqa: F401
+)
