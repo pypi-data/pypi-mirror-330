54705a891beecd4db786bf725eee9b8605ce6c2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-01 03:15:44 +0000

    Add package byzh-rc/1.3.2/byzh_rc-1.3.2-py3-none-any.whl

---
+++ packages/byzh-rc/byzh_rc-1.3.2-py3-none-any.whl/byzh_rc/deeplearning/early_stop/stopbylossdelta.py
@@ -0,0 +1,22 @@
+class BStopByLossDelta:
+    def __init__(self, rounds, delta=0.002):
+        '''
+        连续rounds次, |before_loss - now_loss| <= delta, 则停止训练
+        '''
+        self.rounds = rounds
+        self.delta = delta
+        self.before_loss = float('inf')
+        self.cnt = 0
+    def __call__(self, train_loss):
+        if -self.delta <= (train_loss - self.before_loss) <= self.delta:
+            self.cnt += 1
+            self.before_loss = train_loss
+        elif self.cnt > 0:
+            self.cnt -= 1
+            self.before_loss = train_loss
+        else:
+            self.before_loss = train_loss
+
+        if self.cnt > self.rounds:
+            return True
+        return False
