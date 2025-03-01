aa8db00864d401b3ddcaf086c383d287c65aad10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-01 06:06:21 +0000

    Add package cyberdrop-dl-patched/6.6.1/cyberdrop_dl_patched-6.6.1-py3-none-any.whl

---
+++ packages/cyberdrop-dl-patched/cyberdrop_dl_patched-6.6.1-py3-none-any.whl/cyberdrop_dl/config_definitions/authentication_settings.py
@@ -0,0 +1,83 @@
+from pydantic import BaseModel, Field
+
+from .custom.types import AliasModel
+
+
+class ForumAuth(BaseModel):
+    allporncomix_xf_user_cookie: str = ""
+    allporncomix_username: str = ""
+    allporncomix_password: str = ""
+    bellazon_xf_user_cookie: str = ""
+    bellazon_username: str = ""
+    bellazon_password: str = ""
+    celebforum_xf_user_cookie: str = ""
+    celebforum_username: str = ""
+    celebforum_password: str = ""
+    f95zone_xf_user_cookie: str = ""
+    f95zone_username: str = ""
+    f95zone_password: str = ""
+    leakedmodels_xf_user_cookie: str = ""
+    leakedmodels_username: str = ""
+    leakedmodels_password: str = ""
+    nudostar_xf_user_cookie: str = ""
+    nudostar_username: str = ""
+    nudostar_password: str = ""
+    simpcity_xf_user_cookie: str = ""
+    simpcity_username: str = ""
+    simpcity_password: str = ""
+    socialmediagirls_xf_user_cookie: str = ""
+    socialmediagirls_username: str = ""
+    socialmediagirls_password: str = ""
+    titsintops_xf_user_cookie: str = ""
+    titsintops_username: str = ""
+    titsintops_password: str = ""
+    xbunker_xf_user_cookie: str = ""
+    xbunker_username: str = ""
+    xbunker_password: str = ""
+
+
+class CoomerAuth(BaseModel):
+    session: str = ""
+
+
+class XXXBunkerAuth(BaseModel):
+    PHPSESSID: str = ""
+
+
+class ImgurAuth(AliasModel):
+    client_id: str = Field("", validation_alias="imgur_client_id")
+
+
+class JDownloaderAuth(AliasModel):
+    username: str = Field("", validation_alias="jdownloader_username")
+    password: str = Field("", validation_alias="jdownloader_password")
+    device: str = Field("", validation_alias="jdownloader_device")
+
+
+class RedditAuth(AliasModel):
+    personal_use_script: str = Field("", validation_alias="reddit_personal_use_script")
+    secret: str = Field("", validation_alias="reddit_secret")
+
+
+class GoFileAuth(AliasModel):
+    api_key: str = Field("", validation_alias="gofile_api_key")
+
+
+class PixeldrainAuth(AliasModel):
+    api_key: str = Field("", validation_alias="pixeldrain_api_key")
+
+
+class RealDebridAuth(AliasModel):
+    api_key: str = Field("", validation_alias="realdebrid_api_key")
+
+
+class AuthSettings(AliasModel):
+    coomer: CoomerAuth = Field(validation_alias="Coomer", default=CoomerAuth())
+    forums: ForumAuth = Field(validation_alias="Forums", default=ForumAuth())
+    gofile: GoFileAuth = Field(validation_alias="GoFile", default=GoFileAuth())  # type: ignore
+    imgur: ImgurAuth = Field(validation_alias="Imgur", default=ImgurAuth())  # type: ignore
+    jdownloader: JDownloaderAuth = Field(validation_alias="JDownloader", default=JDownloaderAuth())  # type: ignore
+    pixeldrain: PixeldrainAuth = Field(validation_alias="PixelDrain", default=PixeldrainAuth())  # type: ignore
+    realdebrid: RealDebridAuth = Field(validation_alias="RealDebrid", default=RealDebridAuth())  # type: ignore
+    reddit: RedditAuth = Field(validation_alias="Reddit", default=RedditAuth())  # type: ignore
+    xxxbunker: XXXBunkerAuth = Field(validation_alias="XXXBunker", default=XXXBunkerAuth())  # type: ignore
---
+++ packages/cyberdrop-dl-patched/cyberdrop_dl_patched-6.6.1-py3-none-any.whl/cyberdrop_dl/ui/progress/file_progress.py
@@ -0,0 +1,74 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from pydantic import ByteSize
+from rich.markup import escape
+from rich.progress import (
+    BarColumn,
+    DownloadColumn,
+    Progress,
+    SpinnerColumn,
+    TaskID,
+    TimeRemainingColumn,
+    TransferSpeedColumn,
+)
+
+from cyberdrop_dl.ui.progress.deque_progress import DequeProgress, adjust_title
+
+if TYPE_CHECKING:
+    from cyberdrop_dl.managers.manager import Manager
+
+
+class FileProgress(DequeProgress):
+    """Class that manages the download progress of individual files."""
+
+    def __init__(self, visible_tasks_limit: int, manager: Manager) -> None:
+        self.manager = manager
+        self._progress = Progress(
+            SpinnerColumn(),
+            "[progress.description]{task.description}",
+            BarColumn(bar_width=None),
+            "[progress.percentage]{task.percentage:>6.2f}%",
+            "━",
+            DownloadColumn(),
+            "━",
+            TransferSpeedColumn(),
+            "━",
+            TimeRemainingColumn(),
+        )
+        self.downloaded_data = ByteSize(0)
+        super().__init__("Downloads", visible_tasks_limit)
+
+    def get_queue_length(self) -> int:
+        """Returns the number of tasks in the downloader queue."""
+        total = 0
+        unique_crawler_ids = set()
+        for crawler in self.manager.scrape_mapper.existing_crawlers.values():
+            crawler_id = id(crawler)  # Only count each instance of the crawler once
+            if crawler_id in unique_crawler_ids:
+                continue
+            unique_crawler_ids.add(crawler_id)
+            total += getattr(crawler.downloader, "waiting_items", 0)
+
+        return total
+
+    def add_task(self, *, domain: str, filename: str, expected_size: int | None = None) -> TaskID:
+        """Adds a new task to the progress bar."""
+        filename = filename.split("/")[-1].encode("ascii", "ignore").decode().strip()
+        filename = escape(adjust_title(filename))
+        description = f"({domain.upper()}) {filename}"
+        return super().add_task(description, expected_size)
+
+    def advance_file(self, task_id: TaskID, amount: int) -> None:
+        """Advances the progress of the given task by the given amount."""
+        self.downloaded_data += amount
+        self._progress.advance(task_id, amount)
+
+    def get_speed(self, task_id: TaskID) -> float:
+        if task_id not in self._tasks:
+            msg = "Task ID not found"
+            raise ValueError(msg)
+
+        task = self._progress._tasks[task_id]
+        return task.finished_speed or task.speed or 0
