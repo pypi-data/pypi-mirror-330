844fd8bfa0d72c775ed12b8ec21ccecda8dd2899
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-01 05:05:22 +0000

    Add package qti-package-maker/0.7.2/qti_package_maker-0.7.2-py3-none-any.whl

---
+++ packages/qti-package-maker/qti_package_maker-0.7.2-py3-none-any.whl/qti_package_maker/common/base_package_maker.py
@@ -0,0 +1,102 @@
+
+# Standard Library
+import inspect
+
+# Pip3 Library
+
+# QTI Package Maker
+from qti_package_maker.common import validator
+from qti_package_maker.common import string_functions
+
+# engine_base.py (shared base for all engines)
+class BaseEngine:
+	#==============
+	def __init__(self, package_name: str):
+		self.package_name = package_name
+		#need to override is engine class
+		self.add_item = None
+		self.assessment_items_tree = []
+		self.number_of_assessment_items = 0
+
+	#==============
+	def save_package(self):
+		raise NotImplementedError
+
+	#==============
+	@property
+	def engine_name(self):
+		# Dynamically get the class name
+		return self.__class__.__name__
+
+	#==============
+	def add_assessment_item(self, title: str, crc16: str, item_type: str, assessment_item_data):
+		assessment_item_dict = {
+			'title': title,
+			'crc16': crc16,
+			'item_type': item_type,
+			'assessment_item_data': assessment_item_data,
+			}
+		self.assessment_items_tree.append(assessment_item_dict)
+		self.number_of_assessment_items += 1
+		#print(f"added assessment_item number {self.number_of_assessment_items} of type {item_type}")
+
+	#==============
+	def MC(self, question_text: str, choices_list: list, answer_text: str):
+		title = None
+		item_number = self.number_of_assessment_items + 1
+		validator.validate_MC(question_text, choices_list, answer_text)
+		crc16question = string_functions.get_crc16_from_string(question_text)
+		choices_str = '|'.join(choices_list)
+		crc16choice = string_functions.get_crc16_from_string(choices_str)
+		crc16merge = f"{crc16question}_{crc16choice}"
+		assessment_item_data = self.add_item.MC(item_number, crc16merge, question_text, choices_list, answer_text)
+		function_name = inspect.currentframe().f_code.co_name
+		self.add_assessment_item(title, crc16merge, function_name, assessment_item_data)
+
+	#==============
+	def MA(self, question_text: str, choices_list: list, answers_list: list):
+		title = None
+		item_number = self.number_of_assessment_items + 1
+		validator.validate_MA(question_text, choices_list, answers_list)
+		crc16question = string_functions.get_crc16_from_string(question_text)
+		choices_str = '|'.join(choices_list)
+		crc16choices = string_functions.get_crc16_from_string(choices_str)
+		crc16merge = f"{crc16question}_{crc16choices}"
+		assessment_item_data = self.add_item.MA(item_number, crc16merge, question_text, choices_list, answers_list)
+		function_name = inspect.currentframe().f_code.co_name
+		self.add_assessment_item(title, crc16merge, function_name, assessment_item_data)
+
+	#==============
+	def MATCH(self, question_text: str, prompts_list: list, choices_list: list):
+		title = None
+		item_number = self.number_of_assessment_items + 1
+		validator.validate_MATCH(question_text, prompts_list, choices_list)
+		crc16question = string_functions.get_crc16_from_string(question_text)
+		prompts_str = '|'.join(prompts_list)
+		crc16prompts = string_functions.get_crc16_from_string(prompts_str)
+		crc16merge = f"{crc16question}_{crc16prompts}"
+		assessment_item_data = self.add_item.MATCH(item_number, crc16merge, question_text, prompts_list, choices_list)
+		function_name = inspect.currentframe().f_code.co_name
+		self.add_assessment_item(title, crc16merge, function_name, assessment_item_data)
+
+	#==============
+	def get_available_question_types(self) -> list:
+		""" Returns a list of available question types based on callable methods in `add_item`. """
+		if not self.add_item:
+			print(f"No add_item module assigned for engine {self.engine_name}.")
+			return []
+		# Extract function names dynamically
+		functions = [
+			name for name in dir(self.add_item)
+			if callable(getattr(self.add_item, name)) and not name.startswith("__")
+		]
+		return functions
+
+	#==============
+	def show_available_question_types(self):
+		""" Displays the available question types. """
+		available_types = self.get_available_question_types()
+		if available_types:
+			print(f"Available question types: {', '.join(available_types)}")
+		else:
+			print(f"No available question types found for engine {self.engine_name}.")
---
+++ packages/qti-package-maker/qti_package_maker-0.7.2-py3-none-any.whl/qti_package_maker/package_maker.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python
+
+# Standard Library
+import re
+
+# Pip3 Library
+
+# QTI Package Maker
+from qti_package_maker.engines.bbq_text_upload.engine_class import BBQTextEngine
+from qti_package_maker.engines.html_selftest.engine_class import HTMLSelfTest
+from qti_package_maker.engines.human_readable.engine_class import HumanReadable
+from qti_package_maker.engines.canvas_qti_v1_2.engine_class import QTIv1Engine
+from qti_package_maker.engines.blackboard_qti_v2_1.engine_class import QTIv2Engine
+
+class MasterQTIPackage:
+	def __init__(self, package_name: str, engine_name: str):
+		package_name = package_name.strip()
+		if not package_name:
+			raise ValueError("package_name not defined")
+		# Convert to lowercase
+		engine_name = engine_name.lower()
+		# Remove non-alphanumeric characters
+		engine_name = re.sub("[^a-z0-9]", "", engine_name)
+		if engine_name.startswith('bbq') or engine_name.startswith('blackboardtext'):
+			# blackboard_text_upload
+			self.engine = BBQTextEngine(package_name)
+		elif engine_name.startswith('html'):
+			# blackboard_text_upload
+			self.engine = HTMLSelfTest(package_name)
+		elif engine_name.startswith('human'):
+			# human_readable
+			self.engine = HumanReadable(package_name)
+		elif engine_name.startswith('qtiv1') or engine_name.startswith('canvas'):
+			# canvas_qti_v1_2
+			self.engine = QTIv1Engine(package_name)
+		elif engine_name.startswith('qtiv2') or engine_name.startswith('blackboardqti'):
+			# blackboard_qti_v2_1
+			self.engine = QTIv2Engine(package_name)
+		else:
+			raise ValueError(f"Unknown engine: {engine_name}")
+		print(f"Initialized Engine: {self.engine.engine_name}")
+
+	#=====================================================================
+	def add_question(self, question_type: str, question_tuple: tuple):
+		""" General method to add a question to the engine. """
+		supported_types = self.engine.get_available_question_types()
+		if question_type not in supported_types:
+			self.show_available_question_types()
+			raise NotImplementedError(f"Error: Unsupported question type '{question_type}'")
+		add_method = getattr(self, f"add_{question_type}", None)
+		if add_method is None:
+			self.show_available_question_types()
+			raise NotImplementedError(f"Error: No method found for question type '{question_type}'")
+		add_method(*question_tuple)
+
+	def add_MC(self, question_text: str, choices_list: list, answer_text: str):
+		"""Handles adding a Multiple-Choice (MC) question."""
+		self.engine.MC(question_text, choices_list, answer_text)
+
+	def add_MA(self, question_text: str, choices_list: list, answers_list: list):
+		"""Handles adding a Multiple-Answer (MA) question."""
+		self.engine.MA(question_text, choices_list, answers_list)
+
+	def add_MATCH(self, question_text: str, prompts_list: list, choices_list: list):
+		"""Handles adding a Matching (MATCH) question."""
+		self.engine.MATCH(question_text, prompts_list, choices_list)
+
+	def add_FIB(self, question_text: str,  answers_list: list):
+		"""Handles adding a Multiple-Choice (MC) question."""
+		self.engine.FIB(question_text, answers_list)
+
+	def add_MULTI_FIB(self, question_text: str, answer_map: dict):
+		"""Handles adding a Multiple-Choice (MC) question."""
+		self.engine.MULTI_FIB(question_text, answer_map)
+
+	def add_NUM(self, question_text: str, answer_float: float,
+				 tolerance_float: float, tolerance_message: bool=True):
+		"""Handles adding a Multiple-Answer (MA) question."""
+		self.engine.NUM(question_text, answer_float, tolerance_float, tolerance_message)
+
+	def add_ORDER(self, question_text: str,  ordered_answers_list: list):
+		"""Handles adding a Matching (MATCH) question."""
+		self.engine.ORDER(question_text, ordered_answers_list)
+
+	#=====================================================================
+	def save_package(self):
+		print(
+			f"Saving package {self.engine.package_name}\n"
+			f"  with engine {self.engine.engine_name} and\n"
+			f"  {len(self.engine.assessment_items_tree)} questions."
+		)
+		self.engine.save_package()
+
+	#=====================================================================
+	def show_available_question_types(self):
+		self.engine.show_available_question_types()
+
+
+#===========================================================
+# This function serves as the entry point for generating and saving questions.
+def main():
+	"""
+	Main function that orchestrates question generation and file output.
+	"""
+
+	# Parse arguments from the command line
+	#args = parse_arguments()
+
+	qti_packer = MasterQTIPackage('example_pool_from_test_maker')
+	qti_packer.show_available_question_types()
+	question_text = 'What is your favorite color?'
+	answer_text = 'blue'
+	choices_list = ['blue', 'red', 'yellow']
+	qti_packer.add_MC(question_text, choices_list, answer_text)
+
+	question_text = 'Which are types of fruit?'
+	answers_list = ['orange', 'banana', 'apple']
+	choices_list = ['orange', 'banana', 'apple', 'lettuce', 'spinach']
+	qti_packer.add_MA(question_text, choices_list, answers_list)
+
+	qti_packer.save_package()
