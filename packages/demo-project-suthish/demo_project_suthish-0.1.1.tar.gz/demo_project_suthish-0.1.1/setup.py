e6c058b232bf9c0018d88727329e973cf830bb89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-01 16:05:27 +0000

    Add package demo-project-suthish/0.1.1/demo_project_suthish-0.1.1.tar.gz

---
+++ packages/demo-project-suthish/demo_project_suthish-0.1.1.tar.gz/demo_project_suthish-0.1.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="demo-project-suthish",  # Replace with your package name
+    version="0.1.1",
+    packages=find_packages(),
+    install_requires=[],  # Add dependencies here
+    author="Your Name",
+    author_email="your.email@example.com",
+    description="A short description of your package",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/SuthishSurendran/demo-project-suthish",  # Replace with your repo URL
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
