6cf8876c3588cb5a9a649d0b09e6fec8c7ee58c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-01 06:05:42 +0000

    Add package fufpy/0.1.1/fufpy-0.1.1-py3-none-any.whl

---
+++ packages/fufpy/fufpy-0.1.1-py3-none-any.whl/fufpy/__init__.py
@@ -0,0 +1,8 @@
+from .dynamic_partition import (
+    DynamicPartition,
+    dynamic_partition_create,
+    dynamic_partition_representative,
+    dynamic_partition_union,
+    dynamic_partition_subset,
+    dynamic_partition_parts,
+)
