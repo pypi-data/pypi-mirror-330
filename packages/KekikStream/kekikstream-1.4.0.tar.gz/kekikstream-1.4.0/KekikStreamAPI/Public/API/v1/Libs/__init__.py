42013fb0a32a3ccc09f95b5b9bb2cc223656d345
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-01 06:06:07 +0000

    Add package KekikStream/1.4.0/kekikstream-1.4.0.tar.gz

---
+++ packages/KekikStream/kekikstream-1.4.0.tar.gz/kekikstream-1.4.0/KekikStreamAPI/Public/API/v1/Libs/__init__.py
@@ -0,0 +1,7 @@
+# Bu araç @keyiflerolsun tarafından | @KekikAkademi için yazılmıştır.
+
+from KekikStream.Core import PluginManager, ExtractorManager, MediaManager, MovieInfo, SeriesInfo
+
+plugin_manager    = PluginManager()
+extractor_manager = ExtractorManager()
+media_manager     = MediaManager()
---
+++ packages/KekikStream/kekikstream-1.4.0.tar.gz/kekikstream-1.4.0/KekikStreamAPI/README.md
@@ -0,0 +1,9 @@
+# KekikStreamAPI
+
+SubTree » https://github.com/keyiflerolsun/KekikStreamAPI
+
+Güncellemek için;
+
+```bash
+git subtree pull --prefix=KekikStreamAPI https://github.com/keyiflerolsun/KekikStreamAPI.git master --squash
+```
