1066ff1ca00cba6cbac68a9a51a52458dc275667
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-01 05:05:52 +0000

    Add package attpc_engine/0.8.0/attpc_engine-0.8.0.tar.gz

---
+++ packages/attpc_engine/attpc_engine-0.8.0.tar.gz/attpc_engine-0.8.0/src/attpc_engine/kinematics/__init__.py
@@ -0,0 +1,32 @@
+"""This is the parent module for the kinematics phase space"""
+
+from .pipeline import (
+    KinematicsPipeline,
+    run_kinematics_pipeline,
+    KinematicsTargetMaterial,
+)
+
+from .excitation import (
+    ExcitationDistribution,
+    ExcitationGaussian,
+    ExcitationUniform,
+    ExcitationBreitWigner,
+)
+
+from .angle import PolarDistribution, PolarUniform
+
+from .reaction import Reaction, Decay
+
+__all__ = [
+    "KinematicsPipeline",
+    "run_kinematics_pipeline",
+    "KinematicsTargetMaterial",
+    "ExcitationDistribution",
+    "ExcitationGaussian",
+    "ExcitationUniform",
+    "ExcitationBreitWigner",
+    "PolarDistribution",
+    "PolarUniform",
+    "Reaction",
+    "Decay",
+]
