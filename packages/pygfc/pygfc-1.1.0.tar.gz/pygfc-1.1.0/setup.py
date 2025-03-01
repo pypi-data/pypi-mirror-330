d45f4136e19b2f529ade37cf18979b3bc2435501
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-01 03:15:32 +0000

    Add package pygfc/1.1.0/pygfc-1.1.0.tar.gz

---
+++ packages/pygfc/pygfc-1.1.0.tar.gz/pygfc-1.1.0/setup.py
@@ -0,0 +1,29 @@
+from Cython.Build import cythonize
+from setuptools import Extension, setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="pygfc",
+    version="1.1.0",
+    author="Maxime Mouchet",
+    author_email="max@maxmouchet.com",
+    description="Implementation of a Generalized-Feistel Cipher for generating random permutations.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/maxmouchet/gfc",
+    ext_modules=cythonize(
+        [
+            Extension("pygfc", ["python/pygfc.pyx"]),
+        ]
+    ),
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Cython",
+        "Programming Language :: Python :: 3",
+    ],
+    python_requires=">=3.6",
+)
