dcd56422d5de53a73e22106a35d99bae85821a1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-01 05:05:47 +0000

    Add package voiceauthCore/0.7/voiceauthCore-0.7.tar.gz

---
+++ packages/voiceauthCore/voiceauthCore-0.7.tar.gz/voiceauthCore-0.7/setup.py
@@ -0,0 +1,28 @@
+## started the show
+from setuptools import setup, find_packages
+
+setup(
+    name="voiceauthCore",
+    version="0.7",
+    author="sadiq kassamali",
+    author_email="sadiq.kasssamali@gmail.com",
+    description="A deepfake audio detection tool",
+    long_description=open('README.md',encoding="utf8").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/sadiqkassamali/voiceauthCore",
+    packages=find_packages(),
+    install_requires=[
+        "tensorflow", "librosa", "pydub", "numpy", "scipy", "transformers" , "torch", "pillow"
+    ],
+    entry_points={
+        "console_scripts": [
+            "voiceauthCore=voiceauthCore.core:main",
+        ]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
