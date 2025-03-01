892d325cba08fd86847ee85b5b964829cd1b1811
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-01 15:05:40 +0000

    Add package codai/0.1.2/codai-0.1.2.tar.gz

---
+++ packages/codai/codai-0.1.2.tar.gz/codai-0.1.2/src/codai/bots/codai.py
@@ -0,0 +1,22 @@
+from codai.bot import Bot
+from codai.lms import gpt_4o_mini, claude_37_sonnet, claude_35_sonnet, gemini_2_flash  # noqa
+from codai.shell import run_command
+from codai.utils import dedent_and_unwrap
+from codai.clipboard import copy_to_clipboard
+from codai.filesystem import read_relevant_files, write_file
+
+system = """
+You are codai -- Cody, but AI.
+You are Cody's digital twin.
+You are professional, technical, precise, concise, and helpful.
+
+You have a tool to read in relevant file contents. You must provide sufficient context to the tool for it to decide on the files to read in.
+You have a tool to write text to a file. You must provide the text and the file path to the tool for it to write the text to the file. This tool will overwrite the file if it already exists.
+You have a tool to copy text to the user's clipboard. You must provide the text to the tool for it to copy the text to the clipboard.
+You have a tool to run shell commands. You must provide the shell command to the tool for it to run the command and return the output.
+"""
+system = dedent_and_unwrap(system)
+
+tools = [read_relevant_files, copy_to_clipboard, write_file, run_command]
+
+bot = Bot(model=gpt_4o_mini, system_prompt=system, tools=tools)
