58a1e5787f4b09b3ef6de0aa3c5dfe56616d0cf9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-01 03:15:41 +0000

    Add package smsmobileapi-send-email/0.3.3/smsmobileapi_send_email-0.3.3.tar.gz

---
+++ packages/smsmobileapi-send-email/smsmobileapi_send_email-0.3.3.tar.gz/smsmobileapi_send_email-0.3.3/tests/test_email_sender.py
@@ -0,0 +1,18 @@
+from smsmobileapi_send_email import SMSMobileAPIEmailSender
+
+apikey = "9e5b417fb687ac20c80c631f0c8d457c072da4a8bf16fbed"
+apikeybox = "e89606ab468ad76079787a27effa441c5b48a43e6eacbc4fbe275307a45229da"
+
+sender = SMSMobileAPIEmailSender(apikey, apikeybox)
+
+response = sender.send_email(
+    sender_name="SMSMobileAPI",
+    sender_email="info@smtpsecure.smsmobileapi.com",
+    recipient_email="info@ebernimont.be",
+    mail_subject="Test Email",
+    mail_body="Hello, this is a test email!",
+    allow_self_signed="yes", 
+    reply_email = "info@smsmobileapi.com"
+)
+
+print(response)
