7c29aed2cd2f0cdcfd1324790e1c11024a05e2cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-01 16:05:25 +0000

    Add package iputil/0.3.2/iputil-0.3.2.tar.gz

---
+++ packages/iputil/iputil-0.3.2.tar.gz/iputil-0.3.2/iputil/api.py
@@ -0,0 +1,30 @@
+import iputil
+
+
+def set_region_source(data_file):
+    region_db.initDatabase(dbfile=data_file)
+
+
+def get_region(ip):
+    if not valid_ip(ip):
+        return ""
+    try:
+        return iputil.region_db.search(ip)
+    except Exception:
+        return ""
+
+
+def valid_ip(ip):
+    p = ip.split(".")
+
+    if len(p) != 4:
+        return False
+    for pp in p:
+        if not pp.isdigit():
+            return False
+        if len(pp) > 3:
+            return False
+        if int(pp) > 255:
+            return False
+
+    return True
