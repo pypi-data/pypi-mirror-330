08c723bc9f676aca8da0de306ed6f384b485c5b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-02 04:06:08 +0000

    Add package maleo-core/0.1.9/maleo_core-0.1.9-py3-none-any.whl

---
+++ packages/maleo-core/maleo_core-0.1.9-py3-none-any.whl/maleo_core/models/__init__.py
@@ -0,0 +1,2 @@
+from . import schemas as BaseSchemas
+from . import transfers as BaseTransfers
---
+++ packages/maleo-core/maleo_core-0.1.9-py3-none-any.whl/maleo_core/models/transfers/__init__.py
@@ -0,0 +1,3 @@
+from . import payload as Payload
+from . import parameter as Parameter
+from . import result as Result
