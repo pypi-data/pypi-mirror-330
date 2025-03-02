fdae6e6fab12c618a0f7a665bd65dc74d5238bc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-02 14:05:19 +0000

    Add package openapi-wmata/1.0.1/openapi_wmata-1.0.1-py3-none-any.whl

---
+++ packages/openapi-wmata/openapi_wmata-1.0.1-py3-none-any.whl/openapi-wmata/api/__init__.py
@@ -0,0 +1,7 @@
+# flake8: noqa
+
+# import apis into api package
+from openapi-wmata.api.rail_api import RailApi
+from openapi-wmata.api.stations_api import StationsApi
+from openapi-wmata.api.trains_api import TrainsApi
+
