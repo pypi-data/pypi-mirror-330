3883728f74b09fd264a942e7dd9daf8f127d82ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-02 15:05:26 +0000

    Add package mom-scrape/0.0.1/mom_scrape-0.0.1.tar.gz

---
+++ packages/mom-scrape/mom_scrape-0.0.1.tar.gz/mom_scrape-0.0.1/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="mom_scrape",
+    version="0.0.1",
+    author="yxneo",
+    author_email="yuxuanneo@gmail.com",
+    description="Script to parse MOM's website for report and stats updates",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
