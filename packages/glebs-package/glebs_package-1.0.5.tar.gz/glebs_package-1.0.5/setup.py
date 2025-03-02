161c1a232bee4afc5b634604b3044aefd6dac3b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-02 03:13:31 +0000

    Add package glebs-package/1.0.5/glebs_package-1.0.5.tar.gz

---
+++ packages/glebs-package/glebs_package-1.0.5.tar.gz/glebs_package-1.0.5/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="glebs_package",
+    version="1.0.5",
+    packages=find_packages(),
+    install_requires=[],  # Add dependencies if needed
+    python_requires=">=3.7",
+)
