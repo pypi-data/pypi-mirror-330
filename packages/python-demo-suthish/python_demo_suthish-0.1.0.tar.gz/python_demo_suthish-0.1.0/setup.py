9a1c7ab132326652a5b6febf64d4831b0754ed5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-02 03:13:34 +0000

    Add package python-demo-suthish/0.1.0/python_demo_suthish-0.1.0.tar.gz

---
+++ packages/python-demo-suthish/python_demo_suthish-0.1.0.tar.gz/python_demo_suthish-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="python-demo-suthish",
+    version="0.1.0",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    install_requires=[
+        "requests>=2.25.0",
+    ],
+)
