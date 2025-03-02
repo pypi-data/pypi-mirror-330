8e83b3b47548db323f86e3b546699e29be8ff406
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-02 15:05:50 +0000

    Add package nonebot-plugin-sanyao/0.3.1/nonebot_plugin_sanyao-0.3.1.tar.gz

---
+++ packages/nonebot-plugin-sanyao/nonebot_plugin_sanyao-0.3.1.tar.gz/nonebot_plugin_sanyao-0.3.1/nonebot_plugin_sanyao/__init__.py
@@ -0,0 +1,15 @@
+from nonebot.plugin import PluginMetadata
+
+from .config import Config
+from . import __main__
+
+__version__ = "0.3.1"
+__plugin_meta__ = PluginMetadata(
+    name="三爻易数",
+    description="三爻易数排盘",
+    usage="",
+    type="application",
+    homepage="https://github.com/afterow/nonebot-plugin-sanyao",
+    config=Config,
+    supported_adapters=None,
+)
