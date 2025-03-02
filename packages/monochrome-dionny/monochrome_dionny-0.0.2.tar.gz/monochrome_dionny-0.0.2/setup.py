05c362433024e067ee052057ddda30992c75b2bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-02 03:13:54 +0000

    Add package monochrome-dionny/0.0.2/monochrome_dionny-0.0.2.tar.gz

---
+++ packages/monochrome-dionny/monochrome_dionny-0.0.2.tar.gz/monochrome_dionny-0.0.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="monochrome_dionny",
+    version="0.0.2",
+    author="Dionny",
+    description="Transformar as cores de uma imagem em tons de cinza ou binarizadas em preto e branco",
+    long_description=open("README.md", "r", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=[
+        "matplotlib",
+        "numpy",
+        "pillow",
+    ],
+)
