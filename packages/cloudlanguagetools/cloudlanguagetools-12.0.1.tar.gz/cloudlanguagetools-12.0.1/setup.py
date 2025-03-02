e6673afb0cd7f5df0615592f84d9b9c1963bdfed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-02 05:05:17 +0000

    Add package cloudlanguagetools/12.0.1/cloudlanguagetools-12.0.1.tar.gz

---
+++ packages/cloudlanguagetools/cloudlanguagetools-12.0.1.tar.gz/cloudlanguagetools-12.0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+from setuptools.command.install import install
+
+# build instructions
+# python setup.py sdist
+# twine upload dist/*
+
+setup(name='cloudlanguagetools',
+      version='12.0.1',
+      description='Interface with various cloud APIs for language processing such as translation, text to speech',
+      long_description=open('README.rst', encoding='utf-8').read(),
+      url='https://github.com/Language-Tools/cloud-language-tools-core',
+      author='Luc',
+      author_email='languagetools@mailc.net',
+      classifiers=[
+        'Programming Language :: Python :: 3.11',
+        'Topic :: Text Processing :: Linguistic',
+      ],      
+      license='GPL',
+      packages=['cloudlanguagetools'],
+      install_requires=[
+          'clt_requirements>=1.7',
+      ],
+      )
