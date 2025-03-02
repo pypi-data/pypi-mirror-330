c6de5b8e245c02eca24d765f02adbf3e507c0a43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-02 04:06:28 +0000

    Add package PyQt6Ads/4.3.1/pyqt6ads-4.3.1.tar.gz

---
+++ packages/PyQt6Ads/pyqt6ads-4.3.1.tar.gz/pyqt6ads-4.3.1/README.md
@@ -0,0 +1,5 @@
+# PyQt6Ads
+
+PyQt6 Bindings for (the amazing) [Qt-Advanced-Docking-System](https://github.com/githubuser0xFFFF/Qt-Advanced-Docking-System)
+
+[![Video Advanced Docking](https://github.com/githubuser0xFFFF/Qt-Advanced-Docking-System/blob/483bb7354ace8f07b76f483ec167e84467ac3977/doc/advanced-docking_video.png)](https://www.youtube.com/watch?v=7pdNfafg3Qc)
---
+++ packages/PyQt6Ads/pyqt6ads-4.3.1.tar.gz/pyqt6ads-4.3.1/tests/test_examples.py
@@ -0,0 +1,26 @@
+from pathlib import Path
+from pdb import run
+import runpy
+from unittest.mock import patch
+
+import pytest
+from PyQt6.QtWidgets import QApplication
+
+
+HERE = Path(__file__)
+EXAMPLES = HERE.parent.parent / "examples"
+MAINS = EXAMPLES.rglob("main.py")
+
+app = QApplication.instance() or QApplication([])
+
+
+@pytest.mark.parametrize(
+    "main", MAINS, ids=lambda path: str(path.parent.relative_to(EXAMPLES))
+)
+def test_example(main: Path) -> None:
+    with patch("PyQt6.QtWidgets.QApplication"):
+        runpy.run_path(str(main), run_name="__main__")
+        for widget in QApplication.topLevelWidgets():
+            widget.close()
+            widget.deleteLater()
+        QApplication.processEvents()
