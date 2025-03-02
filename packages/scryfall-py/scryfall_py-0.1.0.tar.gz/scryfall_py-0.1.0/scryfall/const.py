ba2ebafc92cf6e8b3f868870f865f11d669e1e99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-02 15:05:26 +0000

    Add package scryfall-py/0.1.0/scryfall_py-0.1.0.tar.gz

---
+++ packages/scryfall-py/scryfall_py-0.1.0.tar.gz/scryfall_py-0.1.0/scryfall/const.py
@@ -0,0 +1,12 @@
+import logging
+
+__version__ = "0.1.0"
+
+logger_name = "scryfall"
+
+_logger = logging.getLogger(logger_name)
+
+
+def get_logger() -> logging.Logger:
+    global _logger
+    return _logger
