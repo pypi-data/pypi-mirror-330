78d08050647af5716d179246d2b20407e1ec34c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-02 04:06:00 +0000

    Add package pynetbox-api/0.0.1/pynetbox_api-0.0.1-py3-none-any.whl

---
+++ packages/pynetbox-api/pynetbox_api-0.0.1-py3-none-any.whl/pynetbox_api/database.py
@@ -0,0 +1,27 @@
+from typing import Annotated
+
+from fastapi import Depends
+from sqlmodel import Field, Session, SQLModel, create_engine
+
+sqlite_file_name = 'database.db'
+sqlite_url = f'sqlite:///{sqlite_file_name}'
+
+connect_args = {'check_same_thread': False}
+engine = create_engine(sqlite_url, connect_args=connect_args)
+
+class NetBoxEndpoint(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    name: str = Field(index=True)
+    ip_address: str = Field(index=True)
+    port: int = Field(default=443)
+    token: str = Field()
+    verify_ssl: bool = Field(default=True)
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+SessionDep = Annotated[Session, Depends(get_session)]
