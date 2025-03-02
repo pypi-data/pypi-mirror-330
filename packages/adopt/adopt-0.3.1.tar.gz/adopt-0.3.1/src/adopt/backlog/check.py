585a85a71570d30dfbaba0205af74adb05014c3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-02 04:06:34 +0000

    Add package adopt/0.3.1/adopt-0.3.1.tar.gz

---
+++ packages/adopt/adopt-0.3.1.tar.gz/adopt-0.3.1/src/adopt/backlog/check.py
@@ -0,0 +1,3 @@
+# TODO: ensure that parent items are in the same iteration as their children
+# TODO: ensure that the (parent) item is active when the (child) item is in the iteration
+# TODO: ensure that the parent item is in the backlog when there is an active child item
