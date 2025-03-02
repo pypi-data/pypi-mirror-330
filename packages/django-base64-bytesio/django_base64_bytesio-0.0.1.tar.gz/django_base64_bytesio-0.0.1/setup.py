305dab7aefd444e7e902dcfd3849a05ea7cd8955
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-02 03:13:32 +0000

    Add package django-base64-bytesio/0.0.1/django_base64_bytesio-0.0.1.tar.gz

---
+++ packages/django-base64-bytesio/django_base64_bytesio-0.0.1.tar.gz/django_base64_bytesio-0.0.1/setup.py
@@ -0,0 +1,36 @@
+'''
+Author: Bryan x23399937@student.ncirl.ie
+Date: 2025-03-01 12:34:59
+LastEditors: Bryan x23399937@student.ncirl.ie
+LastEditTime: 2025-03-01 12:57:00
+FilePath: /cpp-project-library/setup.py
+Description: 
+setup python file
+Copyright (c) 2025 by Bryan Jiang, All Rights Reserved. 
+'''
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="django-base64-bytesio",
+    # Replace with your own username above
+    version="0.0.1",
+    author="Bryan Jiang",
+    author_email="x23399937@student.ncirl.ie",
+    description="Input a base64 string and return BytesIO",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/BryanJiang-NCI/django-base64-bytesio",
+    packages=setuptools.find_packages(),
+    # if you have libraries that your module/package/library
+    #you would include them in the install_requires argument
+    install_requires=[''],
+    classifiers=[
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
