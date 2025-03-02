ed0850658660fb7b0eac0d4c886014a94d0e668b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-02 03:13:26 +0000

    Add package qhchina/0.0.20/qhchina-0.0.20.tar.gz

---
+++ packages/qhchina/qhchina-0.0.20.tar.gz/qhchina-0.0.20/qhchina/analysis/__init__.py
@@ -0,0 +1,5 @@
+from .collocations import find_collocates, cooc_matrix
+from .corpora import compare_corpora
+from .vectors import (project_2d, 
+                      project_bias,
+                      cosine_similarity)
