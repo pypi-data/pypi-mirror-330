ab92f0bb52caa8622336b19b8d6597214a7cf902
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-02 03:13:26 +0000

    Add package caqtus-suite/6.23.1/caqtus_suite-6.23.1-py3-none-any.whl

---
+++ packages/caqtus-suite/caqtus_suite-6.23.1-py3-none-any.whl/caqtus/types/data/__init__.py
@@ -0,0 +1,7 @@
+"""This module define the type of data that can be saved for a shot."""
+
+from ._data_label import DataLabel, is_data_label
+from ._data_type import Data, Array, StructuredData
+from ._is_data import is_data
+
+__all__ = ["Data", "Array", "StructuredData", "is_data", "DataLabel", "is_data_label"]
