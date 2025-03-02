4563a315dd3a024699c3202cddba4334360e1012
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-02 03:13:24 +0000

    Add package rbmotd/3.0.1/rbmotd-3.0.1-py3-none-any.whl

---
+++ packages/rbmotd/rbmotd-3.0.1-py3-none-any.whl/rbmotd/__init__.py
@@ -0,0 +1,84 @@
+"""Message of the day extension for Review Board."""
+
+
+# The version of rbmotd.
+#
+# This is in the format of:
+#
+#   (Major, Minor, Micro, Patch, alpha/beta/rc/final, Release Number, Released)
+#
+VERSION = (3, 0, 1, 0, 'final', 0, True)
+
+
+def get_version_string():
+    """Return the version as a string.
+
+    Returns:
+        str:
+        The version number, formatted as a string.
+    """
+    major, minor, micro, patch, tag, relnum, is_release = VERSION
+
+    version = '%s.%s' % (major, minor)
+
+    if micro or patch:
+        version += '.%s' % micro
+
+        if patch:
+            version += '.%s' % patch
+
+        if tag != 'final':
+            if tag == 'rc':
+                version += ' RC'
+            else:
+                version += ' %s ' % tag
+
+            version += '%s' % relnum
+
+    if not is_release:
+        version += ' (dev)'
+
+    return version
+
+
+def get_package_version():
+    """Return the package version as a string.
+
+    Returns:
+        str:
+        The package version number, formatted as a string.
+    """
+    major, minor, micro, patch, tag, relnum = VERSION[:-1]
+
+    version = '%s.%s' % (major, minor)
+
+    if micro or patch:
+        version += '.%s' % micro
+
+        if patch:
+            version += '.%s' % patch
+
+    if tag != 'final':
+        version += '%s%s' % (
+            {
+                'alpha': 'a',
+                'beta': 'b',
+            }.get(tag, tag),
+            relnum)
+
+    return version
+
+
+def is_release():
+    """Return whether this is a final release.
+
+    Returns:
+        bool:
+        ``True`` if this version is a final release. ``False`` if built from
+        git or a prerelease.
+    """
+    return VERSION[-1]
+
+
+__version_info__ = VERSION[:-1]
+__version__ = get_package_version()
---
+++ packages/rbmotd/rbmotd-3.0.1-py3-none-any.whl/rbmotd/forms.py
@@ -0,0 +1,41 @@
+"""Forms for the rbmotd extension."""
+
+import hashlib
+
+from django import forms
+from django.utils.translation import gettext as _
+from djblets.extensions.forms import SettingsForm
+
+
+class MotdSettingsForm(SettingsForm):
+    """The settings form for the motd extension."""
+
+    enabled = forms.BooleanField(initial=False, required=False)
+    message = forms.CharField(
+        max_length=512,
+        required=False,
+        help_text=_('This field expects valid HTML. Entities must be '
+                    'properly escaped.'),
+        widget=forms.TextInput(attrs={
+            'size': 100,
+        }))
+
+    def set_key_value(self, key, value):
+        """Set the given value in the extension settings.
+
+        This overrides the base settings form method so that when the
+        ``message`` key is set, it will additionally compute and store a
+        ``message_id`` key.
+
+        Args:
+            key (str):
+                The key to set.
+
+            value (object):
+                The value to set.
+        """
+        if key == 'message':
+            message_id = hashlib.sha256(value.encode('utf-8')).hexdigest()
+            self.set_key_value('message_id', message_id)
+
+        super(MotdSettingsForm, self).set_key_value(key, value)
