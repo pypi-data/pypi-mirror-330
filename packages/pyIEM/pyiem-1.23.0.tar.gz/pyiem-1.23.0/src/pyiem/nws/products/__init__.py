88b234b01df265721971cb1030323bca23110fcf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-02 05:05:12 +0000

    Add package pyIEM/1.23.0/pyiem-1.23.0.tar.gz

---
+++ packages/pyIEM/pyiem-1.23.0.tar.gz/pyiem-1.23.0/src/pyiem/nws/products/__init__.py
@@ -0,0 +1,90 @@
+"""A generalized parser frontend."""
+
+from __future__ import absolute_import
+
+from typing import Optional, Union
+
+from pyiem.nws.ugc import UGCProvider
+
+
+def parser(
+    text,
+    utcnow=None,
+    ugc_provider: Optional[Union[UGCProvider, dict]] = None,
+    nwsli_provider=None,
+):
+    """Omnibus parser of NWS Text Data
+
+    This is intended to be a catch-all parser of text data.  As it currently
+    stands, it does not correctly hand products off to the correct
+    sub-processor, but some day it will!
+
+    Args:
+      text (str): The actual product text, this can have the <cntr>-a
+        character to start the string.
+      utcnow (datetime, optional): What is the current time, this is useful
+        for when ingesting old data.  Many times, the product does not contain
+        enough information to assign a current valid timestamp to it.  So we
+        need to know the current timestamp to do the relative computation.
+      ugc_provider (UGCProvider, optional): Provides UGC information for
+        product parsing.
+      nwsli_provider (dict, optional): Provides NWS Location Identifiers to
+        allow lookup of geographic information for station identifiers.
+
+    Returns:
+      TextProduct: A TextProduct instance
+
+    """
+    from pyiem.nws.product import (
+        AFOSRE,
+        TextProduct,
+        TextProductException,
+    )
+    from pyiem.wmo import WMO_RE
+
+    from . import (
+        cli,
+        ero,
+        hwo,
+        lsr,
+        mcd,
+        nhc,
+        saw,
+        sel,
+        spacewx,
+        spcpts,
+        sps,
+        taf,
+        wwp,
+    )
+
+    XREF = {
+        "CLI": cli.parser,
+        "FFG": mcd.parser,
+        "HWO": hwo.parser,
+        "LSR": lsr.parser,
+        "NHC": nhc.parser,
+        "PFW": spcpts.parser,
+        "PTS": spcpts.parser,
+        "RBG": ero.parser,
+        "SAW": saw.parser,
+        "SEL": sel.parser,
+        "SPS": sps.parser,
+        "SWO": mcd.parser,
+        "TAF": taf.parser,
+        "TCP": nhc.parser,
+        "WWP": wwp.parser,
+    }
+    tmp = text[:100].replace("\r\r\n", "\n")
+    m = WMO_RE.search(tmp)
+    if m is not None:
+        d = m.groupdict()
+        if d["cccc"] == "KWNP":
+            return spacewx.parser(text, utcnow, ugc_provider, nwsli_provider)
+
+    tokens = AFOSRE.findall(tmp)
+    if not tokens:
+        raise TextProductException("Could not locate AFOS Identifier")
+    afos = tokens[0]
+    func = XREF.get(afos[:3], TextProduct)
+    return func(text, utcnow, ugc_provider, nwsli_provider)
---
+++ packages/pyIEM/pyiem-1.23.0.tar.gz/pyiem-1.23.0/tests/plot/test_plot_utils.py
@@ -0,0 +1,72 @@
+"""Test pyiem.plot.utils."""
+
+import pytest
+
+from pyiem.plot.util import centered_bins, draw_logo, fitbox, pretty_bins
+
+
+def test_gh871_not_a_zero_in_middle():
+    """Test that the middle value is actually a zero."""
+    bins = centered_bins(0.049999997, bins=10)
+    assert bins[5] == 0.0
+
+
+def test_240320_pretty_bins():
+    """Test that our middle value is nice."""
+    res = pretty_bins(0, 14.876)
+    assert abs(res[4] - 8) < 0.001
+
+
+def test_pretty_bins_large_value():
+    """Test a value larger than we can support."""
+    with pytest.raises(ValueError) as exc_info:
+        pretty_bins(-1e9, 1e9)
+    assert exc_info.match("^step:")
+
+
+def test_gh665_pretty_bins_index_error():
+    """Test that this works."""
+    a = pretty_bins(21.5, 9886.5)
+    assert abs(a[0] - 0) < 0.01
+
+
+def test_pretty_bins():
+    """Test that we get nice pretty bins!"""
+    a = pretty_bins(-1, 10)
+    assert abs(a[-1] - 10.0) < 0.01
+
+
+def test_centered_bins_orig():
+    """See that we can compute some nice centered bins"""
+    a = centered_bins(10)
+    assert a[0] == -10
+    a = centered_bins(55)
+    assert a[0] == -75
+    a = centered_bins(99)
+    assert a[0] == -100
+    a = centered_bins(99, bins=9)
+    assert a[0] == -100
+    a = centered_bins(100, on=100)
+    assert a[0] == 0
+    a = centered_bins(0.9)
+    assert abs(a[-1] - 1.0) < 0.001
+    a = centered_bins(1.2888)
+    assert abs(a[-1] - 1.5) < 0.001
+
+
+def test_centered_bins():
+    """Test that we get what we want from centered bins."""
+    res = centered_bins(10, 0, 6)  # actually get back 5 bins
+    assert abs(res[0] - -10) < 0.01
+    assert abs(res[2] - 0) < 0.01
+    assert abs(res[-1] - 10) < 0.01
+
+
+def test_none_fitbox():
+    """Test that we can give fitbox a None value."""
+    assert fitbox(None, None, 0, 1, 2, 3) is None
+
+
+def test_none_drawlogo():
+    """Test that nothing happens when we pass a None logo."""
+    assert draw_logo(None, None) is None
