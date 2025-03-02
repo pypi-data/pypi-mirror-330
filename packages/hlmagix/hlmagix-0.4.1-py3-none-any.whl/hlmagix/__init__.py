962798c39bb3e88a3b7de047a25edaefa525076f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-02 03:13:44 +0000

    Add package hlmagix/0.4.1/hlmagix-0.4.1-py3-none-any.whl

---
+++ packages/hlmagix/hlmagix-0.4.1-py3-none-any.whl/hlmagix/__init__.py
@@ -0,0 +1,8 @@
+"""
+HLMagix - Python toolset including calculator, models and utilities
+"""
+from . import hlcalculator
+from . import models
+from . import utils
+
+__version__ = "0.4.0"
---
+++ packages/hlmagix/hlmagix-0.4.1-py3-none-any.whl/hlmagix/hlcalculator/__main__.py
@@ -0,0 +1,7 @@
+"""
+Enables running the package as a module: python -m hlcalculator
+"""
+from hlcalculator.cli import main
+
+if __name__ == '__main__':
+    main()
