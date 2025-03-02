b126c621864f887f86116bac5514d83a1550f7b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-02 15:05:22 +0000

    Add package qqmusic-api-python/0.3.1/qqmusic_api_python-0.3.1.tar.gz

---
+++ packages/qqmusic-api-python/qqmusic_api_python-0.3.1.tar.gz/qqmusic_api_python-0.3.1/tests/test_songlist.py
@@ -0,0 +1,14 @@
+import pytest
+
+from qqmusic_api import songlist
+
+pytestmark = pytest.mark.asyncio(loop_scope="session")
+
+
+async def test_get_detail():
+    assert await songlist.get_detail(
+        9069454203,
+        onlysong=False,
+        tag=True,
+        userinfo=True,
+    )
