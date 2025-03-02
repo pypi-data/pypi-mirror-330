60d4d4e5ad3542152aae79c309add81b64613ee0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-02 04:06:05 +0000

    Add package blendpy/25.3.0/blendpy-25.3.0-py3-none-any.whl

---
+++ packages/blendpy/blendpy-25.3.0-py3-none-any.whl/blendpy/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# file: __init__.py
+
+from .alloy import Alloy
+from .blendpy import DSIModel 
+from .blendpy import version
+from .polymorph import Polymorph
+
+__version__ = '25.3.0'
