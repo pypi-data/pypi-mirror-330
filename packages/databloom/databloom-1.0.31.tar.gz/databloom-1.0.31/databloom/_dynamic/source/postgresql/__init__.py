b70800c4dea6dd7d5a3025f1e439417ca4a43696
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-02 15:05:54 +0000

    Add package databloom/1.0.31/databloom-1.0.31.tar.gz

---
+++ packages/databloom/databloom-1.0.31.tar.gz/databloom-1.0.31/databloom/_dynamic/source/postgresql/__init__.py
@@ -0,0 +1,31 @@
+# --- render code block -----
+
+from .bumdata import bumdata
+from .marketing_source2 import marketing_source2
+from .hainv4test2 import hainv4test2
+from .hainv4_source import hainv4_source
+from .neww222 import neww222
+from .chinhtt_marketing import chinhtt_marketing
+from .hainv4sourceabc import hainv4sourceabc
+from .test_source import test_source
+
+__all__ = [
+    
+    "bumdata",
+    
+    "marketing_source2",
+    
+    "hainv4test2",
+    
+    "hainv4_source",
+    
+    "neww222",
+    
+    "chinhtt_marketing",
+    
+    "hainv4sourceabc",
+    
+    "test_source",
+    
+]
+# --- render code block -----
