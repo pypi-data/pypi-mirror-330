bde3357f4346c91e7f9aef7a8585225799ad96bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-02 05:05:17 +0000

    Add package displayhooks/0.0.3/displayhooks-0.0.3.tar.gz

---
+++ packages/displayhooks/displayhooks-0.0.3.tar.gz/displayhooks-0.0.3/tests/test_autorestore.py
@@ -0,0 +1,36 @@
+import sys
+
+import pytest
+import full_match
+
+from displayhooks import autorestore_displayhook
+
+
+def test_restore():
+    hook_before_declaration = sys.displayhook
+    @autorestore_displayhook
+    def do_something():
+        sys.displayhook = 5
+
+    hook_before_calling = sys.displayhook
+
+    do_something()
+
+    assert hook_before_declaration is sys.displayhook
+    assert hook_before_calling is sys.displayhook
+
+
+def test_restore_after_exception():
+    hook_before_declaration = sys.displayhook
+    @autorestore_displayhook
+    def do_something():
+        sys.displayhook = 5
+        raise ValueError('message')
+
+    hook_before_calling = sys.displayhook
+
+    with pytest.raises(ValueError, match=full_match('message')):
+        do_something()
+
+    assert hook_before_declaration is sys.displayhook
+    assert hook_before_calling is sys.displayhook
