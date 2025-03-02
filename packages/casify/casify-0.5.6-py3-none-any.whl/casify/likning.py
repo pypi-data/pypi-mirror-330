84a6649c108c74b225dbba8c8cc776f183e81aa4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-02 03:13:51 +0000

    Add package casify/0.5.6/casify-0.5.6-py3-none-any.whl

---
+++ packages/casify/casify-0.5.6-py3-none-any.whl/casify/likning.py
@@ -0,0 +1,51 @@
+from .equation import solve
+
+
+def nløs(*likninger):
+    løsning = solve(*likninger, numerical=True)
+    if løsning == "No solution":
+        return "Ingen løsning"
+    else:
+        return løsning
+
+
+def løs(*likninger):
+    """Løser én eller flere likninger (et likningssystem), eller én ulikhet.
+
+    Args:
+        *likninger (str): Et variabelt antall likninger eller ulikheter separert med komma som representerer likningene eller ulikheten.
+        pprint (bool): Hvis `True` gir en matematisk tekststreng-representasjon av løsningen. Standardverdi: `True`.
+
+    Returns:
+        str eller list: En tekststreng-representasjon (str) av løsningen(e) hvis `pprint` er `True`. Hvis ikke en liste med dictionaries som inneholder løsningene. Returnerer "Ingen løsning" hvis ingen reelle løsninger finnes.
+
+    Eksempler:
+        >>> from casify import *
+        >>> løs("x**2 - x - 6 = 0")
+        'x = -2    ∨    x = 3'
+        >>> løs("x + y - z = 1", "x + y + 2*z = 3", "-x + y + z = -1")
+        'x = 5/3 ∧ y = 0 ∧ z = 2/3'
+
+        >>> f = funksjon("a * x**2 + b*x + c")
+        >>> løs("f(1) = 2", "f(-1) = 3", "f(3) = 4")
+        'a = 3/8 ∧ b = -1/2 ∧ c = 17/8'
+
+        >>> f = funksjon("x**2 - x - 2")
+        >>> løs("f(x) = 0")
+        'x = -1    ∨    x = 2'
+        >>> løs("f(x) > 0")
+        '(-∞ < x ∧ x < -1) ∨ (2 < x ∧ x < ∞)'
+        >>> løs("f(x) <= 0")
+         '-1 ≤ x ∧ x ≤ 2'
+
+    """
+    løsning = solve(*likninger)
+    if løsning == "No solution":
+        return "Ingen løsning"
+    else:
+        return løsning
+
+
+def Løs(*likninger):
+    """Alternativ skrivemåte for `løs`."""
+    return løs(*likninger)
