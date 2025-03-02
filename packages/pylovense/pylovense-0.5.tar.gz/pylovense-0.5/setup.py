b3efeacbad3f4eb2cd1f51e69835c6801995f90a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-02 03:13:40 +0000

    Add package pylovense/0.5/pylovense-0.5.tar.gz

---
+++ packages/pylovense/pylovense-0.5.tar.gz/pylovense-0.5/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='pylovense',
+    version='0.5',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+    ],
+    author='jinxed-catgirl',
+    description='A python wrapper for the Lovense API',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.12',
+    keywords='lovense api wrapper',
+)
