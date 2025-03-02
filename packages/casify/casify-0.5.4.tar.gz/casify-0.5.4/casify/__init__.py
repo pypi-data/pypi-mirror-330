e74370881f6c3033a7efc83c450ed10048b50f87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-02 03:13:45 +0000

    Add package casify/0.5.4/casify-0.5.4.tar.gz

---
+++ packages/casify/casify-0.5.4.tar.gz/casify-0.5.4/casify/__init__.py
@@ -0,0 +1,55 @@
+from .equation import solve, Solve
+from .likning import løs, Løs
+from .function import function, Function
+from .funksjon import funksjon, Funksjon
+from .algebra import (
+    expand,
+    factor,
+    utvid,
+    Utvid,
+    faktoriser,
+    Faktoriser,
+    div,
+    Div,
+    polynomdivisjon,
+    Polynomdivisjon,
+)
+from .regression import make_model
+from .regresjon import lag_modell
+
+from .vector import vector, Vector2d
+
+from .vektor import vektor, Vektor2d, vinkel
+
+from . import abc
+
+from . import printing
+
+
+__all__ = [
+    "solve",
+    "løs",
+    "Solve",
+    "Løs",
+    "function",
+    "Function",
+    "funksjon",
+    "Funksjon",
+    "expand",
+    "factor",
+    "utvid",
+    "Utvid",
+    "faktoriser",
+    "Faktoriser",
+    "div",
+    "Div",
+    "polynomdivisjon",
+    "Polynomdivisjon",
+    "vector",
+    "Vector2d",
+    "vektor",
+    "Vektor2d",
+    "vinkel",
+    "lag_modell",
+    "make_model",
+]
