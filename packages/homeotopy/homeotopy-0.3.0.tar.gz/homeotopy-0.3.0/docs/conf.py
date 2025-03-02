c1b9bacfc87d6f7acc25493e89116eb9d47c4878
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-02 03:13:57 +0000

    Add package homeotopy/0.3.0/homeotopy-0.3.0.tar.gz

---
+++ packages/homeotopy/homeotopy-0.3.0.tar.gz/homeotopy-0.3.0/docs/conf.py
@@ -0,0 +1,25 @@
+import sys
+import tomllib
+from datetime import date
+from os import path
+
+sys.path.append(path.abspath(".."))
+
+with open("../pyproject.toml", "rb") as fh:
+    toml = tomllib.load(fh)
+
+extensions = [
+    "sphinx.ext.coverage",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "myst_parser",
+]
+
+pyproject = toml["project"]
+
+project = pyproject["name"]
+version = pyproject["version"]
+release = pyproject["version"]
+
+copyright = f"{date.today().year:d} Erik Brinkman"
+author = "Erik Brinkman"
