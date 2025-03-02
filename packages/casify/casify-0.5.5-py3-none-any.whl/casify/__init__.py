ad2c5bb7d0914f9e6c07a8b630fd6853c7ad5ccf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-02 03:13:50 +0000

    Add package casify/0.5.5/casify-0.5.5-py3-none-any.whl

---
+++ packages/casify/casify-0.5.5-py3-none-any.whl/casify/__init__.py
@@ -0,0 +1,56 @@
+from .equation import solve, Solve
+from .likning import løs, Løs, nløs
+from .function import function, Function
+from .funksjon import funksjon, Funksjon
+from .algebra import (
+    expand,
+    factor,
+    utvid,
+    Utvid,
+    faktoriser,
+    Faktoriser,
+    div,
+    Div,
+    polynomdivisjon,
+    Polynomdivisjon,
+)
+from .regression import make_model
+from .regresjon import lag_modell
+
+from .vector import vector, Vector2d
+
+from .vektor import vektor, Vektor2d, vinkel
+
+from . import abc
+
+from . import printing
+
+
+__all__ = [
+    "solve",
+    "løs",
+    "nløs",
+    "Solve",
+    "Løs",
+    "function",
+    "Function",
+    "funksjon",
+    "Funksjon",
+    "expand",
+    "factor",
+    "utvid",
+    "Utvid",
+    "faktoriser",
+    "Faktoriser",
+    "div",
+    "Div",
+    "polynomdivisjon",
+    "Polynomdivisjon",
+    "vector",
+    "Vector2d",
+    "vektor",
+    "Vektor2d",
+    "vinkel",
+    "lag_modell",
+    "make_model",
+]
