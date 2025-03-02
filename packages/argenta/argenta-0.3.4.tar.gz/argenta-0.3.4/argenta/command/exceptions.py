e159baa5c0649ebb35e1a25f8c57c4d392e42f1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-02 05:05:17 +0000

    Add package argenta/0.3.4/argenta-0.3.4.tar.gz

---
+++ packages/argenta/argenta-0.3.4.tar.gz/argenta-0.3.4/argenta/command/exceptions.py
@@ -0,0 +1,39 @@
+from .params.flag.entity import Flag
+
+
+class InvalidCommandInstanceException(Exception):
+    def __str__(self):
+        return "Invalid Command Instance"
+
+
+class InvalidDescriptionInstanceException(Exception):
+    def __str__(self):
+        return "Invalid Description Instance"
+
+
+class InvalidFlagsInstanceException(Exception):
+    def __str__(self):
+        return "Invalid Flags Instance"
+
+
+class UnprocessedInputFlagException(Exception):
+    def __str__(self):
+        return "Unprocessed Input Flags"
+
+
+class RepeatedInputFlagsException(Exception):
+    def __init__(self, flag: Flag):
+        self.flag = flag
+    def __str__(self):
+        return ("Repeated Input Flags\n"
+                f"Duplicate flag was detected in the input: '{self.flag.get_string_entity()}'")
+
+
+class IncorrectNumberOfHandlerArgsException(Exception):
+    def __str__(self):
+        return "Incorrect Input Flags Handler has incorrect number of arguments"
+
+
+class EmptyInputCommandException(Exception):
+    def __str__(self):
+        return "Input Command is empty"
