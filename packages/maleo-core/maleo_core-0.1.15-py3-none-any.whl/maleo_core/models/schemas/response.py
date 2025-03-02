9a38dc6f42a32b8baf326b15dcfff81377095af2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-02 05:05:17 +0000

    Add package maleo-core/0.1.15/maleo_core-0.1.15-py3-none-any.whl

---
+++ packages/maleo-core/maleo_core-0.1.15-py3-none-any.whl/maleo_core/models/schemas/response.py
@@ -0,0 +1,26 @@
+from typing import Literal, Optional, Any
+from pydantic import BaseModel
+from ..transfers.payload import Pagination
+
+#* ----- ----- ----- Base ----- ----- ----- *#
+class Base(BaseModel):
+    success:Literal[True, False]
+    code:str
+    message:str
+    description:str
+
+#* ----- ----- ----- Derived ----- ----- ----- *#
+class Fail(Base):
+    success:Literal[False] = False
+    other:Optional[Any] = None
+
+class SingleData(Base):
+    success:Literal[True] = True
+    data:Any
+    other:Optional[Any] = None
+
+class MultipleData(Base):
+    success:Literal[True] = True
+    data:list[Any]
+    pagination:Pagination
+    other:Optional[Any] = None
