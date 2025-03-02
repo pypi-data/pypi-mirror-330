466a8aefc285f03fbae05f070342cb7cbb60faf8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-02 03:13:33 +0000

    Add package RockyRoad/0.0.444/RockyRoad-0.0.444-py3-none-any.whl

---
+++ packages/RockyRoad/RockyRoad-0.0.444-py3-none-any.whl/rockyroad/modules/warranty_assessments.py
@@ -0,0 +1,28 @@
+from .module_imports import key
+from uplink import (
+    Consumer,
+    get as http_get,
+    returns,
+    headers,
+    retry,
+    Query,
+)
+
+
+@headers({"Ocp-Apim-Subscription-Key": key})
+@retry(max_attempts=4)
+class _Warranty_Assessments(Consumer):
+    """Inteface to warranty assessments resource for the RockyRoad API."""
+
+    def __init__(self, Resource, *args, **kw):
+        self._base_url = Resource._base_url
+        super().__init__(base_url=Resource._base_url, *args, **kw)
+
+    @returns.json
+    @http_get("warranties/assessments")
+    def list(
+        self,
+        machine_uid: Query = None,
+        brand: Query = None,
+    ):
+        """This call will return warranty assessments information for the specified criteria."""
