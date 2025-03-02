ae93a4f1f5e4c77d5a8dd4b97dbe2570ab108772
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-02 05:05:14 +0000

    Add package findus/1.1.0/findus-1.1.0.tar.gz

---
+++ packages/findus/findus-1.1.0.tar.gz/findus-1.1.0/findus/__init__.py
@@ -0,0 +1,13 @@
+from findus.findus import Database
+from findus.findus import Serial
+from findus.findus import MicroPythonScript
+from findus.findus import PicoGlitcherInterface
+from findus.findus import Glitcher
+from findus.findus import PicoGlitcher
+from findus.findus import Helper
+
+#import sys
+#import os
+#print(os.path.dirname(os.path.abspath(__file__)))
+#sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), os.pardir))
+#sys.path.insert(0, "../../lib/")
