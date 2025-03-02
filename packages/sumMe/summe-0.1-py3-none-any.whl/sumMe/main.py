6e42b910b2d2bfff5bf6c40907346b9f6b8baff2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-02 03:13:23 +0000

    Add package sumMe/0.1/summe-0.1-py3-none-any.whl

---
+++ packages/sumMe/summe-0.1-py3-none-any.whl/sumMe/main.py
@@ -0,0 +1,11 @@
+class SumCore:
+    
+    def sum_nums(num_1:int, num_2:int) -> int:
+        return num_1 + num_2
+    
+    def del_nums(num_1:int, num_2:int) -> int:
+        return num_1 - num_2
+    
+        
+    def sub_nums(num_1:int, num_2:int) -> int:
+        return num_1** num_2
