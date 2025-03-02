8dc696b8db1412b05c2fd76414b510724e9cb95a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-02 04:06:02 +0000

    Add package git-draft/0.3.1/git_draft-0.3.1.tar.gz

---
+++ packages/git-draft/git_draft-0.3.1.tar.gz/git_draft-0.3.1/src/git_draft/__main__.py
@@ -0,0 +1,100 @@
+from __future__ import annotations
+
+import importlib.metadata
+import optparse
+import sys
+import textwrap
+
+from .assistants import load_assistant
+from .common import open_editor
+from .manager import Manager, enclosing_repo
+
+
+EPILOG = """\
+    More information via `man git-draft` and https://mtth.github.io/git-draft.
+"""
+
+
+parser = optparse.OptionParser(
+    prog="git-draft",
+    epilog=textwrap.dedent(EPILOG),
+    version=importlib.metadata.version("git_draft"),
+)
+
+parser.disable_interspersed_args()
+
+
+def add_command(name: str, **kwargs) -> None:
+    def callback(_option, _opt, _value, parser) -> None:
+        parser.values.command = name
+
+    parser.add_option(
+        f"-{name[0].upper()}",
+        f"--{name}",
+        action="callback",
+        callback=callback,
+        **kwargs,
+    )
+
+
+add_command("discard", help="discard all drafts associated with a branch")
+add_command("finalize", help="apply the current draft to the original branch")
+add_command("generate", help="draft a new change from a prompt")
+
+parser.add_option(
+    "-a",
+    "--assistant",
+    dest="ASSISTANT",
+    help="assistant key",
+)
+parser.add_option(
+    "-d",
+    "--delete",
+    help="delete the draft after finalizing or discarding",
+    action="store_true",
+)
+parser.add_option(
+    "-p",
+    "--prompt",
+    dest="prompt",
+    help="draft generation prompt, read from stdin if unset",
+)
+parser.add_option(
+    "-r",
+    "--reset",
+    help="reset index before generating a new draft",
+    action="store_true",
+)
+
+
+EDITOR_PLACEHOLDER = """\
+    Enter your prompt here...
+"""
+
+
+def main() -> None:
+    (opts, _args) = parser.parse_args()
+
+    repo = enclosing_repo()
+    manager = Manager(repo)
+
+    command = getattr(opts, "command", "generate")
+    if command == "generate":
+        assistant = load_assistant(opts.assistant, {})
+        prompt = opts.prompt
+        if not prompt:
+            if sys.stdin.isatty():
+                prompt = open_editor(textwrap.dedent(EDITOR_PLACEHOLDER))
+            else:
+                prompt = sys.stdin.read()
+        manager.generate_draft(prompt, assistant, reset=opts.reset)
+    elif command == "finalize":
+        manager.finalize_draft(delete=opts.delete)
+    elif command == "discard":
+        manager.discard_draft(delete=opts.delete)
+    else:
+        assert False, "unreachable"
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/git-draft/git_draft-0.3.1.tar.gz/git_draft-0.3.1/src/git_draft/assistants/__init__.py
@@ -0,0 +1,21 @@
+from typing import Any, Mapping
+
+from .common import Assistant, Session, Toolbox
+
+__all__ = [
+    "Assistant",
+    "Session",
+    "Toolbox",
+]
+
+
+def load_assistant(entry: str, kwargs: Mapping[str, Any]) -> Assistant:
+    if entry == "openai":
+        return _load_openai_assistant(**kwargs)
+    raise NotImplementedError()  # TODO
+
+
+def _load_openai_assistant(**kwargs) -> Assistant:
+    from .openai import OpenAIAssistant
+
+    return OpenAIAssistant(**kwargs)
