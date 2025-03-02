b1d8e3b0b1169f468aef0df964f1450fd364c11d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-02 15:05:46 +0000

    Add package funcnodes-pyodide/0.1.5/funcnodes_pyodide-0.1.5.tar.gz

---
+++ packages/funcnodes-pyodide/funcnodes_pyodide-0.1.5.tar.gz/funcnodes_pyodide-0.1.5/tests/test_import.py
@@ -0,0 +1,17 @@
+import pytest
+
+
+@pytest.mark.imports
+def test_import():
+    from funcnodes_pyodide import PyodideWorker, new_worker
+
+
+@pytest.mark.imports
+def test_patch():
+    from funcnodes_pyodide.patch import patch
+
+    patch()
+
+    from funcnodes_core import FUNCNODES_LOGGER
+
+    assert len(FUNCNODES_LOGGER.handlers) == 1
