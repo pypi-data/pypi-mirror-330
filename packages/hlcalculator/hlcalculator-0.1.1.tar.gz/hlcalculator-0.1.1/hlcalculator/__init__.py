981ef2d8f3b9358482ba777e5142f55bf1a839d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-02 03:14:03 +0000

    Add package hlcalculator/0.1.1/hlcalculator-0.1.1.tar.gz

---
+++ packages/hlcalculator/hlcalculator-0.1.1.tar.gz/hlcalculator-0.1.1/hlcalculator/__init__.py
@@ -0,0 +1,8 @@
+"""
+HLCalculator - An advanced command line calculator
+"""
+from .calculator import ScientificCalculator
+from .parser import parse_arguments
+
+__version__ = "0.1.0"
+__all__ = ['ScientificCalculator', 'parse_arguments']
