e7675bf8f40e13af7db2f8f4d24e5d34e4aaf7a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-02 04:06:52 +0000

    Add package conda-oci-mirror/0.0.0.1/conda-oci-mirror-0.0.0.1.tar.gz

---
+++ packages/conda-oci-mirror/conda-oci-mirror-0.0.0.1.tar.gz/conda-oci-mirror-0.0.0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name="conda-oci-mirror",
+    version="0.0.0.1",
+    author="",
+    author_email="",
+    description="A security place-holder package",
+    classifiers=[
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+    ]
+)
+
