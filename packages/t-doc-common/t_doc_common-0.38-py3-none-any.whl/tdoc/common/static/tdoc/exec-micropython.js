3720ae921938b137df06e41af9124022bc9a146f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-02 03:13:23 +0000

    Add package t-doc-common/0.38/t_doc_common-0.38-py3-none-any.whl

---
+++ packages/t-doc-common/t_doc_common-0.38-py3-none-any.whl/tdoc/common/static/tdoc/exec-micropython.js
@@ -0,0 +1,190 @@
+// Copyright 2025 Remy Blank <remy@c-space.org>
+// SPDX-License-Identifier: MIT
+
+import {dec, element, enc, text} from './core.js';
+import {Executor} from './exec.js';
+import {MicroPython} from './micropython.js';
+import {getSerials, onSerial, requestSerial} from './serial.js';
+
+class MicroPythonExecutor extends Executor {
+    static runner = 'micropython';
+    static highlight = 'python';
+
+    static async init() {}
+
+    constructor(node) {
+        super(node);
+        this.output = this.sectionedOutput();
+        this.console = this.output.consoleOut('990');
+        this.mp = new MicroPython((...args) => this.console.write(...args),
+                                  (...args) => this.onRelease(...args));
+    }
+
+    addControls(controls) {
+        if (this.when !== 'never') {
+            this.runCtrl = controls.appendChild(this.runControl());
+            controls.appendChild(this.toolsControl());
+            this.input = this.inputControl(data => this.mp.send(data + '\r\n'));
+        }
+        super.addControls(controls);
+    }
+
+    toolsControl() {
+        const ctrl = element(`
+<div class="dropstart">\
+<button class="tdoc fa-screwdriver-wrench" title="Tools"\
+ data-bs-toggle="dropdown" data-bs-offset="-7,4"></button>\
+<ul class="dropdown-menu"></ul>\
+</div>`);
+        const ul = ctrl.querySelector('ul');
+        ul.appendChild(this.menuItem('plug', 'Connect', '',
+                                     () => this.connect()));
+        ul.appendChild(this.menuItem(
+            'power-off', 'Hard reset', ' if-connected disabled',
+            () => this.reset()));
+        ul.appendChild(this.menuItem(
+            'file-arrow-down', 'Read <code>main.py</code>',
+            ' if-connected disabled', () => this.readMain()));
+        ul.appendChild(this.menuItem(
+            'file-arrow-up', 'Write to <code>main.py</code>',
+            ' if-connected disabled', () => this.writeMain()));
+        ul.appendChild(this.menuItem(
+            'trash', 'Remove <code>main.py</code>', ' if-connected disabled',
+            () => this.removeMain()));
+        return ctrl;
+    }
+
+    menuItem(icon, text, cls, onClick) {
+        const it = element(`\
+<li><a class="dropdown-item${cls}">\
+<span class="btn__icon-container tdoc fa-${icon}"></span>\
+<span class="btn__text-container">${text}</span>\
+</a></li>`);
+        it.querySelector('a').addEventListener('click', onClick);
+        return it;
+    }
+
+    inputControl(onSend) {
+        const {div} = this.output.lineInput('991', null, input => {
+            const value = input.value;
+            input.value = '';
+            onSend(value);
+        });
+        const stop = div.appendChild(this.stopControl());
+        stop.setAttribute('title', `${stop.getAttribute('title')} (Ctrl+C)`);
+        const input = div.querySelector('input');
+        input.addEventListener('keydown', e => {
+            if (e.ctrlKey && e.key === 'c' && !e.altKey && !e.metaKey
+                    && input.selectionStart === input.selectionEnd) {
+                e.preventDefault();
+                stop.click();
+            }
+        });
+        return div;
+    }
+
+    onReady() {
+        this.enableInput(false);
+        this.setSerial();
+        onSerial(this, {
+            onConnect: s => { if (!this.mp.serial) this.setSerial(s); },
+            onDisconnect: s => { if (this.mp.serial === s) this.setSerial(); }
+        });
+        const serials = getSerials();
+        if (serials.length === 1) this.setSerial(serials[0]);
+    }
+
+    enableInput(enable) {
+        enable = enable ?? this.mp.claim;
+        for (const el of this.input.querySelectorAll('input, button')) {
+            el.disabled = !enable;
+        }
+    }
+
+    setSerial(serial) {
+        this.mp.setSerial(serial);
+        this.runCtrl.disabled = !serial;
+        for (const el of this.node.querySelectorAll(
+                '.tdoc-exec-controls .dropdown-item.if-connected')) {
+            el.classList.toggle('disabled', !serial);
+        }
+    }
+
+    async connect() {
+        this.setSerial();
+        this.console.clear();
+        try {
+            this.setSerial(await requestSerial());
+            await this.mp.claimSerial(false);
+            this.enableInput();
+        } catch (e) {
+            if (e.name !== 'NotFoundError') {
+                this.console.write('err', `${e.toString()}\n`);
+            }
+        }
+    }
+
+    onRelease(reason) {
+        this.enableInput(false);
+        if (reason) this.console.write('err', `${reason}\n`);
+    }
+
+    async rawRepl(fn) {
+        this.console.clear();
+        this.enableInput(false);
+        try {
+            await this.mp.rawRepl(fn);
+        } catch (e) {
+            this.console.write('err', `${e.toString().trimEnd()}\n`);
+        } finally {
+            this.enableInput();
+        }
+    }
+
+    getCode() {
+        const blocks = [];
+        for (const {code} of this.codeBlocks()) blocks.push(code);
+        return blocks.join('');
+    }
+
+    async doRun() {
+        await this.rawRepl(async () => {
+            await this.mp.softReboot();
+            await this.mp.exec(this.getCode(), true);
+        });
+    }
+
+    async doStop() {
+        await this.mp.interrupt();
+    }
+
+    async reset() {
+        await this.rawRepl(async () => {
+            await this.mp.exec(`import machine; machine.reset()`, true);
+        });
+    }
+
+    async readMain() {
+        await this.rawRepl(async () => {
+            const data = await this.mp.readFile('main.py');
+            this.setEditorText(dec.decode(data));
+            this.console.write('', `Program read from main.py\n`);
+        });
+    }
+
+    async writeMain() {
+        await this.rawRepl(async () => {
+            await this.mp.writeFile('main.py', enc.encode(this.getCode()));
+            this.console.write('', `Program written to main.py\n`);
+        });
+    }
+
+    async removeMain() {
+        await this.rawRepl(async () => {
+            await this.mp.removeFile('main.py');
+            this.console.write('', `File main.py removed\n`);
+        });
+    }
+}
+
+Executor.apply(MicroPythonExecutor);
---
+++ packages/t-doc-common/t_doc_common-0.38-py3-none-any.whl/tdoc/common/static/tdoc/load.js
@@ -0,0 +1,44 @@
+// Copyright 2024 Remy Blank <remy@c-space.org>
+// SPDX-License-Identifier: MIT
+
+import {domLoaded, element} from './core.js';
+
+// Prevent doctools.js from capturing editor key events, in case keyboard
+// shortcuts are enabled.
+domLoaded.then(() => {
+    if (typeof BLACKLISTED_KEY_CONTROL_ELEMENTS !== 'undefined') {
+        BLACKLISTED_KEY_CONTROL_ELEMENTS.add('DIV');
+    }
+});
+
+// Handle admonition expansion. The button is needed to enable keyboard focus.
+domLoaded.then(() => {
+    for (const el of document.querySelectorAll('.admonition.dropdown')) {
+        const title = el.querySelector('.admonition-title')
+        title.addEventListener('click', () => {
+            el.classList.toggle('expand');
+        });
+        // Enable keyboard navigation.
+        const btn = title.appendChild(element('<button></button>'));
+        btn.addEventListener('click', (e) => {
+            el.classList.toggle('expand');
+            e.stopPropagation();
+        });
+    }
+});
+
+// Handle server termination.
+globalThis.tdocTerminate = async (ret) => {
+    await fetch(`${location.origin}/*terminate?r=${ret ?? 0}`,
+                {method: 'POST', cache: 'no-cache', referrer: ''});
+};
+
+// Handle solution toggling.
+globalThis.tdocToggleSolutions = () => {
+    const ds = document.documentElement.dataset;
+    if (ds.tdocSolutions === 'hide') {
+        delete ds.tdocSolutions;
+    } else {
+        ds.tdocSolutions = 'hide';
+    }
+};
