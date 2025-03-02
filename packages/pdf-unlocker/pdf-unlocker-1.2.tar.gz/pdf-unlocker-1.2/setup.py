373f55a385a5886ba18424b0d5bdc89f9b9847d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-02 14:05:39 +0000

    Add package pdf-unlocker/1.2/pdf-unlocker-1.2.tar.gz

---
+++ packages/pdf-unlocker/pdf-unlocker-1.2.tar.gz/pdf-unlocker-1.2/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+  long_description = fh.read()
+
+setup(
+  name="pdf-unlocker",
+  version="1.2",
+  packages=find_packages(),
+  entry_points={
+    'console_scripts': [
+      'pdf-unlocker = pdf_unlocker.__main__:main',
+    ],
+  },
+  install_requires=[
+    'pikepdf'
+  ],
+  long_description=long_description,
+  long_description_content_type="text/markdown"
+)
