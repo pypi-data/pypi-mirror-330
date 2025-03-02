7951f414520eec40b0fe0ba1053023ab5b282de9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-02 15:05:26 +0000

    Add package scryfall-py/0.1.1/scryfall_py-0.1.1-py3-none-any.whl

---
+++ packages/scryfall-py/scryfall_py-0.1.1-py3-none-any.whl/scryfall/models/base.py
@@ -0,0 +1,17 @@
+from typing import TYPE_CHECKING
+
+from pydantic import BaseModel
+
+if TYPE_CHECKING:
+    from scryfall.client import Scryfall
+
+
+class BaseAPIModel(BaseModel):
+    """Base API model for base API calls."""
+
+    _client: "Scryfall"
+
+    def __init__(self, **data):
+        client: "Scryfall" = data["_client"]
+        super().__init__(**data)
+        self._client = client
