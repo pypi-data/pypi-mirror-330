02231f328f3d58c13070c34fb69c032a6297a51c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-02 14:05:40 +0000

    Add package pyroblack/2.5.9/pyroblack-2.5.9-py3-none-any.whl

---
+++ packages/pyroblack/pyroblack-2.5.9-py3-none-any.whl/pyrogram/storage/file_storage.py
@@ -0,0 +1,88 @@
+#  Pyrogram - Telegram MTProto API Client Library for Python
+#  Copyright (C) 2017-present Dan <https://github.com/delivrance>
+#
+#  This file is part of Pyrogram.
+#
+#  Pyrogram is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Lesser General Public License as published
+#  by the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  Pyrogram is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public License
+#  along with Pyrogram.  If not, see <http://www.gnu.org/licenses/>.
+
+import logging
+from pathlib import Path
+
+import aiosqlite
+
+from .sqlite_storage import SQLiteStorage
+
+log = logging.getLogger(__name__)
+
+UPDATE_STATE_SCHEMA = """
+CREATE TABLE update_state
+(
+    id   INTEGER PRIMARY KEY,
+    pts  INTEGER,
+    qts  INTEGER,
+    date INTEGER,
+    seq  INTEGER
+);
+"""
+
+
+class FileStorage(SQLiteStorage):
+    FILE_EXTENSION = ".session"
+
+    def __init__(self, name: str, workdir: Path):
+        super().__init__(name)
+
+        self.database = workdir / (self.name + self.FILE_EXTENSION)
+
+    async def update(self):
+        version = await self.version()
+
+        if version == 1:
+            await self.conn.execute("DELETE FROM peers")
+            await self.conn.commit()
+
+            version += 1
+
+        if version == 2:
+            await self.conn.execute("ALTER TABLE sessions ADD api_id INTEGER")
+            await self.conn.commit()
+
+            version += 1
+
+        if version == 3:
+            await self.conn.execute(UPDATE_STATE_SCHEMA)
+            await self.conn.commit()
+
+            version += 1
+
+        await self.version(version)
+
+    async def open(self):
+        path = self.database
+        file_exists = path.is_file()
+
+        self.conn = await aiosqlite.connect(str(path), timeout=1)
+
+        await self.conn.execute("PRAGMA journal_mode=WAL")
+
+        if not file_exists:
+            await self.create()
+        else:
+            await self.update()
+
+        await self.conn.execute("VACUUM")
+        await self.conn.commit()
+
+    async def delete(self):
+        Path(self.database).unlink()
