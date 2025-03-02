f209c0e4b82d2bf8af426215cd416188c2b0d634
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-02 04:06:04 +0000

    Add package pylan-lib/0.1.1/pylan_lib-0.1.1-py3-none-any.whl

---
+++ packages/pylan-lib/pylan_lib-0.1.1-py3-none-any.whl/pylan/patterns/multiply.py
@@ -0,0 +1,32 @@
+from datetime import datetime
+from typing import Any
+
+from pylan.item import Item
+from pylan.patterns import Pattern
+
+
+class Multiply(Pattern):
+    def __init__(
+        self,
+        schedule: Any,
+        value: float | int,
+        start_date: str | datetime = None,
+        offset_start: str = None,
+        end_date: str | datetime = None,
+        offset_end: str = None,
+    ) -> None:
+        self.schedule = schedule
+        self.value = value
+        self.iterations = 0
+        self.dt_schedule = []
+
+        self.start_date = start_date
+        self.offset_start = offset_start
+        self.end_date = end_date
+        self.offset_end = offset_end
+
+    def apply(self, item: Item) -> None:
+        """@private
+        Adds the pattern value to the item value.
+        """
+        item.value *= self.value
