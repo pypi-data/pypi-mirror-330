73e78e8aa9c5baf0f4eaa46401ecb0def741e216
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-02 04:06:12 +0000

    Add package synthgen-client/0.0.1/synthgen_client-0.0.1-py3-none-any.whl

---
+++ packages/synthgen-client/synthgen_client-0.0.1-py3-none-any.whl/synthgen/__init__.py
@@ -0,0 +1,5 @@
+from .sync_client import SynthgenClient
+from .models import TaskStatus, TaskResponse, Batch, BatchList, HealthResponse
+
+__version__ = "0.0.1"
+__all__ = ["SynthgenClient", "TaskStatus", "TaskResponse", "Batch", "BatchList", "HealthResponse"]
