61a9f86e09df1fe3e6a910f5da06eb082f515ee4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-02 04:06:03 +0000

    Add package SimpleSQLite/1.5.4/simplesqlite-1.5.4-py3-none-any.whl

---
+++ packages/SimpleSQLite/simplesqlite-1.5.4-py3-none-any.whl/simplesqlite/converter.py
@@ -0,0 +1,107 @@
+"""
+.. codeauthor:: Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
+"""
+
+from collections.abc import Sequence
+from datetime import datetime
+from decimal import Decimal
+from typing import Any, Callable, Union
+
+from ._logger import logger
+
+
+def default_datetime_converter(value: datetime) -> str:
+    return value.strftime("%Y-%m-%d %H:%M:%S%z")
+
+
+class RecordConvertor:
+    @staticmethod
+    def __to_sqlite_element(
+        value: Any, attr: Union[int, str], datetime_converter: Callable[[datetime], str]
+    ) -> Any:
+        if isinstance(value, Decimal):
+            return float(value)
+
+        if isinstance(value, int):
+            # INTEGER. The value is a signed integer,
+            # stored in 1, 2, 3, 4, 6, or 8 bytes depending on the magnitude of the value.
+            # https://www.sqlite.org/datatype3.html
+            if not (-9223372036854775808 < value < 9223372036854775807):
+                raise OverflowError(attr)
+
+        if isinstance(value, datetime):
+            # TODO: add an interface to specify datetime_converter
+            return datetime_converter(value)
+
+        return value
+
+    @classmethod
+    def to_record(cls, attr_names: Sequence[str], values: Union[Sequence, dict]) -> list:
+        """
+        Convert values to a record to be inserted into a database.
+
+        :param list attr_names:
+            List of attributes for the converting record.
+        :param values: Values to be converted.
+        :type values: |dict|/|namedtuple|/|list|/|tuple|
+        :raises ValueError: If the ``values`` is invalid.
+        """
+
+        try:
+            # from a namedtuple to a dict
+            values = values._asdict()  # type: ignore
+        except AttributeError:
+            pass
+
+        datetime_converter = default_datetime_converter
+
+        if isinstance(values, dict):
+            return [
+                cls.__to_sqlite_element(values.get(attr_name), attr_name, datetime_converter)
+                for attr_name in attr_names
+            ]
+
+        if isinstance(values, (tuple, list)):
+            return [
+                cls.__to_sqlite_element(value, col, datetime_converter)
+                for col, value in enumerate(values)
+            ]
+
+        raise TypeError(f"cannot convert from {type(values)} to list")
+
+    @classmethod
+    def to_records(cls, attr_names: Sequence[str], value_matrix: Sequence) -> list:
+        """
+        Convert a value matrix to records to be inserted into a database.
+
+        :param list attr_names:
+            List of attributes for the converting records.
+        :param value_matrix: Values to be converted.
+        :type value_matrix: list of |dict|/|namedtuple|/|list|/|tuple|
+
+        .. seealso:: :py:meth:`.to_record`
+        """
+
+        records = []
+        error_msgs = []
+
+        for row_idx, record in enumerate(value_matrix):
+            try:
+                records.append(cls.to_record(attr_names, record))
+            except OverflowError as e:
+                try:
+                    if isinstance(e.args[0], int):
+                        col_idx = e.args[0]
+                        col = f"{attr_names[col_idx]} ({col_idx})"
+                    else:
+                        col = e.args[0]
+                except IndexError as e:
+                    logger.error(e)
+                    continue
+
+                error_msgs.append(f"  overflow int found: row={row_idx}, col={col}")
+
+        if error_msgs:
+            raise OverflowError("failed to convert:\n" + "\n".join(error_msgs))
+
+        return records
