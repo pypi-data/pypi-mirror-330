dc4f63b5fa3f482ee5f1223c93aff2c1ad7255a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-03 03:14:01 +0000

    Add package hblang/1.2/hblang-1.2.tar.gz

---
+++ packages/hblang/hblang-1.2.tar.gz/hblang-1.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="hblang",
+    version="1.2",
+    packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            "hblang = hblang.interpreter:main",
+        ],
+    },
+    install_requires=[
+        "requests",
+    ],
+    description="Интерпретатор для языка .hb",
+    author="hosarov",
+    author_email="CndCrime@proton.me",
+    url="https://github.com/Hosarov",
+)
