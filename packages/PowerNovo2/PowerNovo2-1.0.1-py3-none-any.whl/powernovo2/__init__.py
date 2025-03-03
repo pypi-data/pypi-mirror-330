e80f41b45ff2ab0f19fba57388190ee915e3a41a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-03 03:13:57 +0000

    Add package PowerNovo2/1.0.1/PowerNovo2-1.0.1-py3-none-any.whl

---
+++ packages/PowerNovo2/PowerNovo2-1.0.1-py3-none-any.whl/powernovo2/__init__.py
@@ -0,0 +1,13 @@
+
+from denovo import run_inference, main
+
+
+
+__all__ = [
+           "modules",
+           "peptides",
+           "knapsack",
+           "models",
+           "config",
+           "proteins",
+           "utils"]
