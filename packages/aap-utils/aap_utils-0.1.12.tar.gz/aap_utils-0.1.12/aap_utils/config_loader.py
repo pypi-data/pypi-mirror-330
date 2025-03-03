37c303a289619832ab9915b742a2f670cc41828a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-03 07:05:11 +0000

    Add package aap-utils/0.1.12/aap_utils-0.1.12.tar.gz

---
+++ packages/aap-utils/aap_utils-0.1.12.tar.gz/aap_utils-0.1.12/aap_utils/config_loader.py
@@ -0,0 +1,26 @@
+import os
+from hydra import initialize, compose
+from omegaconf import DictConfig
+
+class ConfigLoader:
+    def __init__(self):
+        self.kcn_params = "config.yaml"
+        self.config_path = "configs/"
+        self.overrides = []
+
+    def _load_env_variables(self):
+        """Load environment variables for config parameters and overrides."""
+        if os.environ.get("KCN_PARAMS"):
+            full_path = os.getenv("KCN_PARAMS")
+            self.kcn_params = os.path.basename(full_path)  # Extract the filename
+
+        if os.environ.get("KCN_OVERRIDES"):
+            overrides_env = os.getenv("KCN_OVERRIDES")
+            self.overrides = overrides_env.split("|") if overrides_env else []
+
+    def get_args(self) -> DictConfig:
+        """Load and return the Hydra configuration."""
+        self._load_env_variables()
+        with initialize_config_dir(version_base=None, config_dir=self.config_path):
+            args = compose(config_name=self.kcn_params, overrides=self.overrides)
+        return args
