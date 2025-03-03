a9421b21cfd08cf6e560bf05c77a8181e9aefbd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-03 03:13:40 +0000

    Add package anish/0.0.1/anish-0.0.1.tar.gz

---
+++ packages/anish/anish-0.0.1.tar.gz/anish-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="anish",
+    version="0.0.1",
+    description="anish",
+    author="Anish Agrawal",
+    author_email="your.email@example.com",
+    packages=["anish"],
+)
