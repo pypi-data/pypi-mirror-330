fc6202e4d20594b22eebf2eb29e78100485cbcce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-03 06:06:07 +0000

    Add package Physical-formulas/1.0/physical_formulas-1.0.tar.gz

---
+++ packages/Physical-formulas/physical_formulas-1.0.tar.gz/physical_formulas-1.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+setup(name= 'Physical_formulas',
+      version= '1.0',
+      description= 'Physical formulas on the topics of heat and molecular physics',
+      packages=['Physical_formulas'],
+      author_email= 'svyt7037@gmail.com',
+      author= 'svyt1310',
+      zip_safe= False)
