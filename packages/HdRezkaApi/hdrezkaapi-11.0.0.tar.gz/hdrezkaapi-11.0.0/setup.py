64ea19024552ef42322957a9d7424eda22dfbd9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-03 07:05:17 +0000

    Add package HdRezkaApi/11.0.0/hdrezkaapi-11.0.0.tar.gz

---
+++ packages/HdRezkaApi/hdrezkaapi-11.0.0.tar.gz/hdrezkaapi-11.0.0/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+with open('README.md', 'r', encoding='utf-8') as fh:
+	long_description = fh.read()
+
+setuptools.setup(
+	name='HdRezkaApi',
+	version='11.0.0',
+	author='Super_Zombi',
+	author_email='super.zombi.yt@gmail.com',
+	description='HDRezka Python API',
+	long_description=long_description,
+	long_description_content_type='text/markdown',
+	url='https://github.com/SuperZombi/HdRezkaApi',
+	packages=['HdRezkaApi'],
+	install_requires=["beautifulsoup4", "requests"],
+	include_package_data=True,
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+	python_requires='>=3.9',
+)
