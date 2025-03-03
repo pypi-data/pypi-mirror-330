dfa7947f298cd3c57c5a03d8c5fbb132d9f26406
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-03 07:05:29 +0000

    Add package plexcontentmanager/0.1.1/plexcontentmanager-0.1.1.tar.gz

---
+++ packages/plexcontentmanager/plexcontentmanager-0.1.1.tar.gz/plexcontentmanager-0.1.1/setup.py
@@ -0,0 +1,44 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="plexcontentmanager",
+    version="0.1.1",
+    author="Alex Boutros",
+    author_email="alex@aterrible.day",
+    description="A command-line tool for managing and curating Plex Media Server content",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/alexboutros/plexcontentmanager",
+    project_urls={
+        "Bug Tracker": "https://github.com/alexboutros/plexcontentmanager/issues",
+        "Source Code": "https://github.com/alexboutros/plexcontentmanager",
+        "Documentation": "https://github.com/alexboutros/plexcontentmanager#readme",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Topic :: Multimedia :: Video",
+        "Topic :: Utilities",
+    ],
+    packages=find_packages(),
+    include_package_data=True,
+    python_requires=">=3.6",
+    install_requires=[
+        "plexapi",
+        "click",
+        "colorama",
+        "requests>=2.25.0",
+        "urllib3<2.0.0",
+    ],
+    entry_points={
+        "console_scripts": [
+            "plexcontent=plexcontentmanager.cli:main",
+        ],
+    },
+)
