fe475bdc319b2e5c22763faee99376301716f520
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-03 03:13:40 +0000

    Add package svarascala/1.1.0/svarascala-1.1.0-py3-none-any.whl

---
+++ packages/svarascala/svarascala-1.1.0-py3-none-any.whl/svarascala/__init__.py
@@ -0,0 +1,11 @@
+"""
+SvaraScala - A library for calculating musical frequencies across Western and
+Indian musical systems.
+"""
+
+from .western import WesternMusic
+from .indian import IndianMusic
+from .navarasa import NavarasaMap
+from .modes import WesternModes
+
+__version__ = '1.1.0'
