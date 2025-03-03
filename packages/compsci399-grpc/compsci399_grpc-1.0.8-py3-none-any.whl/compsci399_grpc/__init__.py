0e0d5cba376f53fd607a7cac86fa34d18ea2afb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-03 03:13:57 +0000

    Add package compsci399-grpc/1.0.8/compsci399_grpc-1.0.8-py3-none-any.whl

---
+++ packages/compsci399-grpc/compsci399_grpc-1.0.8-py3-none-any.whl/compsci399_grpc/__init__.py
@@ -0,0 +1,9 @@
+# Auto-generated explicit imports
+__version__ = "1.0.8"
+
+from .greeter_pb2 import *
+from .greeter_pb2_grpc import *
+from .product_pb2 import *
+from .product_pb2_grpc import *
+from .user_pb2 import *
+from .user_pb2_grpc import *
