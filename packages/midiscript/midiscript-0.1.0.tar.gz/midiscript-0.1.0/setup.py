f0e6d9de9f2eab072448fb37e3d9e55b25165eb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-03 03:13:38 +0000

    Add package midiscript/0.1.0/midiscript-0.1.0.tar.gz

---
+++ packages/midiscript/midiscript-0.1.0.tar.gz/midiscript-0.1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="midiscript",
+    version="0.1.0",
+    packages=find_packages(),
+    author="arsnovo",
+    description="Programming language for musicians",
+    python_requires=">=3.6",
+)
