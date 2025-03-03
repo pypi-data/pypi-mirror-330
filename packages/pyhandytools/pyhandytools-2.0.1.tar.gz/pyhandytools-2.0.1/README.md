8e21978e8cfb63c2f7f9b0428a22f70f42a2e8bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-03 06:05:55 +0000

    Add package pyhandytools/2.0.1/pyhandytools-2.0.1.tar.gz

---
+++ packages/pyhandytools/pyhandytools-2.0.1.tar.gz/pyhandytools-2.0.1/README.md
@@ -0,0 +1,24 @@
+# py-handy
+A practical and convenient python toolset
+
+## Usage
+
+### install
+
+```shell
+pip3 install -U pyhandytools
+```
+
+### module
+
+- file
+- env
+- logger
+- crypto
+- calc
+- req
+- cleaner
+
+## PYPI url
+
+- ```https://pypi.org/project/pyhandytools/```
---
+++ packages/pyhandytools/pyhandytools-2.0.1.tar.gz/pyhandytools-2.0.1/setup.py
@@ -0,0 +1,34 @@
+# !/usr/bin/env python3
+# _*_ coding: utf-8 _*_
+from os import path
+
+from setuptools import setup, find_packages
+
+with open(path.join(path.abspath(path.dirname(__file__)), 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='pyhandytools',
+    version='2.0.1',
+    packages=find_packages(),
+    include_package_data=True,
+    url='https://github.com/JimouChen/py-handy',
+    author='Jimou Chen',
+    author_email='jmchen1024@gmail.com',
+    description='Python handy tools',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    keywords=['toolbox', 'kit', 'utils', 'tools'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'loguru',
+        'aiohttp',
+        'pycryptodome',
+        'beautifulsoup4',
+        'lxml'
+    ]
+)
