653e7f62b1a8a90e8f589b2e02593fd0de9eecc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-03 06:05:44 +0000

    Add package myappmaker/0.0.1/myappmaker-0.0.1-py3-none-any.whl

---
+++ packages/myappmaker/myappmaker-0.0.1-py3-none-any.whl/myappmaker/config.py
@@ -0,0 +1,33 @@
+"""Facility for configurations."""
+
+### standard library import
+from pathlib import Path
+
+### third-party imports
+from PySide6.QtCore import QStandardPaths
+
+### local imports
+from .appinfo import APP_TITLE, ORG_DIR_NAME, APP_DIR_NAME
+
+
+
+###
+
+WRITEABLE_DIR = (
+    Path(QStandardPaths.writableLocation(QStandardPaths.AppConfigLocation))
+    / ORG_DIR_NAME
+    / APP_DIR_NAME
+)
+
+
+###
+
+if not WRITEABLE_DIR.exists():
+    WRITEABLE_DIR.mkdir(parents=True)
+
+###
+
+STROKES_DATA_DIR = WRITEABLE_DIR / 'strokes_data'
+
+if not STROKES_DATA_DIR.exists():
+    STROKES_DATA_DIR.mkdir()
