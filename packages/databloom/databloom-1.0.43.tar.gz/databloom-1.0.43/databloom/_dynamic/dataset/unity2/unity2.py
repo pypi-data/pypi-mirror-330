af984af89f056f62128d5a75bfa5cd6d070d53f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-03 03:13:35 +0000

    Add package databloom/1.0.43/databloom-1.0.43.tar.gz

---
+++ packages/databloom/databloom-1.0.43.tar.gz/databloom-1.0.43/databloom/_dynamic/dataset/unity2/unity2.py
@@ -0,0 +1,11 @@
+# ---- render code block -----
+from databloom._core.dataset import DatasetBase
+from .table import *
+
+class unity2(DatasetBase):
+    def __init__(self) -> None:
+        self.database_id = "67c41bf7ead024e27fea7f5c"
+        self.database_name  = "unity2"
+        
+# ---- render code block 
+    
