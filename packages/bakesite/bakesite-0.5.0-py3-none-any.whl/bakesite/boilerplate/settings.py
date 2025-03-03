d2260afe5f35d14dfbb1f29ed3b33c8e0a1ff86b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-03 06:05:47 +0000

    Add package bakesite/0.5.0/bakesite-0.5.0-py3-none-any.whl

---
+++ packages/bakesite/bakesite-0.5.0-py3-none-any.whl/bakesite/boilerplate/settings.py
@@ -0,0 +1,11 @@
+params = {
+    "base_path": "",
+    "subtitle": "<Your subtitle here>",
+    "author": "<Your name here>",
+    "site_url": "https://<your site here>.com",
+    "current_year": 2002,
+    "github_url": "https://github.com/<your github here>",
+    "linkedin_url": "https://www.linkedin.com/<your linkedin here>",
+    "gtag_id": "<Your gtag here>",
+    "cname": "<your CNAME here>",
+}
