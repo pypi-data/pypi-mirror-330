c489b916fdb320765b9cfba5201d27ad9aef4dc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-03 06:06:12 +0000

    Add package disnake-dyn-components/0.2.2/disnake_dyn_components-0.2.2.tar.gz

---
+++ packages/disnake-dyn-components/disnake_dyn_components-0.2.2.tar.gz/disnake_dyn_components-0.2.2/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+
+def readme():
+    with open('README.md', 'r') as f:
+        return f.read()
+
+
+setup(
+  name='disnake_dyn_components',
+  version='0.2.2',
+  author='Lord_Nodus',
+  author_email='LordNodus@mail.ru',
+  description='Library for quick creation of ui components of discord with the ability to pass additional parameters',
+  long_description=readme(),
+  long_description_content_type='text/markdown',
+  url='https://github.com/NodusLorden/DisnakeDynComponents',
+  packages=find_packages(),
+  install_requires=['disnake>=2.10.1'],
+  classifiers=[
+    'Programming Language :: Python :: 3.12',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent'
+  ],
+  keywords='disnake discord bot buttons modals selectmenu',
+  project_urls={
+    'GitHub': 'https://github.com/NodusLorden/DisnakeDynComponents'
+  },
+  python_requires='>=3.12'
+)
