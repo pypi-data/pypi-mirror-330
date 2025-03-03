13b47539adfca833d323ddb2c7b51da30683c10b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-03 06:05:40 +0000

    Add package sfs-da/1.0.1/sfs_da-1.0.1.tar.gz

---
+++ packages/sfs-da/sfs_da-1.0.1.tar.gz/sfs_da-1.0.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setup(
+    name='sfs_da', 
+    version='1.0.1',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'mpmath',
+        'scipy',
+        'scikit-learn'
+    ],
+    description='A package for Statistical Inference for Feature Selection after OT-based Domain Adaptation',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Nguyen Thang Loi',
+    author_email='23520872@gm.uit.edu.vn',
+    license='MIT',
+    url='https://github.com/NT-Loi/SFS_DA.git'
+)
