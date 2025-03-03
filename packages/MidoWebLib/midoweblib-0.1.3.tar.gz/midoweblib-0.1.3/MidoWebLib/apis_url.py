b258d7663f16cd84512b66ba7e8241efaa2edb85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-03 06:05:42 +0000

    Add package MidoWebLib/0.1.3/midoweblib-0.1.3.tar.gz

---
+++ packages/MidoWebLib/midoweblib-0.1.3.tar.gz/midoweblib-0.1.3/MidoWebLib/apis_url.py
@@ -0,0 +1,3 @@
+approved_api = "https://midoghanam2.pythonanywhere.com/api/developers/approved_responce/"
+
+rejected_api = "https://midoghanam2.pythonanywhere.com/api/developers/rejected_responce/"
