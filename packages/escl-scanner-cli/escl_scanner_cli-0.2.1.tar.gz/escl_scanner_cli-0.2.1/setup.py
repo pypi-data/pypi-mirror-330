0244fe43e9146b514a9958a7249486117dcc9fb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-03 07:05:27 +0000

    Add package escl-scanner-cli/0.2.1/escl_scanner_cli-0.2.1.tar.gz

---
+++ packages/escl-scanner-cli/escl_scanner_cli-0.2.1.tar.gz/escl_scanner_cli-0.2.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setup(
+    # https://pypi.org/project/scanner-cli/ already taken
+    name="escl-scanner-cli",
+    version='0.2.1',
+    description='Control eSCL network scanners using a CLI utility',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/PJK/escl-scanner-cli',
+    author='https://github.com/zegman, Pavel Kalvoda',
+    author_email='me@pavelkalvoda.com',
+    license='Apache',
+    py_modules=['scanner'],
+    install_requires=[
+        'requests',
+        'xmltodict',
+        'zeroconf',
+    ],
+    entry_points={
+        'console_scripts': [
+            'escl-scan = scanner:main',
+        ],
+    },
+)
