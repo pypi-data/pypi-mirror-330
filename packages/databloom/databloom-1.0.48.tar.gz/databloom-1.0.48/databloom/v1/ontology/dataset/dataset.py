e9cd99a3c961a0d6bda89dd5a2d79868dd026150
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-03 06:05:40 +0000

    Add package databloom/1.0.48/databloom-1.0.48.tar.gz

---
+++ packages/databloom/databloom-1.0.48.tar.gz/databloom-1.0.48/databloom/v1/ontology/dataset/dataset.py
@@ -0,0 +1,20 @@
+import databloom._dynamic.dataset as ds
+from typing import Callable
+
+class Dataset:
+    """
+    data source type is mysql
+    """
+    def __init__(self) -> None:
+        ## ----render code block-----
+        
+        self.db = ds.db()
+        self.hainv4db = ds.hainv4db()
+        self.hainv4test99 = ds.hainv4test99()
+        self.hoangtestdb = ds.hoangtestdb()
+        self.unity = ds.unity()
+        self.unity2 = ds.unity2()
+        self.hainv4 = ds.hainv4()
+        self.hainv42 = ds.hainv42()
+        ## ----render code block----
+        pass
