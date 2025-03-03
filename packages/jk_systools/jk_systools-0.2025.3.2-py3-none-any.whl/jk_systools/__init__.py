971827e0974b40cf5c39d2ab110ef791a36a4e15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-03 06:06:12 +0000

    Add package jk_systools/0.2025.3.2/jk_systools-0.2025.3.2-py3-none-any.whl

---
+++ packages/jk_systools/jk_systools-0.2025.3.2-py3-none-any.whl/jk_systools/__init__.py
@@ -0,0 +1,14 @@
+﻿
+
+__version__ = "0.2025.3.2"
+
+
+
+from .IExitCodes import IExitCodes
+from .AppRuntime import AppRuntime
+from .CLIRunCtx import CLIRunCtx
+from .AbstractCLICmd import AbstractCLICmd
+from .AbstractMultiCmdCLIApp import AbstractMultiCmdCLIApp
+
+
+
