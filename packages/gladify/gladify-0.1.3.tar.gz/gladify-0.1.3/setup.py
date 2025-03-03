03b3deba247a74ccc4012dba258cc9c55e67c12a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-03 07:05:23 +0000

    Add package gladify/0.1.3/gladify-0.1.3.tar.gz

---
+++ packages/gladify/gladify-0.1.3.tar.gz/gladify-0.1.3/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="gladify",
+    version="0.1.3",
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        "gladify.assets": ["GladUI.ico"],
+    },
+    install_requires=[],
+    author="Navthej",
+    author_email="gladgamingstudio@gmail.com",
+    description="A Python package for various utilities",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
