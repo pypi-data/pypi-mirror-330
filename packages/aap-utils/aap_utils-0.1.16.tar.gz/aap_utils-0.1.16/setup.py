7d592ffab7acd7c0b67961b29363d86dbf5d83b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-03 07:05:13 +0000

    Add package aap-utils/0.1.16/aap_utils-0.1.16.tar.gz

---
+++ packages/aap-utils/aap_utils-0.1.16.tar.gz/aap_utils-0.1.16/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="aap_utils",  # The name of your library
+    version="0.1.16",
+    description="A Python library for registering an IP address for AAP",
+    author="Minh Dang",
+    author_email="danghoangminh86@gmail.com",
+    packages=find_packages(),  # Auto-find packages in the directory
+    install_requires=[
+        "requests",  # Any external dependencies (e.g., 'requests')
+        "pathlib",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
