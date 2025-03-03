2badcf006f93c82b1133088998192ed241e0a342
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-03 07:05:13 +0000

    Add package ludic/0.5.9/ludic-0.5.9.tar.gz

---
+++ packages/ludic/ludic-0.5.9.tar.gz/ludic-0.5.9/examples/__init__.py
@@ -0,0 +1,98 @@
+from dataclasses import asdict, dataclass
+from typing import Any, override
+
+from ludic.attrs import NoAttrs
+from ludic.catalog.layouts import Center, Stack
+from ludic.catalog.pages import Body, Head, HtmlPage
+from ludic.components import Component
+from ludic.html import meta
+from ludic.styles import set_default_theme, themes, types
+from ludic.types import AnyChildren
+
+set_default_theme(themes.LightTheme(measure=types.Size(90, "ch")))
+
+
+@dataclass
+class Model:
+    def dict(self) -> dict[str, Any]:
+        return asdict(self)
+
+
+@dataclass
+class ContactData(Model):
+    id: str
+    first_name: str
+    last_name: str
+    email: str
+
+
+@dataclass
+class PersonData(Model):
+    id: str
+    name: str
+    email: str
+    active: bool = True
+
+
+@dataclass
+class DB:
+    contacts: dict[str, ContactData]
+    people: dict[str, PersonData]
+
+
+def init_db() -> DB:
+    return DB(
+        contacts={
+            "1": ContactData(
+                id="1",
+                first_name="John",
+                last_name="Doe",
+                email="qN6Z8@example.com",
+            )
+        },
+        people={
+            "1": PersonData(
+                id="1",
+                name="Joe Smith",
+                email="joe@smith.org",
+                active=True,
+            ),
+            "2": PersonData(
+                id="2",
+                name="Angie MacDowell",
+                email="angie@macdowell.org",
+                active=True,
+            ),
+            "3": PersonData(
+                id="3",
+                name="Fuqua Tarkenton",
+                email="fuqua@tarkenton.org",
+                active=True,
+            ),
+            "4": PersonData(
+                id="4",
+                name="Kim Yee",
+                email="kim@yee.org",
+                active=False,
+            ),
+        },
+    )
+
+
+class Page(Component[AnyChildren, NoAttrs]):
+    @override
+    def render(self) -> HtmlPage:
+        return HtmlPage(
+            Head(
+                meta(charset="utf-8"),
+                meta(name="viewport", content="width=device-width, initial-scale=1.0"),
+                title="HTMX Examples",
+            ),
+            Body(
+                Center(
+                    Stack(*self.children, id="content"),
+                    style={"padding": self.theme.sizes.xxl},
+                ),
+                htmx_version="2.0.2",
+            ),
+        )
