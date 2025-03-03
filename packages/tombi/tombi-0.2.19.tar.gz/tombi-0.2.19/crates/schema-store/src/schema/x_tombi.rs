580a97d8dc28d412256a61e575d85f5db897980e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-03 07:05:25 +0000

    Add package tombi/0.2.19/tombi-0.2.19.tar.gz

---
+++ packages/tombi/tombi-0.2.19.tar.gz/tombi-0.2.19/crates/schema-store/src/schema/x_tombi.rs
@@ -0,0 +1,3 @@
+pub const X_TOMBI_TOML_VERSION: &str = "x-tombi-toml-version";
+pub const X_TOMBI_ARRAY_VALUES_ORDER: &str = "x-tombi-array-values-order";
+pub const X_TOMBI_TABLE_KEYS_ORDER: &str = "x-tombi-table-keys-order";
