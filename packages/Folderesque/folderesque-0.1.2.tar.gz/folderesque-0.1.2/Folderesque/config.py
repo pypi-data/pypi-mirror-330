542efb2301950e346374c456648ef7f274fb47dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-03 06:06:06 +0000

    Add package Folderesque/0.1.2/folderesque-0.1.2.tar.gz

---
+++ packages/Folderesque/folderesque-0.1.2.tar.gz/folderesque-0.1.2/Folderesque/config.py
@@ -0,0 +1,8 @@
+INPUT_PATH = "daskruns"
+OUTPUT_PATH = "testscaling"
+MODEL_PATH = "models\RealESRGAN_x4plus_anime_6B.pth"
+SCALE_FACTOR = 4
+DEVICE = "cuda"
+TILE_SIZE = 400
+THREAD_WORKERS = 4
+BATCH_SIZE = 16
---
+++ packages/Folderesque/folderesque-0.1.2.tar.gz/folderesque-0.1.2/setup.py
@@ -0,0 +1,27 @@
+import pathlib
+from setuptools import setup, find_packages
+
+setup_path = pathlib.Path(__file__).parent
+readme = (setup_path / "README.md").read_text(encoding="utf-8")
+
+with (setup_path / "requirements.txt").open() as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="Folderesque",
+    version="0.1.2",
+    description="Python Script to process and upscale images in specified folders using RRDB models.",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Sevilze/Folderesque",
+    entry_points={
+        "console_scripts": [
+            "Folderesque = Folderesque.__main__:main",
+        ],
+    },
+    python_requires=">=3.7",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=requirements,
+    package_data={"Folderesque": ["config.py"]},
+)
